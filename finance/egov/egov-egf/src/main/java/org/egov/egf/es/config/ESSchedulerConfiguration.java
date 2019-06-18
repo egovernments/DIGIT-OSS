d306f1cd757d1c48dfa25c7510b0b916cac736fe
atique2992 <mohammad.atiqurrahaman@walkingtree.in>
2019-06-18 14:36:54 +0530

    minor bug in schedular

--- finance/egov/egov-egf/src/main/java/org/egov/egf/es/config/ESSchedulerConfiguration.java
+++ finance/egov/egov-egf/src/main/java/org/egov/egf/es/config/ESSchedulerConfiguration.java
@@ -20,13 +20,19 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 @Configuration
 @Conditional(SchedulerConfigCondition.class)
 public class ESSchedulerConfiguration extends QuartzSchedulerConfiguration{
+    private static final String FIN_ROLLOUT_DASHBOARD_TRIGGER = "FIN_ROLLOUT_DASHBOARD__TRIGGER";
+    private static final String FIN_ROLLOUT_DASHBOARD_TRIGGER_GROUP = "FIN_ROLLOUT_DASHBOARD_TRIGGER_GROUP";
+    private static final String FINANCE_SCHEDULER = "finance-scheduler";
+    private static final String MODULE_NAME = "finance";
+    private static final String FIN_ROLLOUT_DASHBOARD_RECON_JOB = "FIN_ROLLOUT_DASHBOARD_RECON_JOB";
+    private static final String FIN_ROLLOUT_DASHBOARD_JOB_GROUP = "FIN_ROLLOUT_DASHBOARD_JOB_GROUP";
     @Value("${finance-rollout-dashboard-cron-expression}")
     private String finRolloutCronExp;
     
     @Bean(destroyMethod = "destroy")
     public SchedulerFactoryBean esDashboardScheduler(DataSource dataSource) {
         SchedulerFactoryBean esDashboardScheduler = createScheduler(dataSource);
-        esDashboardScheduler.setSchedulerName("finance-rollout-scheduler");
+        esDashboardScheduler.setSchedulerName(FINANCE_SCHEDULER);
         esDashboardScheduler.setAutoStartup(true);
         esDashboardScheduler.setOverwriteExistingJobs(true);
         esDashboardScheduler.setTriggers(
@@ -38,8 +44,8 @@ public class ESSchedulerConfiguration extends QuartzSchedulerConfiguration{
     public CronTriggerFactoryBean esDashboardCronTrigger() {
         CronTriggerFactoryBean esDashboardCron = new CronTriggerFactoryBean();
         esDashboardCron.setJobDetail(esDashboardJobDetail().getObject());
-        esDashboardCron.setGroup("FIN_ROLLOUT_DASHBOARD_TRIGGER_GROUP");
-        esDashboardCron.setName("FIN_ROLLOUT_DASHBOARD__TRIGGER");
+        esDashboardCron.setGroup(FIN_ROLLOUT_DASHBOARD_TRIGGER_GROUP);
+        esDashboardCron.setName(FIN_ROLLOUT_DASHBOARD_TRIGGER);
         esDashboardCron.setCronExpression(finRolloutCronExp);
         esDashboardCron.setMisfireInstruction(MISFIRE_INSTRUCTION_DO_NOTHING);
         return esDashboardCron;
@@ -48,8 +54,8 @@ public class ESSchedulerConfiguration extends QuartzSchedulerConfiguration{
     @Bean
     public JobDetailFactoryBean esDashboardJobDetail() {
         JobDetailFactoryBean esDashboardJobDetail = new JobDetailFactoryBean();
-        esDashboardJobDetail.setGroup("FIN_ROLLOUT_DASHBOARD_JOB_GROUP");
-        esDashboardJobDetail.setName("FIN_ROLLOUT_DASHBOARD_RECON_JOB");
+        esDashboardJobDetail.setGroup(FIN_ROLLOUT_DASHBOARD_JOB_GROUP);
+        esDashboardJobDetail.setName(FIN_ROLLOUT_DASHBOARD_RECON_JOB);
         esDashboardJobDetail.setDurability(true);
         esDashboardJobDetail.setJobClass(ESDashboardJob.class);
         esDashboardJobDetail.setRequestsRecovery(true);
@@ -63,7 +69,7 @@ public class ESSchedulerConfiguration extends QuartzSchedulerConfiguration{
         Map<String, String> jobDetailMap = new HashMap<>();
         jobDetailMap.put("userName", "system");
         jobDetailMap.put("cityDataRequired", "true");
-        jobDetailMap.put("moduleName", "finance");
+        jobDetailMap.put("moduleName", MODULE_NAME);
         return jobDetailMap;
     }
     
