77c4bf3a4a8ab75cd4e07f42b1aab0bdda7f22bb
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-09-17 14:53:22 +0530

    forgot pwd logo hidded

--- frontend/old-ui/web/rainmaker/packages/employee/src/modules/employee/User/ForgotPassword/index.js
+++ frontend/old-ui/web/rainmaker/packages/employee/src/modules/employee/User/ForgotPassword/index.js
@@ -3,15 +3,27 @@ import formHoc from "egov-ui-kit/hocs/form";
 import { Banner } from "modules/common";
 import { Screen } from "modules/common";
 import ForgotPasswd from "./components/ForgotPasswd";
+import get from "lodash/get";
+import { connect } from "react-redux";
 
 const ForgotPasswdHOC = formHoc({ formKey: "employeeForgotPasswd" })(ForgotPasswd);
 
-const ForgotPassword = () => {
+const ForgotPassword = ({ bannerUrl, logoUrl }) => {
   return (
-    <Banner>
-      <ForgotPasswdHOC />
+    <Banner hideBackButton={true} bannerUrl={bannerUrl} logoUrl={logoUrl}>
+      <ForgotPasswdHOC logoUrl={logoUrl} />
     </Banner>
   );
 };
 
-export default ForgotPassword;
+const mapStateToProps = ({ common }) => {
+  const { stateInfoById } = common;
+  let bannerUrl = get(stateInfoById, "0.bannerUrl");
+  let logoUrl = get(stateInfoById, "0.logoUrl");
+  return { bannerUrl, logoUrl };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(ForgotPassword);
