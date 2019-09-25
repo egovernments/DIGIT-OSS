e0fd23d2e68da34af8cec3ab9504c7ee99fa6711
Jagankumar <jagan.kumar@egovernments.org>
2019-09-25 10:51:58 +0530

    Multiple Owner related changes

--- frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/components/OwnerInfo/index.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/components/OwnerInfo/index.js
@@ -17,137 +17,126 @@ const getOwnerInfo = (latestPropertyDetails, generalMDMSDataById) => {
     owner = ownerDetails[0];
   }
 
-
   return (
-    ownerDetails && [
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME_INSTI", localizationLabelsData),
-          value: (institution && institution.name) || "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME", localizationLabelsData),
-          value: owner.name || "NA",
-        },
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_DESIGNATION", localizationLabelsData),
-          value: institution.designation || "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_SEARCHPROPERTY_TABEL_GUARDIANNAME", localizationLabelsData),
-          value: owner.fatherOrHusbandName || "NA",
-        },
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_TYPE_INSTI", localizationLabelsData),
-          value:
-            (institution &&
-              institution.type &&
-              generalMDMSDataById &&
-              generalMDMSDataById["SubOwnerShipCategory"] &&
-              generalMDMSDataById["SubOwnerShipCategory"][institution.type].name) ||
-            "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_GENDER", localizationLabelsData),
-          value: owner.gender || "NA",
-        },
-      isInstitution
-        ? {
-          // key: getTranslatedLabel("PT_OWNERSHIP_INFO_TYPE_INSTI", localizationLabelsData),
-          // value:
-          //   (institution &&
-          //     institution.type &&
-          //     generalMDMSDataById &&
-          //     generalMDMSDataById["SubOwnerShipCategory"] &&
-          //     generalMDMSDataById["SubOwnerShipCategory"][institution.type].name) ||
-          //   "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_DOB", localizationLabelsData),
-          value: owner.dob || "NA",
-        },
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME_OF_AUTH", localizationLabelsData),
-          value: owner.name || "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_MOBILE_NO", localizationLabelsData),
-          value: owner.mobileNumber || "NA",
-        },
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_TEL_NO", localizationLabelsData),
-          value: owner.altContactNumber || "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_EMAIL_ID", localizationLabelsData),
-          value: owner.emailId || "NA",
-        },
-      isInstitution
-        ? {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_MOBILE_NO", localizationLabelsData),
-          value: owner.mobileNumber || "NA",
-        }
-        : {
-          key: getTranslatedLabel("PT_OWNERSHIP_INFO_USER_CATEGORY", localizationLabelsData),
-          value:
-            (owner &&
-              owner.ownerType &&
-              generalMDMSDataById &&
-              generalMDMSDataById["OwnerType"] &&
-              generalMDMSDataById["OwnerType"][owner.ownerType].name) ||
-            "NA",
-        },
-      {
-        key: getTranslatedLabel("PT_OWNERSHIP_INFO_CORR_ADDR", localizationLabelsData),
-        value: owner.permanentAddress || "NA",
-      },
-    ]
-  );
+    ownerDetails && ownerDetails.map((owner) => {
+      return ({
+        items: [
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME_INSTI", localizationLabelsData),
+              value: (institution && institution.name) || "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME", localizationLabelsData),
+              value: owner.name || "NA",
+            },
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_DESIGNATION", localizationLabelsData),
+              value: institution.designation || "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_SEARCHPROPERTY_TABEL_GUARDIANNAME", localizationLabelsData),
+              value: owner.fatherOrHusbandName || "NA",
+            },
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_TYPE_INSTI", localizationLabelsData),
+              value:
+                (institution &&
+                  institution.type &&
+                  generalMDMSDataById &&
+                  generalMDMSDataById["SubOwnerShipCategory"] &&
+                  generalMDMSDataById["SubOwnerShipCategory"][institution.type].name) ||
+                "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_GENDER", localizationLabelsData),
+              value: owner.gender || "NA",
+            },
+          isInstitution
+            ? {
+              // key: getTranslatedLabel("PT_OWNERSHIP_INFO_TYPE_INSTI", localizationLabelsData),
+              // value:
+              //   (institution &&
+              //     institution.type &&
+              //     generalMDMSDataById &&
+              //     generalMDMSDataById["SubOwnerShipCategory"] &&
+              //     generalMDMSDataById["SubOwnerShipCategory"][institution.type].name) ||
+              //   "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_DOB", localizationLabelsData),
+              value: owner.dob || "NA",
+            },
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_NAME_OF_AUTH", localizationLabelsData),
+              value: owner.name || "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_MOBILE_NO", localizationLabelsData),
+              value: owner.mobileNumber || "NA",
+            },
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_TEL_NO", localizationLabelsData),
+              value: owner.altContactNumber || "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_EMAIL_ID", localizationLabelsData),
+              value: owner.emailId || "NA",
+            },
+          isInstitution
+            ? {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_MOBILE_NO", localizationLabelsData),
+              value: owner.mobileNumber || "NA",
+            }
+            : {
+              key: getTranslatedLabel("PT_OWNERSHIP_INFO_USER_CATEGORY", localizationLabelsData),
+              value:
+                (owner &&
+                  owner.ownerType &&
+                  generalMDMSDataById &&
+                  generalMDMSDataById["OwnerType"] &&
+                  generalMDMSDataById["OwnerType"][owner.ownerType].name) ||
+                "NA",
+            },
+          {
+            key: getTranslatedLabel("PT_OWNERSHIP_INFO_CORR_ADDR", localizationLabelsData),
+            value: owner.permanentAddress || "NA",
+          },
+        ]
+      })
+    })
+  )
+
 };
-// const getAddressItems = () => {
-//   return (
-//     [
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_CITY", localizationLabelsData),
-//         value: 'addressObj.city' || "NA",
-//       },
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_HOUSE_NO", localizationLabelsData),
-//         value: 'addressObj.doorNo' || "NA",
-//       },
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_COLONY_NAME", localizationLabelsData),
-//         value: 'addressObj.buildingName' || "NA",
-//       },
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_STREET_NAME", localizationLabelsData),
-//         value: 'addressObj.street' || "NA",
-//       },
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_MOHALLA", localizationLabelsData),
-//         value: 'addressObj.locality.name' || "NA",
-//       },
-//       {
-//         key: getTranslatedLabel("PT_PROPERTY_ADDRESS_PINCODE", localizationLabelsData),
-//         value: 'addressObj.pincode' || "NA",
-//       }
-//     ]
-//   );
-// }
+const formatOwnerInfo = (itemsArray = []) => {
+  let ownersInfo = []
+  if (itemsArray.length == 1) {
+    ownersInfo = itemsArray[0].items;
+  } else {
+    const emptySpaces = [{ key: ' ', value: ' ' }, { key: ' ', value: ' ' }, { key: ' ', value: ' ' }];
+    const ownerHeader = { key: ' ', value: 'Owner-' };
+    for (let index = 0; index < itemsArray.length; index++) {
+      let ownerInfo = [];
+      ownerInfo = itemsArray[index].items;    
+      ownerInfo.unshift(...emptySpaces);
+      ownerInfo.unshift({ ...ownerHeader, value: ownerHeader.value + '' + (index + 1) });
+      ownersInfo.push(...ownerInfo)
+    }
+  }
+  return ownersInfo;
+}
 
 const OwnerInfo = ({ properties, editIcon }) => {
-
   let ownerItems = [];
   const header = 'PT_OWNERSHIP_INFO_SUB_HEADER';
   if (properties) {
     const { propertyDetails } = properties;
-    console.log(properties, 'OwnerInfo properties-----');
     if (propertyDetails && propertyDetails.length > 0) {
-      ownerItems = getOwnerInfo(propertyDetails[0]);
+      ownerItems = formatOwnerInfo(getOwnerInfo(propertyDetails[0]));
     }
   }
   return (
