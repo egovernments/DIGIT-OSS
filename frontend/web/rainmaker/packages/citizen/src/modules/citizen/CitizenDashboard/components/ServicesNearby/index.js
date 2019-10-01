157e8b9618f7771b4ab873e9b55b2a74006ace8f
Shreya B <shreya.b@egovernments.org>
2019-10-01 16:22:00 +0530

    Changed services nearby style

--- frontend/web/rainmaker/packages/citizen/src/modules/citizen/CitizenDashboard/components/ServicesNearby/index.js
+++ frontend/web/rainmaker/packages/citizen/src/modules/citizen/CitizenDashboard/components/ServicesNearby/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon } from "components";
+import { Icon, Card } from "components";
 import Grid from "@material-ui/core/Grid";
 import Label from "egov-ui-kit/utils/translationNode";
 import "./index.css";
@@ -20,28 +20,44 @@ class ServicesNearby extends React.Component {
     ];
   };
 
-  render() {
-    const { getServicesNearBy } = this;
-    const { onSeviceClick } = this.props;
-    return (
-      <Grid container>
-        {getServicesNearBy().map((service) => {
-          return (
-            // <Grid item xs={3} sm={2} align="center" 
-            //   style={{ padding: "0px 8px", cursor: "pointer" }} 
+  // render() {
+  //   const { getServicesNearBy } = this;
+  //   const { onSeviceClick } = this.props;
+  //   return {
+
+  /* <Grid item xs={3} sm={2} align="center"
+            //   style={{ padding: "0px 8px", cursor: "pointer" }}
             //   onClick={() =>onSeviceClick(service.route)}>
             //   <div className="service-nearby-icon-cont">{service.icon}</div>
             //   <Label dark={true} className="service-label-cont" fontSize={14} label={service.label} />
-            // </Grid>
-            <Grid item xs={6} sm={6} align="center" 
-              style={{ paddingRight: "8px", cursor: "pointer" }} 
-              onClick={() =>onSeviceClick(service.route)}>
-              <div className="local-information-cont" >{service.icon}
-              <Label labelStyle={{ paddingLeft: "10px" }} dark={true} fontSize={14} label={service.label}  />
+            // </Grid> */
+  //   };
+  // }
+
+  render() {
+    const { onSeviceClick } = this.props;
+    return (
+      <Grid container>
+        <Grid item xs={6} sm={2} align="center" style={{ paddingRight : 8,cursor: "pointer" }} >
+          <Card className="servicelist-style"
+            textChildren={
+              <div className="local-information-cont" onClick={() => onSeviceClick("events")}>
+                <Icon className="service-icon" action="custom" name="calendar" />
+                <Label labelStyle={{ paddingLeft: "10px" }} dark={true} fontSize={14} label={"COMMON_EVENTS_LABEL"} />
               </div>
-            </Grid>
-          );
-        })}
+            }
+          />
+        </Grid>
+        <Grid item xs={6} sm={2} align="center" style={{ cursor: "pointer" }} >
+          <Card className="servicelist-style"
+            textChildren={
+              <div className="local-information-cont" onClick={() => onSeviceClick("my-city")}>
+                <Icon className="service-icon" action="custom" name="home-city-outline" />
+                <Label labelStyle={{ paddingLeft: "10px" }} dark={true} fontSize={14} label={"COMMON_MYCITY_LABEL"} />
+              </div>
+            }
+          />
+        </Grid>
       </Grid>
     );
   }
