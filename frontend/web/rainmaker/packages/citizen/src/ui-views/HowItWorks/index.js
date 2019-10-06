ce5bd2565600123b4f5f989a62b4112ad008d3d3
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-10-06 20:13:26 +0530

    changed tl  howitworks user manual pdf path for uk

--- frontend/web/rainmaker/packages/citizen/src/ui-views/HowItWorks/index.js
+++ frontend/web/rainmaker/packages/citizen/src/ui-views/HowItWorks/index.js
@@ -4,7 +4,7 @@ const HowItWorks = (props) => {
   return (
     <div style={{ height: "100vh" }}>
       <iframe
-        src="https://s3.ap-south-1.amazonaws.com/pb-egov-assets/pb/TL_UserManual_Citizen.pdf#view=FitH&embedded=true"
+        src="https://s3.ap-south-1.amazonaws.com/ukd-egov-assets/TL_UserManual_Citizen.pdf#view=FitH&embedded=true"
         style={{ width: "100%", height: "90%" }}
         frameborder="0"
       />
