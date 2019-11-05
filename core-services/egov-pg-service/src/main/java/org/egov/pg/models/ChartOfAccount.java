26a3f86ad3c2f8c7d8f805c35f7fa656e3929b5b
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-11-05 18:52:08 +0530

    constructors

--- core-services/egov-pg-service/src/main/java/org/egov/pg/models/ChartOfAccount.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/models/ChartOfAccount.java
@@ -1,7 +1,10 @@
 package org.egov.pg.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,6 +12,8 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChartOfAccount {
     private Long id;
 
