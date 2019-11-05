e5548b26bdbf2ab0794e17c3bbe5055c83cfd75d
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-11-05 19:06:15 +0530

    constructors

--- core-services/egov-pg-service/src/main/java/org/egov/pg/models/IdResponse.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/models/IdResponse.java
@@ -2,11 +2,13 @@ package org.egov.pg.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @ToString
+@NoArgsConstructor
 public class IdResponse {
 
     private String id;
