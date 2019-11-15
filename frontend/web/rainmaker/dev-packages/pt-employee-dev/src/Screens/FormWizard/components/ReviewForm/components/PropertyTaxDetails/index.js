8036f70a88b2abce04aa0970399f1e827d293958
manas-egov <https://www.github.com/manas-egov>
2019-11-15 20:13:41 +0530

    Summary alignment issue
    alignment issue

--- frontend/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/FormWizard/components/ReviewForm/components/PropertyTaxDetails/index.js
+++ frontend/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/FormWizard/components/ReviewForm/components/PropertyTaxDetails/index.js
@@ -104,10 +104,10 @@ class PropertyTaxDetails extends React.Component {
                       return (
                         !isUndefined(item.estimateAmount) && (
                           <div className="clearfix" style={{ marginBottom: 8 }}>
-                            <div className="col-sm-9" style={{ padding: 0 }}>
+                            <div className="col-sm-9 col-xs-9" style={{ padding: 0 }}>
                               <Label label={item.taxHeadCode} />
                             </div>
-                            <div className="col-sm-3">
+                            <div className="col-sm-3 col-xs-3" style={{padding:0,whiteSpace:"no-wrap"}}>
                               <Label
                                 containerStyle={{ textAlign: "right" }}
                                 className="pt-rf-price"
@@ -132,10 +132,10 @@ class PropertyTaxDetails extends React.Component {
                     lineStyle={{ marginLeft: 0, marginRight: 0, height: 2 }}
                   />
                   <div className="clearfix" style={{ marginTop: 8 }}>
-                    <div className="col-sm-9" style={{ padding: 0 }}>
+                    <div className="col-sm-9 col-xs-9" style={{ padding: 0 }}>
                       <Label label="PT_FORM4_TOTAL" />
                     </div>
-                    <div className="col-sm-3">
+                    <div className="col-sm-3 col-xs-3" style={{padding:0}}>
                       <Label
                         containerStyle={{ textAlign: "right" }}
                         labelStyle={{
