e9da7ab2e5c7132d5abab0ca7ca80d7261145833
Siva Nalluri <siva.nalluri@egovernments.org>
2019-11-21 12:58:24 +0530

    Adding redirect endpoint

---
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/web/controllers/RedirectController.java
@@ -0,0 +1,45 @@
+package org.egov.pg.web.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class RedirectController {
+
+    @Value("${egov.default.citizen.url}")
+    private String defaultURL;
+
+    @Value("${easypay.original.return.url.key:originalreturnurl}")
+    private String returnUrlKey;
+
+    @RequestMapping(value = "/payment/_redirect", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> method(@RequestBody MultiValueMap<String, String> formData) {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(UriComponentsBuilder.fromHttpUrl(formData.get(returnUrlKey).get(0))
+                .queryParams(formData).build().encode().toUri());
+        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleError(Exception e) {
+        log.error("EXCEPTION_WHILE_REDIRECTING", e);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(UriComponentsBuilder.fromHttpUrl(defaultURL).build().encode().toUri());
+        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+    }
+
+}
