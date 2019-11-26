24b4f8265db52be48c19d54fcea3659b84bc70ad
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-11-26 15:06:01 +0530

    bpastakeholder- searchscreen update

--- frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/bpastakeholder/searchResource/functions.js
+++ frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/bpastakeholder/searchResource/functions.js
@@ -103,8 +103,11 @@ export const searchApiCall = async (state, dispatch) => {
       let data = response.Licenses.map(item => ({
         [getTextToLocalMapping("Application No")]:
           item.applicationNumber || "-",
-        [getTextToLocalMapping("License No")]: item.licenseNumber || "-",
-        [getTextToLocalMapping("Trade Name")]: item.tradeName || "-",
+        [getTextToLocalMapping("Appliact Name")]:
+          item.tradeLicenseDetail.owners[0].name || "-",
+        [getTextToLocalMapping("Licensee Type")]:
+          item.tradeLicenseDetail.tradeUnits[0].tradeType || "-",
+        [getTextToLocalMapping("Status")]: item.status || "-",
         [getTextToLocalMapping("Owner Name")]:
           item.tradeLicenseDetail.owners[0].name || "-",
         [getTextToLocalMapping("Application Date")]:
