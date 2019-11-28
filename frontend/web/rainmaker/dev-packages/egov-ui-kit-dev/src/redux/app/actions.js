6dbba2d930ff70b04eb8934258d4474237fe8057
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-11-28 18:55:44 +0530

    bpastakeholder- added  module for localization fetch

--- frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/app/actions.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/app/actions.js
@@ -41,7 +41,8 @@ export const toggleSnackbarAndSetText = (open, message = {}, variant) => {
 
 export const fetchLocalizationLabel = (locale, module, tenantId) => {
   return async (dispatch) => {
-    const commonModules = "rainmaker-pgr,rainmaker-pt,rainmaker-tl,finance-erp,rainmaker-common,rainmaker-hr,rainmaker-uc,rainmaker-noc";
+    const commonModules =
+      "rainmaker-pgr,rainmaker-pt,rainmaker-tl,finance-erp,rainmaker-common,rainmaker-hr,rainmaker-uc,rainmaker-noc,rainmaker-abg,rainmaker-bpareg";
     try {
       const payload1 = await httpRequest(LOCALATION.GET.URL, LOCALATION.GET.ACTION, [
         { key: "module", value: commonModules },
