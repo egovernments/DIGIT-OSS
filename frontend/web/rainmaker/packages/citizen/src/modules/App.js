dc56c14939d0fc658bfeb03e1f0975e48fb67602
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-11-29 16:48:57 +0530

    added route path to unauth

--- frontend/web/rainmaker/packages/citizen/src/modules/App.js
+++ frontend/web/rainmaker/packages/citizen/src/modules/App.js
@@ -83,13 +83,15 @@ class App extends Component {
   };
 
   componentWillReceiveProps(nextProps) {
-    const { route: nextRoute, authenticated } = nextProps;
+    const { route: nextRoute, authenticated, location } = nextProps;
     const { route: currentRoute, history, setRoute } = this.props;
     if (nextRoute && currentRoute !== nextRoute) {
       history.push(nextRoute);
       setRoute("");
     }
-    if (nextProps.hasLocalisation !== this.props.hasLocalisation && !authenticated && !getQueryArg("", "smsLink")) {
+    const isWithoutAuthSelfRedirect = location && location.pathname && location.pathname.includes("whitelisted");
+
+    if (nextProps.hasLocalisation !== this.props.hasLocalisation && !authenticated && !getQueryArg("", "smsLink") && !isWithoutAuthSelfRedirect) {
       nextProps.hasLocalisation && this.props.history.replace("/language-selection");
     }
   }
