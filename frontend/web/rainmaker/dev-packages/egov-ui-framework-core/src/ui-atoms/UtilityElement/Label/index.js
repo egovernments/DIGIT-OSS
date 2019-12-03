f1b06e8e808731e6e2c749e79f803e976136edcb
Ashok <ashok.kumar@egovernments.org>
2019-12-03 11:19:02 +0530

    In Trade License summary if data is not available its showing blank it should show "NA".

--- frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-atoms/UtilityElement/Label/index.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-atoms/UtilityElement/Label/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Label = ({ label, jsonPath, ...rest }) => (
-  <span {...rest}>{label ? label : label === 0 ? 0 : ""}</span>
+  <span {...rest}>{label ? label : label === 0 ? 0 : "NA"}</span>
 );
 
 export default Label;
