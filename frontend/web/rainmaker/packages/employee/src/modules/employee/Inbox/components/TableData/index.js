2cc4e92e825b8d7b5cabb7ae045906000ea9f460
Ashok <ashok.kumar@egovernments.org>
2019-12-03 15:15:42 +0530

    Url change to edit page and labels in the Module/Service column

--- frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
+++ frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
@@ -39,7 +39,7 @@ const prepareInboxDataRows = (data) => {
   return data.map((item) => {
     var sla = item.businesssServiceSla && item.businesssServiceSla / (1000 * 60 * 60 * 24);
     let dataRows = [
-      { text: _.get(item, "moduleName", "--"), subtext: `CS_COMMON_INBOX_${item.businessService.toUpperCase()}` },
+      { text: `CS_COMMON_INBOX_${item.businessService.toUpperCase()}`, subtext: "New License"  },
       { text: item.businessId },
       {
         text: item.state ? (
