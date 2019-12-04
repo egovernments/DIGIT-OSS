0e5e8beb90b521b5a9d33a151a184475f4db9a66
ashok-eGov <57890337+ashok-eGov@users.noreply.github.com>
2019-12-04 16:03:30 +0530

    Update workflowRedirectionConfig.js

--- frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/Table/workflowRedirectionConfig.js
+++ frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/Table/workflowRedirectionConfig.js
@@ -1,6 +1,6 @@
 export const getWFConfig = module => {
   switch (module.toUpperCase()) {
-    case "TL-SERVICES":
+    case "CS_COMMON_INBOX_NEWTL":
       return {
         INITIATED: "/tradelicence/apply",
         DEFAULT: "/tradelicence/search-preview"
