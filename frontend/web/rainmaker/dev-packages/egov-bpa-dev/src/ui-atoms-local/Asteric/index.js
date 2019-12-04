c882f524339c963de070692414dfcd13ca32eb61
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-12-04 12:37:21 +0530

    bpastakeholder --removed from Nov-15-release

--- frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-atoms-local/Asteric/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Asteric = () => {
-  return <sup style={{ color: "#E54D42" }}>*</sup>;
-};
-
-export default Asteric;
--- frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/routes/mihy/route-names.js
+++
@@ -1,1 +0,0 @@
-export const SCREEN_INTERFACE="/egov-ui-framework/:path?/:screenKey?/:hasRemoteConfig?";
