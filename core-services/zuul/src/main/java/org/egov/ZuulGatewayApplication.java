bb4485cbf516e328ad94abae62370baefb79aab9
talele08 <talele08@users.noreply.github.com>
2019-12-06 14:48:35 +0530

    Zuul mixedendpoint changes (#18)
    * enriching default userInfo for mixed endpoint
    
    * not throwing error if default user not found
    
    * referring existing user host path
    
    * correction to test case
    
    * moved zuul routes to different config file
    
    * default route properties taken from classpath
    
    * added pt registry routing
    
    * Update routes.properties
    
    * spelling mistake correction
    
    * Update routes.properties
    
    * Update routes.properties
    
    * logging error instead of throwing in userUtils

--- core-services/zuul/src/main/java/org/egov/ZuulGatewayApplication.java
+++ core-services/zuul/src/main/java/org/egov/ZuulGatewayApplication.java
@@ -2,25 +2,29 @@ package org.egov;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.egov.Utils.UserUtils;
 import org.egov.filters.pre.AuthFilter;
 import org.egov.filters.pre.AuthPreCheckFilter;
 import org.egov.filters.pre.RbacFilter;
 import org.egov.filters.pre.RbacPreCheckFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 @EnableZuulProxy
+@EnableCaching
 @SpringBootApplication
+@PropertySource("${zuul.routes.filepath}")
 public class ZuulGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
@@ -44,31 +48,30 @@ public class ZuulGatewayApplication {
     @Value("${egov.authorize.access.control.host}${egov.authorize.access.control.uri}")
     private String authorizationUrl;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserUtils userUtils;
+
     @Bean
     public AuthPreCheckFilter authCheckFilter() {
         return new AuthPreCheckFilter(new HashSet<>(Arrays.asList(openEndpointsWhitelist)),
-            new HashSet<>(Arrays.asList(mixedModeEndpointsWhitelist)));
+            new HashSet<>(Arrays.asList(mixedModeEndpointsWhitelist)),userUtils);
     }
 
     @Bean
     public AuthFilter authFilter() {
         final ProxyRequestHelper proxyRequestHelper = new ProxyRequestHelper();
-        return new AuthFilter(proxyRequestHelper, restTemplate(), authServiceHost, authServiceUri);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        return new AuthFilter(proxyRequestHelper, restTemplate, authServiceHost, authServiceUri);
     }
 
     @Bean
     public RbacFilter rbacFilter() {
-        return new RbacFilter(restTemplate(), authorizationUrl, objectMapper());
-    }
-    
-    @Bean
-    public RestTemplate restTemplate() {
-    	return  new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+        return new RbacFilter(restTemplate, authorizationUrl, objectMapper);
     }
 
     @Bean
