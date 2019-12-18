79be9abf6f36483b671aa07ab0235d129c675482
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2019-12-18 18:10:48 +0530

    Updated get tenantId (#133)

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/localStorageUtils/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/localStorageUtils/index.js
@@ -1,3 +1,5 @@
+
+
 const appName = process.env.REACT_APP_NAME;
 
 //GET methods
@@ -8,6 +10,9 @@ export const getUserInfo = () => {
   return localStorageGet("user-info");
 };
 export const getTenantId = () => {
+  if(process.env.REACT_APP_NAME === "Citizen") {
+    return JSON.parse(getUserInfo()).permanentCity;
+  }
   return localStorageGet("tenant-id");
 };
 export const getLocalization = (key) => {
