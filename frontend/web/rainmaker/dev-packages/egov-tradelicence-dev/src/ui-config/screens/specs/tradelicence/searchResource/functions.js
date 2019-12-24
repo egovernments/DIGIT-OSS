cbcabec6259f2568810fda22824443a632305797
manas-egov <56586896+manas-egov@users.noreply.github.com>
2019-12-24 11:28:25 +0530

    RAIN-256, 246, 203 fix (#172)

--- frontend/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/searchResource/functions.js
+++ frontend/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/searchResource/functions.js
@@ -110,6 +110,7 @@ export const searchApiCall = async (state, dispatch) => {
         [getTextToLocalMapping("Application Date")]:
           convertEpochToDate(item.applicationDate) || "-",
         [getTextToLocalMapping("Status")]: item.status || "-",
+        ["status1"]: item.status || "-",
         ["tenantId"]: item.tenantId
       }));
 
