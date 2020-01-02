366b682629af79f1e01e2f79bd4daa89134a7ad6
gopesh67 <43036707+gopesh67@users.noreply.github.com>
2020-01-02 15:06:05 +0530

    Bpaedcr newsprint (#219)
    * my application common card changes
    
    * sort based on application date
    
    * bpastakeholder-  added tenantId in myapplicaition data
    
    * Workflow jk (#208)
    
    * added spinner
    
    * SLA condition changes
    
    * SLA configuration
    
    * Assignee Change
    
    * Performance Improvement
    
    * Update index.js
    
    * Workflow jk (#209)
    
    * added spinner
    
    * SLA condition changes
    
    * SLA configuration
    
    * Assignee Change
    
    * Performance Improvement
    
    * Update index.js
    
    * Redirection Bug fix
    
    * Bug fixes in past payments, search WOR-69 WOR-70 WOR-71 (#210)
    
    * RAIN-355 : Workflow 2.0 Sort icon for SLA in dashboard (#211)
    
    * Dashboard iframe mobile screen alignment issue fix (#213)
    
    * Work flow error fix (#212)
    
    * Button css Issue Fix for smaller devices (#214)
    
    * css fix for smaller devices - add meter reading (#217)
    
    * mobileView fix (#216)
    
    * Work flow error fix viewBill (#215)
    
    * RAIN-354 (#218)
    
    * send uuid,tenant to edcr
    
    Co-authored-by: sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
    Co-authored-by: Satyam-egov-tarento <56678653+Satyam-egov-tarento@users.noreply.github.com>
    Co-authored-by: yogendra-egov <56108459+yogendra-egov@users.noreply.github.com>
    Co-authored-by: sandhya-jagadish-egov-tarento <56677624+sandhya-jagadish-egov-tarento@users.noreply.github.com>
    Co-authored-by: manas-egov <56586896+manas-egov@users.noreply.github.com>

--- frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/edcrscrutiny/functions.js
+++ frontend/web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/edcrscrutiny/functions.js
@@ -11,8 +11,10 @@ import { httpRequest } from "egov-ui-framework/ui-utils/api.js";
 import get from "lodash/get";
 import set from "lodash/set";
 import { validateFields } from "../utils";
-import { getTenantId} from "egov-ui-kit/utils/localStorageUtils";
+import {  getTenantId, getUserInfo} from "egov-ui-kit/utils/localStorageUtils";
 
+const userTenant = getTenantId();
+const userUUid = get(JSON.parse(getUserInfo()),"uuid");
 export const fetchData = async (
   action,
   state,
@@ -81,8 +83,8 @@ const getSearchResultsfromEDCR = async (action, state, dispatch) => {
           correlationId: "wefiuweiuff897",
           authToken: "",
           userInfo: {
-            id: 1,
-            tenantId: "generic"
+            id: userUUid,
+            tenantId: userTenant
           }
         }
       },
@@ -117,8 +119,8 @@ export const getSearchResultsfromEDCRWithApplcationNo = async (
           correlationId: "wefiuweiuff897",
           authToken: "",
           userInfo: {
-            id: 1,
-            tenantId: "generic"
+            id: userUUid,
+            tenantId: userTenant
           }
         }
       },
@@ -151,7 +153,8 @@ const scrutinizePlan = async (state, dispatch) => {
         msgId: "",
         correlationId: "",
         userInfo: {
-          id: ""
+          id: userUUid,
+          tenantId: userTenant
         }
       }
     };
