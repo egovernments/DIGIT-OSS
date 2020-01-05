9ed0c753fea6dc84a7f6af15db29b90356596f88
yogendra-egov <56108459+yogendra-egov@users.noreply.github.com>
2020-01-05 11:38:36 +0530

    RAIN-384 : Sort Icon : Web View seems breaking for Sort Icon positioning (#253)

--- frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/Table/index.js
+++ frontend/web/rainmaker/packages/employee/src/modules/employee/Inbox/components/Table/index.js
@@ -5,8 +5,8 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import Hidden from "@material-ui/core/Hidden";
 import { TaskDialog } from "egov-workflow/ui-molecules-local";
 import { addWflowFileUrl, orderWfProcessInstances } from "egov-ui-framework/ui-utils/commons";
@@ -21,18 +21,21 @@ import { getWFConfig } from "./workflowRedirectionConfig";
 import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
 import "./index.css";
 
-
 class InboxData extends React.Component {
   state = {
     dialogOpen: false,
     workflowHistory: [],
-    sortOrder: 'asc',
-    isSorting: false
+    sortOrder: "asc",
+    isSorting: false,
   };
 
   getProcessIntanceData = async (pid) => {
     const tenantId = getTenantId();
-    const queryObject = [{ key: "businessIds", value: pid }, { key: "history", value: true }, { key: "tenantId", value: tenantId }];
+    const queryObject = [
+      { key: "businessIds", value: pid },
+      { key: "history", value: true },
+      { key: "tenantId", value: tenantId },
+    ];
     const payload = await httpRequest("egov-workflow-v2/egov-wf/process/_search?", "", queryObject);
     const processInstances = payload && payload.ProcessInstances.length > 0 && orderWfProcessInstances(payload.ProcessInstances);
     return processInstances;
@@ -72,10 +75,7 @@ class InboxData extends React.Component {
     }
 
     let baseUrl = document.location.origin;
-    let contextPath =
-      status === "Initiated"
-        ? getWFConfig(row[0].hiddenText).INITIATED
-        :  getWFConfig(row[0].hiddenText).DEFAULT;
+    let contextPath = status === "Initiated" ? getWFConfig(row[0].hiddenText).INITIATED : getWFConfig(row[0].hiddenText).DEFAULT;
 
     let queryParams = `applicationNumber=${taskId}&tenantId=${tenantId}`;
     this.props.setRoute(`${contextPath}?${queryParams}`);
@@ -87,154 +87,172 @@ class InboxData extends React.Component {
     const MAX_SLA = businessServiceSla[businessService];
     let slaValue = "";
     if (sla <= 0) {
-      slaValue = "redSlab"
-    } else if (0 < sla && sla <= (MAX_SLA - MAX_SLA / 3)) {
-      slaValue = "yellowSlab"
+      slaValue = "redSlab";
+    } else if (0 < sla && sla <= MAX_SLA - MAX_SLA / 3) {
+      slaValue = "yellowSlab";
     } else {
-      slaValue = "greenSlab"
+      slaValue = "greenSlab";
     }
     switch (slaValue) {
-      case "greenSlab": return "inbox-cell-badge-primary sla-positive-value"
-      case "yellowSlab": return "inbox-cell-badge-primary sla-middle-value"
-      case "redSlab": return "inbox-cell-badge-primary sla-negative-value"
+      case "greenSlab":
+        return "inbox-cell-badge-primary sla-positive-value";
+      case "yellowSlab":
+        return "inbox-cell-badge-primary sla-middle-value";
+      case "redSlab":
+        return "inbox-cell-badge-primary sla-negative-value";
     }
-  }
+  };
 
   sortingTable = (order) => {
     const { sortOrder } = this.state;
     if (sortOrder !== order) {
       this.setState({
         sortOrder: order,
-        isSorting: true
-      })
+        isSorting: true,
+      });
     }
-  }
+  };
 
   render() {
     const { data, ProcessInstances } = this.props;
     const { onHistoryClick, onDialogClose, getModuleLink } = this;
     const { isSorting, sortOrder } = this.state;
-    if(isSorting){
+    if (isSorting) {
       data.rows.reverse();
     }
     return (
       <div>
-    <Hidden only={['xs']}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {data.headers.map((item, index) => {
-              let classNames = `inbox-data-table-headcell inbox-data-table-headcell-${index}`;
-              return <TableCell className={classNames}>
-                {<Label label={item} labelStyle={{ fontWeight: "500" }} color="#000000" />}
-                {index===4 && <span className="arrow-icon-container">
-                  {sortOrder === "desc" && <div className="arrow-icon-style" onClick={()=>this.sortingTable('asc')}><ArrowDropUpIcon /></div>}
-                  {sortOrder === "asc" && <div className="arrow-icon-style" onClick={()=>this.sortingTable('desc')}><ArrowDropDownIcon /></div>}
-                  </span>}
-                </TableCell>;
-            })}
-          </TableRow>
-        </TableHead>
-        {data.rows.length === 0 ? (
-          <TableBody>
-            <Label labelClassName="" label="COMMON_INBOX_NO_DATA" />
-          </TableBody>
-        ) : (
-            <TableBody>
-              {data.rows.map((row, i) => {
+        <Hidden only={["xs"]}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {data.headers.map((item, index) => {
+                  let classNames = `inbox-data-table-headcell inbox-data-table-headcell-${index}`;
+                  return (
+                    <TableCell className={classNames}>
+                      {<Label label={item} labelStyle={{ fontWeight: "500" }} color="#000000" />}
+                    </TableCell>
+                  );
+                })}
+                <TableCell className="inbox-data-table-headcell inbox-data-table-headcell-4">
+                  {sortOrder === "desc" && (
+                    <div className="arrow-icon-style" onClick={() => this.sortingTable("asc")}>
+                      <ArrowDropUpIcon />
+                    </div>
+                  )}
+                  {sortOrder === "asc" && (
+                    <div className="arrow-icon-style" onClick={() => this.sortingTable("desc")}>
+                      <ArrowDropDownIcon />
+                    </div>
+                  )}
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            {data.rows.length === 0 ? (
+              <TableBody>
+                <Label labelClassName="" label="COMMON_INBOX_NO_DATA" />
+              </TableBody>
+            ) : (
+              <TableBody>
+                {data.rows.map((row, i) => {
+                  return (
+                    <TableRow key={i} className="inbox-data-table-bodyrow">
+                      {row.map((item, index) => {
+                        let classNames = `inbox-data-table-bodycell inbox-data-table-bodycell-${index}`;
+                        if (item.subtext) {
+                          return (
+                            <TableCell className={classNames}>
+                              <div onClick={() => getModuleLink(item, row, index)} className="inbox-cell-text">
+                                {<a>{item.text} </a>}
+                              </div>
+                              <div className="inbox-cell-subtext">
+                                {<Label label={`CS_COMMON_INBOX_${item.subtext.toUpperCase()}`} color="#000000" />}
+                              </div>
+                            </TableCell>
+                          );
+                        } else if (item.badge) {
+                          return (
+                            <TableCell className={classNames}>
+                              <span class={this.getSlaColor(item.text, row[2].text.props.label.split("_")[1])}>{item.text}</span>
+                            </TableCell>
+                          );
+                        } else if (item.historyButton) {
+                          return (
+                            <TableCell className={classNames}>
+                              <div onClick={() => onHistoryClick(row[0])} style={{ cursor: "pointer" }}>
+                                <i class="material-icons">history</i>
+                              </div>
+                            </TableCell>
+                          );
+                        } else {
+                          return (
+                            <TableCell className={classNames}>
+                              <div>{item.text}</div>
+                            </TableCell>
+                          );
+                        }
+                      })}
+                    </TableRow>
+                  );
+                })}
+                <TaskDialog open={this.state.dialogOpen} onClose={onDialogClose} history={ProcessInstances} />
+              </TableBody>
+            )}
+          </Table>
+        </Hidden>
+        <Hidden only={["sm", "md", "lg", "xl"]} implementation="css">
+          {data.rows.length === 0 ? (
+            <Card textChildren={<Label labelClassName="" label="COMMON_INBOX_NO_DATA" />} />
+          ) : (
+            <div>
+              {data.rows.map((row, index) => {
                 return (
-                  <TableRow key={i} className="inbox-data-table-bodyrow">
-                    {row.map((item, index) => {
-                      let classNames = `inbox-data-table-bodycell inbox-data-table-bodycell-${index}`;
-                      if (item.subtext) {
-                        return (
-                          <TableCell className={classNames}>
-                            <div onClick={() => getModuleLink(item, row, index)} className="inbox-cell-text">{<a >{item.text} </a>}</div>
-                            <div className="inbox-cell-subtext">{<Label label={`CS_COMMON_INBOX_${item.subtext.toUpperCase()}`} color="#000000" />}</div>
-                          </TableCell>
-                        );
-                      } else if (item.badge) {
-                        return (
-                          <TableCell className={classNames}>
-                            <span
-                              class={this.getSlaColor(item.text, row[2].text.props.label.split('_')[1])}
-                            >
-                              {item.text}
-                            </span>
-                          </TableCell>
-                        );
-                      } else if (item.historyButton) {
-                        return (
-                          <TableCell className={classNames}>
-                            <div onClick={() => onHistoryClick(row[0])} style={{ cursor: "pointer" }}>
-                              <i class="material-icons">history</i>
-                            </div>
-                          </TableCell>
-                        );
-                      } else {
-                        return (
-                          <TableCell className={classNames}>
-                            <div>
-                              {item.text}
-                            </div>
-                          </TableCell>
-                        );
-                      }
-                    })}
-                  </TableRow>
+                  <Card
+                    key={index}
+                    textChildren={
+                      <div>
+                        <div className="head" onClick={() => getModuleLink(row[0], row, 0)}>
+                          <a style={{ color: "#FE7A51" }}>{row[0].text}</a>
+                        </div>
+                        <div className="head">
+                          <Label label={`CS_COMMON_INBOX_${row[0].subtext.toUpperCase()}`} color="#000000" />
+                        </div>
+
+                        <div className="card-div-style">
+                          <Label label={data.headers[1]} labelStyle={{ fontWeight: "500" }} />
+                        </div>
+                        <div className="card-div-style">{row[1].text}</div>
+
+                        <div className="card-div-style">
+                          <Label label={data.headers[2]} labelStyle={{ fontWeight: "500" }} />
+                        </div>
+                        <div className="card-div-style">{row[2].text}</div>
+
+                        <div className="card-div-style">
+                          <Label label={data.headers[3]} labelStyle={{ fontWeight: "500" }} />
+                        </div>
+                        <div className="card-div-style">{row[3].text}</div>
+
+                        <div className="card-div-style">
+                          <Label label={data.headers[4]} labelStyle={{ fontWeight: "500" }} />
+                        </div>
+                        <div className="card-sladiv-style">
+                          <span class={this.getSlaColor(row[4].text, row[2].text.props.label.split("_")[1])}>{row[4].text}</span>
+                        </div>
+
+                        <div className="card-viewHistory-icon" onClick={() => onHistoryClick(row[0])}>
+                          <i class="material-icons">history</i>
+                        </div>
+                      </div>
+                    }
+                  />
                 );
               })}
               <TaskDialog open={this.state.dialogOpen} onClose={onDialogClose} history={ProcessInstances} />
-            </TableBody>
+            </div>
           )}
-      </Table>
-    </Hidden>
-    <Hidden only={['sm','md','lg','xl']} implementation="css">
-    {data.rows.length === 0 ? (
-          <Card 
-            textChildren={
-              <Label labelClassName="" label="COMMON_INBOX_NO_DATA" />
-            }        
-          />
-        ) : (
-        <div>
-        {
-          data.rows.map((row, index) => {
-            return (
-              <Card 
-              key={index}
-              textChildren={
-                <div>
-                  <div className="head" onClick={() => getModuleLink(row[0], row, 0)} ><a style={{color:"#FE7A51"}}>{row[0].text}</a></div>
-                  <div className="head"><Label label={`CS_COMMON_INBOX_${row[0].subtext.toUpperCase()}`} color="#000000" /></div>
-              
-              <div className="card-div-style"><Label label={data.headers[1]} labelStyle={{ fontWeight: "500" }}/></div>
-              <div className="card-div-style">{row[1].text}</div>
-                  
-              <div className="card-div-style"><Label label={data.headers[2]} labelStyle={{ fontWeight: "500" }} /></div>
-              <div className="card-div-style">{row[2].text}</div>
-
-                  <div className="card-div-style"><Label label={data.headers[3]} labelStyle={{ fontWeight: "500" }} /></div>
-                  <div className="card-div-style">{row[3].text}</div>
-
-                  <div className="card-div-style"><Label label={data.headers[4]} labelStyle={{ fontWeight: "500" }} /></div>
-                  <div className="card-sladiv-style"><span class={this.getSlaColor(row[4].text, row[2].text.props.label.split('_')[1])}>{row[4].text}</span></div>
-                  
-                  <div className="card-viewHistory-icon" onClick={() => onHistoryClick(row[0])}>
-                      <i class="material-icons">history</i>
-                  </div>
-
-                </div>
-              }
-            />
-            )
-        })
-      }
-      <TaskDialog open={this.state.dialogOpen} onClose={onDialogClose} history={ProcessInstances} />
+        </Hidden>
       </div>
-    )}
-    </Hidden>
-    </div>
     );
   }
 }
@@ -249,16 +267,13 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    setRoute: url => dispatch(setRoute(url)),
+    setRoute: (url) => dispatch(setRoute(url)),
     toggleSnackbarAndSetText: (open, message) => dispatch(toggleSnackbarAndSetText(open, message)),
     prepareFinalObject: (path, value) => dispatch(prepareFinalObject(path, value)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(InboxData);
+export default connect(mapStateToProps, mapDispatchToProps)(InboxData);
 
 export const Taskboard = ({ data }) => {
   return (
