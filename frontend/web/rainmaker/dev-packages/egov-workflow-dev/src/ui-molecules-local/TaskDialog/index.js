bd78f08f520c5d812a6b54b7bfd4f5a3ec09149e
yogendra-egov <56108459+yogendra-egov@users.noreply.github.com>
2020-01-06 18:16:51 +0530

    RAIN-382: Fullscreen popup zIndex increased to place it top of header in mobile screen (#266)

--- frontend/web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/TaskDialog/index.js
+++ frontend/web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/TaskDialog/index.js
@@ -18,16 +18,16 @@ const styles = theme => ({
 const TaskDialog = props => {
   const { open, onClose, history } = props;
   let fullscreen = false;
-  // Fullscreen covering full mobile screen making it impossible to close dialog. Hence commenting out below line
-  // if (window.innerWidth <= 768) {
-  //   fullscreen = true;
-  // }
+  if (window.innerWidth <= 768) {
+    fullscreen = true;
+  }
   return (
     <Dialog
       fullScreen={fullscreen}
       open={open}
       onClose={onClose}
       maxWidth={false}
+      style={{zIndex:2000}}
     >
       <DialogContent
         children={
