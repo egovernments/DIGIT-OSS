a88cf57aa1a70b20c425e2f19fe145b410596d60
gopesh67 <43036707+gopesh67@users.noreply.github.com>
2020-01-06 17:01:52 +0530

    bpaedcr -- language selection for unauthorized page (#262)
    Co-authored-by: sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>

--- frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/hocs/withoutAuthorization.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/hocs/withoutAuthorization.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import AppBar from "@material-ui/core/AppBar";
+// import AppBar from "@material-ui/core/AppBar";
 import "./index.css";
-import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
+import { getLocale, getTenantId, getUserInfo } from "egov-ui-kit/utils/localStorageUtils";
 import digitLogo from "egov-ui-kit/assets/images/Digit_logo.png";
 import Label from "egov-ui-kit/utils/translationNode";
 import get from "lodash/get";
+import { fetchLocalizationLabel } from "egov-ui-kit/redux/app/actions";
+import { DropDown, AppBar } from "components";
+import { getQueryArg } from "egov-ui-kit/utils/commons";
+import Toolbar from "material-ui/Toolbar";
 
 const getUlbGradeLabel = (ulbGrade) => {
   if (ulbGrade) {
@@ -19,36 +23,119 @@ const getUlbGradeLabel = (ulbGrade) => {
 
 const withoutAuthorization = (redirectionUrl) => (Component) => {
   class Wrapper extends React.Component {
+    state = {
+      languageSelected: getLocale(),
+    };
+    style = {
+      baseStyle: {
+        background: "#ffffff",
+        height: "65px",
+        marginRight: "30px",
+        width: "98px",
+        marginBottom: "24px",
+      },
+      label: {
+        color: "#5F5C57",
+        fontSize: "12px",
+        paddingRight: "0px",
+      },
+      arrowIconStyle: {
+        marginTop: "7px",
+        marginLeft: "10px",
+      },
+      iconStyle: {
+        marginRight: "30px",
+      },
+      listStyle: {
+        display: "block",
+      },
+      listInnerDivStyle: {
+        padding: "10px",
+        display: "flex",
+        alignItems: "center",
+      },
+      baseTenantStyle: {
+        background: "#ffffff",
+        height: "65px",
+        marginRight: "30px",
+        width: "120px",
+        marginBottom: "24px",
+      },
+      titleStyle: { fontSize: "20px", fontWeight: 500 },
+    };
+
     componentDidMount() {
       if (this.props.authenticated) {
         this.props.history.push(redirectionUrl);
       }
     }
-    render() {
-      const { isOpenLink, ulbLogo, defaultTitle, ulbName } = this.props;
 
+    onLanguageChange = (event, index, value) => {
+      //const {setRote} = this.props;
+      this.setState({ languageSelected: value });
+      let tenantId = getTenantId();
+
+      if (process.env.REACT_APP_NAME === "Citizen") {
+        const tenantInfo = getQueryArg(window.location.href, "tenantId");
+        const userInfo = JSON.parse(getUserInfo());
+        tenantId = userInfo && userInfo.permanentCity;
+        tenantId = tenantInfo ? tenantInfo : tenantId;
+      }
+      this.props.fetchLocalizationLabel(value, tenantId, tenantId);
+    };
+
+    render() {
+      const { isOpenLink, ulbLogo, defaultTitle, ulbName, hasLocalisation, languages, ...rest } = this.props;
+      const { languageSelected } = this.state;
+
+      const { style } = this;
       return (
         <div>
           {/* FIXME need to move appbar as new component */}
           {isOpenLink ? (
             <div className="rainmaker-header-cont" style={{ position: "relative" }}>
               <div style={{ lineHeight: "64px" }}>
-                <AppBar className="rainmaker-header">
-                  <div className="citizen-header-logo-label">
-                    <div className="citizen-header-logo">
-                      <img src={ulbLogo ? ulbLogo : pbLogo} onError={(event) => event.target.setAttribute("src", pbLogo)} />
+                <AppBar
+                  className="rainmaker-header"
+                  title={
+                    <div className="citizen-header-logo-label">
+                      <div className="citizen-header-logo">
+                        <img src={ulbLogo ? ulbLogo : pbLogo} onError={(event) => event.target.setAttribute("src", pbLogo)} />
+                      </div>
+                      <div className="rainmaker-displayInline">
+                        <Label
+                          containerStyle={{ marginLeft: "10px" }}
+                          className="screenHeaderLabelStyle appbar-municipal-label"
+                          label={ulbName && `TENANT_TENANTS_${ulbName.toUpperCase().replace(/[.]/g, "_")}`}
+                        />
+                        <Label
+                          containerStyle={{ marginLeft: "4px" }}
+                          className="screenHeaderLabelStyle appbar-municipal-label"
+                          label={defaultTitle}
+                        />
+                      </div>
                     </div>
-                    <div className="rainmaker-displayInline">
-                      <Label
-                        containerStyle={{ marginLeft: "10px" }}
-                        className="screenHeaderLabelStyle appbar-municipal-label"
-                        label={ulbName && `TENANT_TENANTS_${ulbName.toUpperCase().replace(/[.]/g, "_")}`}
-                      />
-                      <Label containerStyle={{ marginLeft: "4px" }} className="screenHeaderLabelStyle appbar-municipal-label" label={defaultTitle} />
-                    </div>
-                    <div style={{ position: "absolute", right: "12px" }}>
-                      <img src={digitLogo} />
-                    </div>
+                  }
+                  titleStyle={style.titleStyle}
+                  {...rest}
+                >
+                  <Toolbar className="app-toolbar" style={{ padding: "0px", height: "64px", background: "#ffffff" }}>
+                    {hasLocalisation && (
+                      <div className="userSettingsContainer">
+                        <DropDown
+                          onChange={this.onLanguageChange}
+                          listStyle={style.listStyle}
+                          style={style.baseStyle}
+                          labelStyle={style.label}
+                          dropDownData={languages}
+                          value={languageSelected}
+                          underlineStyle={{ borderBottom: "none" }}
+                        />
+                      </div>
+                    )}
+                  </Toolbar>
+                  <div className="appbar-right-logo">
+                    <img src={digitLogo} />
                   </div>
                 </AppBar>
               </div>
@@ -81,9 +168,22 @@ const withoutAuthorization = (redirectionUrl) => (Component) => {
       hasLocalisation = stateInfoById[0].hasLocalisation;
       defaultUrl = stateInfoById[0].defaultUrl;
     }
-    return { authenticated, hasLocalisation, defaultUrl, isOpenLink, ulbLogo, ulbName, defaultTitle };
+    let languages = get(stateInfoById, "0.languages", []);
+
+    return { authenticated, hasLocalisation, defaultUrl, isOpenLink, ulbLogo, ulbName, defaultTitle, languages };
   };
-  return connect(mapStateToProps)(Wrapper);
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      // logout: () => dispatch(logout()),
+      fetchLocalizationLabel: (locale, tenants, tenant) => dispatch(fetchLocalizationLabel(locale, tenants, tenant)),
+      // updateActiveRoute: (routepath, menuName) => dispatch(updateActiveRoute(routepath, menuName)),
+    };
+  };
+
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Wrapper);
 };
 
 export default withoutAuthorization;
