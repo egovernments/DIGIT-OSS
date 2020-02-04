4381d4139a07ad72c1e7db7998f32bb9b8ec378a
Hemanth <k.hemanth@egovernments.org>
2020-02-04 12:54:44 +0530

    Owner Photo upload validation

--- frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-utils/commons.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-utils/commons.js
@@ -330,16 +330,22 @@ export const handleFileUpload = (event, handleDocument, props) => {
     endPoint: "filestore/v1/files"
   };
   let uploadDocument = true;
+  let docName="";
+  props.documents.forEach(item => {
+    if(item.name == "OWNERPHOTO"){
+      docName=item.name;
+    }
+  })
   const { inputProps, maxFileSize, moduleName } = props;
   const input = event.target;
   if (input.files && input.files.length > 0) {
     const files = input.files;
     Object.keys(files).forEach(async (key, index) => {
       const file = files[key];
-      const fileValid = isFileValid(file, acceptedFiles(inputProps.accept));
+      const fileValid = docName == "OWNERPHOTO" ? isFileValid(file, acceptedFiles(props.imageProps.accept)): isFileValid(file, acceptedFiles(inputProps.accept));
       const isSizeValid = getFileSize(file) <= maxFileSize;
       if (!fileValid) {
-        alert(`Only image or pdf files can be uploaded`);
+        docName == "OWNERPHOTO"? alert(`Only image files can be uploaded`) : alert(`Only image or pdf files can be uploaded`);
         uploadDocument = false;
       }
       if (!isSizeValid) {
