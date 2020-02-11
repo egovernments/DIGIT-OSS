b069e48862ba84be97533aeebf39a499b1cf86f8
Shreya B <shreya.b@egovernments.org>
2020-02-11 12:48:17 +0530

    Changed logic for setting FY

--- municipal-services/tl-services/src/main/java/org/egov/tl/service/EnrichmentService.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/service/EnrichmentService.java
@@ -67,9 +67,12 @@ public class EnrichmentService {
                     //TLR Changes
                     if(tradeLicense.getApplicationType() != null && tradeLicense.getApplicationType().toString().equals(TLConstants.APPLICATION_TYPE_RENEWAL)){
                         tradeLicense.setLicenseNumber(tradeLicenseRequest.getLicenses().get(0).getLicenseNumber());
-                        Map<String, Long> taxPeriods = tradeUtil.getTaxPeriodsforRenewal(tradeLicense, mdmsData);
-                        tradeLicense.setValidTo(taxPeriods.get(TLConstants.MDMS_ENDDATE));
-                        tradeLicense.setValidFrom(taxPeriods.get(TLConstants.MDMS_STARTDATE));
+                        Map<String, String> taxPeriods = tradeUtil.getTaxPeriodsforRenewal(tradeLicense, mdmsData);
+                        tradeLicense.setValidTo(Long.parseLong(taxPeriods.get(TLConstants.MDMS_ENDDATE)));
+                        tradeLicense.setValidFrom(Long.parseLong(taxPeriods.get(TLConstants.MDMS_STARTDATE)));
+                        if(taxPeriods.containsKey(TLConstants.FINANCIAL_YEAR)) {
+                            tradeLicense.setFinancialYear(taxPeriods.get(TLConstants.FINANCIAL_YEAR));
+                        }
                     }else{
                         Map<String, Long> taxPeriods = tradeUtil.getTaxPeriods(tradeLicense, mdmsData);
                         if (tradeLicense.getLicenseType().equals(TradeLicense.LicenseTypeEnum.PERMANENT) || tradeLicense.getValidTo() == null)
