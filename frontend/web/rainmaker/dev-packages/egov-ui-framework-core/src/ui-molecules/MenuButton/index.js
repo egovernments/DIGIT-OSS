4d5ddb2cd0fa347b3d6fe30af07453938986934f
Ashok <ashok.kumar@egovernments.org>
2020-03-12 14:54:12 +0530

    quick action dropdown fix

--- frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/MenuButton/index.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/MenuButton/index.js
@@ -69,7 +69,7 @@ class MenuListComposition extends React.Component {
               <span style={{marginLeft:30 ,color : data.props.color}}> |  </span>
             <Icon className={classes.rightIcon} iconName={data.rightIcon} />
           </Button>
-          <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+          <Popper style={{zIndex:9}} open={open} anchorEl={this.anchorEl} transition disablePortal>
             {({ TransitionProps, placement }) => (
               <Grow
                 {...TransitionProps}
@@ -81,7 +81,7 @@ class MenuListComposition extends React.Component {
               >
                 <Paper>
                   <ClickAwayListener onClickAway={this.handleClose}>
-                    <MenuList className="menu-list-style">
+                    <MenuList className="menu-list-style" >
                       {data.menu.map((item, key) => {
                         const { labelName, labelKey } = item;
                         return (
