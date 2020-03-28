81afbf2ccc06866c62e702b370abb314ce5ad533
Tarun Lalwani <tarunlalwani@gmail.com>
2020-03-28 22:54:27 +0530

    Fix the view errors

--- utilities/egov-pdf/src/app.js
+++ utilities/egov-pdf/src/app.js
@@ -7,10 +7,6 @@ var epassRouter = require('./routes/epass');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
