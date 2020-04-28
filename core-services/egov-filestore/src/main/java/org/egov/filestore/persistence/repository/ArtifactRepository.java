5e854ae4dddcd2b3ea1fede15f16f3751b0c843f
Manikanta <manikanta@egovernments.org>
2020-04-28 21:00:36 +0530

    Fixed S3 pick for get by id api

--- core-services/egov-filestore/src/main/java/org/egov/filestore/persistence/repository/ArtifactRepository.java
+++ core-services/egov-filestore/src/main/java/org/egov/filestore/persistence/repository/ArtifactRepository.java
@@ -18,26 +18,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class ArtifactRepository {
 
-	
 	private FileStoreJpaRepository fileStoreJpaRepository;
-	
+	@Autowired
+	private AwsS3Repository s3Repository;
+
 	@Autowired
 	private CloudFilesManager cloudFilesManager;
-	
+
 	@Value("${isS3Enabled}")
 	private Boolean isS3Enabled;
-	
+
 	@Value("${isAzureStorageEnabled}")
 	private Boolean isAzureStorageEnabled;
-	
+
 	@Value("${source.s3}")
 	private String awsS3Source;
-	
+
 	@Value("${source.azure.blob}")
 	private String azureBlobSource;
 
+	@Value("${source.disk}")
+	private String diskFileStorage;
+
 	public ArtifactRepository(FileStoreJpaRepository fileStoreJpaRepository) {
-	
+
 		this.fileStoreJpaRepository = fileStoreJpaRepository;
 	}
 
@@ -47,11 +51,10 @@ public class ArtifactRepository {
 		artifacts.forEach(artifact -> {
 			artifactEntities.add(mapToEntity(artifact));
 		});
-		return fileStoreJpaRepository.save(artifactEntities).stream()
-				.map(Artifact::getFileStoreId)
+		return fileStoreJpaRepository.save(artifactEntities).stream().map(Artifact::getFileStoreId)
 				.collect(Collectors.toList());
 	}
-	
+
 	/**
 	 * Converts POJO artifact to JPA Entity artifact
 	 * 
@@ -61,50 +64,65 @@ public class ArtifactRepository {
 	private Artifact mapToEntity(org.egov.filestore.domain.model.Artifact artifact) {
 
 		FileLocation fileLocation = artifact.getFileLocation();
-		Artifact entityArtifact = Artifact.builder().fileStoreId(fileLocation.getFileStoreId()).fileName(fileLocation.getFileName())
-				.contentType(artifact.getMultipartFile().getContentType()).module(fileLocation.getModule())
-				.tag(fileLocation.getTag()).tenantId(fileLocation.getTenantId()).fileSource(fileLocation.getFileSource()).build();
-		if(isAzureStorageEnabled)
+		Artifact entityArtifact = Artifact.builder().fileStoreId(fileLocation.getFileStoreId())
+				.fileName(fileLocation.getFileName()).contentType(artifact.getMultipartFile().getContentType())
+				.module(fileLocation.getModule()).tag(fileLocation.getTag()).tenantId(fileLocation.getTenantId())
+				.fileSource(fileLocation.getFileSource()).build();
+		if (isAzureStorageEnabled)
 			entityArtifact.setFileSource(azureBlobSource);
-		if(isS3Enabled)
+		if (isS3Enabled)
 			entityArtifact.setFileSource(awsS3Source);
 
 		return entityArtifact;
 	}
 
-/*	private List<Artifact> mapArtifactsListToEntitiesList(List<org.egov.filestore.domain.model.Artifact> artifacts) {
-		return artifacts.stream()
-				.map(this::mapToEntity)
-				.collect(Collectors.toList());
-	}
-
-	private Artifact mapToEntity(org.egov.filestore.domain.model.Artifact artifact) {
-
-		FileLocation fileLocation = artifact.getFileLocation();
-		return Artifact.builder().fileStoreId(fileLocation.getFileStoreId()).fileName(fileLocation.getFileName())
-				.contentType(artifact.getMultipartFile().getContentType()).module(fileLocation.getModule())
-				.tag(fileLocation.getTag()).tenantId(fileLocation.getTenantId()).build();
-	}*/
-
+	/*
+	 * private List<Artifact>
+	 * mapArtifactsListToEntitiesList(List<org.egov.filestore.domain.model.
+	 * Artifact> artifacts) { return artifacts.stream() .map(this::mapToEntity)
+	 * .collect(Collectors.toList()); }
+	 * 
+	 * private Artifact mapToEntity(org.egov.filestore.domain.model.Artifact
+	 * artifact) {
+	 * 
+	 * FileLocation fileLocation = artifact.getFileLocation(); return
+	 * Artifact.builder().fileStoreId(fileLocation.getFileStoreId()).fileName(
+	 * fileLocation.getFileName())
+	 * .contentType(artifact.getMultipartFile().getContentType()).module(
+	 * fileLocation.getModule())
+	 * .tag(fileLocation.getTag()).tenantId(fileLocation.getTenantId()).build();
+	 * }
+	 */
+/**
+ * 
+ * @param fileStoreId
+ * @param tenantId
+ * @return
+ * @throws IOException
+ * This api needs to be enhanced to pick right object .All repositories should implement cloudmanager and it should provide 
+ * simple get api too
+ */
 	public Resource find(String fileStoreId, String tenantId) throws IOException {
 		Artifact artifact = fileStoreJpaRepository.findByFileStoreIdAndTenantId(fileStoreId, tenantId);
 		if (artifact == null)
 			throw new ArtifactNotFoundException(fileStoreId);
 
-
-		
-		//if only DiskFileStoreRepository use read else ignore
-		if(cloudFilesManager.getClass().getSimpleName().equalsIgnoreCase("DiskFileStoreRepository"))
-		{
-		DiskFileStoreRepository diskfileStore=(DiskFileStoreRepository) cloudFilesManager;
+		org.springframework.core.io.Resource resource = null;
+	if (artifact.getFileLocation().getFileSource().equals(diskFileStorage)) {
+			// if only DiskFileStoreRepository use read else ignore
+			DiskFileStoreRepository diskfileStore = (DiskFileStoreRepository) cloudFilesManager;
+			resource = diskfileStore.read(artifact.getFileLocation());
+		} else if (artifact.getFileLocation().getFileSource().equals(awsS3Source)) {
+			 
+		//	AwsS3Repository s3Repository = (AwsS3Repository) cloudFilesManager;
+			resource = s3Repository.getObject(artifact.getFileLocation().getFileName());
+	}
 		 
-		org.springframework.core.io.Resource resource = diskfileStore.read(artifact.getFileLocation());
-		return new Resource(artifact.getContentType(), artifact.getFileName(), resource, artifact.getTenantId(),""+resource.getFile().length()+" bytes");
-		}
-		else
-		{
-			return null;
-		}
+      if(null!=resource)
+		return new Resource(artifact.getContentType(), artifact.getFileName(), resource, artifact.getTenantId(),
+				"" + resource.getFile().length() + " bytes");
+      else
+    	  return null;
 	}
 
 	public List<FileInfo> findByTag(String tag, String tenantId) {
@@ -113,13 +131,13 @@ public class ArtifactRepository {
 	}
 
 	private FileInfo mapArtifactToFileInfo(Artifact artifact) {
-		FileLocation fileLocation = new FileLocation(artifact.getFileStoreId(), artifact.getModule(),
-				 artifact.getTag(),artifact.getTenantId(),artifact.getFileName(),artifact.getFileSource());
+		FileLocation fileLocation = new FileLocation(artifact.getFileStoreId(), artifact.getModule(), artifact.getTag(),
+				artifact.getTenantId(), artifact.getFileName(), artifact.getFileSource());
 
 		return new FileInfo(artifact.getContentType(), fileLocation, artifact.getTenantId());
 	}
-	
-	public List<Artifact> getByTenantIdAndFileStoreIdList(String tenantId, List<String> fileStoreIds){
+
+	public List<Artifact> getByTenantIdAndFileStoreIdList(String tenantId, List<String> fileStoreIds) {
 		return fileStoreJpaRepository.findByTenantIdAndFileStoreIdList(tenantId, fileStoreIds);
 	}
 }
