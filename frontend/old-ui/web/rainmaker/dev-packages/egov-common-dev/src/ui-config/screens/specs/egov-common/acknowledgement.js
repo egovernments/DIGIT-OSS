dd5011dde3ba23fb80fb6cf4e84823bb29eaa5f3
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2020-05-19 13:24:03 +0530

    Citizen Redirect Issue Fixed (#1262)

--- frontend/old-ui/web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgement.js
+++ frontend/old-ui/web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgement.js
@@ -1,84 +1,84 @@
-import { paymentFooter } from "./acknowledgementResource/paymentFooter";
-import acknowledgementCard from "./acknowledgementResource/acknowledgementUtils";
 import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
-import set from "lodash/set";
 import get from "lodash/get";
+import set from "lodash/set";
+import { download } from "../../../../ui-utils/commons";
 import { ifUserRoleExists } from "../utils";
-import {download} from  "../../../../ui-utils/commons";
-import {getHeader} from "./pay";
+import acknowledgementCard from "./acknowledgementResource/acknowledgementUtils";
+import { paymentFooter } from "./acknowledgementResource/paymentFooter";
 import './index.css';
+import { getHeader } from "./pay";
 
 
-const downloadprintMenu=(state,applicationNumber,tenantId,uiCommonPayConfig)=>{
+const downloadprintMenu = (state, applicationNumber, tenantId, uiCommonPayConfig) => {
     const receiptKey = get(uiCommonPayConfig, "receiptKey")
-      let receiptDownloadObject = {
+    let receiptDownloadObject = {
         label: { labelName: "DOWNLOAD RECEIPT", labelKey: "COMMON_DOWNLOAD_RECEIPT" },
         link: () => {
             const receiptQueryString = [
                 { key: "receiptNumbers", value: applicationNumber },
                 { key: "tenantId", value: tenantId }
             ]
-            download(receiptQueryString , "download" , receiptKey, state);
-          
+            download(receiptQueryString, "download", receiptKey, state);
+
         },
         leftIcon: "receipt"
-      };
-      let receiptPrintObject = {
+    };
+    let receiptPrintObject = {
         label: { labelName: "PRINT RECEIPT", labelKey: "COMMON_PRINT_RECEIPT" },
         link: () => {
             const receiptQueryString = [
                 { key: "receiptNumbers", value: applicationNumber },
                 { key: "tenantId", value: tenantId }
             ]
-            download(receiptQueryString  ,"print" , receiptKey ,state);
+            download(receiptQueryString, "print", receiptKey, state);
         },
         leftIcon: "receipt"
-      };
+    };
     let downloadMenu = [];
     let printMenu = [];
-        downloadMenu = [receiptDownloadObject];
-        printMenu = [receiptPrintObject];
-    
-  
-      return {
+    downloadMenu = [receiptDownloadObject];
+    printMenu = [receiptPrintObject];
+
+
+    return {
         uiFramework: "custom-atoms",
         componentPath: "Div",
         props: {
-          className:"downloadprint-commonmenu",
-          style: { textAlign: "right", display: "flex" }
+            className: "downloadprint-commonmenu",
+            style: { textAlign: "right", display: "flex" }
         },
         children: {
-          downloadMenu: {
-            uiFramework: "custom-molecules",
-            componentPath: "DownloadPrintButton",
-            props: {
-              data: {
-                label: {labelName : "DOWNLOAD" , labelKey :"TL_DOWNLOAD"},
-                 leftIcon: "cloud_download",
-                rightIcon: "arrow_drop_down",
-                props: { variant: "outlined", style: { height: "60px", color : "#FE7A51" }, className: "tl-download-button" },
-                menu: downloadMenu
-              }
+            downloadMenu: {
+                uiFramework: "custom-molecules",
+                componentPath: "DownloadPrintButton",
+                props: {
+                    data: {
+                        label: { labelName: "DOWNLOAD", labelKey: "TL_DOWNLOAD" },
+                        leftIcon: "cloud_download",
+                        rightIcon: "arrow_drop_down",
+                        props: { variant: "outlined", style: { height: "60px", color: "#FE7A51" }, className: "tl-download-button" },
+                        menu: downloadMenu
+                    }
+                }
+            },
+            printMenu: {
+                uiFramework: "custom-molecules",
+                componentPath: "DownloadPrintButton",
+                props: {
+                    data: {
+                        label: { labelName: "PRINT", labelKey: "TL_PRINT" },
+                        leftIcon: "print",
+                        rightIcon: "arrow_drop_down",
+                        props: { variant: "outlined", style: { height: "60px", color: "#FE7A51" }, className: "tl-print-button" },
+                        menu: printMenu
+                    }
+                }
             }
-          },
-          printMenu: {
-            uiFramework: "custom-molecules",
-            componentPath: "DownloadPrintButton",
-            props: {
-              data: {
-                label: {labelName : "PRINT" , labelKey :"TL_PRINT"},
-                leftIcon: "print",
-                rightIcon: "arrow_drop_down",
-                props: { variant: "outlined", style: { height: "60px", color : "#FE7A51"}, className: "tl-print-button" },
-                menu: printMenu
-              }
-            }
-          }
-    
+
         },
-      }
-     
-  }
+    }
+
+}
 const getAcknowledgementCard = (
     state,
     dispatch,
@@ -91,11 +91,11 @@ const getAcknowledgementCard = (
     let header = getHeader(state);
     const businessService = getQueryArg(window.location.href, "businessService");
     const transBusinessService = businessService ? businessService.toUpperCase().replace(/[._:-\s\/]/g, "_") : "DEFAULT";
-    const uiCommonPayConfig = get(state.screenConfiguration.preparedFinalObject , "commonPayInfo");
+    const uiCommonPayConfig = get(state.screenConfiguration.preparedFinalObject, "commonPayInfo");
     if (status === "success") {
         return {
             header,
-            headerdownloadprint:downloadprintMenu(state,receiptNumber,tenant,uiCommonPayConfig),
+            headerdownloadprint: downloadprintMenu(state, receiptNumber, tenant, uiCommonPayConfig),
             applicationSuccessCard: {
                 uiFramework: "custom-atoms",
                 componentPath: "Div",
@@ -103,20 +103,20 @@ const getAcknowledgementCard = (
                     card: acknowledgementCard({
                         icon: "done",
                         backgroundColor: "#39CB74",
-                            header: {
-                                labelKey: roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE`
-                            },
-                            body: {
-                                labelKey: roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE_DETAIL` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE_DETAIL`
-                            },
-                            tailText: {
-                                labelKey : roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_RECEIPT_NO` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_RECEIPT_NO`
-                            },
+                        header: {
+                            labelKey: roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE`
+                        },
+                        body: {
+                            labelKey: roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE_DETAIL` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_MESSAGE_DETAIL`
+                        },
+                        tailText: {
+                            labelKey: roleExists ? `CITIZEN_SUCCESS_${transBusinessService}_PAYMENT_RECEIPT_NO` : `EMPLOYEE_SUCCESS_${transBusinessService}_PAYMENT_RECEIPT_NO`
+                        },
                         number: receiptNumber
                     })
                 }
             },
-            paymentFooter: paymentFooter(state,consumerCode, tenant, status)
+            paymentFooter: paymentFooter(state, consumerCode, tenant, status, businessService)
         };
     } else if (status === "failure") {
         return {
@@ -137,7 +137,7 @@ const getAcknowledgementCard = (
                     })
                 }
             },
-            paymentFooter: paymentFooter(state,consumerCode, tenant,status)
+            paymentFooter: paymentFooter(state, consumerCode, tenant, status, businessService)
         };
     }
 };
@@ -168,7 +168,7 @@ const screenConfig = {
             consumerCode,
             tenant
         );
-        set(action, "screenConfig.components.div.children", data);        
+        set(action, "screenConfig.components.div.children", data);
         return action;
     }
 };
