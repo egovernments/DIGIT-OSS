2e411a8397e95dbf01cc2e2b59e49daca9eb4bb4
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2020-06-22 12:49:17 +0530

    Mdms maven upgrade (#153)
    * Upgraded maven dependency for mdms service
    
    * Added actuator for health check
    
    * change in server.context-path and server.servlet.context-path
    
    * Updated CHANGELOG.md
    
    * Add previous MDMS upgrades and remove update codes and API
    
    * Update pom.xml
    
    Co-authored-by: Tarun Lalwani <tarunlalwani@gmail.com>
    Co-authored-by: Tarun Lalwani <tarunlalwani@users.noreply.github.com>

--- core-services/egov-mdms-service/src/main/java/org/egov/MDMSApplicationRunnerImpl.java
+++ core-services/egov-mdms-service/src/main/java/org/egov/MDMSApplicationRunnerImpl.java
@@ -1,10 +1,9 @@
 package org.egov;
 
+import java.lang.reflect.*;
+import java.util.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -13,177 +12,189 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+
 import javax.annotation.PostConstruct;
 
+import com.fasterxml.jackson.core.type.*;
+import org.apache.commons.io.*;
+import org.egov.infra.mdms.utils.MDMSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 
+
 @Component
 @Slf4j
 public class MDMSApplicationRunnerImpl {
 
-	@Autowired
-	public ResourceLoader resourceLoader;
+    @Autowired
+    public ResourceLoader resourceLoader;
 
-	@Value("${egov.mdms.conf.path}")
-	public String mdmsFileDirectory;
+    @Value("${egov.mdms.conf.path}")
+    public String mdmsFileDirectory;
 
-	@Value("${masters.config.url}")
-	public String masterConfigUrl;
+    @Value("${masters.config.url}")
+    public String masterConfigUrl;
 
-	private static Map<String, Map<String, Map<String, JSONArray>>> tenantMap = new HashMap<>();
+    @Value("${egov.mdms.stopOnAnyConfigError:true}")
+    public boolean stopOnAnyConfigError;
 
-	private static Map<String, Map<String, Object>> masterConfigMap = new HashMap<>();
+    private static Map<String, Map<String, Map<String, JSONArray>>> tenantMap = new HashMap<>();
 
-	ObjectMapper objectMapper = new ObjectMapper();
+    private static Map<String, Map<String, Object>> masterConfigMap = new HashMap<>();
 
-	@PostConstruct
-	public void run() {
-		try {
-			log.info("Reading yaml files from: " + mdmsFileDirectory);
-			readFiles(mdmsFileDirectory);
-			readMdmsConfigFiles(masterConfigUrl);
-			log.info("tenantMap1:" + tenantMap);
+    ObjectMapper objectMapper = new ObjectMapper();
 
-		} catch (Exception e) {
-			log.error("Exception while loading yaml files: ", e);
-		}
+    @PostConstruct
+    public void run() {
+        try {
+            log.info("Reading files from: " + mdmsFileDirectory);
+            LinkedList<String> errorFilesList = new LinkedList<>();
+            readMdmsConfigFiles(masterConfigUrl);
+            readFiles(mdmsFileDirectory, errorFilesList);
+            log.info("List Of Files which has Error while parsing " + errorFilesList);
+            if (!errorFilesList.isEmpty() && stopOnAnyConfigError) {
+                log.info("Stopping as all files could not be loaded");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            log.error("Exception while loading yaml files: ", e);
+        }
 
-	}
+    }
 
-	public void readFiles(String baseFoderPath) {
-		ObjectMapper jsonReader = new ObjectMapper();
-		File folder = new File(baseFoderPath);
-		File[] listOfFiles = folder.listFiles();
+    public void readFiles(String baseFolderPath, LinkedList<String> errorList) {
+        File folder = new File(baseFolderPath);
+        File[] listOfFiles = folder.listFiles();
+        if (listOfFiles != null) {
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+                    String name = file.getName();
+                    String fileExtension = FilenameUtils.getExtension(file.getAbsolutePath()).toLowerCase();
 
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				log.info("File " + listOfFiles[i].getName());
-				File file = listOfFiles[i];
-				String name = file.getName();
-				String[] fileName = name.split("[.]");
-				if (fileName[fileName.length - 1].equals("json")) {
-					log.debug("Reading json file....:- " + name);
-					try {
-						Map<String, Object> jsonMap = jsonReader.readValue(file, Map.class);
-						prepareTenantMap(jsonMap);
-						log.debug("json str:" + jsonMap);
-					} catch (JsonGenerationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (JsonMappingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				} else {
-					log.info("file is not of a valid type please change and retry");
-					log.info("Note: file can either be .yml/.yaml or .json");
 
-				}
+                    if (fileExtension.equalsIgnoreCase("json")
+                            || fileExtension.equalsIgnoreCase("yaml")
+                            || fileExtension.equalsIgnoreCase("yml")
+                    ) {
+                        log.debug("Reading file....:- " + file.getAbsolutePath());
+                        try {
+                            Map<String, Object> jsonMap = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {
+                                @Override
+                                public Type getType() {
+                                    return super.getType();
+                                }
+                            });
+                            prepareTenantMap(jsonMap);
+                        } catch (Exception e) {
+                            log.error("Error occurred while loading file", e);
+                            errorList.add(file.getAbsolutePath());
+                        }
+                    }
+                } else if (file.isDirectory()) {
+                    readFiles(file.getAbsolutePath(), errorList);
+                }
+            }
+        }
+    }
 
-			} else if (listOfFiles[i].isDirectory()) {
-				log.info("Directory " + listOfFiles[i].getName());
-				readFiles(listOfFiles[i].getAbsolutePath());
-			}
-		}
+    public void prepareTenantMap(Map<String, Object> map) {
 
-	}
+        String tenantId = (String) map.get("tenantId");
+        String moduleName = (String) map.get("moduleName");
+        Set<String> masterKeys = map.keySet();
+        String nonMasterKeys = "tenantId,moduleName";
+        List<String> ignoreKey = new ArrayList<String>(Arrays.asList(nonMasterKeys.split(",")));
+        masterKeys.removeAll(ignoreKey);
+        boolean isMergeAllowed;
+        Map<String, JSONArray> masterDataMap = new HashMap<>();
+        Iterator<String> masterKeyIterator = masterKeys.iterator();
+        String masterName = null;
+        JSONArray masterDataJsonArray = null;
+        while (masterKeyIterator.hasNext()) {
+            masterName = masterKeyIterator.next();
+            try {
+                masterDataJsonArray = JsonPath.read(objectMapper.writeValueAsString(map.get(masterName)),
+                        "$");
+            } catch (JsonProcessingException e) {
+                log.error("Error while parsing file", e);
+            }
 
-	@SuppressWarnings("unchecked")
-	public void prepareTenantMap(Map<String, Object> map) {
-		// ObjectMapper objectMapper = new ObjectMapper();
+            if (!tenantMap.containsKey(tenantId)) {
+                Map<String, Map<String, JSONArray>> moduleMap = new HashMap<>();
+                moduleMap.put(moduleName, masterDataMap);
+                tenantMap.put(tenantId, moduleMap);
+            } else {
+                Map<String, Map<String, JSONArray>> tenantModule = tenantMap.get(tenantId);
 
-		String tenantId = (String) map.get("tenantId");
-		String moduleName = (String) map.get("moduleName");
-		Set<String> masterKeys = map.keySet();
-		String nonMasterKeys = "tenantId,moduleName";
-		List<String> ignoreKey = new ArrayList<String>(Arrays.asList(nonMasterKeys.split(",")));
-		masterKeys.removeAll(ignoreKey);
+                if (!tenantModule.containsKey(moduleName)) {
+                    tenantModule.put(moduleName, masterDataMap);
+                } else {
+                    Map<String, JSONArray> moduleMaster = tenantModule.get(moduleName);
+                    isMergeAllowed = isMergeAllowedForMaster(moduleName, masterName);
 
-		Map<String, JSONArray> masterDataMap = new HashMap<>();
+                    if (!moduleMaster.containsKey(masterName)) {
+                        masterDataMap.put(masterName, masterDataJsonArray);
+                        moduleMaster.putAll(masterDataMap);
+                        tenantModule.put(moduleName, moduleMaster);
+                    } else if (moduleMaster.containsKey(masterName) && isMergeAllowed) {
+                        JSONArray existingMasterDataJsonArray = moduleMaster.get(masterName);
+                        existingMasterDataJsonArray.merge(masterDataJsonArray);
+                    } else if ((moduleMaster.containsKey(masterName) && !isMergeAllowed)) {
+                        log.error("merge is not allowed for master ++" + moduleName + " " + masterName);
+                    }
+                }
+                tenantMap.put(tenantId, tenantModule);
+            }
+            masterDataMap.put(masterName, masterDataJsonArray);
+        }
+    }
 
-		Iterator<String> masterKeyIterator = masterKeys.iterator();
-		String masterName = null;
-		JSONArray masterDataJsonArray = null;
-		while (masterKeyIterator.hasNext()) {
-			masterName = masterKeyIterator.next();
+    public void readMdmsConfigFiles(String masterConfigUrl) {
+        log.info("Loading master configs from: " + masterConfigUrl);
+        Resource resource = resourceLoader.getResource(masterConfigUrl);
+        try {
+            masterConfigMap = objectMapper.readValue(resource.getInputStream(), new TypeReference<Map<String, Map<String, Object>>>() {
+            });
+        } catch (IOException e) {
+            log.error("Exception while fetching service map for: ", e);
+        }
 
-			try {
-				masterDataJsonArray = JsonPath.read(objectMapper.writeValueAsString((List<Object>) map.get(masterName)),
-						"$");
-			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			masterDataMap.put(masterName, masterDataJsonArray);
-		}
+        log.info("the Master config Map : " + masterConfigMap);
+    }
 
-		if (!tenantMap.containsKey(tenantId)) {
-			Map<String, Map<String, JSONArray>> moduleMap = new HashMap<>();
-			moduleMap.put(moduleName, masterDataMap);
-			tenantMap.put(tenantId, moduleMap);
-		} else {
-			Map<String, Map<String, JSONArray>> tenantModule = tenantMap.get(tenantId);
+    public boolean isMergeAllowedForMaster(String moduleName, String masterName) {
+        boolean isMergeAllowed = false;
 
-			if (!tenantModule.containsKey(moduleName)) {
-				tenantModule.put(moduleName, masterDataMap);
-			} else {
-				Map<String, JSONArray> moduleMaster = tenantModule.get(moduleName);
-				moduleMaster.putAll(masterDataMap);
-				// moduleMaster.put(masterName, masterDataJsonArray);
-				tenantModule.put(moduleName, moduleMaster);
-			}
+        if (masterConfigMap.containsKey(moduleName) && masterConfigMap.get(moduleName).containsKey(masterName)) {
+            Object masterData = masterConfigMap.get(moduleName).get(masterName);
+            if (masterData != null) {
+                try {
+                    isMergeAllowed = JsonPath.read(objectMapper.writeValueAsString(masterData),
+                            MDMSConstants.MERGE_FILES);
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return isMergeAllowed;
+    }
 
-			tenantMap.put(tenantId, tenantModule);
-		}
 
-	}
+    public static Map<String, Map<String, Map<String, JSONArray>>> getTenantMap() {
+        return tenantMap;
+    }
 
-	public void readMdmsConfigFiles(String masterConfigUrl) {
-		ObjectMapper jsonReader = new ObjectMapper();
-
-		log.info("GitHub Url TO Fetch MasterConfigs: " + masterConfigUrl);
-		Map file = null;
-		URL yamlFile = null;
-		try {
-			yamlFile = new URL(masterConfigUrl);
-		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
-			log.error("Exception while fetching service map for: " + e1.getMessage());
-		}
-		try {
-			file = jsonReader.readValue(new InputStreamReader(yamlFile.openStream()), Map.class);
-		} catch (Exception e) {
-			log.error("Exception while fetching service map for: " + e.getMessage());
-		}
-
-		masterConfigMap = file;
-		log.info("the Master config Map : " + masterConfigMap);
-
-	}
-
-	public static Map<String, Map<String, Map<String, JSONArray>>> getTenantMap() {
-		return tenantMap;
-	}
-	
-	public static Map<String, Map<String, Object>> getMasterConfigMap() {
-		return masterConfigMap;
-	}
+    public static Map<String, Map<String, Object>> getMasterConfigMap() {
+        return masterConfigMap;
+    }
 
 }
