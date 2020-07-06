d23705916c5035fcd22c4e273d840ca9555a1724
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2020-07-06 13:53:16 +0530

    constructer added to msgReq (#203)
    * constructer added to msgReq
    
    * snapshot url added
    
    * jpa query issue fix

--- core-services/egov-localization/src/main/java/org/egov/domain/model/MessageRequest.java
+++ core-services/egov-localization/src/main/java/org/egov/domain/model/MessageRequest.java
@@ -2,13 +2,17 @@ package org.egov.domain.model;
 
 import org.egov.common.contract.request.RequestInfo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MessageRequest {
 
 	private RequestInfo RequestInfo;
