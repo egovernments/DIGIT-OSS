37bf7061cb92099fe1ca9caa6d372cd05c1bb356
Shreya <34509189+shreyab30-eGov@users.noreply.github.com>
2020-07-06 12:33:24 +0530

    RAIN-1229 : Renewal_Period Configuration (#1595)

--- frontend/old-ui/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/search-preview.js
+++ frontend/old-ui/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/search-preview.js
@@ -191,6 +191,10 @@ const beforeInitFn = async (action, state, dispatch, applicationNumber) => {
           {
             moduleName: "common-masters",
             masterDetails: [{ name: "uiCommonPay",filter: `[?(@.code=="${businessService}")]` }]
+          },
+          {
+            moduleName: "TradeLicense",
+            masterDetails: [{ name: "TradeRenewal"}]
           }
         ]
       }
@@ -204,7 +208,7 @@ const beforeInitFn = async (action, state, dispatch, applicationNumber) => {
         [],
         mdmsBody
       );
-      console.log("payload...",payload)
+      dispatch(prepareFinalObject("renewalPeriod", get(payload.MdmsRes , "TradeLicense.TradeRenewal[0].renewalPeriod")));
       dispatch(prepareFinalObject("uiCommonConfig", get(payload.MdmsRes , "common-masters.uiCommonPay[0]")));
     } catch (e) {
       console.log(e);
@@ -346,21 +350,6 @@ const beforeInitFn = async (action, state, dispatch, applicationNumber) => {
       "components.div.children.headerDiv.children.header1.children.headertop",
       headerrow
     );
-
- 
-    // const footer = footerReview(
-    //   action,
-    //   state,
-    //   dispatch,
-    //   status,
-    //   applicationNumber,
-    //   tenantId,
-    //   financialYear
-    // );
-
-    // process.env.REACT_APP_NAME === "Citizen"
-    //   ? set(action, "screenConfig.components.div.children.footer", footer)
-    //   : set(action, "screenConfig.components.div.children.footer", {});
 
     if (status === "cancelled")
       set(
