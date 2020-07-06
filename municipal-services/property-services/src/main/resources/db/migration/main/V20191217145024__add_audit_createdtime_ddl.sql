4f5294f67170f354830b6e6f69ad37dfaf79bbb5
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2020-07-06 11:01:15 +0530

    Property boundary conflict resolved (#323)
    * migration class removed
    
    * Revert "migration class removed"
    
    This reverts commit 7374e862b41509956799c877f30ef0d1370ceccb.
    
    * boundary conflict removed
    
    * unwanted ddls removed

--- municipal-services/property-services/src/main/resources/db/migration/main/V20191217145024__add_audit_createdtime_ddl.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE eg_pt_assessments_audit ADD COLUMN auditcreatedtime BIGINT;
-ALTER TABLE eg_pt_unit_audit ADD COLUMN auditcreatedtime BIGINT;
--- municipal-services/property-services/src/main/resources/db/migration/main/V20200107100737__add_secondary_indexes_ddl.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX indx_status ON eg_pt_assessments ("status");
-CREATE INDEX indx_assessmentdate ON eg_pt_assessments ("assessmentdate");
-CREATE INDEX indx_propertyid ON eg_pt_assessments ("propertyid");
-CREATE INDEX indx_assessmentnumber ON eg_pt_assessments ("assessmentnumber");
-
-CREATE INDEX indx_assessmentid ON eg_pt_unit ("assessmentid");
