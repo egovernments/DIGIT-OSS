d27d7a3e91fd8161b4284de8c49732db90b1290e
priyankarani-eGov <priyanka.rani@egovernments.org>
2020-07-09 20:34:01 +0530

    [UKI-326]:UDK DEV : SMS Concerns

--- municipal-services/tl-services/src/main/java/org/egov/tl/util/NotificationUtil.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/util/NotificationUtil.java
@@ -102,6 +102,11 @@ public class NotificationUtil {
 			messageTemplate = getMessageTemplate(TLConstants.NOTIFICATION_CANCELLED, localizationMessage);
 			message = getCancelledMsg(license, messageTemplate);
 			break;
+			
+		case ACTION_STATUS_SENDBACK:
+			messageTemplate = getMessageTemplate(TLConstants.NOTIFICATION_SENDBACK_TO_INSPECTION, localizationMessage);
+			message = getSendBackToInspcetionMsg(license, messageTemplate);
+			break;
 		}
 
 		return message;
@@ -519,4 +524,10 @@ public class NotificationUtil {
 		producer.push(config.getSaveUserEventsTopic(), request);
 	}
 
+	private String getSendBackToInspcetionMsg(TradeLicense license, String message) {
+		message = message.replace("<2>", license.getTradeName());
+		message = message.replace("<3>", license.getApplicationNumber());
+
+		return message;
+	}
 }
--- municipal-services/tl-services/src/main/java/org/egov/tl/util/TLConstants.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/util/TLConstants.java
@@ -74,6 +74,8 @@ public class TLConstants {
     public static final String NOTIFICATION_FORWARD_CITIZEN = "tl.en.forward.citizen";
 
     public static final String NOTIFICATION_TL_REMINDER = "tl.en.reminder";
+    
+    public static final String NOTIFICATION_SENDBACK_TO_INSPECTION= "tl.en.sendback.to.inspection";
 
 
     public static final String DEFAULT_OBJECT_MODIFIED_MSG = "Dear <1>,Your Trade License with application number <APPLICATION_NUMBER> was modified.";
@@ -208,6 +210,8 @@ public class TLConstants {
     public static final String ACTION_SENDBACKTOCITIZEN_FIELDINSPECTION  = "SENDBACKTOCITIZEN_CITIZENACTIONREQUIRED";
 
     public static final String ACTION_FORWARD_CITIZENACTIONREQUIRED  = "FORWARDTOEMPLOYEE_FIELDINSPECTION";
+    
+    public static final String ACTION_STATUS_SENDBACK  = "SENDBACK_APPLIED";
 
 
     public static final String ACTION_STATUS_PAID  = "PAID";
