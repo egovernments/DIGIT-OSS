203015eca577984568320dc8d5cef9b8ccf18184
muralimgwl <murali.m@goodworklabs.com>
2020-07-29 03:27:44 +0530

    framework enhancement for putting nested json

--- frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-redux/screen-configuration/middlewares/handleScreenConfigurationFieldChange.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-redux/screen-configuration/middlewares/handleScreenConfigurationFieldChange.js
@@ -1,5 +1,7 @@
 import * as screenActionTypes from "../actionTypes";
+import { addComponentJsonpath } from "../../../ui-utils/commons";
 import get from "lodash/get";
+import set from "lodash/set";
 import {validate} from "../utils";
 
 const handleScreenConfigurationFieldChange = (store) => (next) => (action) => {
@@ -11,7 +13,13 @@ const handleScreenConfigurationFieldChange = (store) => (next) => (action) => {
       const componentObject=get(state,`screenConfiguration.screenConfig.${screenKey}.${componentJsonpath}`);
       validate(screenKey,{...componentObject,value},dispatch);
       next(action);
-  } else {
+  }
+  else if (type === screenActionTypes.HANDLE_SCREEN_CONFIGURATION_FIELD_CHANGE && action.value && action.value.children) {
+    set(action,"value.componentJsonpath",`${action.componentJsonpath}.${action.property}`);
+    addComponentJsonpath(action.value.children,`${action.componentJsonpath}.${action.property}.children`)
+    next(action);
+  }
+  else {
     next(action);
   }
 };
