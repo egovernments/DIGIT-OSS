ab686dae3b1278205b2d28e34fbcdc1951f4a46d
khemanth-eGov <k.hemanth@egovernments.org>
2020-07-30 17:24:22 +0530

    Dupplicate License No in Search View Issue

--- frontend/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-atoms-local/ApplicationNo/index.js
+++ frontend/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-atoms-local/ApplicationNo/index.js
@@ -7,13 +7,13 @@ import "./index.css";
 function ApplicationNoContainer(props) {
 
   const { number } = props;
-  const isEditRenewal = getQueryArg(window.location.href,"action") === "EDITRENEWAL"||getQueryArg(window.location.href,"action") === "DIRECTRENEWAL"
-  const isSubmitRenewal = getQueryArg(window.location.href,"purpose") === "EDITRENEWAL"||getQueryArg(window.location.href,"purpose") === "DIRECTRENEWAL"
-  if(isEditRenewal ||isSubmitRenewal){
-    const licenseNumber=getQueryArg(window.location.href, "licenseNumber") || "";
-  return <div className="application-no-container"><LabelContainer labelName="License No." labelKey ={"TL_LICENSE_NO_CODE"} dynamicArray={[licenseNumber]}/></div>;
-  }
-else
+//   const isEditRenewal = getQueryArg(window.location.href,"action") === "EDITRENEWAL"||getQueryArg(window.location.href,"action") === "DIRECTRENEWAL"
+//   const isSubmitRenewal = getQueryArg(window.location.href,"purpose") === "EDITRENEWAL"||getQueryArg(window.location.href,"purpose") === "DIRECTRENEWAL"
+//   if(isEditRenewal ||isSubmitRenewal){
+//     const licenseNumber=getQueryArg(window.location.href, "licenseNumber") || "";
+//   return <div className="application-no-container"><LabelContainer labelName="License No." labelKey ={"TL_LICENSE_NO_CODE"} dynamicArray={[licenseNumber]}/></div>;
+//   }
+// else
   return <div className="application-no-container"><LabelContainer labelName="Application No." labelKey ={"TL_APPLICATION_NO_CODE"} dynamicArray={[number]}/></div>;
 }
 export default ApplicationNoContainer;
