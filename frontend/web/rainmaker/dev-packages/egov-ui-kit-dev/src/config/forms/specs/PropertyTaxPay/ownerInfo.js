2bb7e730cba5d0eac94f6aab226131bac173ee61
Ashok <ashok.kumar@egovernments.org>
2020-09-10 14:38:01 +0530

    Name fields changes

--- frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/config/forms/specs/PropertyTaxPay/ownerInfo.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/config/forms/specs/PropertyTaxPay/ownerInfo.js
@@ -20,7 +20,7 @@ const formConfig = {
       hintText: "PT_FORM3_OWNER_NAME_PLACEHOLDER",
       required: true,
       errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
-      pattern: /^[a-zA-Z]+$/,
+      pattern: /^[^{0-9}^\$\"'<>?\\\\~`!@#$%^()+={}\[\]*,_:;“”‘’]{1,64}$/i,
       errorMessage: "PT_NAME_ERROR_MESSAGE",
     },
     ownerMobile: {
@@ -40,7 +40,7 @@ const formConfig = {
       type: "textfield",
       floatingLabelText: "PT_SEARCHPROPERTY_TABEL_GUARDIANNAME",
       hintText: "PT_FORM3_GUARDIAN_PLACEHOLDER",
-      pattern: /^[a-zA-Z]+$/,
+      pattern: /^[^{0-9}^\$\"'<>?\\\\~`!@#$%^()+={}\[\]*,_:;“”‘’]{1,64}$/i,
       required: true,
       errorMessage: "PT_NAME_ERROR_MESSAGE",
       errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
