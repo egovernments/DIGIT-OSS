2f6023885748349d4a06bed5ad0ec7720faa116b
yogendra-egov <56108459+yogendra-egov@users.noreply.github.com>
2020-09-11 14:24:07 +0530

    RAIN-1499 : PT :: Wrong DOB for owner shows on view property page (#1902)

--- frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/screens/specs/utils/index.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/screens/specs/utils/index.js
@@ -432,13 +432,18 @@ export const getLabelWithValue = (label, value, props = {}) => {
 };
 
 export const convertEpochToDate = dateEpoch => {
-  const dateFromApi = new Date(dateEpoch);
-  let month = dateFromApi.getMonth() + 1;
-  let day = dateFromApi.getDate();
-  let year = dateFromApi.getFullYear();
-  month = (month > 9 ? "" : "0") + month;
-  day = (day > 9 ? "" : "0") + day;
-  return `${day}/${month}/${year}`;
+  // Returning null in else case because new Date(null) returns initial date from calender
+  if(dateEpoch){
+    const dateFromApi = new Date(dateEpoch);
+    let month = dateFromApi.getMonth() + 1;
+    let day = dateFromApi.getDate();
+    let year = dateFromApi.getFullYear();
+    month = (month > 9 ? "" : "0") + month;
+    day = (day > 9 ? "" : "0") + day;
+    return `${day}/${month}/${year}`;
+  } else {
+    return null;
+  }
 };
 
 export const convertDateToEpoch = (dateString, dayStartOrEnd = "dayend") => {
