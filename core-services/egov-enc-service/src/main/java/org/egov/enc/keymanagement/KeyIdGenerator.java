2fd8df9b8900917f815a9745926862f7463bcf7c
rushang7-eGov <rushang.dhanesha@egovernments.org>
2020-09-28 10:55:51 +0530

    Enc: Interface out master key provider (Support for AWS KMS) (#133)
    * Support for external MasterPasswordProvider like AWS KMS.
    
    * Support for AWS KMS as master password provider.
    
    * Refactor: MasterPasswordProvider to MasterKeyProvider
    
    * Move Master Password config to SoftwareBasedMasterKey as these are not mandatory when using any other KeyProvider.
    
    * Version Upgrade: Backward Compatible with master

--- core-services/egov-enc-service/src/main/java/org/egov/enc/keymanagement/KeyIdGenerator.java
+++ core-services/egov-enc-service/src/main/java/org/egov/enc/keymanagement/KeyIdGenerator.java
@@ -13,7 +13,7 @@ import java.security.Security;
 import java.util.ArrayList;
 
 @Component
-@Order(2)
+@Order(3)
 public class KeyIdGenerator implements ApplicationRunner {
 
     private SecureRandom secureRandom;
