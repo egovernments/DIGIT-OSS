d6d08e417b4078da5eccd1175ccb0f53b6a780f5
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2020-10-02 07:55:57 +0530

    RAIN-1607: Merging DIGiT 2.1 release changes into master from develop f… (#468)
    * RAIN-1607: Merging JUN-SEP release changes into master from develop for TL Calculator service
    
    * RAIN-1607: Update CHANGELOG of tl-calculator
    
    * RAIN-1607: Upgrade version from 1.1.0 to 1.1.1 for tl-calculator

--- municipal-services/tl-calculator/CHANGELOG.md
+++ municipal-services/tl-calculator/CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this module will be documented in this file.
 
+## 1.1.1 - 2020-10-1
+
+- Added Index to achieve performance benefits.
+
 ## 1.1.0 - 2020-06-22
 - Added typescript definition generation plugin
 - Upgraded to `tracer:2.0.0-SNAPSHOT`
---
+++ municipal-services/tl-calculator/src/main/resources/db/migration/main/V20200925205939__tl_billingslab_index.sql
@@ -0,0 +1,11 @@
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_tenantid ON eg_tl_billingslab (tenantid);
+
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_accessorycategory ON eg_tl_billingslab (accessorycategory);
+
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_tradetype ON eg_tl_billingslab (tradetype);
+
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_fromUom ON eg_tl_billingslab (fromUom);
+
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_toUom ON eg_tl_billingslab (toUom);
+
+CREATE INDEX IF NOT EXISTS index_eg_tl_billingslab_uom ON eg_tl_billingslab (uom);
