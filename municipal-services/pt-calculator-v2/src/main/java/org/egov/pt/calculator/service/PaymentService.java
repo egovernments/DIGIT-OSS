9b053a930f1c848f087c342da473d7172559690b
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2020-10-02 19:48:42 +0530

    circular dependency fix (#472)

--- municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/PaymentService.java
+++ municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/PaymentService.java
@@ -1,9 +1,20 @@
 package org.egov.pt.calculator.service;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.egov.pt.calculator.util.CalculatorConstants.ALLOWED_RECEIPT_STATUS;
+import static org.egov.pt.calculator.util.CalculatorConstants.CONSUMER_CODE_SEARCH_FIELD_NAME_PAYMENT;
+import static org.egov.pt.calculator.util.CalculatorConstants.SEPARATER;
+import static org.egov.pt.calculator.util.CalculatorConstants.STATUS_FIELD_FOR_SEARCH_URL;
+import static org.egov.pt.calculator.util.CalculatorConstants.TENANT_ID_FIELD_FOR_SEARCH_URL;
+import static org.egov.pt.calculator.util.CalculatorConstants.URL_PARAMS_SEPARATER;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.egov.pt.calculator.repository.Repository;
-import org.egov.pt.calculator.util.CalculatorUtils;
+import org.egov.pt.calculator.util.CalculatorConstants;
+import org.egov.pt.calculator.util.Configurations;
 import org.egov.pt.calculator.web.models.collections.Payment;
 import org.egov.pt.calculator.web.models.collections.PaymentResponse;
 import org.egov.pt.calculator.web.models.collections.PaymentSearchCriteria;
@@ -11,36 +22,23 @@ import org.egov.pt.calculator.web.models.demand.Demand;
 import org.egov.pt.calculator.web.models.property.Property;
 import org.egov.pt.calculator.web.models.property.RequestInfoWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentService {
 
 
+	@Autowired
     private Repository repository;
 
-    private CalculatorUtils utils;
-
+	@Autowired
     private ObjectMapper mapper;
 
-    private JdbcTemplate jdbcTemplate;
-
-
     @Autowired
-    public PaymentService(Repository repository, CalculatorUtils utils, ObjectMapper mapper, JdbcTemplate jdbcTemplate) {
-        this.repository = repository;
-        this.utils = utils;
-        this.mapper = mapper;
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
+    private Configurations configurations;
 
 
     /**
@@ -86,11 +84,27 @@ public class PaymentService {
      * @return
      */
     public List<Payment> getPayments(PaymentSearchCriteria criteria, RequestInfoWrapper requestInfoWrapper) {
-        StringBuilder url = utils.getPaymentSearchUrl(criteria);
+        StringBuilder url = getPaymentSearchUrl(criteria);
         return mapper.convertValue(repository.fetchResult(url, requestInfoWrapper), PaymentResponse.class).getPayments();
     }
+    
+    /**
+     * Returns the Receipt search Url with tenantId, cosumerCode,service name and tax period
+     * parameters
+     *
+     * @param criteria
+     * @return
+     */
+    public StringBuilder getPaymentSearchUrl(PaymentSearchCriteria criteria) {
 
 
-
+        return new StringBuilder().append(configurations.getCollectionServiceHost())
+                .append(configurations.getPaymentSearchEndpoint()).append(URL_PARAMS_SEPARATER)
+                .append(TENANT_ID_FIELD_FOR_SEARCH_URL).append(criteria.getTenantId())
+                .append(SEPARATER).append(CONSUMER_CODE_SEARCH_FIELD_NAME_PAYMENT)
+                .append(criteria.getConsumerCodes())
+                .append(CalculatorConstants.SEPARATER).append(STATUS_FIELD_FOR_SEARCH_URL)
+                .append(ALLOWED_RECEIPT_STATUS);
+    }
 
 }
