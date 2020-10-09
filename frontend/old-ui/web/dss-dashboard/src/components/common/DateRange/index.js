19acbd46c60ad1e2ab7c0d3abac26eaa05285ba2
Jagankumar-Egov <jagan.kumar@egovernments.org>
2020-10-09 21:47:11 +0530

    RAIN-1652 : DSS fix for the Timefilter Mobile view

--- frontend/old-ui/web/dss-dashboard/src/components/common/DateRange/index.js
+++ frontend/old-ui/web/dss-dashboard/src/components/common/DateRange/index.js
@@ -234,7 +234,7 @@ class DateRange extends React.Component {
         <DialogTitle style={{ fontFamily: 'Roboto', fontSize: '10px', color: '#fe7a51' }}>
           {this.state.title}
         </DialogTitle>
-        <DialogContent dividers>
+        <DialogContent dividers className={"dss-date-time-filter"}>
           <DateRangePicker
             onChange={this.onChange}
             showSelectionPreview={true}
