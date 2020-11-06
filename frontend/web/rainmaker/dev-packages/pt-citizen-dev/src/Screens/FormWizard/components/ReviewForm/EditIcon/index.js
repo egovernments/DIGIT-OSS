38eff5f7a5fcfcc8eb5e9283e5934a99a05bd0ec
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2020-11-06 16:09:06 +0530

    missing files

---
+++ frontend/web/rainmaker/dev-packages/pt-citizen-dev/src/Screens/FormWizard/components/ReviewForm/EditIcon/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Icon } from "components";
+import Label from "egov-ui-kit/utils/translationNode";
+
+const defaultIconStyle = {
+  fill: "#767676",
+  width: 19,
+  height: 20,
+  marginRight: 8,
+  fill: "#fe7a51",
+};
+
+const EditIcon = ({ onIconClick }) => {
+  return (
+    <div className="rainmaker-displayInline" onClick={onIconClick} style={{ cursor: "pointer", marginRight: 5 }}>
+      <Icon style={defaultIconStyle} action="image" name="edit" />
+      <Label label="PT_EDIT_TEXT" color="#fe7a51" fontSize="16px" />
+    </div>
+  );
+};
+
+export default EditIcon;
