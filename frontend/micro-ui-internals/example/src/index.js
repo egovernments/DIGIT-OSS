4e57f2dcd450cda95cb15202ed3dfcd0fee7056a
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-11-09 23:43:34 +0530

    added libraries to start and build

--- frontend/micro-ui-internals/example/src/index.js
+++ frontend/micro-ui-internals/example/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import initLibraries from "@egovernments/digit-ui-libraries";
 import PGRApp from "@egovernments/digit-ui-module-pgr";
+
+initLibraries();
 
 ReactDOM.render(
   <>
