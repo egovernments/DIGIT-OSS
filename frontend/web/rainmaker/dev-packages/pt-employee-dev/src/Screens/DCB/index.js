9a9084e7c8d29cbcea061facab1a99820248ee94
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2020-11-12 16:11:17 +0530

    Rebate issue code changes

--- frontend/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/DCB/index.js
+++ frontend/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/DCB/index.js
@@ -1331,12 +1331,18 @@ class FormWizardDataEntry extends Component {
                       previousKey = key;
                     }
 
-                    if (data.demand[data1][data2].PT_TAXHEAD === "PT_TIME_REBATE"           || data.demand[data1][data2].PT_TAXHEAD === "PT_PROMOTIONAL_REBATE" && data.demand[data1][data2].PT_DEMAND ==='0') 
+                     if (data.demand[data1][data2].PT_TAXHEAD === "PT_TIME_REBATE" &&  data.demand[data1][data2].PT_DEMAND ==='0') 
                     {  
                     hasPropertyTax = true;  
                       data.demand[data1][data2].PT_DEMAND = -0;                   
                      
-                    }
+                    } 
+                    if (data.demand[data1][data2].PT_TAXHEAD === "PT_PROMOTIONAL_REBATE" && data.demand[data1][data2].PT_DEMAND ==='0') 
+                    {  
+                      hasPropertyTax = true;  
+                        data.demand[data1][data2].PT_DEMAND = -0;                   
+                     } 
+                    
                     // }
                   }
                 });                
