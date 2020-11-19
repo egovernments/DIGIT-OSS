e20fdf3553937a332d438784a705e6de48a86011
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-11-19 12:29:11 +0530

    auth

--- frontend/micro-ui-internals/example/src/index.js
+++ frontend/micro-ui-internals/example/src/index.js
@@ -7,7 +7,7 @@ import { Body, TopBar } from "@egovernments/digit-ui-react-components";
 
 initLibraries();
 
-const citAuth = "c54c09cd-56c5-4193-a59d-76c3867500c8";
+const citAuth = "da40258e-9598-44fe-8262-4d08a00c8bd1";
 
 Digit.SessionStorage.set("citizen.token", citAuth);
 
