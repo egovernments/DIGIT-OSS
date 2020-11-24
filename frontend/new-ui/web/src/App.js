62571a642fea9576f24bb8463b2e4e24eee0e9a7
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-11-25 01:16:34 +0530

    routing added, homepage with links

--- frontend/new-ui/web/src/App.js
+++ frontend/new-ui/web/src/App.js
@@ -1,14 +1,31 @@
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import PGRApp from "@egovernments/digit-ui-module-pgr";
 import { Body, TopBar } from "@egovernments/digit-ui-react-components";
 
+const GetLink = ({ to, children }) => (
+  <Link to={to} style={{ marginLeft: 16 }}>
+    {children}
+  </Link>
+);
+
 function App() {
   return (
-    <>
+    <Router>
       <Body>
         <TopBar />
-        <PGRApp stateCode="pb" cityCode="pb.amritsar" moduleCode="PGR" />
+        <Switch>
+          <Route path="/digit-ui/pgr">
+            <PGRApp stateCode="pb" cityCode="pb.amritsar" moduleCode="PGR" />
+          </Route>
+          <Route>
+            <Header>Home page</Header>
+            <GetLink to="/digit-ui/pgr/citizen/create-complaint">Create Complaint</GetLink>
+            <br />
+            <GetLink to="/digit-ui/pgr/citizen/complaints">My Complaint</GetLink>
+          </Route>
+        </Switch>
       </Body>
-    </>
+    </Router>
   );
 }
 
