0653c80a0ecbe22d1306b91dc28c57d6dd89fdd8
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-11-25 11:33:54 +0530

    topbar img

--- frontend/micro-ui-internals/packages/react-components/src/TopBar.js
+++ frontend/micro-ui-internals/packages/react-components/src/TopBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const TopBar = (props) => {
   return (
     <div className="navbar">
-      <img src={props.img} alt="mSeva Logo" />
+      <img src={props.img || "https://unpkg.com/browse/@egovernments/digit-ui-css/img/browser-icon.png"} alt="mSeva Logo" />
     </div>
   );
 };
