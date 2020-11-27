08e3fdf787918d1707deed3f0774a6ee1c8e5a5d
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-11-27 07:39:04 +0530

    upgrade to v1.0.14

--- frontend/new-ui/web/src/App.js
+++ frontend/new-ui/web/src/App.js
@@ -1,12 +1,6 @@
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
-import PGRApp from "@egovernments/digit-ui-module-pgr";
-import { Body, Header, TopBar } from "@egovernments/digit-ui-react-components";
-
-const GetLink = ({ to, children }) => (
-  <Link to={to} style={{ marginLeft: 16 }}>
-    {children}
-  </Link>
-);
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { PGRModule, PGRLinks } from "@egovernments/digit-ui-module-pgr";
+import { Body, TopBar } from "@egovernments/digit-ui-react-components";
 
 function App() {
   return (
@@ -15,13 +9,10 @@ function App() {
         <TopBar />
         <Switch>
           <Route path="/digit-ui/pgr">
-            <PGRApp stateCode="pb" cityCode="pb.amritsar" moduleCode="PGR" />
+            <PGRModule stateCode="pb" cityCode="pb.amritsar" moduleCode="PGR" />
           </Route>
           <Route>
-            <Header>Home page</Header>
-            <GetLink to="/digit-ui/pgr/citizen/create-complaint">Create Complaint</GetLink>
-            <br />
-            <GetLink to="/digit-ui/pgr/citizen/complaints">My Complaint</GetLink>
+            <PGRLinks />
           </Route>
         </Switch>
       </Body>
