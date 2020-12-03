f2f0a09215114a7a82d2181c0412cc96cdaccf2c
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2020-12-03 11:20:20 +0530

    Hiding Add demand button for assessment for

--- frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/PTAcknowledgement/index.js
+++ frontend/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/PTAcknowledgement/index.js
@@ -537,7 +537,7 @@ class PTAcknowledgement extends React.Component {
                           style={{ lineHeight: "auto", minWidth: "inherit", width: "200px" }}
                         />
                       )}
-                     {Button3 && Button3.visibility && (
+                     {properties && properties.source != "MUNICIPAL_RECORDS" && Button3 && Button3.visibility && (
                         <Button
                           onClick={Button3.buttonClick}
                           label={<Label buttonLabel={true} label={Button3.name} fontSize="16px" />}
