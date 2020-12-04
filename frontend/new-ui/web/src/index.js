57f5da982c2d1c50c3ab8bfad8715cd0fc492152
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-12-04 17:18:24 +0530

    import fixes

--- frontend/new-ui/web/src/index.js
+++ frontend/new-ui/web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import initLibraries from "@egovernments/digit-ui-libraries";
+import { initLibraries } from "@egovernments/digit-ui-libraries";
 
 import "./index.css";
 import App from './App';
