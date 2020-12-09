b3116ecba60852bbee58da0f0767e932d5609cae
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-12-09 15:30:45 +0530

    v1.0.17

--- frontend/new-ui/web/src/index.js
+++ frontend/new-ui/web/src/index.js
@@ -7,18 +7,28 @@ import App from './App';
 
 initLibraries();
 
-// const token = window.localStorage.getItem("token");
-const citizenToken = window.localStorage.getItem("Citizen.token");
-const citizenInfo = window.localStorage.getItem("Citizen.user-info");
-// const employeeToken = window.localStorage.getItem("Employee.token");
+const token = window.localStorage.getItem("token")
 
-// const isLoggedIn = token || citizenToken || employeeToken;
-// const isCitizenLogin = token === citizenToken;
+const citizenToken = window.localStorage.getItem("Citizen.token")
+const citizenInfo = window.localStorage.getItem("Citizen.user-info")
+const citizenTenantId = window.localStorage.getItem("Citizen.tenant-id")
 
+const employeeToken = window.localStorage.getItem("Employee.token")
+const employeeInfo = window.localStorage.getItem("Employee.user-info")
+const employeeTenantId = window.localStorage.getItem("Employee.tenant-id")
 
-window.Digit.SessionStorage.set("citizen.token", citizenToken);
-window.Digit.SessionStorage.set("citizen.userServiceData", { userInfo: citizenInfo });
-window.Digit.SessionStorage.set("User", { token: citizenToken, mobileNumber: citizenInfo.mobileNumber });
+const userType = token === citizenToken ? "citizen" : "employee";
+Digit.SessionStorage.set("user_type", userType);
+Digit.SessionStorage.set("userType", userType);
+
+const getUserDetails = (token, info) => ({ token, info })
+
+const userDetails = userType === "citizen" ? getUserDetails(citizenToken, citizenInfo) : getUserDetails(employeeToken, employeeInfo)
+
+Digit.SessionStorage.set("User", userDetails);
+
+Digit.SessionStorage.set("Citizen.tenantId", citizenTenantId);
+Digit.SessionStorage.set("Employee.tenantId", employeeTenantId);
 
 ReactDOM.render(
   <React.StrictMode>
