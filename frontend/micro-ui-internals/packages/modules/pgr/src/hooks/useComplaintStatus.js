44e44dad1dfcbc5b9d8f40fe944190ec34b2b707
nikhil24 <nikhil15.1990@gmail.com>
2020-12-24 12:43:51 +0530

    ulb service added

--- frontend/micro-ui-internals/packages/modules/pgr/src/hooks/useComplaintStatus.js
+++ frontend/micro-ui-internals/packages/modules/pgr/src/hooks/useComplaintStatus.js
@@ -4,8 +4,7 @@ import { useTranslation } from "react-i18next";
 const useComplaintStatus = () => {
   const { t } = useTranslation();
   const [complaintStatus, setComplaintStatus] = useState([]);
-  const user = Digit.UserService.getUser();
-  const tenantId = user?.info?.tenantId;
+  const tenantId = Digit.ULBService.getCurrentTenantId();
 
   useEffect(() => {
     let WorkflowService = null;
