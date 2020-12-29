ecf7f8c0c89fd3eef3f0f4c3b7d3943f83587ef2
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-12-29 09:41:42 +0530

    v1.0.39

--- frontend/new-ui/web/src/App.js
+++ frontend/new-ui/web/src/App.js
@@ -1,8 +1,23 @@
+import { PGRModule, PGRLinks, PGRReducers } from "@egovernments/digit-ui-module-pgr";
+import { FSMModule, FSMLinks } from "@egovernments/digit-ui-module-fsm";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
+
+import Registry from "./ComponentRegistry";
+
+const enabledModules = ["PGR", "FSM"];
+const registry = new Registry({
+  PGRLinks,
+  PGRModule,
+  FSMModule,
+  FSMLinks,
+});
+const moduleReducers = (initData) => ({
+  pgr: PGRReducers(initData),
+});
 
 function App() {
   return (
-    <DigitUI stateCode="pb" />
+    <DigitUI stateCode="pb" registry={registry} enabledModules={enabledModules} moduleReducers={moduleReducers} />
   );
 }
 
