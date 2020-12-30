60d0dcb4e504ddecb1911e3c203eec471bfb9ab8
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2020-12-30 16:59:12 +0530

    tenantId fix

--- frontend/micro-ui-internals/packages/libraries/src/services/molecules/Ulb/index.js
+++ frontend/micro-ui-internals/packages/libraries/src/services/molecules/Ulb/index.js
@@ -6,7 +6,7 @@ export const ULBService = {
     // TODO: change when setter is done.
     const user = UserService.getUser();
     //TODO: fix tenant id from userinfo
-    const tenantId = user?.info.type === "CITIZEN" ? "pb.amritsar" : user?.info?.tenantId;
+    const tenantId = user?.info?.type === "EMPLOYEE" && user?.info?.tenantId ? user?.info?.tenantId : "pb.amritsar";
     return tenantId;
   },
   getCurrentUlb: () => {
