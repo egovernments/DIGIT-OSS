5d2bee28ef228b5dcc0a240a0ccbd89e270f7d88
nikhil24 <nikhil15.1990@gmail.com>
2020-12-31 12:15:07 +0530

    minor change

--- frontend/micro-ui-internals/packages/modules/pgr/src/components/inbox/ComplaintTable.js
+++ frontend/micro-ui-internals/packages/modules/pgr/src/components/inbox/ComplaintTable.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Loader, Table } from "@egovernments/digit-ui-react-components";
 
 const ComplaintTable = ({ columns, data, getCellProps }) => {
-  return <div>{data.length > 0 ? <Table data={data} columns={columns} getCellProps={getCellProps} /> : <Loader />}</div>;
+  return <React.Fragment>{data.length > 0 ? <Table data={data} columns={columns} getCellProps={getCellProps} /> : <Loader />}</React.Fragment>;
 };
 
 export default ComplaintTable;
