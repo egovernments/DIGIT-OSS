5a07a03f8b7c6b59d9c3dee01035697bf366c044
harit-egov <harit.saxena@egovernments.org>
2020-12-31 16:02:10 +0530

    added-babel-loader

---
+++ frontend/micro-ui/web/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var UnminifiedWebpackPlugin = require("unminified-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    // new UnminifiedWebpackPlugin(),
+    // new webpack.SourceMapDevToolPlugin({}),
+    new HtmlWebpackPlugin(),
+  ],
+};
