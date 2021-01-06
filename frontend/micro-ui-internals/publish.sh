163f8397aa0090c85aa7512f513f23aec3614ee3
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-01-06 17:31:30 +0530

    adding upgrade

--- frontend/micro-ui-internals/publish.sh
+++ frontend/micro-ui-internals/publish.sh
@@ -6,6 +6,7 @@ msg() {
   echo -e "\n\n\033[32;32m$1\033[0m"
 }
 
+msg "Pre-building all packages"
 yarn build
 sleep 5
 
@@ -20,6 +21,10 @@ cd "$BASEDIR/packages/libraries" && yarn publish --access public
 msg "Building and publishing react-components"
 cd "$BASEDIR/packages/react-components" && yarn publish --access public
 
+sleep 10
+msg "Updating dependencies"
+cd "$BASEDIR" && yarn upgrade -S @egovernments
+sleep 5
 
 msg "Building and publishing PGR module"
 cd "$BASEDIR/packages/modules/pgr" && yarn publish --access public
