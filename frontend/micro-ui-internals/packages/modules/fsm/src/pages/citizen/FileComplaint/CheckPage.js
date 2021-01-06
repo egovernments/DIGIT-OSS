2d75b4d1a4ea137108c8e8b021a86433dfb52e2a
ayan-egov <ayan.majumder@egovernments.org>
2021-01-06 11:01:08 +0530

    Replace sample data with form data in check page

--- frontend/micro-ui-internals/packages/modules/fsm/src/pages/citizen/FileComplaint/CheckPage.js
+++ frontend/micro-ui-internals/packages/modules/fsm/src/pages/citizen/FileComplaint/CheckPage.js
@@ -23,29 +23,31 @@ const ActionButton = ({ jumpTo }) => {
   return <LinkButton label="CHANGE" style={{ color: "#F47738" }} onClick={routeTo} />;
 };
 
-const CheckPage = ({ onSubmit }) => {
+const CheckPage = ({ onSubmit, value }) => {
   const history = useHistory();
+
+  const { city_complaint, propertyType, subtype, pitDetail } = value;
 
   return (
     <Card>
       <CardHeader>Check Your Answers</CardHeader>
       <CardSubHeader>Property Details</CardSubHeader>
       <StatusTable>
-        <Row label="Property Type" text="Residential" actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/property-type" />} />
+        <Row label="Property Type" text={propertyType} actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/property-type" />} />
         <Row
           label="Property Sub-Type"
-          text="Apartment"
+          text={subtype.name}
           actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/property-subtype" />}
         />
-        <Row
-          label="Address"
-          text="Back side Post Office Patiala Road Alakapuri Berhampur"
-          actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/pincode" />}
-        />
+        <Row label="Address" text={city_complaint.address} actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/pincode" />} />
       </StatusTable>
       <CardSubHeader>Pit/Septic Tank Details</CardSubHeader>
       <StatusTable>
-        <Row label="Size" text="1m x 1m x 1m" actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/tank-size" />} />
+        <Row
+          label="Size"
+          text={Object.values(pitDetail).join(" x ")}
+          actionButton={<ActionButton jumpTo="/digit-ui/citizen/fsm/new-application/tank-size" />}
+        />
       </StatusTable>
       <SubmitBar label="Submit" onSubmit={onSubmit} />
     </Card>
