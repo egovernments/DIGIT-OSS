110678c99d79a63c9c54bd4691cbc4b1285e5f23
saurabh-egov <saurabh.s@egovernments.org>
2021-01-27 22:37:41 +0530

    publish v1.0.82

--- frontend/new-ui/web/src/App.js
+++ frontend/new-ui/web/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import { PGRModule, PGRLinks, PGRReducers } from "@egovernments/digit-ui-module-pgr";
 import { FSMModule, FSMLinks } from "@egovernments/digit-ui-module-fsm";
+import { PaymentModule, PaymentLinks } from "@egovernments/digit-ui-module-common";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
 
 import Registry from "./ComponentRegistry";
 
-const enabledModules = ["PGR", "FSM"];
+const enabledModules = ["PGR", "FSM", "Payment"];
 const registry = new Registry({
   PGRLinks,
   PGRModule,
   FSMModule,
   FSMLinks,
+  PaymentModule,
+  PaymentLinks,
 });
 const moduleReducers = (initData) => ({
   pgr: PGRReducers(initData),
