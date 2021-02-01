90fd8006d531a1d5a75b66a92d07cd10f836d047
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2021-02-01 10:51:41 +0530

    UC Search issue fix

--- frontend/web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/universalCollectionResources/function.js
+++ frontend/web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/universalCollectionResources/function.js
@@ -141,8 +141,8 @@ export const searchApiCall = async (state, dispatch) => {
           handleField(
             "search",
             "components.div.children.searchResults",
-            "props.title",
-            "Search Results for Receipt (" + data.length + ")"
+            "props.rows",
+            response.length
           )
         );
 
