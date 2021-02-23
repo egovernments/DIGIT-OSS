cb3cdbc914927e22c526e31bc2abdb3e2b25c17f
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-02-23 11:11:42 +0530

    internals direct added

--- frontend/micro-ui/web/build.sh
+++ frontend/micro-ui/web/build.sh
@@ -2,4 +2,14 @@
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
+if [$BRANCH == "dev"] || [$BRANCH == "master"]; then
+
+  INTERNALS="digit-ui-internals"
+  rm -rf $INTERNALS
+  git clone -b development https://github.com/egovernments/digit-ui-internals.git $INTERNALS
+
+  rm yarn.lock
+
+fi
+
 echo $BRANCH
