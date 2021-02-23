e463fa43e8ca34af963ca6e25fe1276e27947c58
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-02-23 10:46:12 +0530

    build test

--- frontend/micro-ui/web/src/index.js
+++ frontend/micro-ui/web/src/index.js
@@ -9,47 +9,47 @@ initLibraries();
 
 window.Digit.Customizations = { PGR: {} };
 
-// login detection
+const user = window.Digit.SessionStorage.get("User");
 
-const parseValue = (value) => {
-  try {
-    return JSON.parse(value)
-  } catch (e) {
-    return value
+if (!user || !user.access_token || !user.info) {
+  // login detection
+
+  const parseValue = (value) => {
+    try {
+      return JSON.parse(value)
+    } catch (e) {
+      return value
+    }
   }
-}
 
-const getFromStorage = (key) => {
-  const value = window.localStorage.getItem(key);
-  return value && value !== "undefined" ? parseValue(value) : null;
-}
+  const getFromStorage = (key) => {
+    const value = window.localStorage.getItem(key);
+    return value && value !== "undefined" ? parseValue(value) : null;
+  }
 
-const token = getFromStorage("token")
+  const token = getFromStorage("token")
 
-const citizenToken = getFromStorage("Citizen.token")
-const citizenInfo = getFromStorage("Citizen.user-info")
-const citizenTenantId = getFromStorage("Citizen.tenant-id")
+  const citizenToken = getFromStorage("Citizen.token")
+  const citizenInfo = getFromStorage("Citizen.user-info")
+  const citizenTenantId = getFromStorage("Citizen.tenant-id")
 
-const employeeToken = getFromStorage("Employee.token")
-const employeeInfo = getFromStorage("Employee.user-info")
-const employeeTenantId = getFromStorage("Employee.tenant-id")
+  const employeeToken = getFromStorage("Employee.token")
+  const employeeInfo = getFromStorage("Employee.user-info")
+  const employeeTenantId = getFromStorage("Employee.tenant-id")
 
-const userType = token === citizenToken ? "citizen" : "employee";
-window.Digit.SessionStorage.set("user_type", userType);
-window.Digit.SessionStorage.set("userType", userType);
+  const userType = token === citizenToken ? "citizen" : "employee";
+  window.Digit.SessionStorage.set("user_type", userType);
+  window.Digit.SessionStorage.set("userType", userType);
 
-const getUserDetails = (access_token, info) => ({ token: access_token, access_token, info })
+  const getUserDetails = (access_token, info) => ({ token: access_token, access_token, info })
 
-const userDetails = userType === "citizen" ? getUserDetails(citizenToken, citizenInfo) : getUserDetails(employeeToken, employeeInfo)
+  const userDetails = userType === "citizen" ? getUserDetails(citizenToken, citizenInfo) : getUserDetails(employeeToken, employeeInfo)
 
-// if (userType !== "citizen") {
   window.Digit.SessionStorage.set("User", userDetails);
-// }
-
-window.Digit.SessionStorage.set("Citizen.tenantId", citizenTenantId);
-window.Digit.SessionStorage.set("Employee.tenantId", employeeTenantId);
-
-// end
+  window.Digit.SessionStorage.set("Citizen.tenantId", citizenTenantId);
+  window.Digit.SessionStorage.set("Employee.tenantId", employeeTenantId);
+  // end
+}
 
 ReactDOM.render(
   <React.StrictMode>
