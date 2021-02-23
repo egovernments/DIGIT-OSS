d192013858fd9d18585f1ea5708acaa6974913ce
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-02-23 19:28:17 +0530

    added last msg log

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -9,7 +9,7 @@ if [ "$BRANCH" != "dev" ] || [ "$BRANCH" != "master" ]; then
   INTERNALS="digit-ui-internals"
   rm -rf $INTERNALS
   git clone -b development https://github.com/egovernments/digit-ui-internals.git $INTERNALS
-  cd $INTERNALS && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
+  cd $INTERNALS && echo "$(git log -1 --pretty=%B)" && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
   cd ..
 
   rm -rf node_modules
