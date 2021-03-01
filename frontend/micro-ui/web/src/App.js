9cba3c6b00074799ba28a5dc2c7188b4bb8e8f3e
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-03-01 14:51:49 +0530

    fsm init added

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 import { PGRModule, PGRLinks, PGRReducers } from "@egovernments/digit-ui-module-pgr";
-import { FSMModule, FSMLinks } from "@egovernments/digit-ui-module-fsm";
+import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
 import { PTModule, PTLinks } from "@egovernments/digit-ui-module-pt";
 import { PaymentModule, PaymentLinks } from "@egovernments/digit-ui-module-common";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
@@ -13,13 +13,13 @@ const enabledModules = ["PGR", "FSM", "Payment", "PT"];
 window.Digit.ComponentRegistryService.setupRegistry({
   PGRLinks,
   PGRModule,
-  FSMModule,
-  FSMLinks,
   PTModule,
   PTLinks,
   PaymentModule,
   PaymentLinks,
 });
+
+initFSMComponents();
 
 const moduleReducers = (initData) => ({
   pgr: PGRReducers(initData),
