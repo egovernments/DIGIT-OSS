6bda49a53cfe1d582df8f6693a0932bd35284ff5
Abhinav Kushwaha <abhinav.kushwaha@egovernments.org>
2021-03-08 11:05:48 +0530

    v1.0.137

--- frontend/new-ui/web/src/App.js
+++ frontend/new-ui/web/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import { PGRModule, PGRLinks, PGRReducers } from "@egovernments/digit-ui-module-pgr";
 import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
+import { PTModule, PTLinks } from "@egovernments/digit-ui-module-pgr";
 import { PaymentModule, PaymentLinks } from "@egovernments/digit-ui-module-common";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
 
-const enabledModules = ["PGR", "FSM", "Payment"];
+const enabledModules = ["PGR", "FSM", "Payment", "PT"];
 window.Digit.ComponentRegistryService.setupRegistry({
   PGRLinks,
   PGRModule,
+  PTModule,
+  PTLinks,
   PaymentModule,
   PaymentLinks,
 });
