df02872e9e7942a8d456dabe7d8f64186e3c7fbc
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2021-06-04 14:36:34 +0530

    user login redirect to login page change

--- frontend/web/rainmaker/packages/citizen/src/modules/citizen/User/LanguageSelection/index.js
+++ frontend/web/rainmaker/packages/citizen/src/modules/citizen/User/LanguageSelection/index.js
@@ -29,7 +29,7 @@ class LanguageSelection extends Component {
   };
 
   onLanguageSelect = () => {
-    this.props.history.push("/user/register");
+    this.props.history.push("/user/login");
   };
 
   render() {
