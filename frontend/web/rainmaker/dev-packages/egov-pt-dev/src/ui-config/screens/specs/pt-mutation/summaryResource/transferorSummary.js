fdc33fb4ac304cd36fa9dd64a53c3c55b7d1a73f
saumya-egov <74046603+saumya-egov@users.noreply.github.com>
2021-06-11 14:57:27 +0530

    field added in summary for new mobile number

--- frontend/web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/summaryResource/transferorSummary.js
+++ frontend/web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/summaryResource/transferorSummary.js
@@ -29,7 +29,7 @@ export const transferorSummary = getCommonGrayCard({
           labelName: "Transferor Details",
           labelKey: "PT_MUTATION_TRANSFEROR_DETAILS"
         })
-      }
+      },
       // editSection: {
       //   componentPath: "Button",
       //   props: {
@@ -198,7 +198,40 @@ export const transferorSummary = getCommonGrayCard({
       afterPrefixJsonPath: "children.value.children.key"
     },
     type: "array"
-  }
+  },
+  transferorMobileCard: getCommonGrayCard({
+    header: {
+      uiFramework: "custom-atoms",
+      componentPath: "Container",
+      props: {
+        style: { marginBottom: "10px" }
+      },
+      children: {
+        header: {
+          gridDefination: {
+            xs: 8
+          },
+          ...getCommonSubHeader({
+            labelName: "Mobile Number To be Updated of transferor",
+            labelKey: "PT_MUTATION_TRANSFEROR_CORRECTED_NEW_NUMBER"
+          })
+        }
+      }
+    },
+    mobileNumber: getLabelWithValue(
+        {
+          labelName: "New Mobile No. of Transferor",
+          labelKey: "PT_MUTATION_TRANSFEROR_NEW_CORRECTED_NUMBER_LABEL"
+        },
+        {
+          jsonPath:
+            "Property.oldmobileNumber",
+          callBack: checkValueForNA
+        }
+      ),
+    }
+  )
+  
 });
 
 export const transferorInstitutionSummary = getCommonGrayCard({
