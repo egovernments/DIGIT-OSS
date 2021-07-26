ef4cfd1d5a982040dc9b45820382e53fc0653a02
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-07-26 23:12:10 +0530

    RAIN-3253 : DIGIT-2.5 Release changes and sem version incremented to 1.3.0 (#28)
    * added paymentconfig to componentRegistryService
    
    * fixed build failure
    
    * Add quick pay links in the enabledModules
    
    * changed source branch to bug/fsm-hotfixes-3
    
    * reverted back to development branch as source
    
    * added DSS module
    
    * added dss initializations
    
    * imported PT components
    
    * Added and enabled Mcollect module
    
    * Digit UI Added , enabled HRMS and TL Modules
    
    * RAIN-2725 : Digit UI Added Receipts Module
    
    * Initial commit for develop branch
    
    * Create CODEOWNERS
    
    * Revert the deletion of yarnlock file (#26)
    
    * Revert the deletion of yarnlock file
    
    This reverts commit aa221d2e72f9eb420a32db2c8b00ff4a4df3b8da.
    
    * modified branch name to develop from dev
    
    * RAIN-3253 : DIGIT-2.5  Release sem version incremented to 1.3.0
    
    * Updated the branch to build as Main for master branch
    
    Co-authored-by: ssaurabh <saurabh.s@egovernments.org>
    Co-authored-by: Ayan Majumder <ayan@techtonic.asia>
    Co-authored-by: abhinav-egov <67637729+abhinav-egov@users.noreply.github.com>
    Co-authored-by: saurabh-egov <71308823+saurabh-egov@users.noreply.github.com>
    Co-authored-by: nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -4,11 +4,11 @@ BRANCH="$(git branch --show-current)"
 
 echo "Main Branch: $BRANCH"
 
-if [ "$BRANCH" != "dev" ] || [ "$BRANCH" != "master" ]; then
+if [ "$BRANCH" != "develop" ] || [ "$BRANCH" != "master" ]; then
 
   INTERNALS="digit-ui-internals"
   rm -rf $INTERNALS
-  git clone -b development https://github.com/egovernments/digit-ui-internals.git $INTERNALS
+  git clone -b main https://github.com/egovernments/digit-ui-internals.git $INTERNALS
   cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
   cd ..
 
--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -3,24 +3,40 @@ import React from 'react';
 import { initPGRComponents, PGRReducers } from "@egovernments/digit-ui-module-pgr";
 import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
 import { PTModule, PTLinks, PTComponents } from "@egovernments/digit-ui-module-pt";
+import { MCollectModule, MCollectLinks, initMCollectComponents } from "@egovernments/digit-ui-module-mcollect";
+import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
 import { PaymentModule, PaymentLinks, paymentConfigs } from "@egovernments/digit-ui-module-common";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
 import { initLibraries } from "@egovernments/digit-ui-libraries";
+import { HRMSModule ,initHRMSComponents  } from "@egovernments/digit-ui-module-hrms";
+import { TLModule, TLLinks,initTLComponents } from "@egovernments/digit-ui-module-tl";
+import { initReceiptsComponents, ReceiptsModule } from "@egovernments/digit-ui-module-receipts";
 
 initLibraries();
 
-const enabledModules = ["PGR", "FSM", "Payment","PT", "QuickPayLinks"];
+const enabledModules = ["PGR", "FSM", "Payment", "PT", "QuickPayLinks", "DSS", "MCollect","HRMS", "TL","Receipts"];
 window.Digit.ComponentRegistryService.setupRegistry({
-  PaymentModule,
-  PaymentLinks,
   ...paymentConfigs,
   PTModule,
   PTLinks,
+  PaymentModule,
+  PaymentLinks,
   ...PTComponents,
+  MCollectLinks,
+  MCollectModule,
+  HRMSModule,
+  TLModule,
+  TLLinks,
+  ReceiptsModule
 });
 
 initPGRComponents();
 initFSMComponents();
+initDSSComponents();
+initMCollectComponents();
+initHRMSComponents()
+initTLComponents();
+initReceiptsComponents();
 
 const moduleReducers = (initData) => ({
   pgr: PGRReducers(initData),
