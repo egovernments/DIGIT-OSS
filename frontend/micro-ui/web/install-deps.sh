0b58467573cab3aea69e001b0dd1852d2f2e616d
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-08-10 18:23:03 +0530

    Update install-deps.sh

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -8,7 +8,7 @@ if [ "$BRANCH" != "develop" ] || [ "$BRANCH" != "master" ]; then
 
   INTERNALS="digit-ui-internals"
   rm -rf $INTERNALS
-  git clone -b main https://github.com/egovernments/digit-ui-internals.git $INTERNALS
+  git clone -b develop https://github.com/egovernments/digit-ui-internals.git $INTERNALS
   cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
   cd ..
 
