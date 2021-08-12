a718128605acabaa22660e05bd3d77491ca22ce7
Sarvesh-eGov <sarvesh.mukesh@egovernments.org>
2021-08-12 13:21:17 +0530

    [RAIN-3013]: Reverted unnecessary changes

--- municipal-services/property-services/src/main/java/org/egov/pt/service/NotificationService.java
+++ municipal-services/property-services/src/main/java/org/egov/pt/service/NotificationService.java
@@ -23,7 +23,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import static org.egov.pt.util.PTConstants.*;
+import lombok.extern.slf4j.Slf4j;
 
+import static org.egov.pt.util.PTConstants.*;
+@Slf4j
 @Service
 public class NotificationService {
 
@@ -284,7 +287,12 @@ public class NotificationService {
 			if (owner.getMobileNumber() != null)
 				mobileNumberToOwner.put(owner.getMobileNumber(), owner.getName());
 		});
-
+		
+		property.getAlternateMobileNumberDetails().forEach(entry ->{
+				mobileNumberToOwner.put(entry.getMobileNumber(), entry.getName());
+				
+		});
+		
 		List<SMSRequest> smsRequests = notifUtil.createSMSRequest(msg, mobileNumberToOwner);
 		notifUtil.sendSMS(smsRequests);
 
