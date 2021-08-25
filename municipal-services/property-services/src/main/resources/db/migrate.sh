b139feb355efd1959445a601ece01eb64b60109b
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2021-08-25 18:14:15 +0530

    Update migrate.sh

--- municipal-services/property-services/src/main/resources/db/migrate.sh
+++ municipal-services/property-services/src/main/resources/db/migrate.sh
@@ -4,5 +4,5 @@ do
     echo "the schema name : $i"
     #!/bin/sh
 
-flyway -url=$DB_URL -schemas=$SCHEMA_NAME -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true -ignoreMissingMigrations=true migrate
+flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true -ignoreMissingMigrations=true migrate
 done
