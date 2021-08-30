239bd341abcc38b42dc0a5b1c41059c216e37b78
narendrabandhamneni-wt-egov <61416451+narendrabandhamneni-wt-egov@users.noreply.github.com>
2021-08-30 10:37:21 +0530

    FSM-624 count is not proper for inbox (#100)

--- municipal-services/inbox/src/main/java/org/egov/inbox/util/FSMConstants.java
+++ municipal-services/inbox/src/main/java/org/egov/inbox/util/FSMConstants.java
@@ -5,5 +5,6 @@ public class FSMConstants {
 	public static final String FSM_MODULE="FSM";
 	public static final String APPLICATION_NUMBER_PARAM = "applicationNo";
 	public static  final String APPLICATION_NUMBER="applicationNos";
+	public static final String  FSM_DSO = "FSM_DSO";
 
 }
