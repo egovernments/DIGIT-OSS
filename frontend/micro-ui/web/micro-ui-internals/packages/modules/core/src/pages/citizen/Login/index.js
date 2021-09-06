d5f5708243c196c87af488fa13c49b4b5ca7ed4b
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-09-06 22:28:18 +0530

    RAIN-3613 : Citizen enabled the backward compatablity to support login in old ui (#171)
    * RAIN-3613 : Citizen enabled the backward compatablity to support login in old ui
    
    * Updated the setting locale from initdata

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/index.js
@@ -12,6 +12,20 @@ const TYPE_LOGIN = { type: "login" };
 const DEFAULT_USER = "digit-user";
 const DEFAULT_REDIRECT_URL = "/digit-ui/citizen";
 
+/* set citizen details to enable backward compatiable */
+const setCitizenDetail=(userObject,token,tenantId)=>{
+  let locale=JSON.parse(sessionStorage.getItem("Digit.initData"))?.value?.selectedLanguage;
+  localStorage.setItem("Citizen.tenant-id",tenantId);
+  localStorage.setItem("tenant-id",tenantId);
+  localStorage.setItem("citizen.userRequestObject",JSON.stringify(userObject));
+  localStorage.setItem("locale",locale);
+  localStorage.setItem("Citizen.locale",locale);
+  localStorage.setItem("token",token);
+  localStorage.setItem("Citizen.token",token);
+  localStorage.setItem("user-info",JSON.stringify(userObject));
+  localStorage.setItem("Citizen.user-info",JSON.stringify(userObject));  
+}
+
 const getFromLocation = (state, searchParams) => {
   return state?.from || searchParams?.from || DEFAULT_REDIRECT_URL;
 };
@@ -52,6 +66,7 @@ const Login = ({ stateCode, isUserRegistered = true }) => {
       return;
     }
     Digit.UserService.setUser(user);
+    setCitizenDetail(user?.info,user?.access_token,stateCode)
     const redirectPath = location.state?.from || DEFAULT_REDIRECT_URL;
     history.replace(redirectPath);
   }, [user]);
@@ -130,6 +145,7 @@ const Login = ({ stateCode, isUserRegistered = true }) => {
         };
 
         const { ResponseInfo, UserRequest: info, ...tokens } = await Digit.UserService.authenticate(requestData);
+
         if (location.state?.role) {
           const roleInfo = info.roles.find((userRole) => userRole.code === location.state.role);
           if (!roleInfo || !roleInfo.code) {
