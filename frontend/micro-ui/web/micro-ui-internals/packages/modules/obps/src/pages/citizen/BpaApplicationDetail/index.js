c7f3501a4b8b6d86b0cf2a35caed46aba805b379
ayan-egov <75366065+ayan-egov@users.noreply.github.com>
2021-09-17 09:35:32 +0530

    RAIN-3531: Fixed application timeline (#263)
    Co-authored-by: ayan <ayan.majumder@egov.org>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
@@ -1,7 +1,9 @@
-import { CardHeader, Header, Card, StatusTable, Row, Loader, PDFSvg } from "@egovernments/digit-ui-react-components";
+import { CardHeader, Header, Card, StatusTable, Row, Loader, PDFSvg, SubmitBar, LinkButton } from "@egovernments/digit-ui-react-components";
 import React, { Fragment, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import BPAApplicationTimeline from "./BPAApplicationTimeline";
+import DocumentDetails from "../../../components/DocumentDetails";
 
 const BpaApplicationDetail = () => {
   const { id } = useParams();
@@ -16,6 +18,10 @@ const BpaApplicationDetail = () => {
     moduleCode: "BPA",
   });
 
+  const downloadDiagram = (val) => {
+    location.href = val;
+  }
+
   if (isLoading) {
     return <Loader />
   }
@@ -23,27 +29,39 @@ const BpaApplicationDetail = () => {
   return (
     <Fragment>
       <Header>{t("ES_TITLE_APPLICATION_DETAILS")}</Header>
-      {data?.applicationDetails?.map((detail, index) => {
+      {data?.applicationDetails?.map((detail, index, arr) => {
         return (
-          <Card>
+          <Card key={index}>
             <CardHeader>{t(detail?.title)}</CardHeader>
             <StatusTable>
               {detail?.values?.map((value) => (
                 <Row className="border-none" label={t(value?.title)} text={t(value?.value) || t("CS_NA")} />
               ))}
-              {detail?.additionalDetails?.documents &&
+              {detail?.additionalDetails?.documents?.[0]?.values && (
                 <Fragment>
                   <Row className="border-none" label={t(detail?.additionalDetails?.documents?.[0].title)} />
-                  {detail?.additionalDetails?.documents?.[0]?.values?.map(() => {
-                    <a>
-                      <PDFSvg />
-                    </a>
-                  })}
+                  <DocumentDetails documents={detail?.additionalDetails?.documents?.[0]?.values} />
                 </Fragment>
+              )}
+              {detail?.additionalDetails?.scruntinyDetails &&
+                // <DocumentDetails documents={detail?.additionalDetails?.scruntinyDetails} />
+                detail?.additionalDetails?.scruntinyDetails.map((scrutiny) => (
+                  <Fragment>
+                    <Row className="border-none" label={t(scrutiny?.title)} />
+                    <LinkButton onClick={() => downloadDiagram(scrutiny?.value)} label={<PDFSvg />}>
+                    </LinkButton>
+                  </Fragment>
+                ))
               }
             </StatusTable>
+            {index === arr.length - 1 && (
+              <Fragment>
+                <BPAApplicationTimeline application={data?.applicationData} id={id} />
+                <SubmitBar label={t("COMMON_MAKE_PAYMENT")} />
+              </Fragment>
+            )}
           </Card>
-        )
+        );
       })}
     </Fragment>
   )
