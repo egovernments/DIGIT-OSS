bd1fa31dfe5f5c4f736555eac777775ca9667dac
saurabh-egov <saurabh.s@egovernments.org>
2021-09-18 22:58:09 +0530

    removed css temporarily

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/.storybook/preview.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/.storybook/preview.js
@@ -1,4 +1,4 @@
-import "@egovernments/digit-ui-css/dist/index.css"
+// import "@egovernments/digit-ui-css/dist/index.css"
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 
 // TODO: It should be removed bcz we should not use any library in components
