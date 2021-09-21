6328b34e8a1e9d6fcbb83406970c2356f27a0fc2
talele08 <talele08@users.noreply.github.com>
2021-09-21 16:49:44 +0530

    RAIN-3799: added tenantId in update (#284)

--- municipal-services/pgr-services/src/main/java/org/egov/pgr/validator/ServiceRequestValidator.java
+++ municipal-services/pgr-services/src/main/java/org/egov/pgr/validator/ServiceRequestValidator.java
@@ -58,11 +58,12 @@ public class ServiceRequestValidator {
     public void validateUpdate(ServiceRequest request, Object mdmsData){
 
         String id = request.getService().getId();
+        String tenantId = request.getService().getTenantId();
         validateSource(request.getService().getSource());
         validateMDMS(request, mdmsData);
         validateDepartment(request, mdmsData);
         validateReOpen(request);
-        RequestSearchCriteria criteria = RequestSearchCriteria.builder().ids(Collections.singleton(id)).build();
+        RequestSearchCriteria criteria = RequestSearchCriteria.builder().ids(Collections.singleton(id)).tenantId(tenantId).build();
         criteria.setIsPlainSearch(false);
         List<ServiceWrapper> serviceWrappers = repository.getServiceWrappers(criteria);
 
