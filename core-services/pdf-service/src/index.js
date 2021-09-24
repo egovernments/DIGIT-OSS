7f7f135300660a518d7706d8798cf0390590cfbb
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2021-09-24 10:53:04 +0530

    Added locale for odiya language in defaultFontMapping (#315)

--- core-services/pdf-service/src/index.js
+++ core-services/pdf-service/src/index.js
@@ -102,7 +102,8 @@ var fontDescriptors = {
 var defaultFontMapping = {
   en_IN: 'default',
   hi_IN: 'default',
-  pn_IN: 'BalooPaaji'
+  pn_IN: 'BalooPaaji',
+  od_IN: 'BalooBhaina'
 }
 
 const printer = new pdfMakePrinter(fontDescriptors);
