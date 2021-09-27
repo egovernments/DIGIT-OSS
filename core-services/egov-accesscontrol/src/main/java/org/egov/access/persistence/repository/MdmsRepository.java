50e5e3e4c5027db886601a75111dc346bc1464e6
Shashwat Mishra <shashwat.mishra@egovernments.org>
2021-09-27 14:27:05 +0530

    [RAIN-3266] Added logic to set header dynamically in access control

--- core-services/egov-accesscontrol/src/main/java/org/egov/access/persistence/repository/MdmsRepository.java
+++ core-services/egov-accesscontrol/src/main/java/org/egov/access/persistence/repository/MdmsRepository.java
@@ -15,6 +15,8 @@ import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -87,6 +89,11 @@ public class MdmsRepository {
         MdmsCriteriaReq mcq = new MdmsCriteriaReq();
         mcq.setRequestInfo(requestInfo);
         mcq.setMdmsCriteria(mc);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("tenantid", tenantId);
+
+        HttpEntity<MdmsCriteriaReq> request = new HttpEntity<>(mcq, headers);
 
         @SuppressWarnings("unchecked")
         Map<String, Map<String, List>> response = (Map<String, Map<String, List>>) restTemplate.postForObject(url, mcq,
