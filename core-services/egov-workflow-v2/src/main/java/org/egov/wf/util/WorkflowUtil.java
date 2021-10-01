09f751f242c694a823a1fb5102bf078c8115c528
Shashwat Mishra <shashwat.mishra@egovernments.org>
2021-10-01 14:51:42 +0530

    Passed tenantId to fetch all business services pathway

--- core-services/egov-workflow-v2/src/main/java/org/egov/wf/util/WorkflowUtil.java
+++ core-services/egov-workflow-v2/src/main/java/org/egov/wf/util/WorkflowUtil.java
@@ -240,7 +240,7 @@ public class WorkflowUtil {
      */
     public void enrichStatusesInSearchCriteria(RequestInfo requestInfo, ProcessInstanceSearchCriteria criteria){
 
-        Map<String, Map<String,List<String>>> roleTenantAndStatusMapping = businessServiceRepository.getRoleTenantAndStatusMapping();
+        Map<String, Map<String,List<String>>> roleTenantAndStatusMapping = businessServiceRepository.getRoleTenantAndStatusMapping(criteria.getTenantId());
         Map<String,List<String>> roleToTenantIdMap = getRoleToTenantId(requestInfo);
 
         List<String> tenantSpecificStatuses = new LinkedList<>();
