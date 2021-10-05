4bbeac2a1f0e6f7c2becda315095226046aea5a3
Shashwat Mishra <71879793+shashwat-egov@users.noreply.github.com>
2021-10-05 18:34:04 +0530

    [RAIN-3235] Added logic to send notification upon bill cancellation (#441)

--- business-services/billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
+++ business-services/billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
@@ -79,6 +79,7 @@ import org.egov.demand.model.TaxHeadMaster;
 import org.egov.demand.model.TaxHeadMasterCriteria;
 import org.egov.demand.model.UpdateBillCriteria;
 import org.egov.demand.model.UpdateBillRequest;
+import org.egov.demand.producer.Producer;
 import org.egov.demand.repository.BillRepositoryV2;
 import org.egov.demand.repository.IdGenRepo;
 import org.egov.demand.repository.ServiceRequestRepository;
@@ -138,7 +139,13 @@ public class BillServicev2 {
 	
 	@Autowired
 	private BillValidator billValidator;
-	
+
+	@Autowired
+	private Producer producer;
+
+	@Value("${kafka.topics.cancel.bill.topic.name}")
+	private String billCancelTopic;
+
 	@Value("${kafka.topics.billgen.topic.name}")
 	private String notifTopicName;
 	
@@ -161,9 +168,28 @@ public class BillServicev2 {
 			
 			throw new CustomException("EG_BS_CANCEL_BILL_ERROR", "Only one consumer code can be provided in the Cancel request");
 		} else {
-
+			sendNotificationForBillCancellation(updateBillRequest.getRequestInfo(), cancelBillCriteria);
 			return billRepository.updateBillStatus(cancelBillCriteria);
 		}
+	}
+
+	private void sendNotificationForBillCancellation(RequestInfo requestInfo, UpdateBillCriteria cancelBillCriteria) {
+		Set<String> consumerCodes = cancelBillCriteria.getConsumerCodes();
+		if(CollectionUtils.isEmpty(consumerCodes))
+			return;
+
+		List<BillV2> bills =  billRepository.findBill(BillSearchCriteria.builder()
+				.service(cancelBillCriteria.getBusinessService())
+				.tenantId(cancelBillCriteria.getTenantId())
+				.consumerCode(consumerCodes)
+				.build());
+
+		if (CollectionUtils.isEmpty(bills))
+			return;
+
+		BillRequestV2 req = BillRequestV2.builder().bills(bills).requestInfo(requestInfo).build();
+		producer.push(billCancelTopic, req);
+
 	}
 
 	/**
