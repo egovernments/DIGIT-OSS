c6bd1508d6ede8e69ee3d5eed6d32cbdf4aef15a
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2021-10-20 20:40:53 +0530

    RAIN-4144:integrated comparison report and occupancy certidicate (#604)

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/OBPS.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/OBPS.js
@@ -186,6 +186,13 @@ export const OBPSService = {
     BPA.riskType = riskType;
     const nocResponse = await OBPSService.NOCSearch(BPA?.tenantId, { sourceRefId: BPA?.applicationNo });
     const noc = nocResponse?.Noc;
+    const filter = { approvalNo: edcr?.permitNumber};
+    const bpaResponse = await OBPSService.BPASearch(tenantId, { ...filter });
+       const comparisionRep = {
+         ocdcrNumber: BPA?.edcrNumber,
+         edcrNumber: bpaResponse?.BPA?.[0]?.edcrNumber
+      }
+    const comparisionReport = await OBPSService.comparisionReport(tenantId, { ...comparisionRep });
 
     const nocDetails = noc
       ?.map((nocDetails, index) => ({
@@ -554,7 +561,8 @@ export const OBPSService = {
       applicationDetails: bpaFilterDetails,
       tenantId: BPA?.tenantId,
       edcrDetails: edcr,
-      nocData: noc
+      nocData: noc,
+      comparisionReport: comparisionReport?.comparisonDetail
     }
   }
 }
