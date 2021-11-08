85dc0f97d58bc5f0903737933e640294772d2a32
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-11-08 21:13:10 +0530

    RAIN-4276 : Added Image not found logo

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/index.js
@@ -172,7 +172,7 @@ class Header extends Component {
     const tenantId = role.toLowerCase() === "citizen" ? commonConfig.tenantId: getTenantId();
     const currentCity = cities.filter((item) => item.code === tenantId);
     const ulbLogo =
-      currentCity.length > 0 ? get(currentCity[0], "logoId") : "https://s3.ap-south-1.amazonaws.com/pb-egov-assets/pb.amritsar/logo.png";
+      currentCity.length > 0 ? get(currentCity[0], "logoId") : "https://s3.ap-south-1.amazonaws.com/in-central-instance-assets/logoNotFound.png";
     return (
       <div style={headerStyle}>
         <AppBar
