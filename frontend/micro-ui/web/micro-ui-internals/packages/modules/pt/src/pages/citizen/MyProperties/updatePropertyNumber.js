e23b47e7a9327322d04f80f4ac8eaeaeefa3e4c7
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-11-16 20:26:45 +0530

    RAIN-3831 : PT Update no popup integrated

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/MyProperties/updatePropertyNumber.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/MyProperties/updatePropertyNumber.js
@@ -1,10 +1,28 @@
-import { CloseSvg, SubmitBar, Header, StatusTable, Row, TextInput, MobileNumber, CardLabel } from "@egovernments/digit-ui-react-components";
+import {
+  CloseSvg,
+  SubmitBar,
+  Header,
+  StatusTable,
+  Row,
+  TextInput,
+  MobileNumber,
+  SearchForm,
+  CardLabel,
+  CardLabelError,
+} from "@egovernments/digit-ui-react-components";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const UpdatePropertyNumber = ({ showPopup, property, t }) => {
-    
-    const  SelectOtp = Digit?.ComponentRegistryService?.getComponent('SelectOtp');
+  const SelectOtp = Digit?.ComponentRegistryService?.getComponent("SelectOtp");
+  const { register, control, handleSubmit, setValue, watch, getValues, reset, formState } = useForm({
+    defaultValues: {
+      mobileNumber: "",
+      otp: "",
+    },
+  });
+  const formValue = watch();
 
   return (
     <div className="popup-module updatePropertyNumber">
@@ -13,25 +31,58 @@ const UpdatePropertyNumber = ({ showPopup, property, t }) => {
       </div>
 
       <Header>{t("PTUPNO_HEADER")}</Header>
-      <div className="pt-update-no-popup-wrapper">
-        <StatusTable >
+      <SearchForm onSubmit={(_data) => console.log(_data)} handleSubmit={handleSubmit}>
+        <StatusTable>
           <Row label={t("PTUPNO_OWNER_NAME")} text={`${property?.owners?.[0]?.name || t("CS_NA")}`} />
           <Row label={t("PTUPNO_CURR_NO")} text={`${property?.owners?.[0]?.mobileNumber || t("CS_NA")}`} />
           <CardLabel style={{ marginBottom: "8px" }}>{t("PT_UPDATE_NEWNO")}</CardLabel>
-          <MobileNumber className="field pt-update-no-field" onChange={(e) => console.log(e)} value={""} disable={false} />
-          <SelectOtp
-          userType="employee"
-            config={{"header":"OTP Verification","cardText":"Enter the OTP sent to 9965664222","nextText":"Next","submitBarLabel":"Next"}}
-            onOtpChange={(e) => console.log(e)}
-            onResend={(e) => console.log(e)}
-            onSelect={(e) => console.log(e)}
-            otp={''}
-            error={false}
-            t={t}
+          <MobileNumber
+            className="field pt-update-no-field"
+            name="mobileNumber"
+            inputRef={register({
+              value: getValues("mobileNumber"),
+              shouldUnregister: true,
+              ...{
+                required: "MANDATORY_MOBILE",
+                minLength: {
+                  value: 10,
+                  message: "CORE_COMMON_MOBILE_ERROR",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "CORE_COMMON_MOBILE_ERROR",
+                },
+                pattern: {
+                  value: /[789][0-9]{9}/,
+                  message: "CORE_COMMON_MOBILE_ERROR",
+                },
+              },
+            })}
+            disable={false}
+          />
+          <CardLabelError style={{ marginTop: "-10px" }}>{t(formState?.errors?.mobileNumber?.message)}</CardLabelError>
+          <Controller
+            control={control}
+            name="otp"
+            render={(props, customProps) => (
+              <SelectOtp
+                userType="employee"
+                config={{ header: "OTP Verification", cardText: "Enter the OTP sent to 9965664222", nextText: "Next", submitBarLabel: "Next" }}
+                onOtpChange={(d) => {
+                  props.onChange(d);
+                }}
+                onResend={(e) => console.log(e)}
+                onSelect={(e) => console.log(e)}
+                error={false}
+                t={t}
+                otp={props.value}
+              />
+            )}
           />
         </StatusTable>
-        <SubmitBar label={t("PTUPNO_SENDOTP")} onClick={(e) => console.log("clicked", e)} />
-      </div>
+        {/* <SubmitBar label={t("PTUPNO_SENDOTP")} onClick={(e) => console.log("clicked", e)} /> */}
+        <SubmitBar label={t("PTUPNO_SENDOTP")} submit />
+      </SearchForm>
     </div>
   );
 };
