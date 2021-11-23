bd18a1aa1ac933a56c16463d4004e5359ddb973e
rohit-eGov <rohit.shende@egovernments.org>
2021-11-23 17:29:59 +0530

    RAIN-4459 updated password execption message

--- core-services/egov-user/src/main/java/org/egov/user/domain/service/UserService.java
+++ core-services/egov-user/src/main/java/org/egov/user/domain/service/UserService.java
@@ -554,7 +554,7 @@ public class UserService {
      */
     private void validateExistingPassword(User user, String existingRawPassword) {
         if (!passwordEncoder.matches(existingRawPassword, user.getPassword())) {
-            throw new RuntimeException("No such username or password");
+            throw new PasswordMismatchException("Invalid username or password");
         }
     }
 
