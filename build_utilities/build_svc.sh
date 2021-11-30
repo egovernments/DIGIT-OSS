050f11f7d01480c02c81da840c091e02d3e8191f
nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
2021-11-30 14:44:17 +0530

    Update build_svc.sh

--- build_utilities/build_svc.sh
+++ build_utilities/build_svc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git diff HEAD~1 | grep 'git' | cut -d " " -f 3 | cut -d "/" -f 3 | uniq | sort -nr | xargs > servicelist
+git diff HEAD~1 | grep 'git' | cut -d " " -f 3 | cut -d "/" -f 3 | uniq | sort -nr > servicelist
 go get gopkg.in/yaml.v2
