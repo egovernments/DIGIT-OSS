5069d98a1b8a2efe562a0ba034a6566b345cdded
tulika kundu <tulika.kundu@egovernments.org>
2021-12-15 15:54:04 +0530

    RAIN-4136:Mcollect few audit fixes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/mCollect/src/pages/employee/NewChallan/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/mCollect/src/pages/employee/NewChallan/index.js
@@ -97,6 +97,9 @@ const NewChallan = ({ChallanData}) => {
     clearSuccessData();
   }, []);
 
+  window.onunload = function () {
+    sessionStorage.removeItem("mcollectFormData");
+  }
   const onFormValueChange = (setValue, formData, formState) => {
     let mcollectFormValue = JSON.parse(sessionStorage.getItem("mcollectFormData"));
     console.log(formState);
@@ -119,8 +122,8 @@ const NewChallan = ({ChallanData}) => {
   const onSubmit = (data) => {
     let mcollectFormValue = JSON.parse(sessionStorage.getItem("mcollectFormData"));
     data = mcollectFormValue? mcollectFormValue : data?.consomerDetails1?.[0];
-    let TaxHeadMasterKeys= Object.keys(data[`${data?.category?.code?.split(".")[0]}`]);
-    let TaxHeadMasterValues = Object.values(data[`${data?.category?.code?.split(".")[0]}`]);
+    let TaxHeadMasterKeys= data[`${data?.category?.code?.split(".")[0]}`] ? Object.keys(data[`${data?.category?.code?.split(".")[0]}`]) : [];
+    let TaxHeadMasterValues = data[`${data?.category?.code?.split(".")[0]}`] ? Object.values(data[`${data?.category?.code?.split(".")[0]}`]) : [];
     let Challan = {};
     if(!isEdit){
       let temp = data?.category?.code;
@@ -130,7 +133,7 @@ const NewChallan = ({ChallanData}) => {
           mobileNumber: data.mobileNumber,
         },
         //businessService: selectedCategoryType ? temp + "." + humanized(selectedCategoryType.code, temp) : "",
-        businessService:data?.category?.code,
+        businessService:data?.categoryType?.code,
         consumerType: data?.category?.code?.split(".")[0],
         description: data?.comments,
         taxPeriodFrom: Date.parse(data?.fromDate),
@@ -198,6 +201,7 @@ const NewChallan = ({ChallanData}) => {
         .then((result, err) => {
           if (result.challans && result.challans.length > 0) {
             const challan = result.challans[0];
+            sessionStorage.removeItem("mcollectFormData");
             Digit.MCollectService.generateBill(challan.challanNo, tenantId, challan.businessService, "challan").then((response) => {
               if (response.Bill && response.Bill.length > 0) {
                 history.push(
@@ -208,7 +212,7 @@ const NewChallan = ({ChallanData}) => {
             });
           }
         })
-        .catch((e) => setShowToast({ key: true, label: e?.response?.data?.Errors[0].message }));
+        .catch((e) => {setShowToast({ key: true, label: e?.response?.data?.Errors[0].message })});
     }
   };
   let configs = newConfig || [];
@@ -234,7 +238,7 @@ const NewChallan = ({ChallanData}) => {
   return (
     <div>
       <div style={{ marginLeft: "15px" }}>
-        <Header>{t("UC_COMMON_HEADER")}</Header>
+        <Header>{isEdit ? t("UC_UPDATE_CHALLAN"):t("UC_COMMON_HEADER")}</Header>
       </div>
       <FormComposer
         heading={t("")}
