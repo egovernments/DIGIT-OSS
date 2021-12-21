88012adff2239cbfc4201ef19d2c659225847257
rohit-eGov <rohit.shende@egovernments.org>
2021-12-21 13:28:57 +0530

    RAIN-4605 : PT Payment history download receipt , download through receipt number

--- utilities/egov-pdf/src/routes/payments.js
+++ utilities/egov-pdf/src/routes/payments.js
@@ -20,20 +20,29 @@ router.post(
     var consumerCode = req.query.consumerCode;
     var bussinessService = req.query.bussinessService;
     var receiptKey = req.query.pdfKey;
+    var receiptNumbers = req.query.receiptNumbers;
     var requestinfo = req.body;
     if (requestinfo == undefined) {
       return renderError(res, "requestinfo can not be null", 400);
     }
-    if (!tenantId || !consumerCode) {
+    if (!tenantId) {
       return renderError(
         res,
-        "tenantId and consumerCode are mandatory to generate the receipt",
+        "tenantId is mandatory to generate the receipt",
         400
       );
     }
+    else if (!receiptNumbers && !consumerCode) {
+      return renderError(
+        res,
+        "consumerCode or receiptNumbers are mandatory to generate the receipt",
+        400
+      );
+    }
+
     try {
       try {
-        resProperty = await search_payment(consumerCode, tenantId, requestinfo, bussinessService);
+        resProperty = await search_payment(consumerCode, tenantId, requestinfo, bussinessService, receiptNumbers);
       } catch (ex) {
         
         if (ex.response && ex.response.data) console.log(ex.response.data);
