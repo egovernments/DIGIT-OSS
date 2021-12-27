e057d648ee729e674ff0d4bb7efedf6b726cb5b0
Sushant Agrawal <sushant@trisysit.com>
2021-12-27 16:11:00 +0530

    RAIN-4146: Changes in name references from eg_chat... to eg_webchat...

--- core-services/xstate-webchat/nodejs/src/session/repo/postgres-repo.js
+++ core-services/xstate-webchat/nodejs/src/session/repo/postgres-repo.js
@@ -2,19 +2,19 @@ const pool = require('./postgres-config');
 
 class StateRepository {
   async insertNewState(userId, active, state) {
-    const query = 'INSERT INTO eg_chat_state_v2 (user_id, active, state) VALUES ($1, $2, $3)';
+    const query = 'INSERT INTO eg_webchat_state_v2 (user_id, active, state) VALUES ($1, $2, $3)';
     const result = await pool.query(query, [userId, active, state]);
     return result;
   }
 
   async updateState(userId, active, state) {
-    const query = 'UPDATE eg_chat_state_v2 SET active = $2, state = $3 WHERE user_id = $1';
+    const query = 'UPDATE eg_webchat_state_v2 SET active = $2, state = $3 WHERE user_id = $1';
     const result = await pool.query(query, [userId, active, state]);
     return result;
   }
 
   async getActiveStateForUserId(userId) {
-    const query = 'SELECT (state) FROM eg_chat_state_v2 WHERE user_id = $1 AND active = true';
+    const query = 'SELECT (state) FROM eg_webchat_state_v2 WHERE user_id = $1 AND active = true';
     const result = await pool.query(query, [userId]);
     if (result.rowCount >= 1) {
       const { state } = result.rows[0];
