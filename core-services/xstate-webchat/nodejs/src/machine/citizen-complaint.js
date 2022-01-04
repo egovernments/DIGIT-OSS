6697e4657c0236a462d3450a92b77f709e3ec617
Sushant Agrawal <sushant@trisysit.com>
2022-01-04 11:13:40 +0530

    CORE-86: Update state machine logic to handle Billing is incorrect option

--- core-services/xstate-webchat/nodejs/src/machine/citizen-complaint.js
+++ core-services/xstate-webchat/nodejs/src/machine/citizen-complaint.js
@@ -4,141 +4,139 @@ const dialog = require('./util/dialog');
 const messages = require('./messages/complaint-messages');
 
 const citizenComplaint = {
-    id: 'citizenComplaint',
-    initial: 'complaintCategory',
-    onEntry: assign((context, event) => {
-        context.slots.pgr = {}
-    }),
-    states: {
-        complaintCategory: {
-            id: 'complaintCategory',
-            initial: 'question',
-            states: {
-                question: {
-                        onEntry: assign((context, event) => {
-                        let message = dialog.get_message(messages.complaintMenu.prompt, context.user.locale);
-                        const grammer = dialog.constructContextGrammer(messages.complaintMenu.options.list,
-                                    messages.complaintMenu.options.messageBundle, context.user.locale);
-                        context.grammer = grammer;
-                        dialog.sendMessage(context, message, true);
-                    }),
-                    on: {
-                        USER_MESSAGE: 'process'
-                    }
-                }, //question
-                process: {
-                    onEntry: assign((context, event) => {
-                        context.intention = dialog.get_intention(context.grammer, event, true)
-                    }),
-                    always: [
-                        {
-                            target: '#complaintItem',
-                            cond: (context) => context.intention != dialog.INTENTION_UNKOWN,
-                            actions: assign((context, event) => {
-                                context.slots.pgr["complaint"] = context.intention;
-                                context.slots.pgr["complaintItem"] = context.intention;
-                            })
-                        },
-                        {
-                            target: 'error'
-                        }
-                    ]
-                }, // process
-                error: {
-                    onEntry: assign((context, event) => {
-                        dialog.sendMessage(context, dialog.get_message(dialog.global_messages.error.retry, context.user.locale), false);
-                    }),
-                    always: 'question',
-                } // error
-            } // states of complaintCategory
-        }, // complaintCategory
+  id: 'citizenComplaint',
+  initial: 'complaintCategory',
+  onEntry: assign((context, event) => {
+    context.slots.pgr = {};
+  }),
+  states: {
+    complaintCategory: {
+      id: 'complaintCategory',
+      initial: 'question',
+      states: {
+        question: {
+          onEntry: assign((context, event) => {
+            const message = dialog.get_message(messages.complaintMenu.prompt, context.user.locale);
+            const grammer = dialog.constructContextGrammer(messages.complaintMenu.options.list,
+              messages.complaintMenu.options.messageBundle, context.user.locale);
+            context.grammer = grammer;
+            dialog.sendMessage(context, message, true);
+          }),
+          on: {
+            USER_MESSAGE: 'process',
+          },
+        }, // question
+        process: {
+          onEntry: assign((context, event) => {
+            context.intention = dialog.get_intention(context.grammer, event, true);
+          }),
+          always: [
+            {
+              target: '#complaintItem',
+              cond: (context) => context.intention != dialog.INTENTION_UNKOWN,
+              actions: assign((context, event) => {
+                context.slots.pgr.complaint = context.intention;
+                context.slots.pgr.complaintItem = context.intention;
+              }),
+            },
+            {
+              target: 'error',
+            },
+          ],
+        }, // process
+        error: {
+          onEntry: assign((context, event) => {
+            dialog.sendMessage(context, dialog.get_message(dialog.global_messages.error.retry, context.user.locale), false);
+          }),
+          always: 'question',
+        }, // error
+      }, // states of complaintCategory
+    }, // complaintCategory
 
-        complaintItem: {
-            id: 'complaintItem',
-            initial: 'question',
-            states: {
-                question: {
-                        onEntry: assign((context, event) => {
-                        let complaintItem = context.slots.pgr.complaintItem;
-                        let messageBundleForCode = messages.complaintCategoryItems[complaintItem].messageBundle;
-                        let message = dialog.get_message(messageBundleForCode, context.user.locale);
-                        let nextStepList = messages.complaintCategoryItems[complaintItem].nextStep;
-                        let grammer = dialog.constructContextGrammer(nextStepList, messageBundleForCode, context.user.locale);
-                        context.grammer = grammer;
-                        dialog.sendMessage(context, message, true);
-                    }),
-                    on: {
-                        USER_MESSAGE: 'process'
-                    }
-                }, //question
-                process: {
-                    onEntry: assign((context, event) => {
-                        context.intention = dialog.get_intention(context.grammer, event, true)
-                    }),
-                    always: [
-                        {
-                            cond: (context) => context.intention == dialog.INTENTION_GOBACK,
-                            target: '#complaintCategory'
+    complaintItem: {
+      id: 'complaintItem',
+      initial: 'question',
+      states: {
+        question: {
+          onEntry: assign((context, event) => {
+            const { complaintItem } = context.slots.pgr;
+            const messageBundleForCode = messages.complaintCategoryItems[complaintItem].messageBundle;
+            const message = dialog.get_message(messageBundleForCode, context.user.locale);
+            const nextStepList = messages.complaintCategoryItems[complaintItem].nextStep;
+            const grammer = dialog.constructContextGrammer(nextStepList, messageBundleForCode, context.user.locale);
+            context.grammer = grammer;
+            dialog.sendMessage(context, message, true);
+          }),
+          on: {
+            USER_MESSAGE: 'process',
+          },
+        }, // question
+        process: {
+          onEntry: assign((context, event) => {
+            context.intention = dialog.get_intention(context.grammer, event, true);
+          }),
+          always: [
+            {
+              cond: (context) => context.intention == dialog.INTENTION_GOBACK,
+              target: '#complaintCategory',
 
-                        },
-                        {
-                            cond: (context) => context.intention == 'persistComplaint',
-                            target: '#persistComplaint',
-                            actions: assign((context, event) => {
-                                context.slots.pgr["complaintItem"] = context.intention;
-                            })
-                        },
-                        {
-                            cond: (context) => context.intention != dialog.INTENTION_UNKOWN,
-                            target: '#complaintItem',
-                            actions: assign((context, event) => {
-                                context.slots.pgr["complaintItem"] = context.intention;
-                            })
-                        },
+            },
+            {
+              cond: (context) => context.intention == 'persistComplaint',
+              target: '#persistComplaint',
+              actions: assign((context, event) => {
+                context.slots.pgr.complaintItem = context.intention;
+              }),
+            },
+            {
+              cond: (context) => context.intention != dialog.INTENTION_UNKOWN,
+              target: '#complaintItem',
+              actions: assign((context, event) => {
+                context.slots.pgr.complaintItem = context.intention;
+              }),
+            },
 
-                        {
-                            target: 'error'
-                        }
-                    ]
-                }, // process
-                error: {
-                    onEntry: assign((context, event) => {
-                        dialog.sendMessage(context, dialog.get_message(dialog.global_messages.error.retry, context.user.locale), false);
-                    }),
-                    always: 'question',
-                } // error
-            } // states of complaintItem
-        }, // complaintItem
+            {
+              target: 'error',
+            },
+          ],
+        }, // process
+        error: {
+          onEntry: assign((context, event) => {
+            dialog.sendMessage(context, dialog.get_message(dialog.global_messages.error.retry, context.user.locale), false);
+          }),
+          always: 'question',
+        }, // error
+      }, // states of complaintItem
+    }, // complaintItem
 
-
-
-        persistComplaint: {
-            id: 'persistComplaint',
-            invoke: {
-                id: 'persistComplaint',
-                src: (context) => pgrService.persistComplaint(context.user, context.slots.pgr, context.extraInfo,context.user.locale),
-                onDone: {
-                    target: '#endstate',
-                    actions: assign((context, event) => {
-                        let complaintDetails = event.data;
-                        let message = dialog.get_message(messages.persistComplaint, context.user.locale);
-                        //Email Notification here
-                        let complaintType='Technical';
-                        let complaintSubType = dialog.get_message(messages.complaintMenu.options.messageBundle[context.slots.pgr.complaint], context.user.locale);
-                        let complaintId=complaintDetails.complaintNumber;
-                        let application_number='';
-                        let phoneNumber=context.user.mobileNumber;
-                        let ComplaintComments=context.extraInfo.comments;
-                        let imageFilestoreId=context.extraInfo.filestoreId;
-                        //Insert the code for pushing email message to the Kafka topic here
-                        //Email notification ends
-                         message = message.replace('{{complaintNumber}}', complaintDetails.complaintNumber);
-                         dialog.sendMessage(context, message);
-                    })
-                }
-            }
+    persistComplaint: {
+      id: 'persistComplaint',
+      invoke: {
+        id: 'persistComplaint',
+        src: (context) => pgrService.persistComplaint(context.user, context.slots.pgr, context.extraInfo, context.user.locale),
+        onDone: {
+          target: '#endstate',
+          actions: assign((context, event) => {
+            const complaintDetails = event.data;
+            let message = dialog.get_message(messages.persistComplaint, context.user.locale);
+            // Email Notification here
+            const complaintType = 'Technical';
+            const complaintSubType = dialog.get_message(messages.complaintMenu.options.messageBundle[context.slots.pgr.complaint], context.user.locale);
+            const complaintId = complaintDetails.complaintNumber;
+            const application_number = '';
+            const phoneNumber = context.user.mobileNumber;
+            const ComplaintComments = context.extraInfo.comments;
+            const imageFilestoreId = context.extraInfo.filestoreId;
+            // Insert the code for pushing email message to the Kafka topic here
+            // Email notification ends
+            message = message.replace('{{complaintNumber}}', complaintDetails.complaintNumber);
+            dialog.sendMessage(context, message);
+          }),
         },
-    }, 
-}; 
+      },
+    },
+  },
+};
 
 module.exports = citizenComplaint;
--- core-services/xstate-webchat/nodejs/src/machine/messages/complaint-messages.js
+++ core-services/xstate-webchat/nodejs/src/machine/messages/complaint-messages.js
@@ -1,39 +1,43 @@
 const messages = {
   complaintMenu: {
-     prompt: {
-        en_IN: { message: 'What is your complaint about?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'Not Receiving OTP'}, { key: '2', value: 'Unable to Proceed Forward'}, { key: '3', value: 'Bill Amount is incorrect'}, { key: '4', value: 'Application Process taking long time'}, { key: '5', value: 'Application is getting rejected'}, { key: '6', value: 'Others'}] },
-        hi_IN: { message: 'What is your complaint about?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'ओटीपी प्राप्त नहीं हो रहा है'}, { key: '2', value: 'आगे बढ़ने में असमर्थ'}, { key: '3', value: 'बिल राशि गलत है'}, { key: '4', value: 'आवेदन प्रक्रिया में लंबा समय लग रहा है'}, { key: '5', value: 'आवेदन खारिज हो रहा है'}, { key: '6', value: 'अन्य'}] }
+    prompt: {
+      en_IN: {
+        message: 'What is your complaint about?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'Not Receiving OTP' }, { key: '2', value: 'Unable to Proceed Forward' }, { key: '3', value: 'Bill Amount is incorrect' }, { key: '4', value: 'Application Process taking long time' }, { key: '5', value: 'Application is getting rejected' }, { key: '6', value: 'Others' }],
       },
-      options: {
-        list: ['nootp', 'unabletoproceed', 'billincorrect', 'apptakinglong', 'apprejected', 'others'],
-        messageBundle: {
-          nootp: {
-            en_IN: 'Not Receiving OTP',
-            hi_IN: 'कोविड में अपना ख्याल कैसे रखें?',
-          },
-          unabletoproceed: {
-            en_IN: 'Unable to Proceed Forward',
-            hi_IN: 'फतेह किट का इस्तेमाल कैसे करें',
-          },
-          billincorrect: {
-            en_IN: 'Bill Amount is incorrect',
-            hi_IN: 'कोविड बेड की उपलब्धता के बारे में जानकारी',
-          },
-          apptakinglong: {
-            en_IN: 'Application Process taking long time',
-            hi_IN: 'टीकाकरण केंद्रों की जानकारी',
-          },
-          apprejected: {
-            en_IN: 'Application is getting rejected?',
-            hi_IN: 'टीकाकरण के लिए मुझे कहां पंजीकरण कराना चाहिए?',
-          },
-          others: {
-            en_IN: 'Others',
-            hi_IN: 'काले फंगस के बारे में विस्तृत जानकारी और क्या करें और क्या न करें',
-          },
+      hi_IN: {
+        message: 'What is your complaint about?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'ओटीपी प्राप्त नहीं हो रहा है' }, { key: '2', value: 'आगे बढ़ने में असमर्थ' }, { key: '3', value: 'बिल राशि गलत है' }, { key: '4', value: 'आवेदन प्रक्रिया में लंबा समय लग रहा है' }, { key: '5', value: 'आवेदन खारिज हो रहा है' }, { key: '6', value: 'अन्य' }],
+      },
+    },
+    options: {
+      list: ['nootp', 'unabletoproceed', 'billincorrect', 'apptakinglong', 'apprejected', 'others'],
+      messageBundle: {
+        nootp: {
+          en_IN: 'Not Receiving OTP',
+          hi_IN: 'कोविड में अपना ख्याल कैसे रखें?',
+        },
+        unabletoproceed: {
+          en_IN: 'Unable to Proceed Forward',
+          hi_IN: 'फतेह किट का इस्तेमाल कैसे करें',
+        },
+        billincorrect: {
+          en_IN: 'Bill Amount is incorrect',
+          hi_IN: 'कोविड बेड की उपलब्धता के बारे में जानकारी',
+        },
+        apptakinglong: {
+          en_IN: 'Application Process taking long time',
+          hi_IN: 'टीकाकरण केंद्रों की जानकारी',
+        },
+        apprejected: {
+          en_IN: 'Application is getting rejected?',
+          hi_IN: 'टीकाकरण के लिए मुझे कहां पंजीकरण कराना चाहिए?',
+        },
+        others: {
+          en_IN: 'Others',
+          hi_IN: 'काले फंगस के बारे में विस्तृत जानकारी और क्या करें और क्या न करें',
         },
       },
-    
+    },
+
   },
 
   complaintCategoryItems: {
@@ -134,28 +138,67 @@ const messages = {
         },
       },
     },
-  others: {
+    others: {
       nextStep: ['persistComplaint'],
-      messageBundle:{
-        en_IN: { message: 'Let us know about your complaint', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }] },
-        hi_IN: { message: 'हमें अपनी शिकायत के बारे में बताएं।', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }] }
-    },
-
-
-  },
-  complaintComments: {
-        nextStep: ['persistComplaint'],
-        messageBundle:{
-          en_IN: { message: 'Let us know about your complaint', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }] },
-          hi_IN: { message: 'हमें अपनी शिकायत के बारे में बताएं।', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }] }
+      messageBundle: {
+        en_IN: {
+          message: 'Let us know about your complaint', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }],
+        },
+        hi_IN: {
+          message: 'हमें अपनी शिकायत के बारे में बताएं।', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }],
+        },
       },
 
-  
-  },
+    },
+    complaintComments: {
+      nextStep: ['persistComplaint'],
+      messageBundle: {
+        en_IN: {
+          message: 'Let us know about your complaint', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }],
+        },
+        hi_IN: {
+          message: 'हमें अपनी शिकायत के बारे में बताएं।', step: 'last', optionType: 'textarea', option: [{ key: '1', value: '' }],
+        },
+      },
+
+    },
+    billincorrect: {
+      nextStep: ['selectproperty'],
+      messageBundle: {
+        en_IN: {
+          message: 'Please enter your mobile number', step: 'intermediate', optionType: 'textbox', option: [{ key: '1', value: '' }],
+        },
+        hi_IN: {
+          message: 'अपना मोबाइल नंबर दर्ज करें', step: 'intermediate', optionType: 'textbox', option: [{ key: '1', value: '' }],
+        },
+      },
+    },
+    selectproperty: {
+      nextStep: ['lastbillgenerated'],
+      messageBundle: {
+        en_IN: {
+          message: 'Please select your property from the below list which you want to report', step: 'intermediate', optionType: 'button', option: [],
+        },
+        hi_IN: {
+          message: 'कृपया नीचे दी गई सूची में से अपनी संपत्ति का चयन करें जिसकी आप रिपोर्ट करना चाहते हैंxdder', step: 'intermediate', optionType: 'button', option: [],
+        },
+      },
+    },
+    lastbillgenerated: {
+      nextStep: ['complaintComments'],
+      messageBundle: {
+        en_IN: {
+          message: 'Last Bill Generated <Bill ID> is Rs. <Amount>. Do you want to report this as incorrect?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'Yes' }, { key: '2', value: 'No' }],
+        },
+        hi_IN: {
+          message: 'अंतिम उत्पन्न बिल <बिल आईडी> <राशि> रुपये है। क्या आप इसे गलत के रूप में रिपोर्ट करना चाहते हैं?', step: 'intermediate', optionType: 'button', option: [{ key: '1', value: 'Yes' }, { key: '2', value: 'No' }],
+        },
+      },
+    },
   },
   persistComplaint: {
     en_IN: 'Complaint created successfully \n Complaint ID: {{complaintNumber}} \n Your Complaint has been registered and one of our representatives will get in touch with you regarding the issue\n',
-    hi_IN: 'धन्यवाद! आपने सफलतापूर्वक शिकायत दर्ज की है।\nआपकी शिकायत संख्या: {{complaintNumber}}\n'
+    hi_IN: 'धन्यवाद! आपने सफलतापूर्वक शिकायत दर्ज की है।\nआपकी शिकायत संख्या: {{complaintNumber}}\n',
   },
   closingStatement: {
     en_IN: '\nIn case of any help please type and send "mseva"',
