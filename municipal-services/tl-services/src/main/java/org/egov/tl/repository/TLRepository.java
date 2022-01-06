83e15f352dfdfc3abe3f7a72145a37051f746fd3
vinothrallapalli-eGov <vinoth.r@egovernments.org>
2022-01-06 10:38:30 +0530

    RAIN-4776 Fixed stakeholder documents order issue (#1168)

--- municipal-services/tl-services/src/main/java/org/egov/tl/repository/TLRepository.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/repository/TLRepository.java
@@ -128,6 +128,13 @@ public class TLRepository {
             license.getTradeLicenseDetail().getTradeUnits().sort(Comparator.comparing(TradeUnit::getId));
             if(!CollectionUtils.isEmpty(license.getTradeLicenseDetail().getAccessories()))
                 license.getTradeLicenseDetail().getAccessories().sort(Comparator.comparing(Accessory::getId));
+            // By default it is returning reverse order instead of saved order, to get in the saved order
+            // reversing the documents.
+            List<Document> applnDocuments = license.getTradeLicenseDetail().getApplicationDocuments();
+            if(!CollectionUtils.isEmpty(applnDocuments)) {
+                Collections.reverse(applnDocuments);
+                license.getTradeLicenseDetail().setApplicationDocuments(applnDocuments);
+            }
             if(!CollectionUtils.isEmpty(license.getTradeLicenseDetail().getVerificationDocuments()))
                 license.getTradeLicenseDetail().getVerificationDocuments().sort(Comparator.comparing(Document::getId));
         });
