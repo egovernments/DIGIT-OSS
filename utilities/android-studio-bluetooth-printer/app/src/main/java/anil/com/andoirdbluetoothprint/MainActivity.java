a9642bf1b4c695ad3979c4029c10011dbbdafd1f
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-01-17 17:30:20 +0530

    Update MainActivity.java

--- utilities/android-studio-bluetooth-printer/app/src/main/java/anil/com/andoirdbluetoothprint/MainActivity.java
+++ utilities/android-studio-bluetooth-printer/app/src/main/java/anil/com/andoirdbluetoothprint/MainActivity.java
@@ -131,17 +131,21 @@ public class MainActivity extends Activity {
     }
 
    private void updateReceiptText() {
-        Uri data = getIntent().getData();
-        params = data.getPathSegments();
-        printText=new StringBuffer("");
-        for(String str:params)
-        {
-            printText.append(str).append('/');
+        try{
+            Uri data = getIntent().getData();
+            params = data.getPathSegments();
+            printText=new StringBuffer("");
+            for(String str:params)
+            {
+                printText.append(str).append('/');
+            }
+            printText.deleteCharAt(printText.length()-1);
+            printFinal = printText.toString().replace(NEW_LINE_SYMBOL, "\n");
+            if (printFinal != null)
+                receiptText.setText(printFinal);
+        }catch(Exception ex){
+            Log.e(TAG, "Bluetooth connection failed", ex);
         }
-        printText.deleteCharAt(printText.length()-1);
-        printFinal = printText.toString().replace(NEW_LINE_SYMBOL, "\n");
-        if (printFinal != null)
-            receiptText.setText(printFinal);
     }
 
 }
