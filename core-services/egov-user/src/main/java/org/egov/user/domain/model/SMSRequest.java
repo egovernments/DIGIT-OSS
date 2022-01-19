8402ce2b0f9075f16cac01e01537dcf78ba02624
shubhang-eGov <shubhang.dhanesha@egovernments.org>
2022-01-19 14:29:27 +0530

    Email change notification enhancement

--- core-services/egov-user/src/main/java/org/egov/user/domain/model/SMSRequest.java
+++ core-services/egov-user/src/main/java/org/egov/user/domain/model/SMSRequest.java
@@ -11,12 +11,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @ToString
 public class SMSRequest {
+private String mobileNumber;
 
-    @Pattern(regexp = "^[0-9]{10}$", message = "MobileNumber should be 10 digit number")
-    private String mobileNumber;
-
-    @Size(max = 1000)
     private String message;
-    private String category = "NOTIFICATION";
-    private Long expiryTime;
 }
