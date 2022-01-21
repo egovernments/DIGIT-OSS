d3890dcd32c9064610307f0cf88e86bbed0e6f0c
talele08 <talele.aniket@gmail.com>
2022-01-21 17:59:58 +0530

    removed ssl from pool props

--- municipal-services/firenoc-services/src/db.js
+++ municipal-services/firenoc-services/src/db.js
@@ -8,7 +8,6 @@ const pool = new Pool({
   host: envVariables.DB_HOST,
   database: envVariables.DB_NAME,
   password: envVariables.DB_PASSWORD,
-  ssl: envVariables.DB_SSL,
   port: envVariables.DB_PORT,
   max: envVariables.DB_MAX_POOL_SIZE,
   idleTimeoutMillis: 30000,
