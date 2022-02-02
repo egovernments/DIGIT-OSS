0975a5bf0dd8a0c626252e080d7e38057e5f84cc
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-02-02 11:07:47 +0530

    RAIN-4885 NOC single instance changes

--- municipal-services/noc-services/src/main/java/org/egov/noc/repository/NOCRepository.java
+++ municipal-services/noc-services/src/main/java/org/egov/noc/repository/NOCRepository.java
@@ -3,66 +3,79 @@ package org.egov.noc.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.egov.common.exception.InvalidTenantIdException;
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.noc.config.NOCConfiguration;
 import org.egov.noc.producer.Producer;
 import org.egov.noc.repository.builder.NocQueryBuilder;
 import org.egov.noc.repository.rowmapper.NocRowMapper;
+import org.egov.noc.util.NOCConstants;
 import org.egov.noc.web.model.Noc;
 import org.egov.noc.web.model.NocRequest;
 import org.egov.noc.web.model.NocSearchCriteria;
+import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class NOCRepository {
-	
-	@Autowired
-	private Producer producer;
-	
-	@Autowired
-	private NOCConfiguration config;	
 
-	@Autowired
-	private NocQueryBuilder queryBuilder;
+    @Autowired
+    private Producer producer;
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private NOCConfiguration config;
 
-	@Autowired
-	private NocRowMapper rowMapper;
-	
-	/**
-	 * push the nocRequest object to the producer on the save topic
-	 * @param nocRequest
-	 */
-	public void save(NocRequest nocRequest) {
-		producer.push(config.getSaveTopic(), nocRequest);
-	}
-	
-	/**
-	 * pushes the nocRequest object to updateTopic if stateupdatable else to update workflow topic
-	 * @param nocRequest
-	 * @param isStateUpdatable
-	 */
-	public void update(NocRequest nocRequest, boolean isStateUpdatable) {		
-		if (isStateUpdatable) {
-			producer.push(config.getUpdateTopic(), nocRequest);
-		} else {
-		    producer.push(config.getUpdateWorkflowTopic(), nocRequest);
-		}
-	}
-	/**
-	 * using the queryBulider query the data on applying the search criteria and return the data 
-	 * parsing throw row mapper
-	 * @param criteria
-	 * @return
-	 */
-	public List<Noc> getNocData(NocSearchCriteria criteria) {
-		List<Object> preparedStmtList = new ArrayList<>();
-		String query = queryBuilder.getNocSearchQuery(criteria, preparedStmtList);
-		List<Noc> nocList = jdbcTemplate.query(query, preparedStmtList.toArray(), rowMapper);
-		return nocList;
-	}
+    @Autowired
+    private NocQueryBuilder queryBuilder;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NocRowMapper rowMapper;
+
+    @Autowired
+    private MultiStateInstanceUtil centralInstanceUtil;
+
+    /**
+     * push the nocRequest object to the producer on the save topic
+     * @param nocRequest
+     */
+    public void save(NocRequest nocRequest) {
+        producer.push(nocRequest.getNoc().getTenantId(), config.getSaveTopic(), nocRequest);
+    }
+
+    /**
+     * pushes the nocRequest object to updateTopic if stateupdatable else to update workflow topic
+     * @param nocRequest
+     * @param isStateUpdatable
+     */
+    public void update(NocRequest nocRequest, boolean isStateUpdatable) {
+        String tenantId = nocRequest.getNoc().getTenantId();
+        if (isStateUpdatable) {
+            producer.push(tenantId, config.getUpdateTopic(), nocRequest);
+        } else {
+            producer.push(tenantId, config.getUpdateWorkflowTopic(), nocRequest);
+        }
+    }
+
+    /**
+     * using the queryBulider query the data on applying the search criteria and return the data parsing throw row mapper
+     * @param criteria
+     * @return
+     */
+    public List<Noc> getNocData(NocSearchCriteria criteria) {
+        List<Object> preparedStmtList = new ArrayList<>();
+        String query = queryBuilder.getNocSearchQuery(criteria, preparedStmtList);
+        try {
+            query = centralInstanceUtil.replaceSchemaPlaceholder(query, criteria.getTenantId());
+        } catch (InvalidTenantIdException e) {
+            throw new CustomException(NOCConstants.EG_NOC_AS_TENANTID_ERROR,
+                    "TenantId length is not sufficient to replace query schema in a multi state instance");
+        }
+        return jdbcTemplate.query(query, preparedStmtList.toArray(), rowMapper);
+    }
 
 }
--- municipal-services/noc-services/src/main/java/org/egov/noc/util/NotificationUtil.java
+++ municipal-services/noc-services/src/main/java/org/egov/noc/util/NotificationUtil.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.noc.config.NOCConfiguration;
 import org.egov.noc.producer.Producer;
 import org.egov.noc.repository.ServiceRequestRepository;
@@ -29,175 +30,163 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NotificationUtil {
 
-	@Autowired
-	private NOCConfiguration config;
+    @Autowired
+    private NOCConfiguration config;
 
-	@Autowired
-	private Producer producer;
+    @Autowired
+    private Producer producer;
 
-	@Autowired
-	private ServiceRequestRepository serviceRequestRepository;
+    @Autowired
+    private ServiceRequestRepository serviceRequestRepository;
 
-	
+    @Autowired
+    private MultiStateInstanceUtil centralInstanceUtil;
 
-	/**
-	 * Send the SMSRequest on the SMSNotification kafka topic
-	 * 
-	 * @param smsRequestList
-	 *            The list of SMSRequest to be sent
-	 */
-	public void sendSMS(List<SMSRequest> smsRequestList, boolean isSMSEnabled) {
-		if (isSMSEnabled) {
-			if (CollectionUtils.isEmpty(smsRequestList))
-				log.info("Messages from localization couldn't be fetched!");
-			for (SMSRequest smsRequest : smsRequestList) {
-				producer.push(config.getSmsNotifTopic(), smsRequest);
-				log.info("MobileNumber: " + smsRequest.getMobileNumber() + " Messages: " + smsRequest.getMessage());
-			}
-		}
-	}
+    /**
+     * Send the SMSRequest on the SMSNotification kafka topic
+     * 
+     * @param smsRequestList The list of SMSRequest to be sent
+     */
+    public void sendSMS(String tenantId, List<SMSRequest> smsRequestList, boolean isSMSEnabled) {
+        if (isSMSEnabled) {
+            if (CollectionUtils.isEmpty(smsRequestList))
+                log.info("Messages from localization couldn't be fetched!");
+            for (SMSRequest smsRequest : smsRequestList) {
+                producer.push(tenantId, config.getSmsNotifTopic(), smsRequest);
+                log.info("MobileNumber: " + smsRequest.getMobileNumber() + " Messages: " + smsRequest.getMessage());
+            }
+        }
+    }
 
-	/**
-	 * Creates sms request for the each owners
-	 * 
-	 * @param message
-	 *            The message for the specific noc
-	 * @param mobileNumberToOwnerName
-	 *            Map of mobileNumber to OwnerName
-	 * @return List of SMSRequest
-	 */
-	public List<SMSRequest> createSMSRequest(String message, Map<String, String> mobileNumberToOwner) {
-		List<SMSRequest> smsRequest = new LinkedList<>();
-		for (Map.Entry<String, String> entryset : mobileNumberToOwner.entrySet()) {
-			smsRequest.add(new SMSRequest(entryset.getKey(), message));
-		}
-		return smsRequest;
-	}
+    /**
+     * Creates sms request for the each owners
+     * 
+     * @param message The message for the specific noc
+     * @param mobileNumberToOwnerName Map of mobileNumber to OwnerName
+     * @return List of SMSRequest
+     */
+    public List<SMSRequest> createSMSRequest(String message, Map<String, String> mobileNumberToOwner) {
+        List<SMSRequest> smsRequest = new LinkedList<>();
+        for (Map.Entry<String, String> entryset : mobileNumberToOwner.entrySet()) {
+            smsRequest.add(new SMSRequest(entryset.getKey(), message));
+        }
+        return smsRequest;
+    }
 
-	/**
-	 * Returns the uri for the localization call
-	 * 
-	 * @param tenantId
-	 *            TenantId of the propertyRequest
-	 * @return The uri for localization search call
-	 */
-	public StringBuilder getUri(String tenantId, RequestInfo requestInfo) {
-		if (config.getIsLocalizationStateLevel())
-			tenantId = tenantId.split("\\.")[0];
-		String locale = "en_IN";
-		if (!StringUtils.isEmpty(requestInfo.getMsgId()) && requestInfo.getMsgId().split("|").length >= 2)
-			locale = requestInfo.getMsgId().split("\\|")[1];
+    /**
+     * Returns the uri for the localization call
+     * 
+     * @param tenantId TenantId of the propertyRequest
+     * @return The uri for localization search call
+     */
+    public StringBuilder getUri(String tenantId, RequestInfo requestInfo) {
+        if (Boolean.TRUE.equals(config.getIsLocalizationStateLevel()))
+            tenantId = centralInstanceUtil.getStateLevelTenant(tenantId);
+        String locale = "en_IN";
+        if (!StringUtils.isEmpty(requestInfo.getMsgId()) && requestInfo.getMsgId().split("|").length >= 2)
+            locale = requestInfo.getMsgId().split("\\|")[1];
 
-		StringBuilder uri = new StringBuilder();
-		uri.append(config.getLocalizationHost()).append(config.getLocalizationContextPath())
-				.append(config.getLocalizationSearchEndpoint()).append("?").append("locale=").append(locale)
-				.append("&tenantId=").append(tenantId).append("&module=").append(NOCConstants.SEARCH_MODULE);
-		return uri;
-	}
+        StringBuilder uri = new StringBuilder();
+        uri.append(config.getLocalizationHost()).append(config.getLocalizationContextPath())
+                .append(config.getLocalizationSearchEndpoint()).append("?").append("locale=").append(locale)
+                .append("&tenantId=").append(tenantId).append("&module=").append(NOCConstants.SEARCH_MODULE);
+        return uri;
+    }
 
-	/**
-	 * Fetches messages from localization service
-	 * 
-	 * @param tenantId
-	 *            tenantId of the NOC
-	 * @param requestInfo
-	 *            The requestInfo of the request
-	 * @return Localization messages for the module
-	 */
-	@SuppressWarnings("rawtypes")
-	public String getLocalizationMessages(String tenantId, RequestInfo requestInfo) {
-		LinkedHashMap responseMap = (LinkedHashMap) serviceRequestRepository.fetchResult(getUri(tenantId, requestInfo),
-				requestInfo);
-		String jsonString = new JSONObject(responseMap).toString();
-		return jsonString;
-	}
+    /**
+     * Fetches messages from localization service
+     * 
+     * @param tenantId tenantId of the NOC
+     * @param requestInfo The requestInfo of the request
+     * @return Localization messages for the module
+     */
+    @SuppressWarnings("rawtypes")
+    public String getLocalizationMessages(String tenantId, RequestInfo requestInfo) {
+        LinkedHashMap responseMap = (LinkedHashMap) serviceRequestRepository.fetchResult(getUri(tenantId, requestInfo),
+                requestInfo);
+        return new JSONObject(responseMap).toString();
+    }
 
-	/**
-	 * Creates customized message based on noc
-	 * 
-	 * @param noc
-	 *            The noc for which message is to be sent
-	 * @param localizationMessage
-	 *            The messages from localization
-	 * @return customized message based on noc
-	 */
-	public String getCustomizedMsg(RequestInfo requestInfo, Noc noc, String localizationMessage) {
-		String message = null, messageTemplate;
-		String messageCode;
-		if(noc.getWorkflow() == null)
-			messageCode = noc.getWorkflow() + "_" + noc.getApplicationStatus();
-		else 
-			messageCode = noc.getWorkflow().getAction() + "_" + noc.getApplicationStatus();
-		switch (messageCode) {
-		case ACTION_STATUS_CREATED:
-			messageTemplate = getMessageTemplate(messageCode, localizationMessage);
-			if (!StringUtils.isEmpty(messageTemplate))
-				message = getInitiatedMsg(noc, messageTemplate);
-			break;
-		case ACTION_STATUS_INITIATED:
-			messageTemplate = getMessageTemplate(messageCode, localizationMessage);
-			if (!StringUtils.isEmpty(messageTemplate))
-				message = getInitiatedMsg(noc, messageTemplate);
-			break;
-		case ACTION_STATUS_APPROVED:
-			messageTemplate = getMessageTemplate(messageCode, localizationMessage);
-			if (!StringUtils.isEmpty(messageTemplate))
-				message = getInitiatedMsg(noc, messageTemplate);
-			break;
-		case ACTION_STATUS_REJECTED:
-			messageTemplate = getMessageTemplate(messageCode, localizationMessage);
-			if (!StringUtils.isEmpty(messageTemplate))
-				message = getInitiatedMsg(noc, messageTemplate);
-			break;
-		}
-		return message;
-	}
+    /**
+     * Creates customized message based on noc
+     * 
+     * @param noc The noc for which message is to be sent
+     * @param localizationMessage The messages from localization
+     * @return customized message based on noc
+     */
+    public String getCustomizedMsg(RequestInfo requestInfo, Noc noc, String localizationMessage) {
+        String message = null, messageTemplate;
+        String messageCode;
+        if (noc.getWorkflow() == null)
+            messageCode = noc.getWorkflow() + "_" + noc.getApplicationStatus();
+        else
+            messageCode = noc.getWorkflow().getAction() + "_" + noc.getApplicationStatus();
+        switch (messageCode) {
+        case ACTION_STATUS_CREATED:
+            messageTemplate = getMessageTemplate(messageCode, localizationMessage);
+            if (!StringUtils.isEmpty(messageTemplate))
+                message = getInitiatedMsg(noc, messageTemplate);
+            break;
+        case ACTION_STATUS_INITIATED:
+            messageTemplate = getMessageTemplate(messageCode, localizationMessage);
+            if (!StringUtils.isEmpty(messageTemplate))
+                message = getInitiatedMsg(noc, messageTemplate);
+            break;
+        case ACTION_STATUS_APPROVED:
+            messageTemplate = getMessageTemplate(messageCode, localizationMessage);
+            if (!StringUtils.isEmpty(messageTemplate))
+                message = getInitiatedMsg(noc, messageTemplate);
+            break;
+        case ACTION_STATUS_REJECTED:
+            messageTemplate = getMessageTemplate(messageCode, localizationMessage);
+            if (!StringUtils.isEmpty(messageTemplate))
+                message = getInitiatedMsg(noc, messageTemplate);
+            break;
+        }
+        return message;
+    }
 
-	/**
-	 * Extracts message for the specific code
-	 * 
-	 * @param notificationCode
-	 *            The code for which message is required
-	 * @param localizationMessage
-	 *            The localization messages
-	 * @return message for the specific code
-	 */
-	@SuppressWarnings("rawtypes")
-	public String getMessageTemplate(String notificationCode, String localizationMessage) {
-		String path = "$..messages[?(@.code==\"{}\")].message";
-		path = path.replace("{}", notificationCode);
-		String message = null;
-		try {
-			List data = JsonPath.parse(localizationMessage).read(path);
-			if (!CollectionUtils.isEmpty(data))
-				message = data.get(0).toString();
-			else
-				log.error("Fetching from localization failed with code " + notificationCode);
-		} catch (Exception e) {
-			log.warn("Fetching from localization failed", e);
-		}
-		return message;
-	}
+    /**
+     * Extracts message for the specific code
+     * 
+     * @param notificationCode The code for which message is required
+     * @param localizationMessage The localization messages
+     * @return message for the specific code
+     */
+    @SuppressWarnings("rawtypes")
+    public String getMessageTemplate(String notificationCode, String localizationMessage) {
+        String path = "$..messages[?(@.code==\"{}\")].message";
+        path = path.replace("{}", notificationCode);
+        String message = null;
+        try {
+            List data = JsonPath.parse(localizationMessage).read(path);
+            if (!CollectionUtils.isEmpty(data))
+                message = data.get(0).toString();
+            else
+                log.error("Fetching from localization failed with code " + notificationCode);
+        } catch (Exception e) {
+            log.warn("Fetching from localization failed", e);
+        }
+        return message;
+    }
 
-	/**
-	 * Creates customized message for initiate
-	 * 
-	 * @param noc
-	 *            tenantId of the noc
-	 * @param message
-	 *            Message from localization for initiate
-	 * @return customized message for initiate
-	 */
-	private String getInitiatedMsg(Noc noc, String message) {
-		String type = null;
-		if(noc.getNocType().equalsIgnoreCase(NOCConstants.FIRE_NOC_TYPE)){
-			type = "Fire";
-		}else{
-			type = "AAI";
-		}
-		message = message.replace("<1>", type);
-		message = message.replace("<2>", noc.getApplicationNo());
-		return message;
-	}
+    /**
+     * Creates customized message for initiate
+     * 
+     * @param noc tenantId of the noc
+     * @param message Message from localization for initiate
+     * @return customized message for initiate
+     */
+    private String getInitiatedMsg(Noc noc, String message) {
+        String type = null;
+        if (noc.getNocType().equalsIgnoreCase(NOCConstants.FIRE_NOC_TYPE)) {
+            type = "Fire";
+        } else {
+            type = "AAI";
+        }
+        message = message.replace("<1>", type);
+        message = message.replace("<2>", noc.getApplicationNo());
+        return message;
+    }
 
 }
