a3f72f898acdc65c1fa2a8e0cf31c6173b3c167b
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-02-02 11:47:36 +0530

    RAIN-3800 BPA calculator single instance changes

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/kafka/broker/BPACalculatorProducer.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/kafka/broker/BPACalculatorProducer.java
@@ -1,22 +1,31 @@
 package org.egov.bpa.calculator.kafka.broker;
 
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class BPACalculatorProducer {
 
 	@Autowired
-	private CustomKafkaTemplate kafkaTemplate;
+	private CustomKafkaTemplate<String, Object> kafkaTemplate;
+	
+	@Autowired
+        private MultiStateInstanceUtil centralInstanceUtil;
 	
 	/**
 	 * Listener method to push records to kafka queue.
 	 * @param topic The kafka topic to push to
 	 * @param value The object to be pushed
 	 */
-	public void push(String topic, Object value) {
-		kafkaTemplate.send(topic, value);
-	}
+        public void push(String tenantId, String topic, Object value) {
+            String updatedTopic = centralInstanceUtil.getStateSpecificTopicName(tenantId, topic);
+            log.info("The Kafka topic for the tenantId : " + tenantId + " is : " + updatedTopic);
+            kafkaTemplate.send(updatedTopic, value);
+        }
 
 }
