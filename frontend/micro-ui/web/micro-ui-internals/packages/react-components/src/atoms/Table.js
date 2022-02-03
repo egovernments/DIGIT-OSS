15e5332c096a9ec81bd93c7f4ca4e0e673ad4566
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-02-03 19:18:38 +0530

    RAIN-5229 : DSS Table pagination issue fixed

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Table.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Table.js
@@ -2,22 +2,7 @@ import React, { useEffect } from "react";
 import { useGlobalFilter, usePagination, useRowSelect, useSortBy, useTable } from "react-table";
 import { ArrowBack, ArrowForward, ArrowToFirst, ArrowToLast, SortDown, SortUp } from "./svgindex";
 
-// const IndeterminateCheckbox = React.forwardRef(({ indeterminate, ...rest }, ref) => {
-//   const defaultRef = React.useRef();
-//   const resolvedRef = ref || defaultRef;
-//   React.useEffect(() => {
-//     resolvedRef.current.indeterminate = indeterminate;
-//   }, [resolvedRef, indeterminate]);
-
-//   return (
-//     <React.Fragment>
-//       <input type="checkbox" ref={resolvedRef} {...rest} />
-//       {/* <CheckBox ref={resolvedRef} {...rest} /> */}
-//     </React.Fragment>
-//   );
-// });
-
-const noop = () => { };
+const noop = () => {};
 
 const Table = ({
   className = "table",
@@ -86,26 +71,6 @@ const Table = ({
     useSortBy,
     usePagination,
     useRowSelect
-    // (hooks) => {
-    //   hooks.visibleColumns.push((columns) => [
-    //     // Let's make a column for selection
-    //     {
-    //       id: "selection",
-    //       // The header can use the table's getToggleAllRowsSelectedProps method
-    //       // to render a checkbox
-    //       Header: ({ getToggleAllRowsSelectedProps }) => <div>{<IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />}</div>,
-    //       // The cell can use the individual row's getToggleRowSelectedProps method
-    //       // to the render a checkbox
-    //       Cell: ({ row }) => (
-    //         <div>
-    //           <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
-    //           {/* <CheckBox /> */}
-    //         </div>
-    //       ),
-    //     },
-    //     ...columns,
-    //   ]);
-    // }
   );
 
   useEffect(() => {
@@ -120,7 +85,7 @@ const Table = ({
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{verticalAlign: "top"}} >
+                <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{ verticalAlign: "top" }}>
                   {column.render("Header")}
                   <span>{column.isSorted ? column.isSortedDesc ? <SortDown /> : <SortUp /> : ""}</span>
                 </th>
@@ -162,7 +127,7 @@ const Table = ({
           })}
         </tbody>
       </table>
-      {isPaginationRequired && 
+      {isPaginationRequired && (
         <div className="pagination">
           {`${t("CS_COMMON_ROWS_PER_PAGE")} :`}
           <select
@@ -179,24 +144,27 @@ const Table = ({
           </select>
           <span>
             <span>
-              {pageIndex * pageSizeLimit + 1}
+              {pageIndex * pageSize + 1}
               {"-"}
-              {manualPagination ?
-                (currentPage + 1) * pageSizeLimit > totalRecords ? totalRecords : (currentPage + 1) * pageSizeLimit :
-                (pageIndex + 1) * pageSize > rows.length ? rows.length : (pageIndex + 1) * pageSizeLimit
-              }{" "}
+              {manualPagination
+                ? (currentPage + 1) * pageSizeLimit > totalRecords
+                  ? totalRecords
+                  : (currentPage + 1) * pageSizeLimit
+                : (pageIndex * pageSize + page?.length)}{" "}
               {/* {(pageIndex + 1) * pageSizeLimit > rows.length ? rows.length : (pageIndex + 1) * pageSizeLimit}{" "} */}
               {totalRecords ? `of ${manualPagination ? totalRecords : rows.length}` : ""}
             </span>{" "}
           </span>
           {/* to go to first and last page we need to do a manual pagination , it can be updated later*/}
-          {canPreviousPage && manualPagination && onFirstPage && <ArrowToFirst onClick={() => (manualPagination && onFirstPage())} className={"cp"} />}
+          {canPreviousPage && manualPagination && onFirstPage && <ArrowToFirst onClick={() => manualPagination && onFirstPage()} className={"cp"} />}
           {canPreviousPage && <ArrowBack onClick={() => (manualPagination ? onPrevPage() : previousPage())} className={"cp"} />}
           {canNextPage && <ArrowForward onClick={() => (manualPagination ? onNextPage() : nextPage())} className={"cp"} />}
-          {rows.length == pageSizeLimit && canNextPage && manualPagination && onLastPage && <ArrowToLast onClick={() => (manualPagination && onLastPage())} className={"cp"} />}
+          {rows.length == pageSizeLimit && canNextPage && manualPagination && onLastPage && (
+            <ArrowToLast onClick={() => manualPagination && onLastPage()} className={"cp"} />
+          )}
           {/* to go to first and last page we need to do a manual pagination , it can be updated later*/}
         </div>
-      }
+      )}
     </React.Fragment>
   );
 };
