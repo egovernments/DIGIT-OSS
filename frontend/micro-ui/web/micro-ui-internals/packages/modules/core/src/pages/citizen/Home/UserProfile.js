e8eaab7b160a71e34a0f1249e4f99399ca1ad52c
Suraj Gupta <suraj.gupta@egovernments.org>
2022-02-07 20:32:34 +0530

    add form fields

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/UserProfile.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/UserProfile.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { FormStep, TextInput, CardLabel, RadioButtons, LabelFieldPair, Dropdown, Menu, MobileNumber } from "@egovernments/digit-ui-react-components";
 
 const userProfile = () => {
-    return <p> User Profile </p>;
+    return (
+        <><LabelFieldPair>
+         <CardLabel style={editScreen ? { color: "#B1B4B6" } : {}}>{`${t("PT_OWNER_NAME")}`}</CardLabel>
+          <div className="field">
+            <TextInput
+              t={t}
+              type={"text"}
+              isMandatory={false}
+              name="name"
+              value={name}
+              onChange={setOwnerName}
+              {...(validation = {
+                isRequired: true,
+                pattern: "^[a-zA-Z-.`' ]*$",
+                type: "tel",
+                title: t("PT_NAME_ERROR_MESSAGE"),
+              })}
+              disable={editScreen}
+            /></div>
+            </LabelFieldPair>
+            <LabelFieldPair>
+          <CardLabel style={editScreen ? { color: "#B1B4B6" } : {}}>{`${t("PT_FORM3_GENDER")}`}</CardLabel>
+          <Dropdown
+            className="form-field"
+            selected={gender?.length === 1 ? gender[0] : gender}
+            disable={gender?.length === 1 || editScreen}
+            option={menu}
+            select={setGenderName}
+            optionKey="code"
+            t={t}
+            name="gender"
+          />
+        </LabelFieldPair>
+        <LabelFieldPair>
+          <CardLabel style={editScreen ? { color: "#B1B4B6" } : {}}>{`${t("PT_OWNER_EMAIL")}`}</CardLabel>
+          <div className="field">
+            <TextInput
+              t={t}
+              type={"email"}
+              isMandatory={false}
+              optionKey="i18nKey"
+              name="email"
+              value={email}
+              onChange={setOwnerEmail}
+              disable={editScreen}
+            />
+          </div>
+        </LabelFieldPair>
+        </>
+    )
 }
 
 export default userProfile
