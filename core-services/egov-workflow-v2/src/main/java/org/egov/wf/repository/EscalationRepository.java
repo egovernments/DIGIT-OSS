a39e5b15068285e04769a19fedee26dd59f4522a
talele08 <talele.aniket@gmail.com>
2022-02-08 11:17:55 +0530

    added MultiStateInstanceUtil in wf

--- core-services/egov-workflow-v2/src/main/java/org/egov/wf/repository/EscalationRepository.java
+++ core-services/egov-workflow-v2/src/main/java/org/egov/wf/repository/EscalationRepository.java
@@ -2,6 +2,7 @@ package org.egov.wf.repository;
 
 
 import org.egov.wf.repository.querybuilder.EscalationQueryBuilder;
+import org.egov.wf.util.CommonUtil;
 import org.egov.wf.web.models.EscalationSearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,11 +22,16 @@ public class EscalationRepository {
 
     private EscalationQueryBuilder queryBuilder;
 
+    private CommonUtil commonUtil;
+
     @Autowired
-    public EscalationRepository(JdbcTemplate jdbcTemplate, EscalationQueryBuilder queryBuilder) {
+    public EscalationRepository(JdbcTemplate jdbcTemplate, EscalationQueryBuilder queryBuilder, CommonUtil commonUtil) {
         this.jdbcTemplate = jdbcTemplate;
         this.queryBuilder = queryBuilder;
+        this.commonUtil = commonUtil;
     }
+
+
 
 
     /**
@@ -37,6 +43,7 @@ public class EscalationRepository {
 
         List<Object> preparedStmtList = new ArrayList<>();
         String query = queryBuilder.getEscalationQuery(criteria, preparedStmtList);
+        query = commonUtil.replaceSchemaPlaceholder(query, criteria.getTenantId());
         List<String> businessIds = jdbcTemplate.query(query, preparedStmtList.toArray(),  new SingleColumnRowMapper<>(String.class));
         return  businessIds;
 
--- core-services/egov-workflow-v2/src/main/java/org/egov/wf/util/CommonUtil.java
+++ core-services/egov-workflow-v2/src/main/java/org/egov/wf/util/CommonUtil.java
@@ -1,11 +1,17 @@
 package org.egov.wf.util;
 
+import org.egov.common.utils.MultiStateInstanceUtil;
+import org.egov.tracer.model.CustomException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static org.egov.wf.util.WorkflowConstants.SCHEMA_REPLACE_STRING;
 
 @Component
 public class CommonUtil {
+
+
+    @Autowired
+    private MultiStateInstanceUtil multiStateInstanceUtil;
 
     /**
      * Method to fetch the state name from the tenantId
@@ -16,14 +22,16 @@ public class CommonUtil {
      */
     public String replaceSchemaPlaceholder(String query, String tenantId) {
 
-        String finalQuery = null;
-        if (tenantId.contains(".")) {
-            String schemaName = tenantId.split("\\.")[1];
-            finalQuery = query.replace(SCHEMA_REPLACE_STRING, schemaName);
-        } else {
-            finalQuery = query.replace(SCHEMA_REPLACE_STRING.concat("."), "");
+        String modifiedQuery = null;
+        try {
+            modifiedQuery = multiStateInstanceUtil.replaceSchemaPlaceholder(query, tenantId);
         }
-        return finalQuery;
+        catch (Exception e){
+            throw new CustomException("INVALID_TENANTID","The tenantId: "+tenantId+" is not valid.");
+        }
+
+        return modifiedQuery;
+
     }
 
 }
