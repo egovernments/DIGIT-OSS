28dd348a265e99f297aa58f43e8b246e4621bad7
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-02-08 12:15:06 +0530

    Read domain name from config

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrBpaRestService.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrBpaRestService.java
@@ -16,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @Service("edcrBpaRestService")
 public class EdcrBpaRestService {
 
+    private static final String COMMON_DOMAIN_NAME = "common.domain.name";
+    private static final String IS_ENVIRONMENT_CENTRAL_INSTANCE = "is.environment.central.instance";
     private static final String BPACHECKDEMOND = "%s/bpa/rest/stakeholder/check/demand-pending/{userId}";
     private static final String REDIRECTTOCOLLECTION = "%s/bpa/application/stakeholder/generate-bill/{userId}";
     private static final String STAKEHOLDER_VALIDATION_URL = "%s/bpa/rest/validate/stakeholder/{userId}";
@@ -26,14 +28,20 @@ public class EdcrBpaRestService {
 
     public Boolean checkAnyTaxIsPendingToCollectForStakeHolder(final Long userId, final HttpServletRequest request) {
         final RestTemplate restTemplate = new RestTemplate();
-        final String url = String.format(BPACHECKDEMOND, WebUtils.extractRequestDomainURL(request, false));
+        final String url = String.format(BPACHECKDEMOND,
+                WebUtils.extractRequestDomainURL(request, false,
+                        environmentSettings.getProperty(IS_ENVIRONMENT_CENTRAL_INSTANCE, Boolean.class),
+                        environmentSettings.getProperty(COMMON_DOMAIN_NAME)));
         Map<String, Boolean> checkPending = restTemplate.getForObject(url, Map.class, userId);
         return checkPending.get("pending") != null && checkPending.get("pending");
     }
 
     public String generateBillAndRedirectToCollection(final Long userId, final HttpServletRequest request) {
         final RestTemplate restTemplate = new RestTemplate();
-        final String url = String.format(REDIRECTTOCOLLECTION, WebUtils.extractRequestDomainURL(request, false));
+        final String url = String.format(REDIRECTTOCOLLECTION,
+                WebUtils.extractRequestDomainURL(request, false,
+                        environmentSettings.getProperty(IS_ENVIRONMENT_CENTRAL_INSTANCE, Boolean.class),
+                        environmentSettings.getProperty(COMMON_DOMAIN_NAME)));
         return restTemplate.getForObject(url, String.class, userId);
     }
 
@@ -46,13 +54,19 @@ public class EdcrBpaRestService {
             url = String.format(STAKEHOLDER_VALIDATION_URL,
                     environmentSettings.getProperty("stakeholder.domain.url", String.class));
         else
-            url = String.format(STAKEHOLDER_VALIDATION_URL, WebUtils.extractRequestDomainURL(request, false));
+            url = String.format(STAKEHOLDER_VALIDATION_URL,
+                    WebUtils.extractRequestDomainURL(request, false,
+                            environmentSettings.getProperty(IS_ENVIRONMENT_CENTRAL_INSTANCE, Boolean.class),
+                            environmentSettings.getProperty(COMMON_DOMAIN_NAME)));
         return restTemplate.getForObject(url, ErrorDetail.class, userId);
     }
 
     public List<String> getEdcrIntegratedServices(final HttpServletRequest request) {
         final RestTemplate restTemplate = new RestTemplate();
-        final String url = String.format(EDCR_SERVICES_URL, WebUtils.extractRequestDomainURL(request, false));
+        final String url = String.format(EDCR_SERVICES_URL,
+                WebUtils.extractRequestDomainURL(request, false,
+                        environmentSettings.getProperty(IS_ENVIRONMENT_CENTRAL_INSTANCE, Boolean.class),
+                        environmentSettings.getProperty(COMMON_DOMAIN_NAME)));
         return restTemplate.getForObject(url, List.class);
     }
 }
