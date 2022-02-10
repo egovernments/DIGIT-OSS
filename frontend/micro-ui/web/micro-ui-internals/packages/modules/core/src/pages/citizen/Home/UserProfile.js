979e9e4dd982d7e2267ac04794e651e968648c73
burhanj <burhan.j@egovernments.org>
2022-02-10 15:08:36 +0530

    RAIN-5230 Add missing async typo

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/UserProfile.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/UserProfile.js
@@ -15,7 +15,7 @@ const UserProfile = () => {
     );
   };
 
-    const updateProfile = () => {
+    const updateProfile = async () => {
       const requestData = {
         name: 'Test user'
       }
