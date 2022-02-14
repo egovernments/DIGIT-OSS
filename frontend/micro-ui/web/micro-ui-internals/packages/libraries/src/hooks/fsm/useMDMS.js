b9ad465faacc80ec375d0a2ea8d4149021f0d9c5
ayushgupta-eGov <94037835+ayushgupta-eGov@users.noreply.github.com>
2022-02-14 20:20:46 +0530

    SAN-849: Add citizen's choice for payment (#1552)

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/fsm/useMDMS.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/fsm/useMDMS.js
@@ -75,6 +75,10 @@ const useMDMS = (tenantId, moduleCode, type, config = {}, payload = []) => {
     return useQuery("FSM_FSTPO_REJECTION", () => MdmsService.getFSTPORejectionReason(tenantId, moduleCode, type), queryConfig);
   };
 
+  const usePaymentType = () => {
+    return useQuery("FSM_PAYMENT_TYPE", () => MdmsService.getFSMPaymentType(tenantId, moduleCode, type), queryConfig);
+  }; 
+
   switch (type) {
     case "SanitationType":
       return useSanitationType();
@@ -119,6 +123,8 @@ const useMDMS = (tenantId, moduleCode, type, config = {}, payload = []) => {
       return useGenderDetails();
     case "FSTPORejectionReason":
       return useFSTPORejectionReason();
+    case "PaymentType":
+      return usePaymentType();
   }
 };
 
