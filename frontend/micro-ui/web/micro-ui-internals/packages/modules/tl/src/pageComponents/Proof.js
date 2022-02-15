8c9aa32324f1c55bf88efdec7f428feae35a9c63
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2022-02-15 18:17:10 +0530

    RAIN-5591:TL : Added validation for supported document (#1561)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pageComponents/Proof.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pageComponents/Proof.js
@@ -8,6 +8,7 @@ const Proof = ({ t, config, onSelect, userType, formData }) => {
   const [file, setFile] = useState(formData?.owners?.documents?.OwnerPhotoProof);
   const [error, setError] = useState(null);
   const cityDetails = Digit.ULBService.getCurrentUlb();
+  const acceptFormat = ".jpg,.png,.pdf,.jpeg"
 
   const [dropdownValue, setDropdownValue] = useState(formData?.owners?.documents?.OwnerPhotoProof?.documentType || null);
   // let dropdownData = [];
@@ -53,7 +54,11 @@ const Proof = ({ t, config, onSelect, userType, formData }) => {
     (async () => {
       setError(null);
       if (file) {
-        if (file.size >= 2000000) {
+        if(!(acceptFormat.split(",").includes(`.${file.type.split("/").pop()}`)))
+        {
+          setError(t("PT_UPLOAD_FORMAT_NOT_SUPPORTED"));
+        }
+        else if (file.size >= 2000000) {
           setError(t("PT_MAXIMUM_UPLOAD_SIZE_EXCEEDED"));
         } else {
           try {
