b5cd4dedf2b271738dcf308a516adfe75ec0ca3e
burhanj <79191374+shoaibsyed7@users.noreply.github.com>
2022-02-15 12:42:02 +0530

    RAIN-5230- UploadDrawer Changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/ImageUpload/UploadDrawer.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/ImageUpload/UploadDrawer.js
@@ -1,25 +1,23 @@
-import React ,{useState,useEffect}from 'react'
-import{UploadFile} from '@egovernments/digit-ui-react-components';
-const scss={
-  height:"150px",
-  width:'100%'
-}
+import React, { useState, useEffect } from "react";
+import { UploadFile } from "@egovernments/digit-ui-react-components";
+const scss = {
+  height: "150px",
+  width: "100%",
+};
 
 function UploadDrawer({ setProfilePic }) {
   const [uploadedFile, setUploadedFile] = useState(null);
-  const [file,setFile] = useState("")
+  const [file, setFile] = useState("");
   const [error, setError] = useState(null);
 
-  const selectfile = (e) => { 
+  const selectfile = (e) => {
     setFile(e.target.files[0]);
-  }
-    
+  };
+
   // console.log("demo",uploadedFile)
-  const removeimg=()=>{
-    setUploadedFile(null)
-  
-    
-  }
+  const removeimg = () => {
+    setUploadedFile(null);
+  };
 
   useEffect(() => {
     (async () => {
@@ -29,9 +27,9 @@ function UploadDrawer({ setProfilePic }) {
           setError(t("PT_MAXIMUM_UPLOAD_SIZE_EXCEEDED"));
         } else {
           try {
-            const response = await Digit.UploadServices.Filestorage("citizen-profile", file, Digit.ULBService.getStateId());                
+            const response = await Digit.UploadServices.Filestorage("citizen-profile", file, Digit.ULBService.getStateId());
             if (response?.data?.files?.length > 0) {
-              const fileStoreId = response?.data?.files[0]?.fileStoreId; 
+              const fileStoreId = response?.data?.files[0]?.fileStoreId;
               setUploadedFile(fileStoreId);
               setProfilePic(fileStoreId);
             } else {
@@ -45,21 +43,40 @@ function UploadDrawer({ setProfilePic }) {
       }
     })();
   }, [file]);
-  
+
   return (
-    <div style={{width:"100%",bottom:'0',height:'150px',justifyContent:"space-around",backgroundColor:"white"}}>
-      <div style={{width:"50%",float:"left"}}>
-      <UploadFile
-        extraStyleName={"propertyCreate"}
-        accept=".jpg,.png"
-        onUpload={selectfile}
-        // onDelete={() => {
-        //   setUploadedFile(null);}} />UploadDrawer
-      />
+    // <div style={{  filter:"blur(100px)",
+    // backgroundColor:"rgba(0, 0, 0, 0.5)",
+    // userSelect:"none",
+    // pointerEvents:"none"}}>
+    <div
+      style={{
+        width: "90%",
+        justifyContent: "center",
+        backgroundColor: "red",
+        alignItems: "center",
+        position: "absolute",
+        top: "450px",
+        left: "15px",
+        height: "10%",
+        zindex: "2",
+      }}
+    >
+      <div style={{ width: "50%", float: "left" }}>
+        <UploadFile
+          extraStyleName={"propertyCreate"}
+          accept=".jpg,.png"
+          onUpload={selectfile}
+          // onDelete={() => {
+          //   setUploadedFile(null);}} />UploadDrawer
+        />
       </div>
-      <div style={{width:"50%",float:"left",textAlign:"center",justifyContent:"center"}} ><button onClick={removeimg}>Remove</button></div>
+      <div style={{ width: "50%", float: "left", textAlign: "center", justifyContent: "center" }}>
+        <button onClick={removeimg}>Remove</button>
+      </div>
     </div>
-  )
+    // </div>
+  );
 }
 
-export default UploadDrawer
+export default UploadDrawer;
