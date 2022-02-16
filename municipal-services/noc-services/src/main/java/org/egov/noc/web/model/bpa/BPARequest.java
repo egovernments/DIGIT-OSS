be881f0376c02827e5bf9f31492ad0a32d2f8b9f
WalkingTree <lakshmi.sravani@egovernments.org>
2022-02-16 16:51:23 +0530

    Code changes by removing extra params/files.

--- municipal-services/noc-services/src/main/java/org/egov/noc/web/model/bpa/BPARequest.java
+++ municipal-services/noc-services/src/main/java/org/egov/noc/web/model/bpa/BPARequest.java
@@ -9,16 +9,12 @@ import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-/**
- * Contract class to receive request. Array of Property items  are used in case of create . Where as single Property item is used for update
- */
-@ApiModel(description = "Contract class to receive request. Array of Property items  are used in case of create . Where as single Property item is used for update")
+
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:52:32.717Z[GMT]")
 @NoArgsConstructor
--- municipal-services/noc-services/src/main/java/org/egov/noc/web/model/bpa/BPAResponse.java
+++ municipal-services/noc-services/src/main/java/org/egov/noc/web/model/bpa/BPAResponse.java
@@ -10,7 +10,6 @@ import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,10 +17,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * Contains the ResponseHeader and the created/updated property
- */
-@ApiModel(description = "Contains the ResponseHeader and the created/updated property")
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:52:32.717Z[GMT]")
 @NoArgsConstructor
