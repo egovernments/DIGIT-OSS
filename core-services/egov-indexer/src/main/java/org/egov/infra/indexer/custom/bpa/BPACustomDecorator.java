548e369c9c664f6d83ac984d3bd1314e696b5363
Priyanka-eGov <74049060+Priyanka-eGov@users.noreply.github.com>
2022-02-23 17:44:34 +0530

    RAIN-4645 Adding approved plot area in BPA Custom Indexer (#1729)
    * RAIN-4645 changes for plotArea in bpa custom indexer
    
    * RAIN-4645 changes for plotArea in bpa custom indexer

--- core-services/egov-indexer/src/main/java/org/egov/infra/indexer/custom/bpa/BPACustomDecorator.java
+++ core-services/egov-indexer/src/main/java/org/egov/infra/indexer/custom/bpa/BPACustomDecorator.java
@@ -1,5 +1,8 @@
 package org.egov.infra.indexer.custom.bpa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.Filter;
 import com.jayway.jsonpath.JsonPath;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +16,15 @@ import org.egov.mdms.model.MasterDetail;
 import org.egov.mdms.model.MdmsCriteria;
 import org.egov.mdms.model.MdmsCriteriaReq;
 import org.egov.mdms.model.ModuleDetail;
+import org.egov.tracer.model.CustomException;
+import org.egov.tracer.model.ServiceCallException;
+import org.json.JSONObject;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
@@ -30,11 +39,31 @@ public class BPACustomDecorator {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private ObjectMapper mapper;
+
 	@Value("${egov.mdms.host}")
 	private String mdmsHost;
 
 	@Value("${egov.mdms.search.endpoint}")
 	private String mdmsEndpoint;
+
+	// EDCR Service
+	@Value("${egov.edcr.host}")
+	private String edcrHost;
+
+	@Value("${egov.edcr.authtoken.endpoint}")
+	private String edcrAuthEndPoint;
+
+	@Value("${egov.edcr.getPlan.endpoint}")
+	private String getPlanEndPoint;
+
+	//BPA Service
+	@Value("${egov.bpa.host}")
+	private String bpaHost;
+
+	@Value("${egov.bpa.search.endpoint}")
+	private String bpaEndpoint;
 
 	/**
 	 * Transforms data
@@ -44,6 +73,16 @@ public class BPACustomDecorator {
 	 */
 	public EnrichedBPARequest transformData(BPARequest bpaRequest){
 		List<EnrichedUnit> enrichedUnitList = new ArrayList<>();
+        Double plotAreaApproved=null;
+
+		//fetching plotArea after approval based on approval number
+		if(bpaRequest.getBPA().getStatus().equals("APPROVED"))
+		{
+			log.info("Fetching Approved Plot Area ");
+			String edcrnumber = fetchPermitNumber(bpaRequest.getRequestInfo(),bpaRequest.getBPA());
+			plotAreaApproved = getEDCRDetails(edcrnumber,bpaRequest.getRequestInfo(),bpaRequest.getBPA());
+		}
+
 		for(Unit unit:bpaRequest.getBPA().getLandInfo().getUnit())
 		{
 			//Converting comma separated string value to set
@@ -85,6 +124,7 @@ public class BPACustomDecorator {
 				.unit(enrichedUnitList)
 				.additionalDetails(bpaRequest.getBPA().getLandInfo().getAdditionalDetails())
 				.auditDetails(bpaRequest.getBPA().getLandInfo().getAuditDetails())
+				.plotAreaApproved(plotAreaApproved)
 				.build();
 
 		BPA bpa = bpaRequest.getBPA();
@@ -115,6 +155,81 @@ public class BPACustomDecorator {
 
 		return enrichedBPARequest;
 	}
+
+	/**
+	 * fetch the bpa details from the approval number
+	 * @param requestInfo
+	 * @param bpa
+	 * @return
+	 */
+	public String fetchPermitNumber(RequestInfo requestInfo, BPA bpa)
+	{
+		StringBuilder uri = new StringBuilder(bpaHost);
+		uri.append(bpaEndpoint);
+		uri.append("?").append("tenantId=").append(bpa.getTenantId());
+		uri.append("&").append("approvalNo=").append(bpa.getApprovalNo());
+
+		Map<String, Object> apiRequest = new HashMap<>();
+		apiRequest.put("RequestInfo", requestInfo);
+		String edcrNumber="";
+
+		RequestInfo bpaRequestInfo = new RequestInfo();
+		BeanUtils.copyProperties(requestInfo, bpaRequestInfo);
+		LinkedHashMap responseMap = null;
+		try {
+			responseMap = (LinkedHashMap) fetchResult(uri,
+					new RequestInfoWrapper(bpaRequestInfo));
+		} catch (Exception e) {
+			log.error("Exception while fetching edcr number from bpa response ",e);
+		}
+
+		if (CollectionUtils.isEmpty(responseMap)) {
+			log.error("The response from BPA service is empty or null");
+			return "";
+		}
+
+		String jsonString = new JSONObject(responseMap).toString();
+		DocumentContext context = JsonPath.using(Configuration.defaultConfiguration()).parse(jsonString);
+		edcrNumber = context.read("BPA[0].edcrNumber");
+
+		return edcrNumber;
+	}
+
+	/**
+	 * fetch the edcr details from the bpa
+	 * @param requestInfo
+	 * @param bpa
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public Double getEDCRDetails(String edcrNo, RequestInfo requestInfo, BPA bpa) {
+		StringBuilder uri = new StringBuilder(edcrHost);
+		Double plotArea=null;
+
+		uri.append(getPlanEndPoint);
+		uri.append("?").append("tenantId=").append(bpa.getTenantId());
+		uri.append("&").append("edcrNumber=").append(edcrNo);
+
+		RequestInfo edcrRequestInfo = new RequestInfo();
+		BeanUtils.copyProperties(requestInfo, edcrRequestInfo);
+		LinkedHashMap responseMap = null;
+		try {
+			responseMap = (LinkedHashMap) fetchResult(uri,
+					new RequestInfoWrapper(edcrRequestInfo));
+		} catch (Exception e) {
+			log.error("Exception while fetching plot area from edcr response ",e);
+		}
+		if (CollectionUtils.isEmpty(responseMap)) {
+			log.error("The response from BPA service is empty or null");
+			return null;
+		}
+		String jsonString = new JSONObject(responseMap).toString();
+		DocumentContext context = JsonPath.using(Configuration.defaultConfiguration()).parse(jsonString);
+		plotArea = context.read("edcrDetail[0].planDetail.virtualBuilding.totalBuitUpArea");
+
+		return plotArea;
+	}
+
 
 	public Set<String> fetchOccupancyCode(RequestInfo requestInfo, String tenantId, String[] usageCategory){
 		Set<String> occupancyType = new HashSet<>();
@@ -213,5 +328,28 @@ public class BPACustomDecorator {
 		mdmsCriteriaReq.setRequestInfo(requestInfo);
 
 		return mdmsCriteriaReq;
+	}
+
+	/**
+	 * fetchResult form the different services based on the url and request object
+	 * @param uri
+	 * @param request
+	 * @return
+	 */
+	public Object fetchResult(StringBuilder uri, Object request) {
+		Object response = null;
+		log.debug("URI: " + uri.toString());
+		try {
+			log.debug("Request: " + mapper.writeValueAsString(request));
+			response = restTemplate.postForObject(uri.toString(), request, Map.class);
+		} catch (HttpClientErrorException e) {
+			log.error("External Service threw an Exception: ", e);
+			throw new ServiceCallException(e.getResponseBodyAsString());
+		} catch (Exception e) {
+			log.error("Exception while fetching from searcher: ", e);
+			throw new ServiceCallException(e.getMessage());
+		}
+
+		return response;
 	}
 }
