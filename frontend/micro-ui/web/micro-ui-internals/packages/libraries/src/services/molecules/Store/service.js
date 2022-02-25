bc1fa9fdd612741d3db4fc73e657499a4d2b1006
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2022-02-25 15:34:42 +0530

    RAIN-5753:: All Tenants should display in dropdown (#1766)

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/Store/service.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/Store/service.js
@@ -83,8 +83,9 @@ export const StoreService = {
       .flat()
       .reduce((unique, ele) => (unique.find((item) => item.code === ele.code) ? unique : [...unique, ele]), []);
     initData.tenants = MdmsRes?.tenant?.tenants
-      .filter((item) => !!moduleTenants.find((mt) => mt.code === item.code))
-      .map((tenant) => ({ i18nKey: `TENANT_TENANTS_${tenant.code.replace(".", "_").toUpperCase()}`, ...tenant }));
+         .map((tenant) => ({ i18nKey: `TENANT_TENANTS_${tenant.code.replace(".", "_").toUpperCase()}`, ...tenant }));
+      // .filter((item) => !!moduleTenants.find((mt) => mt.code === item.code))
+      // .map((tenant) => ({ i18nKey: `TENANT_TENANTS_${tenant.code.replace(".", "_").toUpperCase()}`, ...tenant }));
 
     await LocalizationService.getLocale({
       modules: [
