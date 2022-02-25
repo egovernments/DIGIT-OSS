ae074a6cd5badc4c0e62c7a30256c38a2a01b2ca
Nipun Arora <aroranipun1@gmail.com>
2022-02-25 10:52:56 +0530

    Update SurveyResults.js
    getting both surveydata and responses data in the surveResults.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/engagement/src/pages/employee/CitizenSurveys/SurveyResults.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/engagement/src/pages/employee/CitizenSurveys/SurveyResults.js
@@ -3,42 +3,77 @@ import { useHistory, useParams } from "react-router-dom";
 import CitizenSurveyForm from "../../../components/Surveys/CitizenSurveyForm";
 import { useQueryClient } from "react-query";
 import { ActionBar, Card, SubmitBar, Menu,Loader } from "@egovernments/digit-ui-react-components";
+import { format } from "date-fns";
+
+const TypeAnswerEnum = {
+  SHORT_ANSWER_TYPE: "Short Answer",
+  LONG_ANSWER_TYPE: "Paragraph",
+  MULTIPLE_ANSWER_TYPE: "Multiple Choice",
+  CHECKBOX_ANSWER_TYPE: "Check Boxes",
+  DATE_ANSWER_TYPE: "Date",
+  TIME_ANSWER_TYPE: "Time",
+};
 
 const SurveyResults = () => {
    
-    const surveyData = location?.state;
+    
     const params = useParams();
-    //console.log(surveyData);
-    // const tenantIds = Digit.SessionStorage.get("CITIZEN.COMMON.HOME.CITY")?.code;
     const mutation = Digit.Hooks.survey.useShowResults()
 
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
   
-  useEffect(() => {
-    const onSuccess = () => {
-      queryClient.clear();
-    };
-    mutation.mutate({
-      surveyId:params.id
-    }, {
-      onSuccess,
-    });
-  }, []);
+    useEffect(() => {
+        const onSuccess = () => {
+        queryClient.clear();
+        };
+        mutation.mutate({
+        surveyId:params.id
+        }, {
+        onSuccess,
+        });
+    }, []);
+    const tenantId = Digit.ULBService.getCurrentTenantId();
 
- // console.log(mutation.data);
+    const { isLoading, data: surveyData } = Digit.Hooks.survey.useSearch(
+    { tenantIds: tenantId, uuid: params.id },
+    {
+      select: (data) => {
+        const surveyObj = data?.Surveys?.[0];
+        return {
+          //tenantIds: { code: surveyObj.tenantId },
+          uuid: surveyObj.uuid,
+          title: surveyObj.title,
+          description: surveyObj.description,
+          collectCitizenInfo: { code: surveyObj.collectCitizenInfo },
+          fromDate: format(new Date(surveyObj.startDate), "yyyy-MM-dd"),
+          toDate: format(new Date(surveyObj.endDate), "yyyy-MM-dd"),
+          fromTime: format(new Date(surveyObj.startDate), "hh:mm"),
+          toTime: format(new Date(surveyObj.endDate), "hh:mm"),
+          questions: surveyObj.questions.map(({ questionStatement, type, required, options, uuid, surveyId }) => ({
+            questionStatement,
+            type: TypeAnswerEnum[type],
+            required,
+            options,
+            uuid,
+            surveyId
+          })),
+          status: surveyObj.status,
+        };
+      },
+    }
+  );
+    if(surveyData) console.log(surveyData,"surveyData");
+    if(mutation.isSuccess) console.log(mutation.data,"mutation");
 
-   if (mutation.isLoading && !mutation.isIdle) {
-    return <Loader />;
-  }
-  
-    if(mutation.isSuccess) return <div>Data fetched successfully</div>
+    if(isLoading || (mutation.isLoading && !mutation.isIdle)) return <Loader />
     if(mutation.isError) return <div>An error occured...</div>
-   
-   
-    // return <CitizenSurveyForm surveyData={surveyData} submitDisabled={true} formdisabled={true} formDefaultValues={{}} />
+    
+    // if (isLoading) return <Loader />;
+    // else console.log(surveyData);
 
-    return <div>Hello World</div>
-
+  return <div>Hello World</div>
+//    return <CitizenSurveyForm surveyData={surveyData} submitDisabled={true} formdisabled={true} formDefaultValues={{}} />
 }
 
 export default SurveyResults
+
