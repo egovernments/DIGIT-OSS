d42ad4a2d55705266b7f848f1622aabf04215e3e
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-02-26 13:10:49 +0530

    Update getPDFHeaderDetails.js

--- frontend/mono-ui/web/dss-dashboard/src/actions/getPDFHeaderDetails.js
+++ frontend/mono-ui/web/dss-dashboard/src/actions/getPDFHeaderDetails.js
@@ -3,7 +3,7 @@ import { getTenantId } from '../utils/commons';
 export default function getPDFHeaderDetails(mdmsData) {
   let returnObj = {logo: '',headerText: ''};
   if(!_.isEmpty(mdmsData,true)){
-    returnObj.logo = mdmsData['tentantLogo'][`${getTenantId())}`];
+    returnObj.logo = mdmsData['tentantLogo'][`${getTenantId()}`];
     returnObj.headerText= mdmsData['tenantName'].toUpperCase() +" "+ mdmsData['corpName'].toUpperCase();    
   }
   return returnObj;
