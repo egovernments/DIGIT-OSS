391f09e36dd742881164c569811c3e70d332a700
talele08 <talele.aniket@gmail.com>
2022-02-28 10:24:33 +0530

    added MultiStateInstanceUtil in PGR

--- municipal-services/pgr-services/src/main/java/org/egov/pgr/util/PGRUtils.java
+++ municipal-services/pgr-services/src/main/java/org/egov/pgr/util/PGRUtils.java
@@ -1,7 +1,10 @@
 package org.egov.pgr.util;
 
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.pgr.web.models.AuditDetails;
 import org.egov.pgr.web.models.Service;
+import org.egov.tracer.model.CustomException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static org.egov.pgr.util.PGRConstants.SCHEMA_REPLACE_STRING;
@@ -10,6 +13,12 @@ import static org.egov.pgr.util.PGRConstants.SCHEMA_REPLACE_STRING;
 public class PGRUtils {
 
 
+    private MultiStateInstanceUtil multiStateInstanceUtil;
+
+    @Autowired
+    public PGRUtils(MultiStateInstanceUtil multiStateInstanceUtil) {
+        this.multiStateInstanceUtil = multiStateInstanceUtil;
+    }
 
     /**
      * Method to return auditDetails for create/update flows
@@ -37,11 +46,12 @@ public class PGRUtils {
     public String replaceSchemaPlaceholder(String query, String tenantId) {
 
         String finalQuery = null;
-        if (tenantId.contains(".")) {
-            String schemaName = tenantId.split("\\.")[1];
-            finalQuery = query.replace(SCHEMA_REPLACE_STRING, schemaName);
-        } else {
-            finalQuery = query.replace(SCHEMA_REPLACE_STRING.concat("."), "");
+
+        try {
+            finalQuery = multiStateInstanceUtil.replaceSchemaPlaceholder(query, tenantId);
+        }
+        catch (Exception e){
+            throw new CustomException("INVALID_TENANTID","Invalid tenantId: "+tenantId);
         }
         return finalQuery;
     }
