7702d3f1f0ceb69c720748523b6db25512b819a8
praneeth <praneeth.p@beehyv.com>
2022-02-28 17:43:39 +0530

    added smsbounce integration

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/models/Report.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/models/Report.java
@@ -1,4 +1,18 @@
 package org.egov.web.notification.sms.models;
 
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Report {
+    private String jobno;
+    private String mobilenumber;
+    private int messagestatus;
+    private String doneTime;
+    private String message;
+    private String receivestamp;
 }
