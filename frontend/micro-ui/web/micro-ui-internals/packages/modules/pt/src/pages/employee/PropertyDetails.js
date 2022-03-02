516cd2dbaf6fc1281a74461f058228154dd12fcd
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2022-03-02 13:40:29 +0530

    PT MUtation property details fix (#1791)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/PropertyDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/PropertyDetails.js
@@ -146,7 +146,7 @@ const PropertyDetails = () => {
   useEffect(() => {
     if (appDetailsToShow?.applicationDetails?.[0]?.values?.[1].title !== "PT_TOTAL_DUES") {
       appDetailsToShow?.applicationDetails?.unshift({
-        title: "",
+        title: " ",
         asSectionHeader: true,
         belowComponent: () => (
           <LinkLabel
