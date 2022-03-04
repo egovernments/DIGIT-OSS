3dc1e8bc13d5a62b968086c99c6d42b7060d0929
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-03-04 20:22:36 +0530

    RAIN-6316 : DSS UI/UX Audit changes for the landing screen (#1857)
    * DSS CARDS to have ndss and sdss
    
    * Removed those additonl download options in home screen
    
    * DSS UI/UX Audit changes for the landing screen

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dss/src/components/DSSCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dss/src/components/DSSCard.js
@@ -1,45 +1,75 @@
-import { EmployeeModuleCard, EventsIconSolid } from "@egovernments/digit-ui-react-components";
+import { ModuleCardFullWidth } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 const nationalScreenURLs = {
-  propertytax: { key: "national-propertytax",stateKey:"propertytax", label: "ACTION_TEST_PROPERTY_TAX" ,active:false,nActive:true},
-  tradelicense: { key: "national-tradelicense",stateKey:"tradelicense", label: "ACTION_TEST_TRADELICENSE" ,active:false,nActive:true},
-  pgr: { key: "national-pgr",stateKey:"pgr", label: "ACTION_TEST_PGR" ,active:false,nActive:true},
-  fsm: { key: "fsm",stateKey:"fsm", label: "ACTION_TEST_FSM" ,active:true,nActive:false},
-  mCollect: { key: "national-mcollect",stateKey:"mCollect", label: "ACTION_TEST_MCOLLECT"  ,active:true,nActive:true},
-  ws: { key: "national-ws",stateKey:"ws", label: "ACTION_TEST_WATER_&_SEWERAGE" ,active:true,nActive:true},
-  obps: { key: "nss-obps",stateKey:"obps", label: "CS_COMMON_OBPS",active:true,nActive:true },
-  noc: { key: "national-firenoc",stateKey:"noc", label: "ACTION_TEST_FIRE_NOC",active:true,nActive:true },
-  overview: { key: "national-overview",stateKey:"overview", label: "ACTION_TEST_OVERVIEW" ,active:false,nActive:false},
+  overview: { key: "national-overview", stateKey: "overview", label: "ACTION_TEST_OVERVIEW", active: false, nActive: true },
+  propertytax: { key: "national-propertytax", stateKey: "propertytax", label: "ACTION_TEST_PROPERTY_TAX", active: false, nActive: true },
+  tradelicense: { key: "national-tradelicense", stateKey: "tradelicense", label: "ACTION_TEST_TRADELICENSE", active: false, nActive: true },
+  pgr: { key: "national-pgr", stateKey: "pgr", label: "CS_COMMON_HOME_COMPLAINTS", active: false, nActive: true },
+  fsm: { key: "fsm", stateKey: "fsm", label: "CS_HOME_FSM_SERVICES", active: true, nActive: false },
+  mCollect: { key: "national-mcollect", stateKey: "mCollect", label: "ACTION_TEST_MCOLLECT", active: true, nActive: true },
+  ws: { key: "national-ws", stateKey: "ws", label: "ACTION_TEST_WATER_&_SEWERAGE", active: true, nActive: true },
+  obps: { key: "nss-obps", stateKey: "obps", label: "ACTION_TEST_BUILDING_PLAN_APPROVAL", active: true, nActive: true },
+  noc: { key: "national-firenoc", stateKey: "noc", label: "ACTION_TEST_FIRE_NOC", active: true, nActive: true },
 };
 
 export const checkCurrentScreen = () => {
-  const moduleName=Digit.Utils.dss.getCurrentModuleName();
+  const moduleName = Digit.Utils.dss.getCurrentModuleName();
   const nationalURLS = Object.keys(nationalScreenURLs).map((key) => nationalScreenURLs[key].key);
   return nationalURLS.some((e) => moduleName?.includes(e));
 };
 
-const DSSCard = () => {
-  const STADMIN = Digit.UserService.hasAccess("STADMIN");
+const NDSSCard = () => {
   const NATADMIN = Digit.UserService.hasAccess("NATADMIN");
   const { t } = useTranslation();
 
-  if (!STADMIN && !NATADMIN) {
+  if (!NATADMIN) {
     return null;
   }
 
-  let links = Object.values(nationalScreenURLs).filter(ele=>ele[NATADMIN?"nActive":"active"]==true).map((obj) => ({
-    label: t(obj?.label),
-    link: `/digit-ui/employee/dss/dashboard/${NATADMIN ? obj?.key : obj?.stateKey}`,
-  }));
+  let links = Object.values(nationalScreenURLs)
+    .filter((ele) => ele["nActive"] == true)
+    .map((obj) => ({
+      label: t(obj?.label),
+      link: `/digit-ui/employee/dss/dashboard/${obj?.key}`,
+    }));
 
   const propsForModuleCard = {
-    Icon: <EventsIconSolid />,
-    moduleName: NATADMIN ? t("ACTION_TEST_NATDASHBOARD") : t("ES_TITLE_DSS"),
+    headerStyle: { border: "none", height: "48px" },
+    moduleName: t("ACTION_TEST_NATDASHBOARD"),
+    subHeader: t("ACTION_TEST_NATDASHBOARD"),
+    subHeaderLink: `/employee/integration/dss/NURT_DASHBOARD}`,
+    className: "employeeCard card-home customEmployeeCard full-width-card full-employee-card-height",
     links: [...links],
   };
-  return <EmployeeModuleCard {...propsForModuleCard} />;
+  return <ModuleCardFullWidth {...propsForModuleCard} />;
 };
 
-export default DSSCard;
+const DSSCard = () => {
+  const STADMIN = Digit.UserService.hasAccess("STADMIN");
+  const { t } = useTranslation();
+
+  if (!STADMIN) {
+    return null;
+  }
+
+  let links = Object.values(nationalScreenURLs)
+    .filter((ele) => ele["active"] == true)
+    .map((obj) => ({
+      label: t(obj?.label),
+      link: `/digit-ui/employee/dss/dashboard/${obj?.stateKey}`,
+    }));
+
+  const propsForModuleCard = {
+    headerStyle: { border: "none", height: "48px" },
+    moduleName: t("ES_TITLE_DSS"),
+    subHeader: t("ACTION_TEST_SURE_DASHBOARD"),
+    subHeaderLink: `/employee/integration/dss/home}`,
+    className: "employeeCard card-home customEmployeeCard full-width-card full-employee-card-height",
+    links: [...links],
+  };
+  return <ModuleCardFullWidth {...propsForModuleCard} styles={{ width: "100%" }} />;
+};
+
+export { DSSCard, NDSSCard };
