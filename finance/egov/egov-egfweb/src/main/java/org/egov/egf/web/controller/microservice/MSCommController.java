a466a49fdfb149e188309017b90178f60e354565
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-03-07 15:25:00 +0530

    Change controller to restcontroller

--- finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/controller/microservice/MSCommController.java
+++ finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/controller/microservice/MSCommController.java
@@ -28,7 +28,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.session.data.redis.RedisOperationsSessionRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,9 +35,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
-@Controller
+@RestController
 @Validated
 public class MSCommController {
 	
