f76c4d04200d7b6e7b39a9ba059c4fee78e6170e
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-03-07 15:40:30 +0530

    Change authtoken read data path

--- utilities/egov-custom-consumer/src/main/java/org/egov/utils/JsonPathConstant.java
+++ utilities/egov-custom-consumer/src/main/java/org/egov/utils/JsonPathConstant.java
@@ -2,7 +2,7 @@ package org.egov.utils;
 
 public class JsonPathConstant {
 	
-	public static final String signOutAccessToken = "$.queryParamMap.access_token[0]";
+	public static final String signOutAccessToken = "$.request.access_token";
 	public static final String request = "$.request";
 	public static final String userInfo = "$.request.RequestInfo.userInfo";
 	public static final String requestInfo = "$.request.RequestInfo";
