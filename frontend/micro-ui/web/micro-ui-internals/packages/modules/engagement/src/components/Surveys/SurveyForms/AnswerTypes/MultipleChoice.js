bda377f7a8723c6139eeca4c6ea4005fb51e780d
Nipun Arora <aroranipun1@gmail.com>
2022-03-08 17:18:58 +0530

    Disabled the required fields

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/engagement/src/components/Surveys/SurveyForms/AnswerTypes/MultipleChoice.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/engagement/src/components/Surveys/SurveyForms/AnswerTypes/MultipleChoice.js
@@ -2,14 +2,14 @@ import { CloseSvg } from "@egovernments/digit-ui-react-components";
 import React, { useEffect, useMemo, useState } from "react";
 import { useDebounce } from "../../../../hooks/useDebounce";
 
-const MultipleChoice = ({ t, options = checkboxlist, updateOption, addOption, removeOption,createNewSurvey,isPartiallyEnabled }) => {
+const MultipleChoice = ({ t, options = checkboxlist, updateOption, addOption, removeOption,createNewSurvey,isPartiallyEnabled,formDisabled }) => {
   return (
     <div className="options_checkboxes">
       {options.map((title, index) => (
         <RadioButtonOption key={index} index={index} title={title} updateOption={updateOption} removeOption={removeOption} />
       ))}
       <div>
-        <button className="unstyled-button link" type="button" disabled={!isPartiallyEnabled && !createNewSurvey} onClick={() => addOption()}>
+        <button className="unstyled-button link" type="button" disabled={!createNewSurvey && formDisabled} onClick={() => addOption()}>
           {t("CS_COMMON_ADD_OPTION")}
         </button>
       </div>
