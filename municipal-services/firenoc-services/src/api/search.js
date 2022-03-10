a63cd35b686925ed84ce73db3f04f61825fcf144
phanikiran-wtt-egov <phani.kiran@egovernments.org>
2022-03-10 09:49:44 +0530

    RAIN-5770 adding pagination for search api for firenoc

--- municipal-services/firenoc-services/src/api/search.js
+++ municipal-services/firenoc-services/src/api/search.js
@@ -40,7 +40,7 @@ export const searchApiResponse = async (request, next = {}) => {
     });
     return;
   }
-  // console.log(queryObj);
+  console.log(queryObj);
   let text =
     "SELECT FN.uuid as FID,FN.tenantid,FN.fireNOCNumber,FN.provisionfirenocnumber,FN.oldfirenocnumber,FN.dateofapplied,FN.createdBy,FN.createdTime,FN.lastModifiedBy,FN.lastModifiedTime,FD.uuid as firenocdetailsid,FD.action,FD.applicationnumber,FD.fireNOCType,FD.applicationdate,FD.financialYear,FD.firestationid,FD.issuedDate,FD.validFrom,FD.validTo,FD.action,FD.status,FD.channel,FD.propertyid,FD.noofbuildings,FD.additionaldetail,FBA.uuid as puuid,FBA.doorno as pdoorno,FBA.latitude as platitude,FBA.longitude as plongitude,FBA.buildingName as pbuildingname,FBA.addressnumber as paddressnumber,FBA.pincode as ppincode,FBA.locality as plocality,FBA.city as pcity,FBA.street as pstreet,FB.uuid as buildingid ,FB.name as buildingname,FB.usagetype,FO.uuid as ownerid,FO.ownertype,FO.applicantcategory,FO.useruuid,FO.relationship,FUOM.uuid as uomuuid,FUOM.code,FUOM.value,FUOM.activeuom,FBD.uuid as documentuuid,FUOM.active,FBD.documentType,FBD.filestoreid,FBD.documentuid,FBD.createdby as documentCreatedBy,FBD.lastmodifiedby as documentLastModifiedBy,FBD.createdtime as documentCreatedTime,FBD.lastmodifiedtime as documentLastModifiedTime FROM eg_fn_firenoc FN JOIN eg_fn_firenocdetail FD ON (FN.uuid = FD.firenocuuid) JOIN eg_fn_address FBA ON (FD.uuid = FBA.firenocdetailsuuid) JOIN eg_fn_owner FO ON (FD.uuid = FO.firenocdetailsuuid) JOIN eg_fn_buidlings FB ON (FD.uuid = FB.firenocdetailsuuid) JOIN eg_fn_buildinguoms FUOM ON (FB.uuid = FUOM.buildinguuid) LEFT OUTER JOIN eg_fn_buildingdocuments FBD on(FB.uuid = FBD.buildinguuid)";
   // FBD.active=true AND FO.active=true AND FUOM.active=true AND";
@@ -174,6 +174,18 @@ export const searchApiResponse = async (request, next = {}) => {
     sqlQuery = `${sqlQuery} FN.createdtime >= ${queryObj.fromDate} ORDER BY FN.uuid`;
   } else if (!isEmpty(queryObj)) {
     sqlQuery = `${sqlQuery.substring(0, sqlQuery.length - 3)} ORDER BY FN.uuid`;
+  }
+
+  if (!isEmpty(queryObj) && ( queryObj.hasOwnProperty("limit" || queryObj.hasOwnProperty("offset")))) {
+      offset =0;
+      limit =10;
+      if( !queryObj.hasOwnProperty("offset") ){
+        offset = queryObj.offset;
+     }
+    if( !queryObj.hasOwnProperty("limit") ){
+      limit = queryObj.limit;
+   }
+    sqlQuery = `${sqlQuery} offset ${offset} limit ${limit}`;
   }
   //console.log("SQL QUery:" +sqlQuery);
   const dbResponse = await db.query(sqlQuery);
