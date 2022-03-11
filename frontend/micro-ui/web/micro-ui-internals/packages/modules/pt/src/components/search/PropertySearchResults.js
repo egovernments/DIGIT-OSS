9b4d8b1e1d49977e3bb181fbae70798b18b4a548
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-03-11 21:34:55 +0530

    RAIN-6409 : PT Search proeprty and pay cache issue fixed (#2000)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/components/search/PropertySearchResults.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/components/search/PropertySearchResults.js
@@ -17,7 +17,7 @@ const SearchPTID = ({ tenantId, t, payload, showToast, setShowToast,ptSearchConf
   const [selectedProperty, setSelectedProperty] = useState(null);
   const [ownerInvalidMobileNumberIndex, setOwnerInvalidMobileNumberIndex] = useState(0);
 
-  const { data, isLoading, error, isSuccess, billData } = Digit.Hooks.pt.usePropertySearchWithDue({
+  const { data, isLoading, error, isSuccess, billData ,revalidate} = Digit.Hooks.pt.usePropertySearchWithDue({
     tenantId,
     filters: searchQuery,
     configs: { enabled: Object.keys(payload).length > 0 ? true : false, retry: false, retryOnMount: false, staleTime: Infinity },
@@ -59,6 +59,7 @@ const SearchPTID = ({ tenantId, t, payload, showToast, setShowToast,ptSearchConf
       setShowModal(true);
       setSelectedProperty(val);
     } else {
+      revalidate();
       history.push(`/digit-ui/employee/payment/collect/PT/${val?.["propertyId"]}`)
     }
 
