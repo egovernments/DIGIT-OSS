985c15b1367d06fe51fdbf4247f1d9aa3f83e510
praneeth <praneeth.p@beehyv.com>
2022-03-15 12:23:56 +0530

    [core-87] added hash function for userid(mobilenumber)

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/CallblackAPI.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/controller/CallblackAPI.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 @Service
@@ -43,6 +46,7 @@ public class CallblackAPI {
         report.setJobno(jobno);
         report.setMessagestatus(status);
         report.setDoneTime(DoneTime);
+        report.setUsernameHash(toHash(mobilenumber));
 
         producer.push(TOPIC, report);
         return ResponseEntity.ok().build();
@@ -61,8 +65,25 @@ public class CallblackAPI {
         report.setJobno(jobno);
         report.setMessagestatus(status);
         report.setDoneTime(DoneTime);
+        report.setUsernameHash(toHash(mobilenumber));
 
         producer.push(TOPIC, report);
         return ResponseEntity.ok().build();
+    }
+
+    private String toHash(String mobileno) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(mobileno.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+        } catch(NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
