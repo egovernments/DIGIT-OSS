4dde572e926bbd773f80ad224e4b0b85e081346f
Nipun Arora <aroranipun1@gmail.com>
2022-03-24 12:55:04 +0530

    Rain 6512 Documents :: Tenants associated to the logged in employee are not displayed on ULB drop down to select multple ULBs (#2193)
    * Update EngagementDocsULB.js
    
    * Update EngagementDocsULB.js
    
    * Update index.js
    
    * RAIN 6512 fix
    
    * fixed for general usecase
    
    * Update index.js
    
    * RAIN-6520: HRMS : UI Boundary type value now auto populating (#2196)
    
    * SAN-969: FSM : UI timeline changes as per new workflow (#2190)
    
    * SAN-968: FSM : UI : Remove filter search for non actionable items (#2186)
    
    Co-authored-by: ayush <ayushgupta@trisysit.com>
    
    Co-authored-by: Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
    Co-authored-by: ayushgupta-eGov <94037835+ayushgupta-eGov@users.noreply.github.com>
    Co-authored-by: ayush <ayushgupta@trisysit.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/Ulb/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/Ulb/index.js
@@ -27,4 +27,53 @@ export const ULBService = {
     const tenantId = ULBService.getCurrentTenantId();
     return initData.tenants.find((tenant) => tenant.code === tenantId);
   },
+  //util function that returns the ulb dropdown data
+  getUserUlbs:(userRole="") => {
+    const stringReplaceAll = (str = "", searcher = "", replaceWith = "") => {
+      if (searcher == "") return str;
+      while (str?.includes(searcher)) {
+        str = str?.replace(searcher, replaceWith);
+      }
+      return str;
+    };
+    
+    const userloggedValues = Digit.SessionStorage.get("citizen.userRequestObject");
+    let teantsArray = [], filteredArray = [];
+    if(userRole===""){
+      
+      userloggedValues?.info?.roles?.forEach(role => teantsArray.push(role.tenantId));
+      let unique = teantsArray.filter((item, i, ar) => ar.indexOf(item) === i);
+    
+      unique?.forEach(uniCode => {
+        filteredArray.push({
+          i18nKey: `TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`,
+          value: uniCode,
+          //label:t(`TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`),
+          code : uniCode,
+          name:uniCode.substring(uniCode.indexOf(".")+1)
+        })
+      });
+      
+      return filteredArray;
+    }
+    else{
+      userloggedValues?.info?.roles?.forEach(role => {
+        if(userRole === role.code)
+          teantsArray.push(role.tenantId)});
+      
+      let unique = teantsArray.filter((item, i, ar) => ar.indexOf(item) === i)
+    
+      unique?.forEach(uniCode => {
+        filteredArray.push({
+          i18nKey: `TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`,
+          value: uniCode,
+          //label:t(`TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`),
+          code : uniCode,
+          name:uniCode.substring(uniCode.indexOf(".")+1)
+        })
+      });
+      
+      return filteredArray;
+    }
+  }
 };
