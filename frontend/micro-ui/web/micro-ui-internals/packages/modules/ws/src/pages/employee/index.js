a56dd4641d046ea83c0e71f6640c909a1fbcba22
saurabh-egov <saurabh.s@egovernments.org>
2022-03-25 10:15:53 +0530

    RAIN-6440 added application details page structure and some services

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/employee/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/employee/index.js
@@ -10,6 +10,7 @@ import ApplicationDetails from "./ApplicationDetails";
 import GetConnectionDetails from "./connectionDetails/connectionDetails";
 import ActivateConnection from "./ActivateConnection";
 import Response from "./Response";
+import ApplicationDetailsBillAmendment from "./ApplicationDetailsBillAmendment";
 // import SearchConnectionComponent from "./SearchConnection";
 // import SearchResultsComponent from "./SearchResults";
 
@@ -24,6 +25,7 @@ const App = ({ path }) => {
         <PrivateRoute path={`${path}/application-details`} component={ApplicationDetails} />
         <PrivateRoute path={`${path}/connection-details`} component={GetConnectionDetails} />
         <PrivateRoute path={`${path}/bill-amendment`} component={() => <ApplicationBillAmendment {...{path}}/>} />
+        <PrivateRoute path={`${path}/application-details-bill-amendment`} component={() => <ApplicationDetailsBillAmendment {...{path}}/>} />
         <PrivateRoute path={`${path}/response`} component={() => <Response {...{path}}/>} />
         <PrivateRoute path={`${path}/required-documents`} component={() => <RequiredDocuments {...{path}}/>} />
         <PrivateRoute path={`${path}/activate-connection`} component={ActivateConnection} />
