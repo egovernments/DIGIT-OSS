700b644c79b09b310d3890108e71015d30fa7c0c
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2022-03-28 17:15:55 +0530

    Rain 6587 (#2242)
    * Added support for search based on receipt number
    
    (cherry picked from commit eb5ead28aa61ebfce963d6967db560a71ef753dd)
    
    * Debugging receipt number value
    
    (cherry picked from commit 07cf162348be3de1c2d361ac64a0006c117c053e)
    
    * Added receipt number in search result
    
    (cherry picked from commit cdc24e5933d035c7b49cb8014a46f86d7d093945)
    
    * Udated change log

--- municipal-services/echallan-services/src/main/java/org/egov/echallan/repository/builder/ChallanQueryBuilder.java
+++ municipal-services/echallan-services/src/main/java/org/egov/echallan/repository/builder/ChallanQueryBuilder.java
@@ -98,6 +98,13 @@ public class ChallanQueryBuilder {
                 addToPreparedStatement(preparedStmtList, status);
             }
 
+            if (criteria.getReceiptNumber() != null) {
+                List<String> receiptNumbers = Arrays.asList(criteria.getReceiptNumber().split(","));
+                addClauseIfRequired(preparedStmtList, builder);
+                builder.append(" challan.receiptnumber IN (").append(createQuery(receiptNumbers)).append(")");
+                addToPreparedStatement(preparedStmtList, receiptNumbers);
+            }
+
 
         }
 
