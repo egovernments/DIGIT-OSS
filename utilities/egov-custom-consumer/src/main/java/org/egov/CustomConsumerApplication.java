75c62cb3568b927506e66377e083eee75e4a0fa7
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-03-28 13:18:17 +0530

    Revert "Redis related code"

--- utilities/egov-custom-consumer/src/main/java/org/egov/CustomConsumerApplication.java
+++ utilities/egov-custom-consumer/src/main/java/org/egov/CustomConsumerApplication.java
@@ -1,11 +1,8 @@
 package org.egov;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,40 +10,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 public class CustomConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomConsumerApplication.class, args);
-	}
-	
-	@Value("egov.redis.host")
-	private String redisHost;
-	
-	@Value("egov.redis.port")
-	private String redisPort;
-	
-	@Bean
-	public RestTemplate restTemplate()	{
-		return new RestTemplate();
-	}
-	
-	@Bean
-	public ObjectMapper objectMapper()	{
-		return new ObjectMapper();
-	}
-	
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		JedisConnectionFactory jedisConFactory
-	      = new JedisConnectionFactory();
-	    jedisConFactory.setHostName(redisHost);
-	    jedisConFactory.setPort(6379);
-	    return jedisConFactory;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomConsumerApplication.class, args);
+    }
 
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-	    RedisTemplate<String, Object> template = new RedisTemplate<>();
-	    template.setConnectionFactory(jedisConnectionFactory());
-	    return template;
-	}
-	
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
 }
--- utilities/egov-custom-consumer/src/main/java/org/egov/service/SignOutService.java
+++ utilities/egov-custom-consumer/src/main/java/org/egov/service/SignOutService.java
@@ -1,19 +1,16 @@
 package org.egov.service;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.egov.models.RequestInfo;
 import org.egov.models.RequestInfoWrapper;
 import org.egov.utils.JsonPathConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,55 +19,32 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SignOutService {
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@Value("${egov.coexistence.hostname}")
-	private String coexistencehost;
+    @Value("${egov.coexistence.hostname}")
+    private String coexistencehost;
 
-	@Value("${egov.coexistence.singout.uri}")
-	private String coexistencelogoutUri;
-	
-	@Autowired
-	private RedisTemplate<String, Object> redisTemplate;
+    @Value("${egov.coexistence.singout.uri}")
+    private String coexistencelogoutUri;
 
-	public void callFinanceForSignOut(DocumentContext documentContext) {
-		ResponseEntity<?> response = null;
-		String accessToken = documentContext.read(JsonPathConstant.signOutAccessToken);
-		documentContext = documentContext.delete(JsonPathConstant.userInfo);
-		documentContext = documentContext.put(JsonPathConstant.requestInfo, "authToken", accessToken);
-		String correlationId = documentContext.read(JsonPathConstant.correlationId);
-		LinkedHashMap<String, Object> jsonRequest = documentContext.read(JsonPathConstant.request);
-		log.info(coexistencehost + coexistencelogoutUri + accessToken);
-		RequestInfoWrapper reqInfoWrapper = new RequestInfoWrapper();
-		RequestInfo requestInfo = new RequestInfo();
-		requestInfo.setAuthToken(accessToken);
-		requestInfo.setCorrelationId(correlationId);
-		reqInfoWrapper.setRequestInfo(requestInfo);
-		log.info("Call signout API");
-		log.info(reqInfoWrapper.toString());
-		String authStr = "auth_test:" + accessToken;
-		log.info("Save to redis*********");
-		redisTemplate.opsForHash().putIfAbsent(authStr, "auth_test", accessToken);
-		log.info("Fetch from redis*********");
-		Object sessionIdFromRedi = redisTemplate.opsForHash().get(authStr, "auth_test");
-		log.info("sessionIdFromRedi---->>>"+ sessionIdFromRedi);
-		/*
-		 * if (Boolean.TRUE.equals(redisTemplate.hasKey(authStr))) {
-		 * log.info("Reading session from Redis"); Object sessionIdFromRedis =
-		 * redisTemplate.opsForValue().get(authStr); if(sessionIdFromRedis != null)
-		 * log.info(sessionIdFromRedis.toString()); ObjectMapper oMapper = new
-		 * ObjectMapper(); Map<String, String> map =
-		 * oMapper.convertValue(sessionIdFromRedis, Map.class);
-		 * log.info(map.toString()); for(Map.Entry<String, String> entries :
-		 * map.entrySet()) {
-		 * log.info("Redis-->Auth Token--->>> Keys::"+entries.getKey()+"::Values"+
-		 * entries.getValue()); redisTemplate.delete(entries.getKey()); }
-		 * redisTemplate.delete(accessToken); }
-		 */
-		
-		response = restTemplate.postForEntity(coexistencehost + coexistencelogoutUri, reqInfoWrapper, ResponseEntity.class);
-		log.info("SignOutService response :" + response.getStatusCode());
-	}
+    public void callFinanceForSignOut(DocumentContext documentContext) {
+        ResponseEntity<?> response = null;
+        String accessToken = documentContext.read(JsonPathConstant.signOutAccessToken);
+        documentContext = documentContext.delete(JsonPathConstant.userInfo);
+        documentContext = documentContext.put(JsonPathConstant.requestInfo, "authToken", accessToken);
+        String correlationId = documentContext.read(JsonPathConstant.correlationId);
+        LinkedHashMap<String, Object> jsonRequest = documentContext.read(JsonPathConstant.request);
+        log.info(coexistencehost + coexistencelogoutUri + accessToken);
+        RequestInfoWrapper reqInfoWrapper = new RequestInfoWrapper();
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setAuthToken(accessToken);
+        requestInfo.setCorrelationId(correlationId);
+        reqInfoWrapper.setRequestInfo(requestInfo);
+        log.info("Call signout API");
+        log.info("Request Info****"+reqInfoWrapper.toString());
+        response = restTemplate.postForEntity(coexistencehost + coexistencelogoutUri, reqInfoWrapper, ResponseEntity.class);
+        log.info("SignOutService response :" + response.getStatusCode());
+    }
 
 }
