6a0e0bc5a38e078419c0317f7e834ba7a93b225a
Mayank-eGov <95083794+Mayank-eGov@users.noreply.github.com>
2022-03-30 09:11:55 +0530

    Update EdcrValidator.java

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
@@ -23,8 +23,8 @@ import org.springframework.stereotype.Service;
 public class EdcrValidator {
     private static Logger LOG = Logger.getLogger(EdcrValidator.class);
     private static final String INVALID_VAL = "The %s value is invalid";
-    private static final String ALPHANUMERIC_WITH_SPECIAL_CHARS = "^[a-zA-Z]*$";
-    private static final String NUMERIC_CHARS = "[a-zA-Z]";
+    private static final String ALPHANUMERIC_WITH_SPECIAL_CHARS = "^[a-zA-Z0-9]+(([ _\\-&:,/.][a-zA-Z0-9])?[a-zA-Z0-9]*)*$";
+    private static final String ALPHA_CHARS = "^[a-zA-Z]*$";
     private static final String INVALID_CHAR_MSG = " _-&:,/.";
     private static final String INVALID_CHAR = "The (%s) contains invalid value. Only following special characters are allowed %s, The String should not start with special characters and should not follow two immediately.";
     
@@ -128,7 +128,16 @@ public class EdcrValidator {
                             error.setErrorMessage(String.format(INVALID_CHAR, f.getName(), INVALID_CHAR_MSG));
                             return error;
                         }
-                       
+                       if(f.getName().equals("applicantName") && StringUtils.isNotBlank(value) && value.length() > 1)
+                        {
+                        	 boolean isAllowName = Pattern.matches(ALPHA_CHARS, value);
+                        	  if (!isAllowName) {
+                                  LOG.info("The Inalid Value is" + value);
+                                  error.setErrorCode("EDCR-31");
+                                  error.setErrorMessage(String.format(INVALID_CHAR, f.getName(), INVALID_CHAR_MSG));
+                                  return error;
+                              }
+                        }
 
                         if (value.length() > 256) {
                             error.setErrorCode("EDCR-32");
