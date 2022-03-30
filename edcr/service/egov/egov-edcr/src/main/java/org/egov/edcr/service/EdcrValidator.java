1815083c268541c52339a8c885bc02dcc8287bd9
Mayank-eGov <95083794+Mayank-eGov@users.noreply.github.com>
2022-03-30 11:21:39 +0530

    edcr validation for applicantName field (#2262)
    Co-authored-by: Mayank <mayanktiwari@trisysit.com>

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
@@ -24,6 +24,7 @@ public class EdcrValidator {
     private static Logger LOG = Logger.getLogger(EdcrValidator.class);
     private static final String INVALID_VAL = "The %s value is invalid";
     private static final String ALPHANUMERIC_WITH_SPECIAL_CHARS = "^[a-zA-Z0-9]+(([ _\\-&:,/.][a-zA-Z0-9])?[a-zA-Z0-9]*)*$";
+    private static final String ALPHA_CHARS = "^[a-zA-Z]*$";
     private static final String INVALID_CHAR_MSG = " _-&:,/.";
     private static final String INVALID_CHAR = "The (%s) contains invalid value. Only following special characters are allowed %s, The String should not start with special characters and should not follow two immediately.";
     
@@ -125,6 +126,17 @@ public class EdcrValidator {
                             error.setErrorCode("EDCR-31");
                             error.setErrorMessage(String.format(INVALID_CHAR, f.getName(), INVALID_CHAR_MSG));
                             return error;
+                        }
+
+                        if(f.getName().equals("applicantName") && StringUtils.isNotBlank(value) && value.length() > 1)
+                        {
+                        	 boolean isAllowName = Pattern.matches(ALPHA_CHARS, value);
+                        	  if (!isAllowName) {
+                                  LOG.info("The Inalid Value is" + value);
+                                  error.setErrorCode("EDCR-31");
+                                  error.setErrorMessage(String.format(INVALID_CHAR, f.getName(), INVALID_CHAR_MSG));
+                                  return error;
+                              }
                         }
 
                         if (value.length() > 256) {
