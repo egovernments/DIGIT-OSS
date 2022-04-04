56b5bfd80705ce81ff655f23dfab8a64535390ce
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-04-04 10:41:59 +0530

    RAIN-6496 : DSS Home screen rounded off all the metric and Added Backbutton in home screens (#2305)
    * RAIN-6496 : DSS Home screen rounded off all the metric
    
    * RAIN-6496 : reverted the token setting changes
    
    * RAIN-6572 :  Core UI Added Backbutton in Allservices screen

--- frontend/mono-ui/web/dss-dashboard/src/components/common/numberFormater/index.js
+++ frontend/mono-ui/web/dss-dashboard/src/components/common/numberFormater/index.js
@@ -11,7 +11,7 @@ export default function NFormatter(props) {
     // const dispatch = useDispatch();
     if (props && Object.keys(props).length > 0) {
         const Rformatter = new Intl.NumberFormat('en-IN', {
-            // maximumFractionDigits:0,
+            maximumFractionDigits:0,
             useGrouping: true,
             // currencyDisplay : Intl.NumberFormatOptions
             style: 'currency',
@@ -23,15 +23,17 @@ export default function NFormatter(props) {
 
                 switch (_.get(GFilterData, 'Denomination')) {
                     case SI_SYMBOL[1]:
-                        return `${Rformatter.format((props.value / 100000).toFixed(2) || 0)}  ${_.get(GFilterData, 'Denomination')}`
-
+                        if((props.value / 100000).toFixed()>0)
+                        return `${Rformatter.format((props.value / 100000).toFixed()||0)}  ${_.get(GFilterData, 'Denomination')}`
+                        else
+                        return `${Rformatter.format((props.value).toFixed()||0)}`
                     case SI_SYMBOL[2]:
-                        return `${Rformatter.format((props.value / 10000000).toFixed(2) || 0)}  ${_.get(GFilterData, 'Denomination')}`
+                        return `${Rformatter.format((props.value / 10000000).toFixed() || 0)}  ${_.get(GFilterData, 'Denomination')}`
                     case SI_SYMBOL[0]:
                         if (props.value <= 9999999) {
                             return `${Rformatter.format(props.value || 0)}`
                         } else {
-                            let value = Rformatter.format((props.value).toFixed(2) || 0).replace('₹ ', '').replace('₹','');
+                            let value = Rformatter.format((props.value).toFixed() || 0).replace('₹ ', '').replace('₹','');
                             var right = value.substring(value.length - 12, value.length);
                             var left = value.substring(0, value.length - 12).replace(',', '');
                             let newVal = '₹ ' + (left ? (left + ',') : '') + right;
@@ -47,7 +49,7 @@ export default function NFormatter(props) {
             case "percentage":
             case "Percentage":
                 const Pformatter = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 });
-                return `${parseFloat(Pformatter.format(props.value) || '0').toFixed(2)} %`;
+                return `${parseFloat(Pformatter.format(props.value) || '0').toFixed()} %`;
             case "text":
             case "Text":
                 return props.value;
