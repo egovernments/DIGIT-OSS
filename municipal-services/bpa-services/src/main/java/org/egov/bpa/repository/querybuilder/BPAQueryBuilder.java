0a90dc81a5b872abbee219910e90189147296d78
naveen-wt-egov <90044067+naveen-wt-egov@users.noreply.github.com>
2022-04-07 12:21:38 +0530

    Rain-5772 bpa _plainsearch api (#2368)

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/repository/querybuilder/BPAQueryBuilder.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/repository/querybuilder/BPAQueryBuilder.java
@@ -197,6 +197,10 @@ public class BPAQueryBuilder {
         int offset = config.getDefaultOffset();
         String finalQuery = paginationWrapper.replace("{}", query);
 
+        if(criteria.getLimit() == null && criteria.getOffset() == null) {
+        	limit = config.getMaxSearchLimit();
+        } 
+        
         if (criteria.getLimit() != null && criteria.getLimit() <= config.getMaxSearchLimit())
             limit = criteria.getLimit();
 
@@ -261,5 +265,30 @@ public class BPAQueryBuilder {
     
     private String addCountWrapper(String query) {
         return countWrapper.replace("{INTERNAL_QUERY}", query);
+    }
+    
+    public String getBPASearchQueryForPlainSearch(BPASearchCriteria criteria, List<Object> preparedStmtList, List<String> edcrNos, boolean isCount) {
+
+        StringBuilder builder = new StringBuilder(QUERY);
+
+        if (criteria.getTenantId() != null) {
+            if (criteria.getTenantId().split("\\.").length == 1) {
+
+                addClauseIfRequired(preparedStmtList, builder);
+                builder.append(" bpa.tenantid like ?");
+                preparedStmtList.add('%' + criteria.getTenantId() + '%');
+            } else {
+                addClauseIfRequired(preparedStmtList, builder);
+                builder.append(" bpa.tenantid=? ");
+                preparedStmtList.add(criteria.getTenantId());
+            }
+        }
+
+
+        if(isCount)
+            return addCountWrapper(builder.toString());
+        
+        return addPaginationWrapper(builder.toString(), preparedStmtList, criteria);
+
     }
 }
