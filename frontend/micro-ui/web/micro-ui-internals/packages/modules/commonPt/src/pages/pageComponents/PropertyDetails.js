d469ce00bee69bb4410ff1a88cd72d45fcea972f
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-04-11 22:29:57 +0530

    RAIN-6734 :: TL Updated the accessory details screen initial loading logic (#2417)
    * RAIN-6734 : Core Added a util method to access citizen selected city
    
    * RAIN-6734 : TL Trade units and accesories added loader and updated to use citizen selected city
    
    * RAIN-6734 : Used common method to access seleceted city in all instance
    
    and formatted code
    
    * RAIN-6734 :: TL Updated the screen loader addition

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/commonPt/src/pages/pageComponents/PropertyDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/commonPt/src/pages/pageComponents/PropertyDetails.js
@@ -1,74 +1,70 @@
-import React, { useState } from "react";
-import { FormStep, LabelFieldPair, CardLabel, Loader, CardHeader, SubmitBar, StatusTable, Row, LinkButton } from "@egovernments/digit-ui-react-components";
+import {
+  CardHeader, FormStep, LinkButton, Loader, Row, StatusTable, SubmitBar
+} from "@egovernments/digit-ui-react-components";
+import React from "react";
+import { Link } from "react-router-dom";
 import Timeline from "../../components/CPTTimeline";
-import { Link, useLocation} from "react-router-dom";
 
 const PropertyDetails = ({ t, config, onSelect, userType, formData }) => {
-  const tenantId = Digit.SessionStorage.get("CITIZEN.COMMON.HOME.CITY")?.code;
-  if(window.location.href.includes("/tl/tradelicence/edit-application/") || window.location.href.includes("/renew-trade/") )
-  {
-    sessionStorage.setItem("EditFormData",JSON.stringify(formData));
+  const tenantId = Digit.ULBService.getCitizenCurrentTenant();
+  if (window.location.href.includes("/tl/tradelicence/edit-application/") || window.location.href.includes("/renew-trade/")) {
+    sessionStorage.setItem("EditFormData", JSON.stringify(formData));
   }
-  const { 
-    isLoading,
-    isError,
-    error,
-    data: propertyDetails
-  } = Digit.Hooks.pt.usePropertySearch(
-    { 
-      filters: 
-      { propertyIds: formData?.cptId?.id }, tenantId: tenantId 
-    }, 
-    { filters: 
-      { propertyIds: formData?.cptId?.id }, tenantId: tenantId 
-    }
+  const { isLoading, isError, error, data: propertyDetails } = Digit.Hooks.pt.usePropertySearch(
+    {
+      filters: { propertyIds: formData?.cptId?.id },
+      tenantId: tenantId,
+    },
+    { filters: { propertyIds: formData?.cptId?.id }, tenantId: tenantId }
   );
 
   const onSkip = () => onSelect();
 
   const goNext = () => {
     sessionStorage.setItem("cpt", propertyDetails?.Properties[0]);
-    onSelect('cpt', {details: propertyDetails?.Properties[0]});
-  }
+    onSelect("cpt", { details: propertyDetails?.Properties[0] });
+  };
 
-  let propertyAddress = '';
-  if(propertyDetails && propertyDetails?.Properties.length){
-    if(propertyDetails?.Properties[0]?.address?.doorNo) {
+  let propertyAddress = "";
+  if (propertyDetails && propertyDetails?.Properties.length) {
+    if (propertyDetails?.Properties[0]?.address?.doorNo) {
       propertyAddress += propertyDetails?.Properties[0]?.address?.doorNo;
-      if(propertyDetails?.Properties[0]?.address?.street) {
-        propertyAddress += ', ';
+      if (propertyDetails?.Properties[0]?.address?.street) {
+        propertyAddress += ", ";
       }
     }
-    if(propertyDetails?.Properties[0]?.address?.street) {
+    if (propertyDetails?.Properties[0]?.address?.street) {
       propertyAddress += propertyDetails?.Properties[0]?.address?.street;
-      if(propertyDetails?.Properties[0]?.address?.landmark) {
-        propertyAddress += ', ';
+      if (propertyDetails?.Properties[0]?.address?.landmark) {
+        propertyAddress += ", ";
       }
     }
-    if(propertyDetails?.Properties[0]?.address?.landmark) {
+    if (propertyDetails?.Properties[0]?.address?.landmark) {
       propertyAddress += propertyDetails?.Properties[0]?.address?.landmark;
-      if(propertyDetails?.Properties[0]?.address?.locality?.code) {
-        propertyAddress += ', ';
+      if (propertyDetails?.Properties[0]?.address?.locality?.code) {
+        propertyAddress += ", ";
       }
     }
-    if(propertyDetails?.Properties[0]?.address?.locality?.code) {
-      propertyAddress += propertyDetails?.Properties[0]?.address?.locality?.code && t(Digit.Utils.pt.getMohallaLocale(propertyDetails?.Properties[0]?.address?.locality?.code,propertyDetails?.Properties[0]?.tenantId));
-      if(propertyDetails?.Properties[0]?.address?.city) {
-        propertyAddress += ', ';
+    if (propertyDetails?.Properties[0]?.address?.locality?.code) {
+      propertyAddress +=
+        propertyDetails?.Properties[0]?.address?.locality?.code &&
+        t(Digit.Utils.pt.getMohallaLocale(propertyDetails?.Properties[0]?.address?.locality?.code, propertyDetails?.Properties[0]?.tenantId));
+      if (propertyDetails?.Properties[0]?.address?.city) {
+        propertyAddress += ", ";
       }
     }
-    if(propertyDetails?.Properties[0]?.address?.city) {
-      propertyAddress += propertyDetails?.Properties[0]?.tenantId&& t(Digit.Utils.pt.getCityLocale(propertyDetails?.Properties[0]?.tenantId));
-      if(propertyDetails?.Properties[0]?.address?.pincode) {
-        propertyAddress += ', ';
+    if (propertyDetails?.Properties[0]?.address?.city) {
+      propertyAddress += propertyDetails?.Properties[0]?.tenantId && t(Digit.Utils.pt.getCityLocale(propertyDetails?.Properties[0]?.tenantId));
+      if (propertyDetails?.Properties[0]?.address?.pincode) {
+        propertyAddress += ", ";
       }
     }
-    if(propertyDetails?.Properties[0]?.address?.pincode) {
+    if (propertyDetails?.Properties[0]?.address?.pincode) {
       propertyAddress += propertyDetails?.Properties[0]?.address?.pincode;
     }
   }
 
-  if(isLoading) {
+  if (isLoading) {
     return <Loader />;
   }
 
@@ -76,33 +72,34 @@ const PropertyDetails = ({ t, config, onSelect, userType, formData }) => {
     <React.Fragment>
       {window.location.href.includes("/citizen") ? <Timeline currentStep={2} /> : null}
       <FormStep t={t} config={config} onSelect={goNext} onSkip={onSkip}>
-       {
-          propertyDetails && propertyDetails?.Properties.length && (
-            <React.Fragment>
-              <CardHeader>{t("PT_DETAILS")}</CardHeader>
-              <StatusTable>
-                <Row className="border-none" label={t(`PROPERTY_ID`)} text={propertyDetails?.Properties[0]?.propertyId} />
-                <Row className="border-none" label={t(`OWNER_NAME`)} text={propertyDetails?.Properties[0]?.owners[0]?.name}/>
-                <Row className="border-none" textStyle={{wordBreak:"break-word"}} label={t(`PROPERTY_ADDRESS`)} text={propertyAddress} />
-                <Row className="border-none" label={t(`PT_MUTATION_STATUS`)} text={propertyDetails?.Properties[0]?.status}/>
-                <div style={{textAlign:"left"}}>
-                <Link to={`/digit-ui/citizen/commonpt/view-property?propertyId=${propertyDetails?.Properties[0]?.propertyId}&tenantId=${propertyDetails?.Properties[0]?.tenantId}`}>
-                  <LinkButton style={{textAlign:"left"}} label={t("PT_VIEW_MORE_DETAILS")} />
+        {propertyDetails && propertyDetails?.Properties.length && (
+          <React.Fragment>
+            <CardHeader>{t("PT_DETAILS")}</CardHeader>
+            <StatusTable>
+              <Row className="border-none" label={t(`PROPERTY_ID`)} text={propertyDetails?.Properties[0]?.propertyId} />
+              <Row className="border-none" label={t(`OWNER_NAME`)} text={propertyDetails?.Properties[0]?.owners[0]?.name} />
+              <Row className="border-none" textStyle={{ wordBreak: "break-word" }} label={t(`PROPERTY_ADDRESS`)} text={propertyAddress} />
+              <Row className="border-none" label={t(`PT_MUTATION_STATUS`)} text={propertyDetails?.Properties[0]?.status} />
+              <div style={{ textAlign: "left" }}>
+                <Link
+                  to={`/digit-ui/citizen/commonpt/view-property?propertyId=${propertyDetails?.Properties[0]?.propertyId}&tenantId=${propertyDetails?.Properties[0]?.tenantId}`}
+                >
+                  <LinkButton style={{ textAlign: "left" }} label={t("PT_VIEW_MORE_DETAILS")} />
                 </Link>
-                <Link to={window.location.href.includes("/edit-application/") || window.location.href.includes("/renew-trade/") ? `/digit-ui/citizen/tl/tradelicence/edit-application/${formData?.applicationNumber}/${formData?.tenantId}/know-your-property` : `/digit-ui/citizen/tl/tradelicence/new-application/know-your-property`}>
-                  <LinkButton style={{textAlign:"left"}} label={t("PT_CHANGE_PROPERTY")} />
+                <Link
+                  to={
+                    window.location.href.includes("/edit-application/") || window.location.href.includes("/renew-trade/")
+                      ? `/digit-ui/citizen/tl/tradelicence/edit-application/${formData?.applicationNumber}/${formData?.tenantId}/know-your-property`
+                      : `/digit-ui/citizen/tl/tradelicence/new-application/know-your-property`
+                  }
+                >
+                  <LinkButton style={{ textAlign: "left" }} label={t("PT_CHANGE_PROPERTY")} />
                 </Link>
-                </div>
-              </StatusTable>
-              <SubmitBar
-                style={{marginTop:"10px"}}
-                onSubmit={goNext}
-                label={t("CS_COMMON_NEXT")}
-              />
-              
-            </React.Fragment>
-          )
-        }
+              </div>
+            </StatusTable>
+            <SubmitBar style={{ marginTop: "10px" }} onSubmit={goNext} label={t("CS_COMMON_NEXT")} />
+          </React.Fragment>
+        )}
       </FormStep>
     </React.Fragment>
   );
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/LocationSelection.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/LocationSelection.js
@@ -1,57 +1,61 @@
-import React, { Fragment, useMemo, useState } from "react"
-import { PageBasedInput, Loader, RadioButtons, CardHeader, BackButton, SearchOnRadioButtons, CardLabelError } from "@egovernments/digit-ui-react-components"
+import React, { Fragment, useMemo, useState } from "react";
+import { PageBasedInput, CardHeader, BackButton, SearchOnRadioButtons, CardLabelError } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 
 const LocationSelection = () => {
-    const { t } = useTranslation()
-    const history = useHistory()
-    
-    const {data: cities, isLoading} = Digit.Hooks.useTenants();
+  const { t } = useTranslation();
+  const history = useHistory();
 
-    const [selectedCity, setSelectedCity ] = useState(() => Digit.SessionStorage.get("CITIZEN.COMMON.HOME.CITY"))
-    const [showError, setShowError] = useState(false)
+  const { data: cities, isLoading } = Digit.Hooks.useTenants();
 
-    const texts = useMemo(() => ({
-        header: t("CS_COMMON_CHOOSE_LOCATION"),
-        submitBarLabel: t( "CORE_COMMON_CONTINUE")
-    }), [t])
+  const [selectedCity, setSelectedCity] = useState(() => ({ code: Digit.ULBService.getCitizenCurrentTenant() }));
+  const [showError, setShowError] = useState(false);
 
-    function selectCity(city) {
-        setSelectedCity(city)
-        setShowError(false)
+  const texts = useMemo(
+    () => ({
+      header: t("CS_COMMON_CHOOSE_LOCATION"),
+      submitBarLabel: t("CORE_COMMON_CONTINUE"),
+    }),
+    [t]
+  );
+
+  function selectCity(city) {
+    setSelectedCity(city);
+    setShowError(false);
+  }
+
+  const RadioButtonProps = useMemo(() => {
+    return {
+      options: cities,
+      optionsKey: "i18nKey",
+      additionalWrapperClass: "reverse-radio-selection-wrapper",
+      onSelect: selectCity,
+      selectedOption: selectedCity,
+    };
+  }, [cities, t, selectedCity]);
+
+  function onSubmit() {
+    if (selectedCity) {
+      Digit.SessionStorage.set("CITIZEN.COMMON.HOME.CITY", selectedCity);
+      history.push("/digit-ui/citizen");
+    } else {
+      setShowError(true);
     }
+  }
 
-    const RadioButtonProps = useMemo(() => {
-        return {
-            options: cities,
-            optionsKey: "i18nKey",
-            additionalWrapperClass: "reverse-radio-selection-wrapper",
-            onSelect: selectCity,
-            selectedOption: selectedCity
-        }
-    },[cities, t, selectedCity])
-
-    function onSubmit(){
-
-        if(selectedCity){
-            Digit.SessionStorage.set("CITIZEN.COMMON.HOME.CITY",selectedCity)
-            history.push("/digit-ui/citizen")
-        }
-        else{
-            setShowError(true)
-        }
-    }
-
-    return isLoading ? <loader/> : 
+  return isLoading ? (
+    <loader />
+  ) : (
     <>
-    <BackButton />
-    <PageBasedInput texts={texts} onSubmit={onSubmit}>
+      <BackButton />
+      <PageBasedInput texts={texts} onSubmit={onSubmit}>
         <CardHeader>{t("CS_COMMON_CHOOSE_LOCATION")}</CardHeader>
         <SearchOnRadioButtons {...RadioButtonProps} placeholder={t("COMMON_TABLE_SEARCH")} />
         {showError ? <CardLabelError>{t("CS_COMMON_LOCATION_SELECTION_ERROR")}</CardLabelError> : null}
-    </PageBasedInput>
+      </PageBasedInput>
     </>
-}
+  );
+};
 
-export default LocationSelection
+export default LocationSelection;
