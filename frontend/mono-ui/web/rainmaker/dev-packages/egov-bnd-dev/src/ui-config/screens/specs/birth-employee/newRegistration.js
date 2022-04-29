fe3ae6eefde95ad33f227a3cdb18514315d595da
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-04-29 18:02:06 +0530

    RAIN-6771, RAIN-6670 :: DSS BND Piechart issue fix and BND Form reg no field disabled on edit (#2641)
    * RAIN-6771: BND Edit Form made registrartion no made disabled
    
    Co-Authored-By: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
    
    * RAIN-6670 : DSS Drilldown for piechart added cursor and changes alignment
    
    Co-Authored-By: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
    
    * Updated dss package versions
    
    Co-Authored-By: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/birth-employee/newRegistration.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/birth-employee/newRegistration.js
@@ -1,13 +1,16 @@
+import { getCommonHeader } from "egov-ui-framework/ui-config/screens/specs/utils";
 import {
-  getCommonHeader
-} from "egov-ui-framework/ui-config/screens/specs/utils";
-import {
-  handleScreenConfigurationFieldChange as handleField, prepareFinalObject
+  handleScreenConfigurationFieldChange as handleField,
+  prepareFinalObject,
 } from "egov-ui-framework/ui-redux/screen-configuration/actions"; //returns action object
 import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
 import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
 import get from "lodash/get";
-import { convertEpochToDateCustom, loadFullCertDetails, loadHospitals } from "../utils";
+import {
+  convertEpochToDateCustom,
+  loadFullCertDetails,
+  loadHospitals,
+} from "../utils";
 import { addHospitalDialog } from "./addHospitalDialog";
 import { newRegistrationForm } from "./newRegistrationCard";
 import { confirmationDialog } from "./newRegistrationConfirmDialog";
@@ -114,8 +117,12 @@ const newRegistration = {
             hospital.name = hospital.hospitalName;
           }
           dispatch(prepareFinalObject("bnd.allHospitals", newList));
-        }else{
-          dispatch(prepareFinalObject("bnd.allHospitals", [{code:"Others",name:"Others"}]));
+        } else {
+          dispatch(
+            prepareFinalObject("bnd.allHospitals", [
+              { code: "Others", name: "Others" },
+            ])
+          );
         }
 
         if (userAction == "EDIT" && id && module) {
@@ -124,7 +131,12 @@ const newRegistration = {
             "components.div2.children.details.children.cardContent.children.registrationInfo.children.cardContent.children.registrationInfoCont.children.hospitalName.props.isDisabled",
             true
           );
-                    loadFullCertDetails(action, state, dispatch, {
+          set(
+            action.screenConfig,
+            "components.div2.children.details.children.cardContent.children.registrationInfo.children.cardContent.children.registrationInfoCont.children.registrationNo.props.disabled",
+            true
+          );
+          loadFullCertDetails(action, state, dispatch, {
             tenantId: getTenantId(),
             id: id,
             module: module,
@@ -143,7 +155,7 @@ const newRegistration = {
               dispatch(
                 prepareFinalObject(
                   "bnd.birth.newRegistration.isLegacyRecord",
-                  response.BirthCertificate[0].counter==1
+                  response.BirthCertificate[0].counter == 1
                 )
               );
               prepareEditScreenData(action, state, dispatch, response);
