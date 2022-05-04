84e563a7bacfc4cdb11cb67e1eaa308a79a72c2b
tulika kundu <tulika.kundu@egovernments.org>
2022-05-04 10:21:00 +0530

    [RAIN-6911,RAIN-6912,RAIN-6913,RAIN-6914] : PT TL UI Bugs fixes, alignment fix, validation for porperty Id added, skip and continue button routing fix, validation for area in create for added

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/commonPt/src/pages/components/PropertyAssemblyDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/commonPt/src/pages/components/PropertyAssemblyDetails.js
@@ -19,6 +19,7 @@ const PropertyAssemblyDetails = ({ t, config, onSelect, userType, formData, form
   const [focusField, setFocusField] = React.useState("");
   const stateId = Digit.ULBService.getStateId();
   const [isErrors, setIsErrors] = useState(false);
+  const isMobile = window.Digit.Utils.browser.isMobile();
   
   let proptype = [];
 
@@ -196,7 +197,7 @@ const PropertyAssemblyDetails = ({ t, config, onSelect, userType, formData, form
             key={config?.key}
             rules={{ 
               required: t("REQUIRED_FIELD"),
-              validate: (val) => /^([0-9]){0,8}$/i.test(val) ? true: t("PT_TOT_CONSTRUCTION_AREA_ERROR_MESSAGE")
+              validate: (val) => /^([0-9]){0,8}$/i.test(val) && assemblyDetails?.floorarea && parseInt(val) < parseInt(assemblyDetails?.floorarea) ? true: t("PT_TOT_CONSTRUCTION_AREA_ERROR_MESSAGE")
              }}
             render={(props) => (
               <TextInput
@@ -218,7 +219,7 @@ const PropertyAssemblyDetails = ({ t, config, onSelect, userType, formData, form
           />
         </div>
       </LabelFieldPair>
-      <CardLabelError style={errorStyle}>{touched?.constructionArea ? errors?.constructionArea?.message : ""}</CardLabelError>
+      <CardLabelError style={isMobile ? {...errorStyle,marginLeft:"0px"} : {...errorStyle}}>{touched?.constructionArea ? errors?.constructionArea?.message : ""}</CardLabelError>
 
       <LabelFieldPair>
         <CardLabel>{`${t("PT_ASSESMENT_INFO_USAGE_TYPE")}*`}</CardLabel>
