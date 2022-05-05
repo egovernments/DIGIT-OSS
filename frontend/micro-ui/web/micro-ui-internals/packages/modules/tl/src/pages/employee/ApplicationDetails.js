771ce97818547653388c0b9aedfb978c9c9ee3ff
tulika kundu <tulika.kundu@egovernments.org>
2022-05-05 15:15:51 +0530

    PT TL : alignment issue fixed and searh screen clear button working fixed

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/ApplicationDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/ApplicationDetails.js
@@ -174,7 +174,7 @@ const ApplicationDetails = () => {
 
   return (
     <div >
-      <div style={{marginLeft: "15px"}}>
+      <div /* style={{marginLeft: "15px"}} */>
         <Header>{(applicationDetails?.applicationData?.workflowCode == "NewTL" && applicationDetails?.applicationData?.status !== "APPROVED") ? t("TL_TRADE_APPLICATION_DETAILS_LABEL") : t("TL_TRADE_LICENSE_DETAILS_LABEL")}</Header>
       </div>
       <ApplicationDetailsTemplate
