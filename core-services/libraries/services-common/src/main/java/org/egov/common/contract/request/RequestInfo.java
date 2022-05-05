35e3034efb7686ff99712f9800f4cf5d8128a952
Rushang Dhanesha <rushangdhanesha@gmail.com>
2022-05-05 12:38:10 +0530

    Refactored attribute name plainRequestFields

--- core-services/libraries/services-common/src/main/java/org/egov/common/contract/request/RequestInfo.java
+++ core-services/libraries/services-common/src/main/java/org/egov/common/contract/request/RequestInfo.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
@@ -37,7 +39,7 @@ public class RequestInfo {
 
     private String correlationId;
 
-    private List<String> plainAccessRequest;
+    private List<String> plainRequestFields;
 
     private User userInfo;
 }
