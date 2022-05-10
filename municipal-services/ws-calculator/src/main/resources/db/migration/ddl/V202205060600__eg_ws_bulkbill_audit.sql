9b5279a2f96cd5ee9105f4a9f91bff7f3aa43d7d
hinamakhija-eGov <hina.makhija@egovernments.org>
2022-05-10 12:23:35 +0530

    Revert "[6448]-Updated migration files"
    This reverts commit 9b5a4fe484aaa0014b9919b017f8e200d90f4c07.

--- municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V202205060600__eg_ws_bulkbill_audit.sql
+++ municipal-services/ws-calculator/src/main/resources/db/migration/ddl/V202205060600__eg_ws_bulkbill_audit.sql
@@ -5,7 +5,5 @@ CREATE TABLE IF NOT EXISTS eg_ws_bulkbill_audit (
   recordCount bigint NOT NULL,
   tenantid CHARACTER VARYING (256) NOT NULL,
   businessservice CHARACTER VARYING (256) NOT NULL,
-  audittime bigint NOT NULL,
-  message CHARACTER VARYING (2048) NOT NULL,
   CONSTRAINT pk_eg_ws_bulkbill_audit_id PRIMARY KEY (id)
 ); 
