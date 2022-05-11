ede46d0e696e99abb7d7fed8a4de7a5b87e76026
Mayank-eGov <95083794+Mayank-eGov@users.noreply.github.com>
2022-05-11 10:10:30 +0530

    RAIN:6999-edcr scrutiny applicant name validation (#2733)
    Co-authored-by: Mayank <mayanktiwari@trisysit.com>

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrValidator.java
@@ -25,7 +25,7 @@ public class EdcrValidator {
     private static Logger LOG = LogManager.getLogger(EdcrValidator.class);
     private static final String INVALID_VAL = "The %s value is invalid";
     private static final String ALPHANUMERIC_WITH_SPECIAL_CHARS = "^[a-zA-Z0-9]+(([ _\\-&:,/.][a-zA-Z0-9])?[a-zA-Z0-9]*)*$";
-    private static final String ALPHA_CHARS = "^[a-zA-Z]*$";
+    private static final String ALPHA_CHARS = "^[a-zA-Z]+(( )+[a-zA-z]+)*$";
     private static final String INVALID_CHAR_MSG = " _-&:,/.";
     private static final String INVALID_CHAR = "The (%s) contains invalid value. Only following special characters are allowed %s, The String should not start with special characters and should not follow two immediately.";
     
