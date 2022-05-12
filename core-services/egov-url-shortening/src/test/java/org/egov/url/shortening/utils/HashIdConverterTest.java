5b0580933e228f87adde7ef016f90cdf69bac6f3
praneeth <praneeth.p@beehyv.com>
2022-05-12 23:58:32 +0530

    changed min.length of hashid

---
+++ core-services/egov-url-shortening/src/test/java/org/egov/url/shortening/utils/HashIdConverterTest.java
@@ -0,0 +1,26 @@
+//package org.egov.url.shortening.utils;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class HashIdConverterTest {
+//
+//    @Autowired
+//    private HashIdConverter hashIdConverter;
+//
+//    Long id = 1L;
+//
+//    @Test
+//    void testcreateHashStringForId() {
+//
+//        String test = hashIdConverter.createHashStringForId(id);
+//        assertEquals("1", test);
+//    }
+//}
