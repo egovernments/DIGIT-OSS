31244ccf8b2c4e41394bf7298de3f402ec78dd5f
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2022-05-12 17:40:12 +0530

    Update BillQueryBuilder.java

--- business-services/billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
+++ business-services/billing-service/src/main/java/org/egov/demand/repository/querybuilder/BillQueryBuilder.java
@@ -22,8 +22,10 @@ public class BillQueryBuilder {
 	public static final String BILL_STATUS_UPDATE_BASE_QUERY = "UPDATE egbs_bill_v1 SET status=? {replace} WHERE status='ACTIVE' AND tenantId = ? ";
 	
 	public static final String INSERT_BILL_QUERY = "INSERT into egbs_bill_v1 "
-			+"(id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber, status, additionaldetails)"
-			+"values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			+ " (id, tenantid, payername, payeraddress, payeremail, isactive, iscancelled, createdby, createddate, lastmodifiedby, lastmodifieddate, mobilenumber, status, additionaldetails)"
+			+ " SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,? "
+			+ " WHERE NOT EXISTS (SELECT distinct(consumercode) FROM egbs_bill_v1 bill INNER JOIN egbs_billdetail_v1 billd ON bill.id=billd.billid "
+			+ " WHERE status = 'ACTIVE' AND consumerCode = ?)";
 	
 	public static final String INSERT_BILLDETAILS_QUERY = "INSERT into egbs_billdetail_v1 "
 			+"(id, tenantid, billid, demandid, fromperiod, toperiod, businessservice, billno, billdate, consumercode, consumertype, billdescription, displaymessage, "
