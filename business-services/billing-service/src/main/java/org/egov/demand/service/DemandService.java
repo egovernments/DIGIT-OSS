c26e340f4bbec2314697b45be71a2c1de114238f
hinamakhija-eGov <96222691+hinamakhija-eGov@users.noreply.github.com>
2022-05-17 17:23:34 +0530

    [7053]- Enhanced search api for multiple status search (#2852)

--- business-services/billing-service/src/main/java/org/egov/demand/service/DemandService.java
+++ business-services/billing-service/src/main/java/org/egov/demand/service/DemandService.java
@@ -51,6 +51,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.demand.amendment.model.Amendment;
@@ -476,7 +477,7 @@ public class DemandService {
 		 */
 		AmendmentCriteria amendmentCriteria = AmendmentCriteria.builder()
 				.tenantId(demands.get(0).getTenantId())
-				.status(AmendmentStatus.ACTIVE)
+				.status(Stream.of(AmendmentStatus.ACTIVE.toString()).collect(Collectors.toSet()))
 				.consumerCode(consumerCodes)
 				.build();
 		List<Amendment> amendmentsFromSearch = amendmentRepository.getAmendments(amendmentCriteria);
