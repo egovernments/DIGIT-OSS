2a3a6e96270af4787f84e8dbe4cd06d859bbda4d
jithendar <jithendar_k@trisysit.com>
2022-05-29 23:20:36 +0530

    added pakacges for driver

---
+++ municipal-services/vendor/src/main/java/org/egov/vendor/driver/web/util/DriverConstants.java
@@ -0,0 +1,34 @@
+package org.egov.vendor.driver.web.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DriverConstants {
+
+	public static final String DRIVER_MODULE_CODE = "DRIVER";
+
+	// mdms master names
+	public static final String SUCTION_TYPE = "SuctionType";
+
+	public static final String MODEL = "Model";
+
+	public static final String EMPLOYEE = "EMPLOYEE";
+	public static final String DRIVER = "DRIVER";
+
+	public static final String EMP_STATUS = "EMPLOYED";
+
+	public static final String EMP_TYPE = "CONTRACT";
+
+	public static final String JURIDICTION_HIERARAHY = "ADMIN";
+
+	public static final String JURIDICTION_BOUNDARYTYPE = "City";
+
+	public static final String ASSIGNMENT_DESGNATION = "DESIG_17";
+
+	public static final String AGENCY_TYPE = "AGENCY_TYPE";
+
+	public static final String DRIVER_MODULE = "DRIVER";
+
+	public static final String UPDATE_ERROR = "Update Error";
+
+}
---
+++ municipal-services/vendor/src/main/java/org/egov/vendor/driver/web/util/DriverErrorConstants.java
@@ -0,0 +1,14 @@
+package org.egov.vendor.driver.web.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DriverErrorConstants {
+
+	public static final String INVALID_SEARCH = "INVALID SEARCH";
+	public static final String INVALID_OWNER_ERROR = "INVALID_OWNER_INFO";
+	public static final String INVALID_DRIVER_ERROR = "INVALID_DRIVER_INFO";
+	public static final String ALREADY_DRIVER_EXIST = "DRIVER_ALREADY_EXIST";
+	public static final String DRIVER_ERROR_MESSAGE="Driver already exist with mobilenumber";
+	public static final String INVALID_TENANT_ID_MDMS_MSG = "INVALID_TENANT_ID_MDMS_MSG";
+}
