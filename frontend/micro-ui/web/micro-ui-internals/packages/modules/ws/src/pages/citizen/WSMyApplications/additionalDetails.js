53279e6d705666bc2f1b9c111865f5962725cb5c
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-06-02 21:36:29 +0530

    RAIN-7182 :: WNS :: Citizen :: My Application :: In the Road Cutting details are displaying as "NA" in the "Additional details"

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/citizen/WSMyApplications/additionalDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/citizen/WSMyApplications/additionalDetails.js
@@ -1,7 +1,7 @@
 import { Card, CardSubHeader, Header, LinkButton, Loader, Row, StatusTable, CardSectionHeader } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { stringReplaceAll } from "../../../utils";
 //import PropertyDocument from "../../pageComponents/PropertyDocument";
 
@@ -9,56 +9,168 @@ const WSAdditionalDetails = () => {
   const { t } = useTranslation();
   const user = Digit.UserService.getUser();
   const tenantId = user?.info?.permanentCity || Digit.ULBService.getCurrentTenantId();
-  const applicationNobyData = window.location.href.substring(window.location.href.indexOf("WS_")) || window.location.href.substring(window.location.href.indexOf("SW_"));
+  const applicationNobyData =
+    window.location.href.substring(window.location.href.indexOf("WS_")) || window.location.href.substring(window.location.href.indexOf("SW_"));
 
-  let filter1 = {tenantId: "pb.amritsar", applicationNumber: applicationNobyData }
-const { isLoading, isError, error, data } = Digit.Hooks.ws.useMyApplicationSearch({ filters: filter1 }, { filters: filter1 });
+  let filter1 = { tenantId: "pb.amritsar", applicationNumber: applicationNobyData };
+  const { isLoading, isError, error, data } = Digit.Hooks.ws.useMyApplicationSearch({ filters: filter1 }, { filters: filter1 });
 
-
-
-if (isLoading) {
-  return <Loader />;
-}
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <React.Fragment>
       <Header>{t("WS_COMMON_ADDN_DETAILS")}</Header>
-      <div className='hide-seperator'>
+      <div className="hide-seperator">
         <Card>
           <CardSubHeader>{t("WS_COMMON_CONNECTION_DETAIL")}</CardSubHeader>
           <StatusTable>
-            <Row className="border-none"  label={t("WS_COMMON_TABLE_COL_CONNECTIONTYPE_LABEL")} text={data?.WaterConnection?.[0]?.connectionType || data?.SewerageConnections?.[0]?.connectionType || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_NO_OF_TAPS")} text={data?.WaterConnection?.[0]?.noOfTaps || data?.SewerageConnections?.[0]?.noOfTaps || t("CS_NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_PIPE_SIZE")} text={data?.WaterConnection?.[0]?.pipeSize || data?.SewerageConnections?.[0]?.pipeSize || t("CS_NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_WATER_SOURCE")} text={t(`WS_SERVICES_MASTERS_WATERSOURCE_${stringReplaceAll(data?.WaterConnection?.[0]?.waterSource,".","_")}`) || t(`WS_SERVICES_MASTERS_WATERSOURCE_${stringReplaceAll(data?.SewerageConnections?.[0]?.waterSource,".","_")}`) || t("CS_NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_WATER_SUB_SOURCE")} text={data?.WaterConnection?.[0]?.waterSubSource || data?.SewerageConnections?.[0]?.waterSubSource || t("CS_NA")} textStyle={{ whiteSpace: "pre" }} />
+            <Row
+              className="border-none"
+              label={t("WS_COMMON_TABLE_COL_CONNECTIONTYPE_LABEL")}
+              text={data?.WaterConnection?.[0]?.connectionType || data?.SewerageConnections?.[0]?.connectionType || t("NA")}
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_NO_OF_TAPS")}
+              text={data?.WaterConnection?.[0]?.noOfTaps || data?.SewerageConnections?.[0]?.noOfTaps || t("CS_NA")}
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_PIPE_SIZE")}
+              text={data?.WaterConnection?.[0]?.pipeSize || data?.SewerageConnections?.[0]?.pipeSize || t("CS_NA")}
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_WATER_SOURCE")}
+              text={
+                t(`WS_SERVICES_MASTERS_WATERSOURCE_${stringReplaceAll(data?.WaterConnection?.[0]?.waterSource?.split(".")?.[0], ".", "_")}`) ||
+                t(`WS_SERVICES_MASTERS_WATERSOURCE_${stringReplaceAll(data?.SewerageConnections?.[0]?.waterSource, ".", "_")}`) ||
+                t("CS_NA")
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_WATER_SUB_SOURCE")}
+              text={t(data?.WaterConnection?.[0]?.waterSource?.split(".")?.[1]) || t("CS_NA")}
+              textStyle={{ whiteSpace: "pre" }}
+            />
           </StatusTable>
         </Card>
-        {<Card>
-          <CardSubHeader>{t("WS_COMMON_PLUMBER_DETAILS")}</CardSubHeader>
-          <StatusTable>
-            <Row className="border-none"  label={t("WS_ADDN_DETAILS_PLUMBER_PROVIDED_BY")} text={(data?.WaterConnection?.[0]?.plumberInfo ? t("WS_PLUMBER_ULB") : t("WS_PLUMBER_SELF")) || (data?.SewerageConnections?.[0]?.plumberInfo? t("WS_PLUMBER_ULB") : t("WS_PLUMBER_SELF")) || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            {data?.WaterConnection?.[0]?.plumberInfo && <div>
-            <Row className="border-none"  label={t("WS_PLUMBER_LIC_NO")} text={data?.WaterConnection?.[0]?.plumberInfo?.licenseNo || data?.SewerageConnections?.[0]?.plumberInfo?.licenseNo || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_ADDN_DETAILS_PLUMBER_NAME_LABEL")} text={data?.WaterConnection?.[0]?.plumberInfo?.name || data?.SewerageConnections?.[0]?.plumberInfo?.name || "NA"} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_PLUMBER_MOB_NO")} text={data?.WaterConnection?.[0]?.plumberInfo?.mobileNumber || data?.SewerageConnections?.[0]?.plumberInfo?.mobileNumber || "NA"} textStyle={{ whiteSpace: "pre" }} />
-            </div>}
-          </StatusTable>
-        </Card>}
+        {
+          <Card>
+            <CardSubHeader>{t("WS_COMMON_PLUMBER_DETAILS")}</CardSubHeader>
+            <StatusTable>
+              <Row
+                className="border-none"
+                label={t("WS_ADDN_DETAILS_PLUMBER_PROVIDED_BY")}
+                text={
+                  (data?.WaterConnection?.[0]?.plumberInfo ? t("WS_PLUMBER_ULB") : t("WS_PLUMBER_SELF")) ||
+                  (data?.SewerageConnections?.[0]?.plumberInfo ? t("WS_PLUMBER_ULB") : t("WS_PLUMBER_SELF")) ||
+                  t("NA")
+                }
+                textStyle={{ whiteSpace: "pre" }}
+              />
+              {data?.WaterConnection?.[0]?.plumberInfo && (
+                <div>
+                  <Row
+                    className="border-none"
+                    label={t("WS_PLUMBER_LIC_NO")}
+                    text={data?.WaterConnection?.[0]?.plumberInfo?.licenseNo || data?.SewerageConnections?.[0]?.plumberInfo?.licenseNo || t("NA")}
+                    textStyle={{ whiteSpace: "pre" }}
+                  />
+                  <Row
+                    className="border-none"
+                    label={t("WS_ADDN_DETAILS_PLUMBER_NAME_LABEL")}
+                    text={data?.WaterConnection?.[0]?.plumberInfo?.name || data?.SewerageConnections?.[0]?.plumberInfo?.name || "NA"}
+                    textStyle={{ whiteSpace: "pre" }}
+                  />
+                  <Row
+                    className="border-none"
+                    label={t("WS_PLUMBER_MOB_NO")}
+                    text={data?.WaterConnection?.[0]?.plumberInfo?.mobileNumber || data?.SewerageConnections?.[0]?.plumberInfo?.mobileNumber || "NA"}
+                    textStyle={{ whiteSpace: "pre" }}
+                  />
+                </div>
+              )}
+            </StatusTable>
+          </Card>
+        }
         <Card>
           <CardSubHeader>{t("WS_ROAD_CUTTING_DETAILS")}</CardSubHeader>
           <StatusTable>
-            <Row className="border-none"  label={t("WS_ADDN_DETAIL_ROAD_TYPE")} text={data?.WaterConnection?.[0]?.roadType || data?.SewerageConnections?.[0]?.roadType || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_ADDN_DETAILS_AREA_LABEL")} text={data?.WaterConnection?.[0]?.roadCuttingArea || data?.SewerageConnections?.[0]?.roadCuttingArea || t("NA")} textStyle={{ whiteSpace: "pre" }} />
+            <Row
+              className="border-none"
+              label={t("WS_ADDN_DETAIL_ROAD_TYPE")}
+              text={
+                t(
+                  `WS_ROADTYPE_${
+                    data?.WaterConnection?.[0]?.roadCuttingInfo?.[0]?.roadType || data?.SewerageConnections?.[0]?.roadCuttingInfo?.[0]?.roadType
+                  }`
+                ) || t("NA")
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_ADDN_DETAILS_AREA_LABEL")}
+              text={
+                data?.WaterConnection?.[0]?.roadCuttingInfo?.[0]?.roadCuttingArea ||
+                data?.SewerageConnections?.roadCuttingInfo?.[0]?.roadCuttingArea ||
+                t("NA")
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
           </StatusTable>
         </Card>
         <Card>
           <CardSubHeader>{t("WS_ACTIVATION_DETAILS")}</CardSubHeader>
           <StatusTable>
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_CONN_EXECUTION_DATE")} text={data?.WaterConnection?.[0]?.dateEffectiveFrom || data?.SewerageConnections?.[0]?.dateEffectiveFrom || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_SERV_DETAIL_METER_ID")} text={data?.WaterConnection?.[0]?.meterId || data?.SewerageConnections?.[0]?.meterId || t("NA")} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_ADDN_DETAIL_METER_INSTALL_DATE")} text={data?.WaterConnection?.[0]?.meterInstallationDate || data?.SewerageConnections?.[0]?.meterInstallationDate || "NA"} textStyle={{ whiteSpace: "pre" }} />
-            <Row className="border-none"  label={t("WS_ADDN_DETAILS_INITIAL_METER_READING")} text={data?.WaterConnection?.[0]?.additionalDetails?.initialMeterReading || data?.SewerageConnections?.[0]?.additionalDetails?.initialMeterReading || "NA"} textStyle={{ whiteSpace: "pre" }} />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_CONN_EXECUTION_DATE")}
+              text={
+                data?.WaterConnection?.[0]?.connectionExecutionDate || data?.SewerageConnections?.[0]?.connectionExecutionDate
+                  ? Digit.DateUtils.ConvertEpochToDate(
+                      data?.WaterConnection?.[0]?.connectionExecutionDate || data?.SewerageConnections?.[0]?.connectionExecutionDate
+                    )
+                  : t("NA")
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_SERV_DETAIL_METER_ID")}
+              text={data?.WaterConnection?.[0]?.meterId || data?.SewerageConnections?.[0]?.meterId || t("NA")}
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_ADDN_DETAIL_METER_INSTALL_DATE")}
+              text={
+                data?.WaterConnection?.[0]?.meterInstallationDate || data?.SewerageConnections?.[0]?.meterInstallationDate
+                  ? Digit.DateUtils.ConvertEpochToDate(
+                      data?.WaterConnection?.[0]?.meterInstallationDate || data?.SewerageConnections?.[0]?.meterInstallationDate
+                    )
+                  : t("NA")
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
+            <Row
+              className="border-none"
+              label={t("WS_ADDN_DETAILS_INITIAL_METER_READING")}
+              text={
+                data?.WaterConnection?.[0]?.additionalDetails?.initialMeterReading ||
+                data?.SewerageConnections?.[0]?.additionalDetails?.initialMeterReading ||
+                "NA"
+              }
+              textStyle={{ whiteSpace: "pre" }}
+            />
           </StatusTable>
         </Card>
       </div>
