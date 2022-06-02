97849661f3dc15e9b83ed431e2c3a42a51950015
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-06-02 20:57:05 +0530

    RAIN-7160 :: WNS Application payment issue resolved

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
@@ -11,7 +11,7 @@ import {
   InfoBanner,
   Loader,
   Toast,
-  CardText
+  CardText,
 } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 import { useForm, Controller } from "react-hook-form";
@@ -26,7 +26,6 @@ export const SelectPaymentType = (props) => {
   const paymentAmount = state?.paymentAmount;
   const { t } = useTranslation();
   const history = useHistory();
-
   const { pathname, search } = useLocation();
   // const menu = ["AXIS"];
   const { consumerCode, businessService } = useParams();
@@ -43,8 +42,9 @@ export const SelectPaymentType = (props) => {
       setShowToast({ key: true, label: "CS_BILL_NOT_FOUND" });
     }
   }, [paymentdetails]);
-  useEffect(()=>{
-    localStorage.setItem("BillPaymentEnabled","true")  },[])
+  useEffect(() => {
+    localStorage.setItem("BillPaymentEnabled", "true");
+  }, []);
   const { name, mobileNumber } = state;
 
   const billDetails = paymentdetails?.Bill ? paymentdetails?.Bill[0] : {};
@@ -52,7 +52,7 @@ export const SelectPaymentType = (props) => {
   const onSubmit = async (d) => {
     const filterData = {
       Transaction: {
-        tenantId: tenantId,
+        tenantId: billDetails?.tenantId,
         txnAmount: paymentAmount || billDetails.totalAmount,
         module: businessService,
         billId: billDetails.id,
@@ -66,9 +66,9 @@ export const SelectPaymentType = (props) => {
           },
         ],
         user: {
-          name: name || userInfo?.info?.name,
-          mobileNumber: mobileNumber || userInfo?.info?.mobileNumber,
-          tenantId: tenantId,
+          name: name || userInfo?.info?.name || billDetails?.payerName,
+          mobileNumber: mobileNumber || userInfo?.info?.mobileNumber || billDetails?.mobileNumber,
+          tenantId: billDetails?.tenantId,
         },
         // success
         callbackUrl: window.location.href.includes("mcollect")
@@ -81,7 +81,7 @@ export const SelectPaymentType = (props) => {
     };
 
     try {
-      const data = await Digit.PaymentService.createCitizenReciept(tenantId, filterData);
+      const data = await Digit.PaymentService.createCitizenReciept(billDetails?.tenantId, filterData);
       const redirectUrl = data?.Transaction?.redirectUrl;
       window.location = redirectUrl;
     } catch (error) {
@@ -97,7 +97,7 @@ export const SelectPaymentType = (props) => {
   if (authorization === "true" && !userInfo.access_token) {
     localStorage.clear();
     sessionStorage.clear();
-     return <Redirect to={`/digit-ui/citizen/login?from=${encodeURIComponent(pathname + search)}`} />;
+    return <Redirect to={`/digit-ui/citizen/login?from=${encodeURIComponent(pathname + search)}`} />;
   }
 
   if (isLoading || paymentLoading) {
@@ -110,7 +110,7 @@ export const SelectPaymentType = (props) => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <Header>{t("PAYMENT_CS_HEADER")}</Header>
         <Card>
-          <div className="payment-amount-info" style={{marginBottom: "26px"}}>
+          <div className="payment-amount-info" style={{ marginBottom: "26px" }}>
             <CardLabel className="dark">{t("PAYMENT_CS_TOTAL_AMOUNT_DUE")}</CardLabel>
             <CardSectionHeader> ₹ {paymentAmount || billDetails?.totalAmount}</CardSectionHeader>
           </div>
