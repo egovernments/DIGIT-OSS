db799ba28fd66004efff07e1f820348b9ba12fa3
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2022-06-09 04:56:34 +0530

    RAIN-7219::WNS Property field fixes on Apply New Connection Screen & Service centre icon change in help section (#3102)
    * RAIN-7219::WNS Apply New Connection Property Id field fixes
    
    * Citizen::Service center Icon change in Help Section

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/svgindex.js
@@ -956,9 +956,13 @@ const HelpLineIcon = ({ className, styles }) => (
 );
 
 const ServiceCenterIcon = ({ className, styles }) => (
-  <svg className={className} style={{ ...styles }} width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M10 0L0 5V7H20V5L10 0Z" fill="#0B0C0C" />
-  </svg>
+  <svg className={className} style={{ ...styles }} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 10H4V17H7V10Z" fill="#0B0C0C"/>
+    <path d="M13.5 10H10.5V17H13.5V10Z" fill="#0B0C0C"/>
+    <path d="M22 19H2V22H22V19Z" fill="#0B0C0C"/>
+    <path d="M20 10H17V17H20V10Z" fill="#0B0C0C"/>
+    <path d="M12 1L2 6V8H22V6L12 1Z" fill="#0B0C0C"/>
+</svg>
 );
 
 const TimerIcon = ({ className, styles }) => (
