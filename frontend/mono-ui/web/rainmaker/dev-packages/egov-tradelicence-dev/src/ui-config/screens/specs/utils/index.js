d2059bb98cfaf132c36edb64323b07bbf1e61583
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-06-13 12:54:51 +0530

    DIVOC POC enabled in tl module

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/utils/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/utils/index.js
@@ -14,6 +14,7 @@ import "./index.css";
 import { showHideAdhocPopup as showReqDocPopup} from "egov-ui-framework/ui-utils/commons";
 import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
 import { getPaymentSearchAPI } from "egov-ui-kit/utils/commons";
+import { downloadPdfFile } from "egov-ui-kit/utils/api";
 
 export const getCommonApplyFooter = children => {
   return {
@@ -1027,7 +1028,7 @@ export const downloadCertificateForm = async (Licenses, mode = 'download') => {
   ]
   const DOWNLOADRECEIPT = {
     GET: {
-      URL: "/pdf-service/v1/_create",
+      URL: "/pdf-service/v1/_createnosave",
       ACTION: "_get",
     },
   };
@@ -1046,17 +1047,21 @@ export const downloadCertificateForm = async (Licenses, mode = 'download') => {
   }
   else {
     try {
-      httpRequest("post", DOWNLOADRECEIPT.GET.URL, DOWNLOADRECEIPT.GET.ACTION, queryStr, { Licenses }, { 'Accept': 'application/json' }, { responseType: 'arraybuffer' })
-        .then(res => {
-          res.filestoreIds[0]
-          if (res && res.filestoreIds && res.filestoreIds.length > 0) {
-            res.filestoreIds.map(fileStoreId => {
-              downloadReceiptFromFilestoreID(fileStoreId, mode)
-            })
-          } else {
-            console.log("Error In Acknowledgement form Download");
-          }
-        });
+      set(Licenses[0],'TradeTypeMessage',getLocaleLabels(
+        `TL_${get(Licenses[0],"tradeLicenseDetail.tradeUnits[0].tradeType",'NA')}`,
+        `TL_${get(Licenses[0],"tradeLicenseDetail.tradeUnits[0].tradeType",'NA')}`));
+      downloadPdfFile(DOWNLOADRECEIPT.GET.URL,'post',queryStr,{ Licenses },{},false,`TL-${applicationNumber}.pdf`,()=>{});
+      // httpRequest("post", DOWNLOADRECEIPT.GET.URL, DOWNLOADRECEIPT.GET.ACTION, queryStr, { Licenses }, { 'Accept': 'application/json' }, { responseType: 'arraybuffer' })
+      //   .then(res => {
+      //     res.filestoreIds[0]
+      //     if (res && res.filestoreIds && res.filestoreIds.length > 0) {
+      //       res.filestoreIds.map(fileStoreId => {
+      //         downloadReceiptFromFilestoreID(fileStoreId, mode)
+      //       })
+      //     } else {
+      //       console.log("Error In Acknowledgement form Download");
+      //     }
+      //   });
     } catch (exception) {
       alert('Some Error Occured while downloading Acknowledgement form!');
     }
