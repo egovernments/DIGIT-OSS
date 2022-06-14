58a5db7274671f913c25bd764b27fcd22b51bb81
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2022-06-14 16:47:40 +0530

    RAIN-7027 : Fix for undefined in sub-usage type not loading (#3202)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/SelectPTUnits.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/SelectPTUnits.js
@@ -32,7 +32,7 @@ const formatUnits = (units = [], currentFloor, isFloor) => {
       builtUpArea: unit?.constructionDetail?.builtUpArea,
       usageCategory: usageCategory ? { code: usageCategory, i18nKey: `PROPERTYTAX_BILLING_SLAB_${usageCategory}` } : {},
       occupancyType: unit?.occupancyType ? { code: unit.occupancyType, i18nKey: `PROPERTYTAX_OCCUPANCYTYPE_${unit?.occupancyType}` } : "",
-      floorNo: unit?.floorNo ? { code: unit.floorNo, i18nKey: `PROPERTYTAX_FLOOR_${unit?.floorNo}` } : {},
+      floorNo: unit?.floorNo || Number.isInteger(unit?.floorNo) ? { code: unit.floorNo, i18nKey: `PROPERTYTAX_FLOOR_${unit?.floorNo}` } : {},
       unitType: unit?.unitType ? { code: unit.unitType, i18nKey: `PROPERTYTAX_BILLING_SLAB_${unit?.unitType}` } : "",
     };
   });
