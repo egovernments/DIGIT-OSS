6d1604adbc1c537a7ce81f68281c6cb1c781d10e
Priyanka-eGov <74049060+Priyanka-eGov@users.noreply.github.com>
2022-06-16 14:26:41 +0530

    RAIN-7323 Fixing Citizen Approval Issue (#3226)
    * RAIN-7323 Adding logs
    
    * RAIN-7323 Adding logs
    
    * RAIN-7323 Fixing distinct uuid
    
    * RAIN-7323 Removing logs
    
    * RAIN-7323 Formatting and removing comments

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/workflow/WorkflowIntegrator.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/workflow/WorkflowIntegrator.java
@@ -88,7 +88,7 @@ public class WorkflowIntegrator {
 		obj.put(MODULENAMEKEY, MODULENAMEVALUE);
 		obj.put(ACTIONKEY, bpa.getWorkflow().getAction());
 		obj.put(COMMENTKEY, bpa.getWorkflow().getComments());
-		
+
 		if (!CollectionUtils.isEmpty(bpa.getWorkflow().getAssignes())) {
 			List<Map<String, String>> uuidmaps = new LinkedList<>();
 			bpa.getWorkflow().getAssignes().forEach(assignee -> {
@@ -98,7 +98,7 @@ public class WorkflowIntegrator {
 			});
 			obj.put(ASSIGNEEKEY, uuidmaps);
 		}
-		
+
 		obj.put(DOCUMENTSKEY, bpa.getWorkflow().getVarificationDocuments());
 		array.add(obj);
 		JSONObject workFlowRequest = new JSONObject();
