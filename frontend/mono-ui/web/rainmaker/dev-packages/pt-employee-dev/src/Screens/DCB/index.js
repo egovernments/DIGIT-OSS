01f2da3f8da26b4ae4b692cccd06ee5b00085122
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2022-06-22 10:14:06 +0530

    Changing url params for reassessment

--- frontend/mono-ui/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/DCB/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/pt-employee-dev/src/Screens/DCB/index.js
@@ -974,7 +974,7 @@ class FormWizardDataEntry extends Component {
     const { location, finalData } = this.props;
     const { search } = location;
     const isCompletePayment = getQueryValue(search, "isCompletePayment");
-    const isAssesment = getQueryValue(search, "assessment");
+    const isAssesment = getQueryValue(search, "newreassessment");
     switch (selected) {
       case 0:
         return (
