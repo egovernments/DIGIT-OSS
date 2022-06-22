eeea3930bb9f80c60a68d4518e89e02a3fcebb57
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-06-22 15:42:20 +0530

    PAYGOV Payment integration , corrected the order

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
@@ -11,14 +11,13 @@ import {
   InfoBanner,
   Loader,
   Toast,
-  CardText
+  CardText,
 } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 import { useForm, Controller } from "react-hook-form";
 import { useParams, useHistory, useLocation, Redirect } from "react-router-dom";
 import { stringReplaceAll } from "../bills/routes/bill-details/utils";
-import $ from 'jquery';
-
+import $ from "jquery";
 
 export const SelectPaymentType = (props) => {
   const { state = {} } = useLocation();
@@ -36,15 +35,18 @@ export const SelectPaymentType = (props) => {
   const stateTenant = Digit.ULBService.getStateId();
   const { control, handleSubmit } = useForm();
   const { data: menu, isLoading } = Digit.Hooks.useCommonMDMS(stateTenant, "DIGIT-UI", "PaymentGateway");
-  const { data: paymentdetails, isLoading: paymentLoading } = Digit.Hooks.useFetchPayment({ tenantId: tenantId, consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode, businessService }, {});
+  const { data: paymentdetails, isLoading: paymentLoading } = Digit.Hooks.useFetchPayment(
+    { tenantId: tenantId, consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode, businessService },
+    {}
+  );
   useEffect(() => {
     if (paymentdetails?.Bill && paymentdetails.Bill.length == 0) {
       setShowToast({ key: true, label: "CS_BILL_NOT_FOUND" });
     }
   }, [paymentdetails]);
-  useEffect(()=>{
-    localStorage.setItem("BillPaymentEnabled","true") 
-   },[])
+  useEffect(() => {
+    localStorage.setItem("BillPaymentEnabled", "true");
+  }, []);
   const { name, mobileNumber } = state;
 
   const billDetails = paymentdetails?.Bill ? paymentdetails?.Bill[0] : {};
@@ -58,7 +60,7 @@ export const SelectPaymentType = (props) => {
         billId: billDetails.id,
         consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode,
         productInfo: "Common Payment",
-        gateway: "PAYGOV",
+        gateway: d?.paymentType || "AXIS",
         taxAndPayments: [
           {
             billId: billDetails.id,
@@ -83,35 +85,64 @@ export const SelectPaymentType = (props) => {
     try {
       const data = await Digit.PaymentService.createCitizenReciept(tenantId, filterData);
       const redirectUrl = data?.Transaction?.redirectUrl;
-      //window.location = redirectUrl;
-      try{
-                   const gatewayParam = redirectUrl?.split("?")?.slice(1)?.join("?")?.split("&")?.reduce((curr,acc)=>{
-      var d=acc.split('=')
-curr[d[0]]=d[1]
-      return curr
-  },{}); 
+      if (d?.paymentType == "AXIS") {
+        window.location = redirectUrl;
+      } else {
+        // new payment gatewayfor UPYOG pay
+        try {
+          const gatewayParam = redirectUrl
+            ?.split("?")
+            ?.slice(1)
+            ?.join("?")
+            ?.split("&")
+            ?.reduce((curr, acc) => {
+              var d = acc.split("=");
+              curr[d[0]] = d[1];
+              return curr;
+            }, {});
+          var newForm = $("<form>", {
+            action: gatewayParam.txURL,
+            method: "POST",
+            target: "_top",
+          });
+          
+          const orderForNDSLPaymentSite = [
+            "checksum",
+            "messageType",
+            "merchantId",
+            "serviceId",
+            "orderId",
+            "customerId",
+            "transactionAmount",
+            "currencyCode",
+            "requestDateTime",
+            "successUrl",
+            "failUrl",
+            "additionalField1",
+            "additionalField2",
+            "additionalField3",
+            "additionalField4",
+            "additionalField5",
+          ];
 
-                console.log("gateway parameter",gatewayParam);
-                var newForm = $('<form>', {
-                  action: gatewayParam.txURL,
-                  method: 'post',
-                  target: '_top',
-                });
-                for (var key in gatewayParam) {
-                  newForm.append(
-                    $('<input>', {
-                      name: key,
-                      value: gatewayParam[key],
-                      type: 'hidden',
-                    }))
-                }
-
-                $(document.body).append(newForm);
-               newForm.submit();
-              }catch (e) {
-                console.log("Error in payment redirect ",e);
-                //window.location = redirectionUrl;
-              }
+          // override default date for UPYOG Custom pay
+          gatewayParam["requestDateTime"] = gatewayParam["requestDateTime"]?.split(new Date().getFullYear()).join(`${new Date().getFullYear()} `);
+          for (var key of orderForNDSLPaymentSite) {
+            newForm.append(
+              $("<input>", {
+                name: key,
+                value: gatewayParam[key],
+                type: "hidden",
+              })
+            );
+          }
+          $(document.body).append(newForm);
+          newForm.submit();
+        } catch (e) {
+          console.log("Error in payment redirect ", e);
+          //window.location = redirectionUrl;
+        }
+      }
     } catch (error) {
       let messageToShow = "CS_PAYMENT_UNKNOWN_ERROR_ON_SERVER";
       if (error.response?.data?.Errors?.[0]) {
@@ -138,7 +169,7 @@ curr[d[0]]=d[1]
       <form onSubmit={handleSubmit(onSubmit)}>
         <Header>{t("PAYMENT_CS_HEADER")}</Header>
         <Card>
-          <div className="payment-amount-info" style={{marginBottom: "26px"}}>
+          <div className="payment-amount-info" style={{ marginBottom: "26px" }}>
             <CardLabel className="dark">{t("PAYMENT_CS_TOTAL_AMOUNT_DUE")}</CardLabel>
             <CardSectionHeader> ₹ {paymentAmount || billDetails?.totalAmount}</CardSectionHeader>
           </div>
