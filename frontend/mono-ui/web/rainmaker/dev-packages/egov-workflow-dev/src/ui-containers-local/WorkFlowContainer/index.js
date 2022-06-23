d713fe7a5ac1c263ccb49a260e8f2f8c2839118b
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2022-06-23 11:44:29 +0530

    adding business service to mutation workflow call

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
@@ -368,7 +368,7 @@ class WorkFlowContainer extends React.Component {
             });
         });
     } else {
-      const states = find(data.states, { uuid: nextAction });
+      const states = find(data && data.states && data.states, { uuid: nextAction });
       states &&
         states.actions &&
         states.actions.forEach(action => {
@@ -387,7 +387,7 @@ class WorkFlowContainer extends React.Component {
     const data = businessServiceData && businessServiceData.length > 0 ? find(businessServiceData, { businessService: moduleName }) : [];
     // const nextState = data && data.length > 0 find(data.states, { uuid: nextStateUUID });
 
-    const isLastState = data ? find(data.states, { uuid: nextStateUUID }).isTerminateState : false;
+    const isLastState = data && data.states? find(data.states, { uuid: nextStateUUID }).isTerminateState : false;
     return isLastState;
   };
 
