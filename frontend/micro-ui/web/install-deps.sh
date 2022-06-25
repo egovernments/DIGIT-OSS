ad3c309137390439ee01cab3405887903801845d
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-06-25 21:19:00 +0530

    try caching

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -6,10 +6,9 @@ echo "Main Branch: $BRANCH"
 
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && echo "installing packages" && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
+cd $INTERNALS && echo "$(git log -1 --pretty=%B)" && echo "installing packages" && yarn install && echo "starting build" && yarn build && echo "building finished"  \; 
 cd ..
 
-rm -rf node_modules
-rm -f yarn.lock
+
 
 # yarn install
