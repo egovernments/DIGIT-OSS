b1231772bd856c99f1c67f37cd59c65fb7916cea
Nipun Arora <aroranipun1@gmail.com>
2022-07-12 10:37:18 +0530

    RAIN-7379 Bill amendment : Reduced amount and Additional amount checkboxes are not aligned properly. (#3416)
    * Update ApplicationBillAmendment.js
    
    * Added download bill header

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/bills/src/pages/employee/DownloadBill/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/bills/src/pages/employee/DownloadBill/index.js
@@ -86,6 +86,7 @@ const DownloadBillInbox = () => {
   }, [isLoading, data]);
   return (
     <div>
+      <Header>{t("ACTION_TEST_DOWNLOADBILLS")}</Header>
       <TableComponent />
       {showToast?.label && (
         <Toast
