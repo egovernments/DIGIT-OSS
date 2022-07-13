96049ff39a7cab87a25b046cdfd3c299c5c71704
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2022-07-13 17:37:58 +0530

    RAIN-6996::TL Acknowledgement PDF Property ID added (#3432)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/utils/getTLAcknowledgementData.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/utils/getTLAcknowledgementData.js
@@ -109,6 +109,7 @@ const getAddressDetails = (application, t) => {
   return {
     title: "",
     values: [
+      { title: t("TL_PROPERTY_ID"), value: application?.tradeLicenseDetail?.additionalDetail?.propertyId || t("CS_NA") },
       { title: t("CORE_COMMON_PINCODE"), value: application?.tradeLicenseDetail?.address?.pincode || t("CS_NA") },
       { title: t("MYCITY_CODE_LABEL"), value: t(application?.tradeLicenseDetail?.address?.city) || t("CS_NA") },
       { title: t("TL_LOCALIZATION_LOCALITY"), value: t(getTransaltedLocality(application?.tradeLicenseDetail?.address)) || t("CS_NA") },
