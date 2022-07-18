54bdbfdfa21803e3f0f8ef49f7f1a54d0fd27626
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-07-18 10:41:33 +0530

    Pag gov integration changes (#3477)
    * tryout for split and get the formadata
    
    * Update index.js
    
    * Update Dockerfile
    
    * PAYGOV Payment integration , corrected the order
    
    * Tryout for payment gateway with fordata submit
    
    * Update payGov.js
    
    * removing double quotes for formdata
    
    * set the transaction url as callbackurl
    
    * Update index.js
    
    * updated tx url to hit pg service update
    
    * Revert "updated tx url to hit pg service update"
    
    This reverts commit d1953d49cf46bc0a5287ab3d68a44ca0195c33c5.
    
    * for pg service redirect
    
    * try for the nudm pay
    
    * Update index.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
@@ -11,14 +11,14 @@ import {
   InfoBanner,
   Loader,
   Toast,
-  CardText
+  CardText,
 } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 import { useForm, Controller } from "react-hook-form";
 import { useParams, useHistory, useLocation, Redirect } from "react-router-dom";
 import { stringReplaceAll } from "../bills/routes/bill-details/utils";
-import $ from 'jquery';
-
+import $ from "jquery";
+import { makePayment } from "./payGov";
 
 export const SelectPaymentType = (props) => {
   const { state = {} } = useLocation();
@@ -36,15 +36,18 @@ export const SelectPaymentType = (props) => {
   const stateTenant = Digit.ULBService.getStateId();
   const { control, handleSubmit } = useForm();
   const { data: menu, isLoading } = Digit.Hooks.useCommonMDMS(stateTenant, "DIGIT-UI", "PaymentGateway");
-  const { data: paymentdetails, isLoading: paymentLoading } = Digit.Hooks.useFetchPayment({ tenantId: tenantId, consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode, businessService }, {});
+  const { data: paymentdetails, isLoading: paymentLoading } = Digit.Hooks.useFetchPayment(
+    { tenantId: tenantId, consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode, businessService },
+    {}
+  );
   useEffect(() => {
     if (paymentdetails?.Bill && paymentdetails.Bill.length == 0) {
       setShowToast({ key: true, label: "CS_BILL_NOT_FOUND" });
     }
   }, [paymentdetails]);
-  useEffect(()=>{
-    localStorage.setItem("BillPaymentEnabled","true") 
-   },[])
+  useEffect(() => {
+    localStorage.setItem("BillPaymentEnabled", "true");
+  }, []);
   const { name, mobileNumber } = state;
 
   const billDetails = paymentdetails?.Bill ? paymentdetails?.Bill[0] : {};
@@ -58,7 +61,7 @@ export const SelectPaymentType = (props) => {
         billId: billDetails.id,
         consumerCode: wrkflow === "WNS" ? stringReplaceAll(consumerCode, "+", "/") : consumerCode,
         productInfo: "Common Payment",
-        gateway: "PAYGOV",
+        gateway: d?.paymentType || "AXIS",
         taxAndPayments: [
           {
             billId: billDetails.id,
@@ -83,30 +86,80 @@ export const SelectPaymentType = (props) => {
     try {
       const data = await Digit.PaymentService.createCitizenReciept(tenantId, filterData);
       const redirectUrl = data?.Transaction?.redirectUrl;
-      //window.location = redirectUrl;
-      try{
-                const gatewayParam = JSON.parse(redirectUrl);
-                console.log("gateway parameter",gatewayParam);
-                var newForm = $('<form>', {
-                  action: gatewayParam.txURL,
-                  method: 'post',
-                  target: '_top',
-                });
-                for (var key in gatewayParam) {
-                  newForm.append(
-                    $('<input>', {
-                      name: key,
-                      value: gatewayParam[key],
-                      type: 'hidden',
-                    }))
-                }
+      if (d?.paymentType == "AXIS") {
+        window.location = redirectUrl;
+      } else {
+        // new payment gatewayfor UPYOG pay
+        try {
 
-                $(document.body).append(newForm);
-               newForm.submit();
-              }catch (e) {
-                console.log("Error in payment redirect ",e);
-                //window.location = redirectionUrl;
-              }
+          const gatewayParam = redirectUrl
+            ?.split("?")
+            ?.slice(1)
+            ?.join("?")
+            ?.split("&")
+            ?.reduce((curr, acc) => {
+              var d = acc.split("=");
+              curr[d[0]] = d[1];
+              return curr;
+            }, {});
+          var newForm = $("<form>", {
+            action: gatewayParam.txURL,
+            method: "POST",
+            target: "_top",
+          });
+          
+          const orderForNDSLPaymentSite = [
+            "checksum",
+            "messageType",
+            "merchantId",
+            "serviceId",
+            "orderId",
+            "customerId",
+            "transactionAmount",
+            "currencyCode",
+            "requestDateTime",
+            "successUrl",
+            "failUrl",
+            "additionalField1",
+            "additionalField2",
+            "additionalField3",
+            "additionalField4",
+            "additionalField5",
+          ];
+
+          // override default date for UPYOG Custom pay
+          gatewayParam["requestDateTime"] = gatewayParam["requestDateTime"]?.split(new Date().getFullYear()).join(`${new Date().getFullYear()} `);
+        
+          gatewayParam["successUrl"]= redirectUrl?.split("successUrl=")?.[1]?.split("eg_pg_txnid=")?.[0]+'eg_pg_txnid=' +gatewayParam?.orderId;
+          gatewayParam["failUrl"]= redirectUrl?.split("failUrl=")?.[1]?.split("eg_pg_txnid=")?.[0]+'eg_pg_txnid=' +gatewayParam?.orderId;
+          // gatewayParam["successUrl"]= data?.Transaction?.callbackUrl;
+          // gatewayParam["failUrl"]= data?.Transaction?.callbackUrl;
+          
+          // var formdata = new FormData();
+          
+          for (var key of orderForNDSLPaymentSite) {
+           
+            // formdata.append(key,gatewayParam[key]);
+           
+            newForm.append(
+              $("<input>", {
+                name: key,
+                value: gatewayParam[key],
+                // type: "hidden",
+              })
+            );
+          }
+          $(document.body).append(newForm);
+          newForm.submit();
+
+        
+          // makePayment(gatewayParam.txURL,formdata);
+
+        } catch (e) {
+          console.log("Error in payment redirect ", e);
+          //window.location = redirectionUrl;
+        }
+      }
     } catch (error) {
       let messageToShow = "CS_PAYMENT_UNKNOWN_ERROR_ON_SERVER";
       if (error.response?.data?.Errors?.[0]) {
@@ -133,7 +186,7 @@ export const SelectPaymentType = (props) => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <Header>{t("PAYMENT_CS_HEADER")}</Header>
         <Card>
-          <div className="payment-amount-info" style={{marginBottom: "26px"}}>
+          <div className="payment-amount-info" style={{ marginBottom: "26px" }}>
             <CardLabel className="dark">{t("PAYMENT_CS_TOTAL_AMOUNT_DUE")}</CardLabel>
             <CardSectionHeader> ₹ {paymentAmount || billDetails?.totalAmount}</CardSectionHeader>
           </div>
---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/payGov.js
@@ -0,0 +1,18 @@
+export const makePayment =async(url,data)=>{
+    var myHeaders = new Headers();
+myHeaders.append("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+myHeaders.append("Access-Control-Allow-Origin", "*");
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: data,
+  "mode": "no-cors",
+  redirect: 'follow'
+};
+
+fetch(url, requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
