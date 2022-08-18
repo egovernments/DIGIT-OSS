775da0ea5a128ed587e06a754fe555a96bbb19fb
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2022-08-18 16:48:12 +0530

    UM-4347 missing usecase

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/universalCollectionResources/function.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/universalCollectionResources/function.js
@@ -140,12 +140,18 @@ export const searchApiCall = async (state, dispatch) => {
       const result = await getPropertySearchResults(queryObject3);
       if(result && result.Properties && result.Properties[0].status ==="INWORKFLOW")
       {
-        response[0].propertystatus = "INWORKFLOW" 
+        for(let i=0;i<response.length;i++)
+         {
+         response[i].propertystatus = "INWORKFLOW" 
+         }
         filetedresult = response;
       }
       else      
       {
-        response[0].propertystatus = "ACTIVE" 
+        for(let i=0;i<response.length;i++)
+         {
+         response[i].propertystatus = "ACTIVE" 
+         }
         filetedresult = response;
       }
      }
