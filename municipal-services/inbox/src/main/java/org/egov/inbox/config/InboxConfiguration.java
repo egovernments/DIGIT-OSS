8e9544c109ef8f44fde48d0dea1ccada45404a80
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2022-09-15 18:06:01 +0530

    DPG-745: Mapped variable from app properties (#3953)

--- municipal-services/inbox/src/main/java/org/egov/inbox/config/InboxConfiguration.java
+++ municipal-services/inbox/src/main/java/org/egov/inbox/config/InboxConfiguration.java
@@ -109,7 +109,7 @@ public class InboxConfiguration {
 	@Value("${egov.internal.microservice.user.uuid}")
 	private String egovInternalMicroserviceUserUuid;
 
-	@Value("${parent.level.tenant.id}")
+	@Value("${egov.state.level.tenant.id}")
 	private String parentLevelTenantId;
 
 	// ES Config
