8325a81ce32a185ef01157ae8eb02f675c34e284
SudeepAccess <sudeep.accessinfotechindia@gmail.com>
2022-11-01 12:26:21 +0530

    downwardjs

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dss/src/components/ArrowDownward.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dss/src/components/ArrowDownward.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import Arrow_Downward from "../images/Arrow_Downward.svg"
-=======
 import Arrow_Downward from "../images/Arrow_Downward.svg";
->>>>>>> b9f8be5d587ed5fdb50ff06c2554ddfdf80257fc
 
 export function ArrowDownwardElement(marginRight, marginLeft) {
   return (
