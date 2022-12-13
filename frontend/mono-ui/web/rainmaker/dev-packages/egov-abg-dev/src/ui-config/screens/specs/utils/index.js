43b86823bb69163d9e58af42f83f651396179dc6
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2022-12-13 11:32:19 +0530

    UM-5266 UKD: Not able to download Group Bill

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/utils/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/utils/index.js
@@ -115,7 +115,7 @@ export const downloadMultipleBill = async (bills = [], configKey, businesService
       item.additionalDetails = addDetail;
     })
 
-    var actualBills = [], size = 40;
+    var actualBills = [], size = 5;
     for (let i = 0; bills.length > 0; i++) {
       actualBills.push(bills.splice(0, size));
     }
@@ -124,9 +124,7 @@ export const downloadMultipleBill = async (bills = [], configKey, businesService
       }
   } catch (error) {
     console.log(error);
-
   }
-
 }
 
 export const downloadPdfs = async (DOWNLOADRECEIPT, queryStr, bills) => {
