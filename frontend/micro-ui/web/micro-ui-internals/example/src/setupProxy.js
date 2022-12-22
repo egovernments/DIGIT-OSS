2c8142fe808e8297048db8bfd9977a63472f7367
parveen.kumari <parveen.kumari@dxc.com>
2022-12-22 15:43:20 +0530

    setup

--- frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const createProxy = createProxyMiddleware({
   //target: process.env.REACT_APP_PROXY_API || "https://uat.digit.org",
   // target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
-  target: process.env.REACT_APP_PROXY_API || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_API || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const assetsProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_ASSETS || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_ASSETS || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const apiSetuProxy = createProxyMiddleware({
@@ -15,23 +15,23 @@ const apiSetuProxy = createProxyMiddleware({
   changeOrigin: true,
 });
 const LicProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_MDMS || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_MDMS || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const docUploadProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_UPLOAD_DOC || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_UPLOAD_DOC || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const devRegistration = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_DEV_REG || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_DEV_REG || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const LicNewRegistration = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_NEW_LIC || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_NEW_LIC || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 const EgScrutinyProxy = createProxyMiddleware({
-	target: process.env.REACT_APP_PROXY_SCRUTINY_EG || "http://103.166.62.118:80",
+	target: process.env.REACT_APP_PROXY_SCRUTINY_EG || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
 module.exports = function (app) {
