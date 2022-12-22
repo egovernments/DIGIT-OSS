1c92483b94c4d5ed3ba8867da9cae039f3f1ac83
riyaAccess <riya@accessinfotechindia.com>
2022-12-22 15:30:21 +0530

    step-4 resolve

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/NewLicense/Step4/LayoutPlan copy.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/NewLicense/Step4/LayoutPlan copy.js
@@ -1,23 +1,23 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import { Row, Col } from "react-bootstrap";
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import { getDocShareholding } from "../docView/docView.help";
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.css";
+// import { Row, Col } from "react-bootstrap";
+// import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+// import VisibilityIcon from "@mui/icons-material/Visibility";
+// import { getDocShareholding } from "../docView/docView.help";
 
-const LayoutPlan = ({ register }) => {
-  return (
-    <Row className="ml-auto" style={{ marginBottom: 5 }}>
-      <div className="col col-12">
-        <h6 style={{ display: "flex" }} data-toggle="tooltip" data-placement="top" title="Upload Document">
-          Upload Layout Plan. &nbsp;&nbsp;<ArrowCircleUpIcon color="primary"></ArrowCircleUpIcon>
-          <VisibilityIcon color="primary" onClick={() => getDocShareholding(DetailsofAppliedLand?.DetailsAppliedLand?.uploadLayoutPlan)}>
-            {" "}
-          </VisibilityIcon>
-        </h6>
-        <input type="file" className="form-control" onChange={(e) => getDocumentData(e?.target?.files[0], "uploadLayoutPlan")} />
-      </div>
-    </Row>
-  );
-};
-export default LayoutPlan;
+// const LayoutPlan = ({ register }) => {
+//   return (
+//     <Row className="ml-auto" style={{ marginBottom: 5 }}>
+//       <div className="col col-12">
+//         <h6 style={{ display: "flex" }} data-toggle="tooltip" data-placement="top" title="Upload Document">
+//           Upload Layout Plan. &nbsp;&nbsp;<ArrowCircleUpIcon color="primary"></ArrowCircleUpIcon>
+//           <VisibilityIcon color="primary" onClick={() => getDocShareholding(DetailsofAppliedLand?.DetailsAppliedLand?.uploadLayoutPlan)}>
+//             {" "}
+//           </VisibilityIcon>
+//         </h6>
+//         <input type="file" className="form-control" onChange={(e) => getDocumentData(e?.target?.files[0], "uploadLayoutPlan")} />
+//       </div>
+//     </Row>
+//   );
+// };
+// export default LayoutPlan;
