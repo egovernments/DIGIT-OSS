9d35e51e82a743e0526b91f649d95439a1f55334
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2022-12-26 18:51:50 +0530

    devissue

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/StakeholderRegistration/StakeholderAcknowledgement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/StakeholderRegistration/StakeholderAcknowledgement.js
@@ -25,7 +25,7 @@ const BannerPicker = (props) => {
     <Banner
       message={GetActionMessage(props)}
       applicationNumber={props.data?.Licenses[0].applicationNumber}
-      info={props.isSuccess ? props.t("BPA_NEW_STAKEHOLDER_REGISTRATION_APP_LABEL") : ""}
+      info={props.isSuccess ? props.t("Your registration is successful") : ""}
       successful={props.isSuccess}
       style={{ padding: "10px" }}
       headerStyles={{fontSize: "32px"}}
@@ -82,14 +82,14 @@ const StakeholderAcknowledgement = ({ data, onSuccess }) => {
         {isOpenLinkFlow && <BackButton style={{ border: "none" }}>{t("CS_COMMON_BACK")}</BackButton>}
         <Card>
           <BannerPicker t={t} data={mutation.data} isSuccess={mutation.isSuccess} isLoading={mutation.isIdle || mutation.isLoading} />
-          {mutation.isSuccess && <CardText>{`${t(`TRADELICENSE_TRADETYPE_${licenseType}`)}${t(`CS_FILE_STAKEHOLDER_RESPONSE`)}`}</CardText>}
+          {/* {mutation.isSuccess && <CardText>{`${t(`TRADELICENSE_TRADETYPE_${licenseType}`)}${t(`CS_FILE_STAKEHOLDER_RESPONSE`)}`}</CardText>} */}
           {!mutation.isSuccess && <CardText>{t("CS_FILE_PROPERTY_FAILED_RESPONSE")}</CardText>}
-          {(mutation.isSuccess && !isOpenLinkFlow) && <Link to={{
+          {/* {(mutation.isSuccess && !isOpenLinkFlow) && <Link to={{
             pathname: `/digit-ui/citizen/payment/collect/${mutation.data.Licenses[0].businessService}/${mutation.data.Licenses[0].applicationNumber}/${mutation.data.Licenses[0].tenantId}?tenantId=${mutation.data.Licenses[0].tenantId}`,
             state: { tenantId: mutation.data.Licenses[0].tenantId },
           }}>
             <SubmitBar label={t("COMMON_MAKE_PAYMENT")} />
-          </Link>}
+          </Link>} */}
           {!isOpenLinkFlow && <Link to={`/digit-ui/citizen`}>
             <LinkButton label={t("CORE_COMMON_GO_TO_HOME")} />
           </Link>}
