83dce00830ebbc439226b076e86295ea3fe6ffab
Suryoday18 <suryoday.yadav@transerve.com>
2023-01-04 20:33:39 +0530

    land-services to tl-services API integration on citizen side SP

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ServicePlan/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ServicePlan/index.js
@@ -23,10 +23,10 @@ const ServicePlanService = () => {
 
     shouldFocusError: true,
   });
-
+  const userInfo = Digit.UserService.getUser();
   const servicePlan = async (data) => {
     const token = window?.localStorage?.getItem("token");
-    console.log(data);
+    console.log(data, "service-service");
     try {
       const postDistrict = {
         requestInfo: {
@@ -39,13 +39,14 @@ const ServicePlanService = () => {
           msg_id: "",
           requester_id: "",
           authToken: token,
+          "userInfo": userInfo.info
         },
 
         ServicePlanRequest: {
           ...data,
         },
       };
-      const Resp = await axios.post("/land-services/serviceplan/_create", postDistrict);
+      const Resp = await axios.post("/tl-services/serviceplan/_create", postDistrict);
       setServicePlanDataLabel(Resp.data);
     } catch (error) {
       console.log(error.message);
