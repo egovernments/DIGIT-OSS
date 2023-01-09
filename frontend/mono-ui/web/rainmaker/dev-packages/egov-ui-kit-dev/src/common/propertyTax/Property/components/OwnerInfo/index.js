aa5201789890d5c7d7cece685bfb85e6c5e9ff53
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2023-01-09 19:26:52 +0530

    Owner details Edit Property Not working

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/components/OwnerInfo/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/components/OwnerInfo/index.js
@@ -510,6 +510,7 @@ class OwnerInfo extends Component {
       // OldProperty,
       updateNumberConfig,
     } = this.props;
+    let showEditIcon = true;
     let ownerInfo = [];
     let multipleOwner = false;
     let mutationCheck = process.env.REACT_APP_NAME === "Citizen" ? false : true;
@@ -521,6 +522,9 @@ class OwnerInfo extends Component {
     // }
     if (properties) {
       const { propertyDetails } = properties;
+      if (properties.propertyId){
+        showEditIcon = false;
+      }
       if (propertyDetails && propertyDetails.length > 0) {
          ownerInfo = this.getOwnerInfo(propertyDetails[0], generalMDMSDataById);
        /* if (ownerInfo.length > 1) {
@@ -559,7 +563,7 @@ class OwnerInfo extends Component {
                       />
                     )}
                   </div>
-                  {{ editIcon } && <span style={{ alignItems: "right" }}>{editIcon}</span>}
+                  {editIcon && showEditIcon && <span style={{ alignItems: "right" }}>{editIcon}</span>}
                   {/* Transfer ownership button and View History button */}
                   {(viewHistory || ownershipTransfer) && (
                     <div id="pt-header-button-container" className="header-button-container">
