6e1fa4161d4a6c032d5a8dab0d73cdf524f7778a
SudeepAccess <sudeep.accessinfotechindia@gmail.com>
2023-01-24 12:58:19 +0530

    Cred

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/CheckCredentials/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/CheckCredentials/index.js
@@ -4,10 +4,15 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
+// const setCitizenDetail=(userObject,token,tenantId)=>{
+//   let locale=JSON.parse(sessionStorage.getItem("Digit.initData"))?.value?.selectedLanguage;
+  
+// }
+
 export default function CheckCredentials() {
   const history = useHistory();
   const queryParameters = new URLSearchParams(window.location.search);
-
+  const [user, setUser] = useState(null);
   const checkCrednetials = async () => {
     const body = {
       "SsoCitizen": {
@@ -20,18 +25,25 @@ export default function CheckCredentials() {
       }
     }
     const response = await axios.post("http://103.166.62.118:80/user/users/_ssoCitizen", body)
-    localStorage.setItem("access_token", response?.data?.TokenId);
-    localStorage.setItem("token", response?.data?.TokenId);
+    localStorage.setItem("access_token", response?.data?.Token?.access_token);
+    localStorage.setItem("token", response?.data?.Token?.refresh_token);
+    localStorage.setItem("Citizen.tenant-id",response?.data?.Token?.UserRequest?.tenantId);
+    localStorage.setItem("tenant-id",response?.data?.Token?.UserRequest?.tenantId);
+    localStorage.setItem("citizen.userRequestObject",JSON.stringify(response?.data?.Token?.UserRequest));
+    localStorage.setItem("locale",response?.data?.Token?.UserRequest?.locale);
+    localStorage.setItem("Citizen.locale",response?.data?.Token?.UserRequest?.locale);
+    localStorage.setItem("token",response?.data?.Token?.access_token);
+    localStorage.setItem("Citizen.token",response?.data?.Token?.access_token);
+    localStorage.setItem("user-info",JSON.stringify(response?.data?.Token?.UserRequest));
+    localStorage.setItem("Citizen.user-info",JSON.stringify(response?.data?.Token?.UserRequest));  
+    sessionStorage.setItem("citizen.userRequestObject",JSON.stringify(response?.data?.Token?.UserRequest));
     // console.log("SSoAUTH",response?.data);
     // console.log("SSoAUTHTOKEN",response?.data);
     // console.log("_ssoCitizen response ",response.data)
     // window.open(response?.data?.ReturnUrl,"_self");
 
     // redirectReturnUrl();
-    const redirectReturnUrl = async () => {
-      const resp = await axios.post(`${response?.data?.ReturnUrl}`);
-      console.log("RETURNURL",resp);
-    }
+    // setCitizenDetail(user?.info,user?.access_token)
 
 
   }
