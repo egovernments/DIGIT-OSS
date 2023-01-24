9838ef50ec584d813e53744bbb54f14af80e0090
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2023-01-24 19:37:19 +0530

    sfsdf

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/BankGuarantee/Inbox/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/BankGuarantee/Inbox/index.js
@@ -1,7 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import MobileInbox from "../../../../components/Inbox/MobileInbox";
-import DesktopInbox from "../../../../components/Inbox/DesktopInbox";
+// import MobileInbox from "../../../../components/Inbox/MobileInbox";
+import MobileInbox from "../../../../components/inbox/MobileInbox";
+import DesktopInbox from "../../../../components/inbox/DesktopInbox";
 import { Header } from "@egovernments/digit-ui-react-components";
 
 
