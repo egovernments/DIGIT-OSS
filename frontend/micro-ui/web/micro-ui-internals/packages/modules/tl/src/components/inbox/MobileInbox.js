bb8869d8fc47fe5553e6e3d59d5da17eb0e0bae1
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2023-01-24 18:46:52 +0530

    Update MobileInbox.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/components/inbox/MobileInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/components/inbox/MobileInbox.js
@@ -28,52 +28,53 @@ const MobileInbox = ({
 }) => {
   const { t } = useTranslation();
   const getData = () => data?.table.map(e => ({
-      [t("WF_INBOX_HEADER_APPLICATION_NO")]:e?.["applicationId"],
-      [t("TL_COMMON_TABLE_COL_APP_DATE")]:convertEpochToDateDMY(e?.["date"]),
-      [t("TL_COMMON_TABLE_COL_APP_TYPE")]:e?.["businessService"]?t(`CS_COMMON_INBOX_${e?.["businessService"]?.toUpperCase()}`):t("NA"),
-      [t("WF_INBOX_HEADER_LOCALITY")]:t(Digit.Utils.locale.getRevenueLocalityCode(e?.["locality"], e?.["tenantId"])),
-      [t("WF_INBOX_HEADER_STATUS")]:t(e?.["businessService"]?`WF_${e["businessService"]?.toUpperCase()}_${e?.["status"]}`:`NA`),
-      [t("WF_INBOX_HEADER_CURRENT_OWNER")]:t(e?.owner),
-      [t("WF_INBOX_HEADER_SLA_DAYS_REMAINING")]:e?.["sla"]}))
+    [t("WF_INBOX_HEADER_APPLICATION_NO")]: e?.["applicationId"],
+    [t("TL_COMMON_TABLE_COL_APP_DATE")]: convertEpochToDateDMY(e?.["date"]),
+    [t("TL_COMMON_TABLE_COL_APP_TYPE")]: e?.["businessService"] ? t(`CS_COMMON_INBOX_${e?.["businessService"]?.toUpperCase()}`) : t("NA"),
+    [t("WF_INBOX_HEADER_LOCALITY")]: t(Digit.Utils.locale.getRevenueLocalityCode(e?.["locality"], e?.["tenantId"])),
+    [t("WF_INBOX_HEADER_STATUS")]: t(e?.["businessService"] ? `WF_${e["businessService"]?.toUpperCase()}_${e?.["status"]}` : `NA`),
+    [t("WF_INBOX_HEADER_CURRENT_OWNER")]: t(e?.owner),
+    [t("WF_INBOX_HEADER_SLA_DAYS_REMAINING")]: e?.["sla"]
+  }))
 
   return (
     <div style={{ padding: 0 }}>
       <div className="inbox-container">
         <div className="filters-container">
-        <ApplicationLinks classNameForMobileView="linksWrapperForMobileInbox" linkPrefix={parentRoute} 
-                    allLinks={[
-                      {
-                        text: "TL_NEW_APPLICATION",
-                        link: "/digit-ui/employee/tl/new-application",
-                        businessService: "TL",
-                        roles: ["TL_CEMP"],
-                      },
-                      {
-                        text: "TL_SEARCH_APPLICATIONS",
-                        link: "/digit-ui/employee/tl/search/application",
-                        businessService: "TL",
-                        roles: ["TL_FIELD_INSPECTOR","TL_APPROVER", "TL_DOC_VERIFIER","TL_CEMP"],
-                      },
-                      {
-                        text: "TL_SEARCH_LICENSE",
-                        link: "/digit-ui/employee/tl/search/license",
-                        businessService: "TL",
-                        roles: ["TL_APPROVER", "TL_DOC_VERIFIER","TL_FIELD_INSPECTOR"],
-                      },
-                      {
-                        text: "TL_RENEWAL_HEADER",
-                        link: "/digit-ui/employee/tl/search/license",
-                        businessService: "TL",
-                        roles: ["TL_CEMP"],
-                      },
-                      {
-                        text: "ACTION_TEST_DASHBOARD",
-                        link: "/digit-ui/employee/dss/dashboard/tradelicence",
-                        businessService: "TL",
-                        roles: ["STADMIN"],
-                      },
-                    ]}
-                    headerText={t("ACTION_TEST_TRADELICENSE")} isMobile={true} />
+          <ApplicationLinks classNameForMobileView="linksWrapperForMobileInbox" linkPrefix={parentRoute}
+            allLinks={[
+              {
+                text: "TL_NEW_APPLICATION",
+                link: "/digit-ui/employee/tl/new-application",
+                businessService: "TL",
+                roles: ["TL_CEMP"],
+              },
+              {
+                text: "TL_SEARCH_APPLICATIONS",
+                link: "/digit-ui/employee/tl/search/application",
+                businessService: "TL",
+                roles: ["TL_FIELD_INSPECTOR", "TL_APPROVER", "TL_DOC_VERIFIER", "TL_CEMP"],
+              },
+              {
+                text: "TL_SEARCH_LICENSE",
+                link: "/digit-ui/employee/tl/search/license",
+                businessService: "TL",
+                roles: ["TL_APPROVER", "TL_DOC_VERIFIER", "TL_FIELD_INSPECTOR"],
+              },
+              {
+                text: "TL_RENEWAL_HEADER",
+                link: "/digit-ui/employee/tl/search/license",
+                businessService: "TL",
+                roles: ["TL_CEMP"],
+              },
+              {
+                text: "ACTION_TEST_DASHBOARD",
+                link: "/digit-ui/employee/dss/dashboard/tradelicence",
+                businessService: "TL",
+                roles: ["STADMIN"],
+              },
+            ]}
+            headerText={t("ACTION_TEST_TRADELICENSE")} isMobile={true} />
           <ApplicationCard
             t={t}
             data={getData()}
