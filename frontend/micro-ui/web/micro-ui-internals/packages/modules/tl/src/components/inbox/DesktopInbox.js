3d03c045a33dc2a1e3359aac4a55aa811f8e5388
Suryoday18 <suryoday.yadav@transerve.com>
2023-01-25 11:26:53 +0530

    Redirection to services

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/components/inbox/DesktopInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/components/inbox/DesktopInbox.js
@@ -38,7 +38,9 @@ const DesktopInbox = ({ tableConfig, filterComponent, columns, isLoading, setSea
         return (
           <div>
             <span className="link">
-              <Link to={`/digit-ui/employee/tl/${row.original["businessService"] === 'NewTL' || 'TL' ? 'scrutiny' : row.original["businessService"] === 'SERVICE_PLAN' ? 'ServiceScrutiny' : null }/` + row.original["applicationId"]}>{row.original["applicationId"]}</Link>
+              <Link to={`/digit-ui/employee/tl/${row.original["businessService"] === ('NewTL' || 'TL') ? 'scrutiny' : row.original["businessService"] === 'SERVICE_PLAN' ? 'ServiceScrutiny' : row.original["businessService"] === 'ELECTRICAL_PLAN' ? 'ElectricalScrutiny' : row.original["businessService"] === "BG_NEW" ? 'ScrutinyForm' : null  }/` + row.original["applicationId"]}>{row.original["applicationId"]}</Link>
+              {/* <Link to={`/digit-ui/employee/tl/${row.original["businessService"] === 'SERVICE_PLAN' ? 'ServiceScrutiny' : row.original["businessService"] === 'ELECTRICAL_PLAN' ? 'ElectricalScrutiny' : row.original["businessService"] === "BG_NEW" ? 'ScrutinyForm' : null }/` + row.original["applicationId"]}>{row.original["applicationId"]}</Link> */}
+
             </span>
           </div>
         );
@@ -98,7 +100,7 @@ const DesktopInbox = ({ tableConfig, filterComponent, columns, isLoading, setSea
         return (
           <div>
             <div className="w-100 text-center link">
-              <Link to={"/digit-ui/employee/tl/scrutiny/" + row.original["applicationId"]}>
+              <Link to={`/digit-ui/employee/tl/${row.original["businessService"] === ('NewTL' || 'TL') ? 'scrutiny' : row.original["businessService"] === 'SERVICE_PLAN' ? 'ServiceScrutiny' : row.original["businessService"] === 'ELECTRICAL_PLAN' ? 'ElectricalScrutiny' : row.original["businessService"] === "BG_NEW" ? 'ScrutinyForm' : null  }/` + row.original["applicationId"]}>
                 <Visibility/>
               </Link>
             </div>
