e26bf310442b7705a7299ee46ca1aa51602af0a6
Debasish Chakraborty <117706910+debasishchakraborty-egovt@users.noreply.github.com>
2023-02-02 11:00:42 +0530

    added survey to master (#5013)
    Co-authored-by: debasishchakraborty-egovt <debasish@egovernmets.org>

---
+++ core-services/egov-survey-services/src/main/resources/db/migration/main/V20220214132245__ss_alter_table_qorder_ddl.sql
@@ -0,0 +1,1 @@
+ALTER TABLE eg_ss_question ADD COLUMN qorder bigint;
---
+++ core-services/egov-survey-services/src/test/java/org/egov/egovsurveyservices/EgovsurveyservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.egov.egovsurveyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgovsurveyservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
