86732d05ce47cb6f82d2b08f9ec37a48523ec867
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2023-02-08 17:51:33 +0530

    HOme Index

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Home/index.js
@@ -178,7 +178,7 @@ const Home = () => {
       },
       {
         name: "Licence",
-        Icon: <LicencingIcon />,
+        Icon: <LicencingIcon className="home-class" />,
         onClick: () => history.push("/digit-ui/citizen/obps/card"),
       },
       {
