5c86af38b5254029a100aea485dc41b034618254
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2023-02-09 12:22:27 +0530

    UM-5642 Added condition for firenoc approver login (#5059)

--- frontend/mono-ui/web/rainmaker/packages/employee/src/modules/employee/Inbox/inbox.js
+++ frontend/mono-ui/web/rainmaker/packages/employee/src/modules/employee/Inbox/inbox.js
@@ -147,13 +147,13 @@ class Inbox extends Component {
             <Label className="landingPageHeader flex-child" label={"CS_LANDING_PAGE_WELCOME_TEXT"} />
             <Label className="landingPageUser flex-child" label={name} />,
           </div>
-          <div className="quick-action-button">
+          {/* <div className="quick-action-button">
             <MenuButton data={buttonItems} />
-          </div>
+          </div> */}
         </div>
-        <div className={"inbox-service-list"}>
+        {/* <div className={"inbox-service-list"}>
           <ServiceList history={history} />
-        </div>
+        </div> */}
         {hasWorkflow && inboxLoading && <div>
           <div className="jk-spinner-wrapper">
             <div className="jk-inbox-loader"></div>
