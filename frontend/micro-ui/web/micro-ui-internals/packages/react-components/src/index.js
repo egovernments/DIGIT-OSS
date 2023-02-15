d085c5b40da43cc88cf546d5a6f3c7fa7874f286
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2023-02-15 18:09:00 +0530

    index

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/index.js
@@ -59,7 +59,6 @@ import Rating from "./atoms/Rating";
 import RoundedLabel from "./atoms/RoundedLabel";
 import SectionalDropdown from "./atoms/SectionalDropdown";
 import MuiRadio from "./atoms/MuiRadio";
-import MuiTables from "./atoms/MuiTables"
 import { LastRow, MediaRow, Row, StatusTable } from "./atoms/StatusTable";
 import SubmitBar from "./atoms/SubmitBar";
 import StandaloneSearchBar from "./atoms/StandaloneSearchBar";
@@ -267,7 +266,6 @@ export {
   PrivateRoute,
   SectionalDropdown,
   MuiRadio,
-  MuiTables,
   RoundedLabel,
   LabelFieldPair,
   BreadCrumb,
