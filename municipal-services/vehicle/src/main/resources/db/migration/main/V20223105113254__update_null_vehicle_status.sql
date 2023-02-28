5682061fd3b3a76d5097e8dc1f03fafa2e3bf253
sardarshaik-eGov <112462979+sardarshaik-eGov@users.noreply.github.com>
2023-02-28 12:00:00 +0400

    Sm 1911 Merging Dev code to Master Branch (#5149)
    * #SM-1911 vehicle flyway issue fixed
    
    * SM-1911 Pom file version change

--- municipal-services/vehicle/src/main/resources/db/migration/main/V20223105113254__update_null_vehicle_status.sql
+++ municipal-services/vehicle/src/main/resources/db/migration/main/V20223105113254__update_null_vehicle_status.sql
@@ -2,4 +2,4 @@ UPDATE eg_vehicle
 		set status='ACTIVE',
 		lastmodifiedby='SAN-1098',
 		lastmodifiedtime=extract(epoch from current_timestamp )*1000
-		where status is null;
+		where status is null; 
