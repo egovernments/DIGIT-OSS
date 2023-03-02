24d266cb3cdd6d665e295d32abc71ac648dc8461
Su Deep <106058085+SudeepAccess@users.noreply.github.com>
2023-03-02 09:40:14 +0530

    setup proxy and merge withakash-tcp,feat/pupose-forms

--- frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
@@ -3,31 +3,35 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const createProxy = createProxyMiddleware({
   //target: process.env.REACT_APP_PROXY_API || "https://uat.digit.org",
   // target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
-  target: process.env.REACT_APP_PROXY_API || "http://103.166.62.118:80",
+  target: process.env.REACT_APP_PROXY_API || "http://tcp.abm.com:80",
   changeOrigin: true,
 });
-const assetsProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_ASSETS || "http://103.166.62.118:80",
-  changeOrigin: true,
-});
+// const assetsProxy = createProxyMiddleware({
+//   target: process.env.REACT_APP_PROXY_ASSETS || "http://tcp.abm.com:80",
+//   changeOrigin: true,
+// });
 const apiSetuProxy = createProxyMiddleware({
   target: process.env.REACT_APP_PROXY_SETU || "https://apisetu.gov.in",
   changeOrigin: true,
 });
-const LicProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_MDMS || "http://103.166.62.118:80",
-  changeOrigin: true,
-});
-const docUploadProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_UPLOAD_DOC || "http://103.166.62.118:80",
-  changeOrigin: true,
-});
-const devRegistration = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_DEV_REG || "http://103.166.62.118:80",
-  changeOrigin: true,
-});
-const EgScrutinyProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_SCRUTINY_EG || "http://103.166.62.118:80",
+// const LicProxy = createProxyMiddleware({
+//   target: process.env.REACT_APP_PROXY_MDMS || "http://tcp.abm.com:80",
+//   changeOrigin: true,
+// });
+// const docUploadProxy = createProxyMiddleware({
+//   target: process.env.REACT_APP_PROXY_UPLOAD_DOC || "http://tcp.abm.com:80",
+//   changeOrigin: true,
+// });
+// const devRegistration = createProxyMiddleware({
+//   target: process.env.REACT_APP_PROXY_DEV_REG || "http://tcp.abm.com:80",
+//   changeOrigin: true,
+// });
+// const EgScrutinyProxy = createProxyMiddleware({
+//   target: process.env.REACT_APP_PROXY_SCRUTINY_EG || "http://tcp.abm.com:80",
+//   changeOrigin: true,
+// });
+const GetCluDetails = createProxyMiddleware({
+  target: process.env.REACT_APP_PROXY_SCRUTINY_EG || "http://182.79.97.53:81",
   changeOrigin: true,
 });
 
@@ -87,12 +91,12 @@ module.exports = function (app) {
     "/land-services/new/licenses/_get",
     "/land-services/electric/plan/_create",
     "/land-services/serviceplan/_create",
-    "/tl-services/bank/guarantee/_create",
+    "/pb-egov-assets",
+    "/user/developer",
+    "/land-services/egscrutiny",
+    "/land-services/new/licenses",
   ].forEach((location) => app.use(location, createProxy));
-  ["/pb-egov-assets"].forEach((location) => app.use(location, assetsProxy));
   ["/mca/v1/companies", "/mca-directors/v1/companies", "/certificate/v3/pan/pancr"].forEach((location) => app.use(location, apiSetuProxy));
   // ["/egov-mdms-service/v1"].forEach((location) => app.use(location, LicProxy));
-  ["/filestore/v1/files"].forEach((location) => app.use(location, docUploadProxy));
-  ["/user/developer"].forEach((location) => app.use(location, devRegistration));
-  ["/land-services/egscrutiny", "/land-services/new/licenses"].forEach((location) => app.use(location, EgScrutinyProxy));
+  ["/api/cis/GetCluDetails", "/api/cis/GetLicenceDetails"].forEach((location) => app.use(location, GetCluDetails));
 };
