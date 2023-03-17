1c6beb2e6d0961e2ea7e374227092934a7d16e9e
Aman Kumar Jha <aman_j@trisysit.com>
2023-03-17 19:46:28 +0530

    revert changes other than FSM

---
+++ business-services/billing-service/src/test/java/org/egov/demand/service/BillingServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package org.egov.demand.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class BillingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ municipal-services/sw-calculator/src/main/java/org/egov/swcalculation/config/KafkaConfiguration.java
@@ -0,0 +1,49 @@
+package org.egov.swcalculation.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
+
+@Configuration
+public class KafkaConfiguration {
+
+        @Autowired
+        private KafkaProperties kafkaProperties;
+
+        @Bean
+        public Map<String, Object> consumerConfigs() {
+            Map<String, Object> props = new HashMap<>(
+                    kafkaProperties.buildConsumerProperties()
+            );
+            return props;
+        }
+        
+
+        @Bean
+        public ConsumerFactory<String, Object> consumerFactory() {
+            return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+        }
+
+        @Bean
+        public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                    new ConcurrentKafkaListenerContainerFactory<>();
+            factory.setConsumerFactory(consumerFactory());
+            factory.setBatchListener(true);
+            factory.getContainerProperties().setAckMode(AckMode.BATCH);
+            return factory;
+        }
+
+    }
+
+
+
+
