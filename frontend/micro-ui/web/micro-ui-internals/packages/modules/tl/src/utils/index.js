48cd57e11f26b9db20d43719aa292c1802d2632a
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2023-04-06 16:51:39 +0530

    [UM-6013] : updated the conditional check for getting FY for request … (#5325)
    * [UM-6013] : updated the conditional check for getting FY for request body
    
    * UM-6013 : incremented the TL module version

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/utils/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/utils/index.js
@@ -536,7 +536,8 @@ export const stringToBoolean = (value) => {
 //FinancialYear
 export const convertToEditTrade = (data, fy = []) => {
   const currrentFYending = fy?.filter(item => item?.code === data?.financialYear)?.[0]?.endingDate;
-  const nextFinancialYearForRenewal = fy?.filter(item => item?.startingDate === currrentFYending)?.[0]?.code;
+  //added 1*1000 to add 1ms to epoch time 
+  const nextFinancialYearForRenewal = fy?.filter(item => item?.startingDate === currrentFYending+1000)?.[0]?.code;
   let isDirectrenewal = stringToBoolean(sessionStorage.getItem("isDirectRenewal"));
   let isSameAsPropertyOwner = sessionStorage.getItem("isSameAsPropertyOwner"); 
   let formdata = {
