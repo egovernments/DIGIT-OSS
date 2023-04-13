572ef263810ab71aa65a0b61528e23057b208e31
nabeelmd-eGov <94039229+nabeelmd-eGov@users.noreply.github.com>
2023-04-13 14:23:42 +0530

    SM-2225: Collect Payment Hide for dso (#5392)
    Co-authored-by: nabeelmd-eGov <nabeel.md@egovernments.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/WorkFlow.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/WorkFlow.js
@@ -261,9 +261,11 @@ export const WorkflowService = {
           } catch (err) { }
         }
 
-        // HANDLING ACTION FOR NEW VEHICLE LOG FROM UI SIDE
-        // HIDING PAY ACTION FOR DSO FROM UI SIDE
-        const nextActions = location.pathname.includes("new-vehicle-entry") ? action_newVehicle : location.pathname.includes("dso") ? actionRolePair.filter((i)=> i.action !== "PAY") : actionRolePair;
+      // TAKING OUT CURRENT APPL STATUS
+      const tempCheckStatus = timeline.map((i) => i.status)[0]
+      // HANDLING ACTION FOR NEW VEHICLE LOG FROM UI SIDE
+      // HIDING PAYMENT OPTION FOR DSO AND WHEN APPLICATION IS NOT IN PAYMENT STATUS 
+      const nextActions = location.pathname.includes("new-vehicle-entry") ? action_newVehicle : location.pathname.includes("dso") ? actionRolePair.filter((i)=> i.action !== "PAY") : tempCheckStatus.includes("WAITING_FOR_DISPOSAL") || tempCheckStatus.includes("PENDING_APPL_FEE_PAYMENT") ? actionRolePair : actionRolePair.filter((i)=> i.action !== "PAY");
 
         if (role !== "CITIZEN" && moduleCode === "PGR") {
           const onlyPendingForAssignmentStatusArray = timeline?.filter(e => e?.status === "PENDINGFORASSIGNMENT")
