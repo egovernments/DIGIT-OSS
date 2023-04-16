e820e6f70041aecc0fd1feec1c83546999f436b8
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-04-16 18:31:33 +0530

    Revert the sidebar child

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/SideBar/EmployeeSideBar.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/SideBar/EmployeeSideBar.js
@@ -2,8 +2,6 @@ import React, { useRef, useEffect, useState } from "react";
 import SubMenu from "./SubMenu";
 import { Loader, SearchIcon } from "@egovernments/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
-import NavItem from "./NavItem";
-import _, { findIndex } from "lodash";
 
 const EmployeeSideBar = () => {
   const sidebarRef = useRef(null);
@@ -37,30 +35,25 @@ const EmployeeSideBar = () => {
       element.style.padding = "0";
     });
   };
-
   const configEmployeeSideBar = {};
-
-  //creating the object structure from mdms value for easy iteration
-  let configEmployeeSideBar1 = {};
-  data?.actions?.filter((e) => e.url === "url")?.forEach((item) => {
-    _.set(configEmployeeSideBar1,item.path,{...item}) 
-  })
-
   data?.actions
     .filter((e) => e.url === "url")
     .forEach((item) => {
       let index = item.path.split(".")[0];
       if (search == "" && item.path !== "") {
-         index = item.path.split(".")[0];
-        if (index === "TradeLicense") index = "Trade License";
+        index = item.path.split(".")[0];
         if (!configEmployeeSideBar[index]) {
           configEmployeeSideBar[index] = [item];
         } else {
           configEmployeeSideBar[index].push(item);
         }
-      } else if (item.path !== "" && t(`ACTION_TEST_${index?.toUpperCase()?.replace(/[ -]/g, "_")}`)?.toLowerCase().includes(search.toLowerCase())) {
-         index = item.path.split(".")[0];
-        if (index === "TradeLicense") index = "Trade License";
+      } else if (
+        item.path !== "" &&
+        t(`ACTION_TEST_${index?.toUpperCase()?.replace(/[ -]/g, "_")}`)
+          ?.toLowerCase()
+          .includes(search.toLowerCase())
+      ) {
+        index = item.path.split(".")[0];
         if (!configEmployeeSideBar[index]) {
           configEmployeeSideBar[index] = [item];
         } else {
@@ -69,30 +62,13 @@ const EmployeeSideBar = () => {
       }
     });
   let res = [];
-
-  //method is used for restructing of configEmployeeSideBar1 nested object into nested array object
-  function restructuringOfConfig (tempconfig){
-    const result = [];
-    for(const key in tempconfig){
-      const value= tempconfig[key];
-      if(typeof value === "object" && !(value?.id)){
-      const children = restructuringOfConfig(value);
-      result.push({label : key,children, icon:children?.[0]?.icon, to:""});
-      }
-      else{
-        result.push({label: key, value, icon:value?.leftIcon, to: key === "Home" ? `/${window?.contextPath}/employee` : value?.navigationURL});
-      }
-    }
-
-    return result
-  }
   const splitKeyValue = () => {
     const keys = Object.keys(configEmployeeSideBar);
     keys.sort((a, b) => a.orderNumber - b.orderNumber);
     for (let i = 0; i < keys.length; i++) {
       if (configEmployeeSideBar[keys[i]][0].path.indexOf(".") === -1) {
         if (configEmployeeSideBar[keys[i]][0].displayName === "Home") {
-          const homeURL =  `/${window?.contextPath}/employee`;
+          const homeURL = `/${window?.contextPath}/employee`;
           res.unshift({
             moduleName: keys[i].toUpperCase(),
             icon: configEmployeeSideBar[keys[i]][0],
@@ -116,35 +92,17 @@ const EmployeeSideBar = () => {
         });
       }
     }
-    if(res.find(a => a.moduleName === "HOME"))
-    {
-      //res.splice(0,1);
-      const indx = res.findIndex(a => a.moduleName === "HOME");
-      const home = res?.filter((ob) => ob?.moduleName === "HOME")
-      let res1 = res?.filter((ob) => ob?.moduleName !== "HOME")
-      res = res1.sort((a,b) => a.moduleName.localeCompare(b.moduleName));
+    if (res.find((a) => a.moduleName === "HOME")) {
+      const home = res?.filter((ob) => ob?.moduleName === "HOME");
+      let res1 = res?.filter((ob) => ob?.moduleName !== "HOME");
+      res = res1.sort((a, b) => a.moduleName.localeCompare(b.moduleName));
       home?.[0] && res.unshift(home[0]);
+    } else {
+      res.sort((a, b) => a.moduleName.localeCompare(b.moduleName));
     }
-    else
-    {
-      res.sort((a,b) => a.moduleName.localeCompare(b.moduleName));
-    }
-    let newconfig = restructuringOfConfig(configEmployeeSideBar1);
-    //below lines are used for shifting home object to first place
-    newconfig.splice(newconfig.findIndex((ob) => ob?.label === ""),1);
-    newconfig.sort((a,b) => a.label.localeCompare(b.label));
-    const fndindex = newconfig?.findIndex((el) => el?.label === "Home");
-    const homeitem = newconfig.splice(fndindex,1);
-    newconfig.unshift(homeitem?.[0]);
-    
-    return (
-      newconfig.map((item, index) => {
-          return <NavItem key={`${item.label}-${index}`} item={item} />;
-        })
-    );
-    // return res?.map((item, index) => {
-    //   return <SubMenu item={item} key={index + 1} />;
-    // });
+    return res?.map((item, index) => {
+      return <SubMenu item={item} key={index + 1} />;
+    });
   };
 
   if (isLoading) {
