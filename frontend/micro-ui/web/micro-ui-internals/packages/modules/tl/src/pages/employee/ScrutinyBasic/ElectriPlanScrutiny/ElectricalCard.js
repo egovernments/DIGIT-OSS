ba5dbba9c5092c5fca27804958cadbf021da0061
akashAccess <akashaccessinfotechindia@gmail.com>
2023-04-19 10:10:56 +0530

    SP and EP Scrutiny

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/ScrutinyBasic/ElectriPlanScrutiny/ElectricalCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pages/employee/ScrutinyBasic/ElectriPlanScrutiny/ElectricalCard.js
@@ -201,7 +201,7 @@ const authToken = Digit.UserService.getUser()?.access_token || null;
 
   return (
     <Card>
-      <Card.Header class="fw-normal" style={{ top: 5, padding: 5 , fontSize: 14 ,height:90, lineHeight:2 }}>
+      {/* <Card.Header class="fw-normal" style={{ top: 5, padding: 5 , fontSize: 14 ,height:90, lineHeight:2 }}>
         <div className="row">
           <div className="col-md-3">
             <p>Application Number:</p>
@@ -223,6 +223,38 @@ const authToken = Digit.UserService.getUser()?.access_token || null;
             <p>TCP Dairy Number: </p>
             <p class="fw-normal">{applicationData?.tcpDairyNumber}</p>
           </div>
+        </div>
+      </Card.Header> */}
+      <Card.Header className="head-application" >
+        <div className="row fw-normal">
+          <div className="col-sm-2">
+            <b><p className="head-font">Application Number:</p></b>
+            <b><p className="head-font">{id}</p></b>
+          </div>
+          <div className="col-sm-2">
+            <b><p className="head-font">Service Id: </p></b>
+            <b><p className="head-font">
+              {applicationData?.businessService}
+              {/* Licence */}
+            </p></b>
+          </div>
+          <div className="col-sm-2">
+            <b><p className="head-font">TCP Application Number:</p></b>
+            {/* {item.name.substring(0, 4)} */}
+            <b><p className="head-font">{applicationData?.tcpApplicationNumber}</p></b>
+          </div>
+          <div className="col-sm-2">
+            <b><p className="head-font">TCP Case Number:</p></b>
+            <b><p className="head-font">{applicationData?.tcpCaseNumber}</p></b>
+          </div>
+          <div className="col-sm-2">
+            <b><p className="head-font">TCP Dairy Number: </p></b>
+            <b><p className="head-font">{applicationData?.tcpDairyNumber}</p></b>
+
+          </div>
+          {/* <div className="col-sm-2">
+            <Button style={{ textAlign: "right" }} value="Submit" id="Submit" onChange1={handleChange} name="Submit" onClick={handleshow19}>Views PDF</Button>
+          </div> */}
         </div>
       </Card.Header>
       <Row style={{ top: 10, padding: 10 }}>
