cddd1fb55c8237803000591d04a5731a799fc720
hinamakhija-eGov <96222691+hinamakhija-eGov@users.noreply.github.com>
2023-04-21 09:47:57 +0530

    Added Service-Request module to UAT (#5397)
    * Added Service-Request module to UAT
    
    * Updated persister file
    
    * Updated pagination for service search

---
+++ core-services/service-request/src/main/java/digit/service/ServiceRequestService.java
@@ -0,0 +1,71 @@
+package digit.service;
+
+import digit.config.Configuration;
+import digit.kafka.Producer;
+import digit.repository.ServiceRequestRepository;
+import digit.validators.ServiceDefinitionRequestValidator;
+import digit.validators.ServiceRequestValidator;
+import digit.web.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ServiceRequestService {
+
+    @Autowired
+    private ServiceRequestValidator serviceRequestValidator;
+
+    @Autowired
+    private ServiceRequestEnrichmentService enrichmentService;
+
+    @Autowired
+    private ServiceRequestRepository serviceRequestRepository;
+
+    @Autowired
+    private Producer producer;
+
+    @Autowired
+    private Configuration config;
+
+    public digit.web.models.Service createService(ServiceRequest serviceRequest) {
+
+        digit.web.models.Service service = serviceRequest.getService();
+
+        // Validate incoming service definition request
+        serviceRequestValidator.validateServiceRequest(serviceRequest);
+
+        // Enrich incoming service definition request
+        Map<String, Object> attributeCodeVsValueMap = enrichmentService.enrichServiceRequest(serviceRequest);
+
+        // Producer statement to emit service definition to kafka for persisting
+        producer.push(config.getServiceCreateTopic(), serviceRequest);
+
+        // Restore attribute values to the type in which it was sent in service request
+        enrichmentService.setAttributeValuesBackToNativeState(serviceRequest, attributeCodeVsValueMap);
+
+        return service;
+    }
+
+    public List<digit.web.models.Service> searchService(ServiceSearchRequest serviceSearchRequest){
+
+        List<digit.web.models.Service> listOfServices = serviceRequestRepository.getService(serviceSearchRequest);
+
+        if(CollectionUtils.isEmpty(listOfServices))
+            return new ArrayList<>();
+
+        return listOfServices;
+    }
+
+    public digit.web.models.Service updateService(ServiceRequest serviceRequest) {
+
+        // TO DO
+
+        return serviceRequest.getService();
+    }
+
+}
