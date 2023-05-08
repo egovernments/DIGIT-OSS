b185d88f534b6730a0c35678999ad065a9ae23e2
hinamakhija-eGov <96222691+hinamakhija-eGov@users.noreply.github.com>
2023-05-08 17:28:45 +0530

    UM-5868 pushing simplified data to indexer topic for service create (… (#5482)
    UM-5868 pushing simplified data to indexer topic for service create (#5390)
    
    * UM-5868 pushing simplified data to indexer topic for service create
    
    * UM-5868 pushing flattened data to indexer topic for service create
    
    * UM-5868 pushing flattened data to indexer topic for service create
    
    * UM-5868 removing logs
    
    * UM-5868 removing attributeReferenceId
    
    Co-authored-by: Priyanka-eGov <74049060+Priyanka-eGov@users.noreply.github.com>

--- core-services/service-request/src/main/java/digit/constants/Constants.java
+++ core-services/service-request/src/main/java/digit/constants/Constants.java
@@ -9,4 +9,16 @@ public class Constants {
 
     public static final String VALUE_JSON_PATH = "$.value";
 
+    // Attribute Keys
+
+    public static final String CONSUMER_CODE = "consumerCode";
+
+    public static final String RATING = "rating";
+
+    public static final String REFERENCE_ID = "referenceId";
+
+    public static final String COMMENTS = "comments";
+
+    public static final String CHANNEL = "channel";
+
 }
