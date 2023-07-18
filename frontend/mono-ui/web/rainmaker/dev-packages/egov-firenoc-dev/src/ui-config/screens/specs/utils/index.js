90968a598729fdc437e633b7ac55389af45a92ab
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2023-07-18 15:30:25 +0530

    UM-6410 Central Instance : FNOC : Citizen/Counter employee unable to create FireNoc application (#5663)

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-config/screens/specs/utils/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-config/screens/specs/utils/index.js
@@ -169,10 +169,10 @@ export const getCurrentFinancialYear = () => {
   var fiscalYr = "";
   if (curMonth > 3) {
     var nextYr1 = (today.getFullYear() + 1).toString();
-    fiscalYr = today.getFullYear().toString() + "-" + nextYr1;
+    fiscalYr = today.getFullYear().toString() + "-" + nextYr1.slice(-2);
   } else {
     var nextYr2 = today.getFullYear().toString();
-    fiscalYr = (today.getFullYear() - 1).toString() + "-" + nextYr2;
+    fiscalYr = (today.getFullYear() - 1).toString() + "-" + nextYr2.slice(-2);
   }
   return fiscalYr;
 };
@@ -183,7 +183,7 @@ export const getCurrentFinancialYearForFireNoc = () => {
   var fiscalYr = "";
   if (curMonth > 3) {
     var nextYr1 = (today.getFullYear() + 1).toString();
-    fiscalYr = today.getFullYear().toString() + "-" + nextYr1;
+    fiscalYr = today.getFullYear().toString() + "-" + nextYr1.slice(-2);
   } else {
     var nextYr2 = today.getFullYear().toString();
     fiscalYr = (today.getFullYear() - 1).toString() + "-" + nextYr2.slice(-2);
