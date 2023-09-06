a772caba50e9760fcc2ff5676fe1c863156a487a
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-09-06 11:31:17 +0530

    fix build

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
     ],
   },
   output: {
