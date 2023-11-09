67c76bf4aba82363db522531284e3f9826977669
himalay <himalayagswm@gmail.com>
2023-11-09 16:55:23 +0530

    base file Home.js

---
+++ frontend/mono-ui/web/egov-common-screen/src/components/module/Home.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Header from "../Header";
+import Cards from "../Cards";
+import Message from "../Message";
+import Faqs from "../Faqs";
+import Testimonials from "../Testimonials";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <div>
+
+      <Header />
+      <Cards />
+      <Message />
+
+      <center>
+        <p
+          style={{
+            fontFamily: "Roboto",
+            fontStyle: "normal",
+            fontWeight: "500",
+            fontSize: "36px",
+            lineHeight: "42px",
+            textAlign: "center",
+            color: "rgba(0, 0, 0, 0.87)",
+            marginLeft: "10%",
+            marginRight: "10%",
+          }}
+        >
+          Frequently Asked Questions
+        </p>
+      </center>
+      <div
+        style={{
+          marginLeft: "45%",
+          marginRight: "45%",
+          marginTop: "-2%",
+          borderBottom: "5px solid #f48952",
+        }}
+      ></div>
+
+      <Faqs />
+
+      <center>
+        <p
+          style={{
+            fontFamily: "Roboto",
+            fontStyle: "normal",
+            fontWeight: "500",
+            fontSize: "36px",
+            lineHeight: "42px",
+            textAlign: "center",
+            color: "rgba(0, 0, 0, 0.87)",
+            marginLeft: "10%",
+            marginRight: "10%",
+          }}
+        >
+          User Testimonials
+        </p>
+      </center>
+      <div
+        style={{
+          marginLeft: "45%",
+          marginTop: "-2%",
+          marginRight: "45%",
+          borderBottom: "5px solid #f48952",
+        }}
+      ></div>
+
+      <Testimonials />
+
+      <Footer />
+      <div style={{ backgroundColor: "#ffffff" }}>
+        <br />
+        <center>
+          {/* <a href="https://www.digit.org/" target="_blank">
+      <img style={{ height: "1.4em" }} src={digitLogo} />
+    </a> */}
+        </center>{" "}
+        <br />
+      </div>
+      <h1>This is home page</h1>
+    </div>
+  );
+}
+export default Home;
